8ca0c743d495d0212f240cb476735df0a2d0c7f3
AndreyYevsega <andrii.yevsega@gmail.com>
2018-09-25 10:08:33 +0300

    update actions

--- src/assessment/src/constants/actions.js
+++ src/assessment/src/constants/actions.js
@@ -1,39 +1,42 @@
 // theme
-export const LOAD_THEME = 'load theme';
+export const LOAD_THEME = '[theme] load theme';
 
 // questions
-export const LOAD_QUESTIONS = 'load questions';
-export const GOTO_QUESTION = 'goto question';
-export const ADD_ANSWER = 'add answer';
-export const CHECK_ANSWER = 'check answer';
-export const ADD_EVALUATION = 'add evaluation';
-export const AUTHOR_QUESTION = 'author question';
+export const LOAD_QUESTIONS = '[questions] load questions';
+export const GOTO_QUESTION = '[questions] goto question';
+export const ADD_ANSWER = '[questions] add answer';
+export const CHECK_ANSWER = '[questions] check answer';
+export const ADD_EVALUATION = '[questions] add evaluation';
+export const AUTHOR_QUESTION = '[questions] author question';
 
 // assessment
-export const LOAD_ASSESSMENT = 'load assessment';
+export const LOAD_ASSESSMENT = '[assessment] load assessment';
 
 // view
-export const CHANGE_VIEW = 'CHANGE_VIEW';
+export const CHANGE_VIEW = '[view] CHANGE_VIEW';
 
 // preview
-export const PREVIEW_UPDATE_LIST = 'PREVIEW_UPDATE_LIST';
-export const CHANGE_PREVIEW_TAB = 'CHANGE_PREVIEW_TAB';
+export const PREVIEW_UPDATE_LIST = '[preview] PREVIEW_UPDATE_LIST';
+export const CHANGE_PREVIEW_TAB = '[preview] CHANGE_PREVIEW_TAB';
 
 // questionsOrderList
-export const ORDERLIST_UPDATE_QUESTIONS_LIST = 'ORDERLIST_UPDATE_QUESTIONS_LIST';
-export const ORDERLIST_UPDATE_VALIDATION = 'ORDERLIST_UPDATE_VALIDATION';
-export const ORDERLIST_UPDATE_STIMULUS = 'ORDERLIST_UPDATE_STIMULUS';
-export const ORDERLIST_SET_STATE = 'ORDERLIST_SET_STATE';
-export const ORDERLIST_CLEAR = 'ORDERLIST_CLEAR';
-export const ORDERLIST_ADD_ALT_RESPONSES = 'ORDERLIST_ADD_ALT_RESPONSES';
-export const ORDERLIST_UPDATE_ALT_VALIDATION_SCORE = 'ORDERLIST_UPDATE_ALT_VALIDATION_SCORE';
-export const ORDERLIST_UPDATE_CORRECT_VALIDATION_SCORE = 'ORDERLIST_UPDATE_CORRECT_VALIDATION_SCORE';
+export const ORDERLIST_UPDATE_QUESTIONS_LIST =
+  '[questionsOrderList] ORDERLIST_UPDATE_QUESTIONS_LIST';
+export const ORDERLIST_UPDATE_VALIDATION = '[questionsOrderList] ORDERLIST_UPDATE_VALIDATION';
+export const ORDERLIST_UPDATE_STIMULUS = '[questionsOrderList] ORDERLIST_UPDATE_STIMULUS';
+export const ORDERLIST_SET_STATE = '[questionsOrderList] ORDERLIST_SET_STATE';
+export const ORDERLIST_CLEAR = '[questionsOrderList] ORDERLIST_CLEAR';
+export const ORDERLIST_ADD_ALT_RESPONSES = '[questionsOrderList] ORDERLIST_ADD_ALT_RESPONSES';
+export const ORDERLIST_UPDATE_ALT_VALIDATION_SCORE =
+  '[questionsOrderList] ORDERLIST_UPDATE_ALT_VALIDATION_SCORE';
+export const ORDERLIST_UPDATE_CORRECT_VALIDATION_SCORE =
+  '[questionsOrderList] ORDERLIST_UPDATE_CORRECT_VALIDATION_SCORE';
 
 // items
-export const RECEIVE_ITEMS_REQUEST = '[Items] receive items request';
-export const RECEIVE_ITEMS_SUCCESS = '[Items] receive items success';
-export const RECEIVE_ITEMS_ERROR = '[Items] receive items error';
+export const RECEIVE_ITEMS_REQUEST = '[items] receive items request';
+export const RECEIVE_ITEMS_SUCCESS = '[items] receive items success';
+export const RECEIVE_ITEMS_ERROR = '[items] receive items error';
 
-export const RECEIVE_ITEM_REQUEST = '[Items] receive item request';
-export const RECEIVE_ITEM_SUCCESS = '[Items] receive item success';
-export const RECEIVE_ITEM_ERROR = '[Items] receive item error';
+export const RECEIVE_ITEM_REQUEST = '[items] receive item request';
+export const RECEIVE_ITEM_SUCCESS = '[items] receive item success';
+export const RECEIVE_ITEM_ERROR = '[items] receive item error';

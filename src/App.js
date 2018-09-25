1b2ba649efd3e093c73d75868b019bc9fe2d61b4
AndreyYevsega <andrii.yevsega@gmail.com>
2018-09-25 10:03:05 +0300

    redirect from / to /student/test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import ItemDetail from './assessment/src/components/ItemDetail';
 
 import Student from './student/src';
 import { QuestionEditor, MultipleChoice } from './author/src';
@@ -15,12 +16,12 @@ class App extends Component {
     return (
       <div>
         <Switch>
-          <Redirect exact path="/" to="/student" />
+          <Redirect exact path="/" to="/student/test" />
           <Route path="/author/mcq" component={MultipleChoice} />
           <Route path="/author/orderlist" component={QuestionEditor} />
           <Route path="/student/test" component={() => <Student defaultAP />} />
           <Route path="/student/practice" component={Student} />
-          {/* <Route path="/items/:id" component={ItemDetail} /> */}
+          <Route path="/items/:id" component={ItemDetail} />
         </Switch>
       </div>
     );

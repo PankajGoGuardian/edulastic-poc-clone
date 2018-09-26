cc4a24dd6a52d8eec475c0ead5a11ee39090419a
AndreyYevsega <andrii.yevsega@gmail.com>
2018-09-26 16:22:15 +0300

    remove questions from assessment

--- modules/assessment/src/reducers/index.js
+++ modules/assessment/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 
-import questions from './questions';
 import assessment from './assessment';
 
 const rootReducer = combineReducers({
-  questions,
   assessment,
 });
 
--- modules/assessment/src/sagas/index.js
+++ modules/assessment/src/sagas/index.js
@@ -1,8 +1,7 @@
 import { all } from 'redux-saga/effects';
 
 import answerSaga from './answer';
-import question from './question';
 
 export default function* () {
-  yield all([answerSaga(), question()]);
+  yield all([answerSaga()]);
 }

d16a6ac670e33681864fbc79b50af313250c9ec3
AndreyYevsega <andrii.yevsega@gmail.com>
2018-09-26 16:08:31 +0300

    remove view from assessment

--- modules/assessment/src/selectors/view.js
+++
@@ -1,6 +0,0 @@
-import { createSelector } from 'reselect';
-
-const moduleName = 'view';
-export const stateSelector = state => state[moduleName];
-
-export const getViewSelector = createSelector(stateSelector, state => state.view);

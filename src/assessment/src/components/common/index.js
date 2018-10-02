7f900ece40cebe4742b0b9d05ec4f70472960c83
Jenya Tkachenko <ekatka@outlook.com>
2018-10-02 11:54:49 +0300

    Merge localization-fix branch

---
+++ src/assessment/src/components/common/index.js
@@ -0,0 +1,4 @@
+export { default as PointField } from './PointField';
+export { default as PointsContainer } from './PointsContainer';
+export { default as Button } from './Button';
+export { default as Header } from './Header';

29211dd4dcb49a468fab920181f73aad9676c148
Jenya Tkachenko <ekatka@outlook.com>
2018-10-02 18:57:00 +0300

    Move MultipleChoice logic in container to Assessment module

--- src/assessment/src/components/MultipleChoice/index.js
+++ src/assessment/src/components/MultipleChoice/index.js
@@ -2,3 +2,4 @@ export { default as MultipleChoiceAuthoring } from './Authoring';
 export { default as MultipleChoiceDisplay } from './Display';
 export { default as MultipleChoiceReport } from './Report';
 export { default as MultipleChoiceEvaluation } from './Evaluation';
+export { default as MultipleChoice } from './MultipleChoice';

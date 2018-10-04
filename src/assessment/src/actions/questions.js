1067c3d6dfff60f28f91f59af4571ce26d31b851
Jenya Tkachenko <ekatka@outlook.com>
2018-10-04 12:15:21 +0300

    Add items and question actions to Assessment to save item/questions

---
+++ src/assessment/src/actions/questions.js
@@ -0,0 +1,26 @@
+import {
+  LOAD_QUESTIONS,
+  GOTO_QUESTION,
+  AUTHOR_QUESTION,
+} from '../constants/actions';
+
+export const loadQuestions = questions => ({
+  type: LOAD_QUESTIONS,
+  payload: {
+    questions,
+  },
+});
+
+export const gotoQuestion = question => ({
+  type: GOTO_QUESTION,
+  payload: {
+    question,
+  },
+});
+
+export const addQuestion = question => ({
+  type: AUTHOR_QUESTION,
+  payload: {
+    ...question,
+  },
+});

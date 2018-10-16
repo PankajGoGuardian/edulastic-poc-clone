395c9dcc928f6111910d12797bdac52ac01be2a3
AndreyYevsega <andrii.yevsega@gmail.com>
2018-10-16 09:57:32 +0300

    fix eslint errors

--- src/assessment/src/themes/common/QuestionSelectDropdown/index.js
+++ src/assessment/src/themes/common/QuestionSelectDropdown/index.js
@@ -4,21 +4,18 @@ import { withNamespaces } from '@edulastic/localization';
 
 import SelectContainer from './SelectContainer';
 import Select from './Select';
-import { gotoQuestion } from '../../../actions/questions';
 
 const QuestionSelectDropdown = ({ gotoQuestion, options, value, t }) => (
   <SelectContainer>
     <Select
       defaultValue={value}
       onChange={(e) => {
-        gotoQuestion(parseInt(e.target.value));
+        gotoQuestion(parseInt(e.target.value, 10));
       }}
     >
       {options.map((item, index) => (
         <option key={index} value={item.value}>
-          {`${t('common.layout.selectbox.question')} ${index + 1}/ ${
-            options.length
-          }`}
+          {`${t('common.layout.selectbox.question')} ${index + 1}/ ${options.length}`}
         </option>
       ))}
     </Select>

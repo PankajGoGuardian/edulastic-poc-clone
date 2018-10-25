1e7596fc820efe25bb66506aba2b89844bf77c24
aswin-sw <aswin.raj@snapwiz.com>
2018-10-25 15:37:05 +0530

    modified script

--- scripts/npm-install.sh
+++ scripts/npm-install.sh
@@ -2,5 +2,5 @@ yum -y install gcc-c++ make -y
 curl -sL https://rpm.nodesource.com/setup_8.x | sudo -E bash -
 yum -y install nodejs 
 npm i -g yarn 
-cd /home/ec2/edulastic-poc
+cd /home/ec2-user/edulastic-poc
 yarn 

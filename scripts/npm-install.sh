28cd99898cc0f1fa567c97c4fb50eb14d0871f61
buildsnapwiz <build@snapwiz.com>
2018-10-25 15:46:28 +0530

    changes in npm-install.sh

--- scripts/npm-install.sh
+++ scripts/npm-install.sh
@@ -1,6 +1,8 @@
+set -e
+yum -y update
 yum -y install gcc-c++ make -y 
 curl -sL https://rpm.nodesource.com/setup_8.x | sudo -E bash -
 yum -y install nodejs 
 npm i -g yarn 
-cd /home/ec2-user/edulastic-poc
+cd ~/edulastic-poc
 yarn 

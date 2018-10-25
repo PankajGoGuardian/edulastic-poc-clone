aec831a0b746cac5fab80d9db94cb74a3830c2b5
Aswin <aswin.raj@snapwiz.com>
2018-10-25 18:34:22 +0530

    change server.js (#64)

--- scripts/npm-start.sh
+++ scripts/npm-start.sh
@@ -1,5 +1,5 @@
 
 set -e
 cd ~/edulastic-poc
-pm2 stop scripts/serve.js
-pm2 start scripts/serve.js
+pm2 stop server.js
+pm2 start server.js
--- scripts/serve.js
+++ scripts/serve.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.use('/', express.static('./build'));
+app.use(express.static(path.relative(__dirname, '../build')));
+
+app.get('*', (req, res) => {
+  let filePath = path.relative(__dirname, '../build/index.html');
+  console.log('filepath', filePath);
+  res.sendFile(filePath);
+});
 
 app.listen(3000);

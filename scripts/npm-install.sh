18c802f0bcdf223c3e06e238575e0b0fafbff115
aswin-sw <aswin.raj@snapwiz.com>
2018-10-25 15:22:38 +0530

    modified script

--- scripts/npm-install.sh
+++ scripts/npm-install.sh
@@ -1,5 +1,5 @@
-yum install -y gcc-c++ make
-curl -sL https://rpm.nodesource.com/setup_8.x | sudo -E bash -
-yum install nodejs
-npm i -g yarn
+yum install -y gcc-c++ make &&
+curl -sL https://rpm.nodesource.com/setup_8.x | sudo -E bash - 
+&& yum install nodejs -y
+npm i -g yarn 
 yarn 

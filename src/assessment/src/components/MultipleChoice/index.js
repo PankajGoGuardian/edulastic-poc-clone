785d5c56b33494efe9e2274624d7849e26fc8909
Aswin <aswin.raj@snapwiz.com>
2018-10-29 09:56:16 +0530

    fixed missing evaluation component (#75)

--- src/assessment/src/components/MultipleChoice/index.js
+++ src/assessment/src/components/MultipleChoice/index.js
@@ -1,5 +1,4 @@
 export { default as MultipleChoiceAuthoring } from './Authoring';
 export { default as MultipleChoiceDisplay } from './Display';
-export { default as MultipleChoiceEvaluation } from './Evaluation';
 export { default as MultipleChoice } from './MultipleChoice';
 export { default as CorrectAnswers } from './CorrectAnswers';

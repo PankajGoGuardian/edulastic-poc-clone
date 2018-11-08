9405f93643d758f7d1943f5d713126abe9d4785f
narutojone1218 <naruto_1218@hotmail.com>
2018-11-07 17:41:00 -0800

    author/tests route path issue fix

--- src/author/src/app.js
+++ src/author/src/app.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Layout } from 'antd';
+import { Progress } from '@edulastic/common';
 import Sidebar from './Sidebar/SideMenu';
 import ItemList from './components/ItemList';
 import ItemDetail from './components/ItemDetail';
+
+const TestList = lazy(() => import('./components/TestList'));
 
 const Author = ({ match }) => (
   <Layout>
@@ -12,6 +15,14 @@ const Author = ({ match }) => (
     <Switch>
       <Route path={`${match.url}/items`} component={ItemList} />
       <Route path={`${match.url}/item/:id/item-detail`} component={ItemDetail} />
+      <Route
+        path={`${match.url}/tests`}
+        render={props => (
+          <Suspense fallback={<Progress />}>
+            <TestList {...props} />
+          </Suspense>
+        )}
+      />
     </Switch>
   </Layout>
 );

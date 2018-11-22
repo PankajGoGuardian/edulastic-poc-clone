1ea7beb4be9cda3be97c470b3b9cb454a233108c
Aswin <aswin.raj@snapwiz.com>
2018-11-22 23:55:31 +0530

    added flow (static-types all D way) (#215)

--- src/server/utils/token.js
+++ src/server/utils/token.js
@@ -1,9 +1,11 @@
+// @flow
 import jwt from 'jsonwebtoken';
 import config from '../config';
 
 const { secret, expiresIn } = config.jwt;
 
-export const generateAuthToken = payload =>
+export const generateAuthToken = (payload: string): string =>
   jwt.sign(payload, secret, { expiresIn });
 
-export const decodeAuthToken = token => jwt.verify(token, secret);
+export const decodeAuthToken = (token: string): string =>
+  jwt.verify(token, secret);

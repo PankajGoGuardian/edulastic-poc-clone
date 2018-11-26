005bf9712a870a662ca687398e0f2a294d76a98a
thomasgit123 <thomasbox123@gmail.com>
2018-11-26 20:36:22 +0300

    Unit test (#243)

--- src/client/author/src/components/TestList/__test__/index.test.js
+++ src/client/author/src/components/TestList/__test__/index.test.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
+
+import configureStore from '../../../../../configureStore';
 
 import TestList from '../TestList';
+
+const store = configureStore();
 
 describe('<TestList />', () => {
   it('should render properly', () => {
     const renderedComponent = shallow(<TestList />);
     expect(renderedComponent.length).toEqual(1);
+  });
+
+  it('contains antd input component with an expectation', () => {
+    const renderedComponent = render(<TestList store={store} />);
+    expect(renderedComponent.find('.ant-input').length).toBe(1);
+  });
+
+  it('contains antd suffix component with an expectation', () => {
+    const renderedComponent = render(<TestList store={store} />);
+    expect(renderedComponent.find('.ant-input-suffix').length).toBe(1);
   });
 });

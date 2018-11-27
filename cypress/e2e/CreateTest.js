d3ce115da5f584610328650f516ec9009f46f945
thomasgit123 <thomasbox123@gmail.com>
2018-11-27 06:33:48 -0500

    feat(cypress-test): create test automation flow (#259)

--- cypress/e2e/CreateTest.js
+++ cypress/e2e/CreateTest.js
@@ -1,7 +1,30 @@
-describe('Create a new test', () => {
+describe('Visit Create Test Page', () => {
   it('Visit Create Test Page', () => {
     cy.setToken().then(() => {
       cy.visit('/author/tests/create');
+    });
+  });
+});
+
+describe('Check Create Test Flow', () => {
+  it('Visit Create Test Page', () => {
+    cy.setToken().then(() => {
+      cy.visit('/author/tests/create')
+        .contains('ADD')
+        .click();
+
+      cy.contains('Summary')
+        .click();
+
+      cy.get('input').first().focus()
+        .clear()
+        .type('Nice Product!');
+
+      cy.get('div').contains('Assign')
+        .click();
+
+      cy.contains('Save changes')
+        .click();
     });
   });
 });

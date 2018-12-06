5043a7e292a055360f2713534857fc837fb13de1
thomasgit123 <thomasbox123@gmail.com>
2018-12-06 13:52:18 -0500

    feat(cypress): multiple choice end to end test (#334)

---
+++ cypress/e2e/MultipleChoice - standard.js
@@ -0,0 +1,78 @@
+describe('Test Multiple Choice Flow', () => {
+  beforeEach(() => {
+    cy.setToken();
+  });
+
+  it('Visit Item List Page', () => {
+    cy.visit('/author/Items');
+  });
+
+  it('Check Flow', () => {
+    cy.get('button').contains('Create').should('be.visible');
+    cy.contains('Create').click();
+    cy.contains('Add New').click();
+
+    cy.get('li').should('contain', 'Multiple Choice');
+    cy.get('li').should('contain', 'Fill in the Blanks');
+    cy.get('li').should('contain', 'Classify, Math & Order');
+    cy.get('li').should('contain', 'Written & Spoken');
+    cy.get('li').should('contain', 'Highlight');
+    cy.get('li').should('contain', 'Math');
+    cy.get('li').should('contain', 'Graphing');
+    cy.get('li').should('contain', 'Charts');
+    cy.get('li').should('contain', 'Chemistry');
+    cy.get('li').should('contain', 'Other');
+  });
+
+  it('Multiple choice - standard Test', () => {
+    // Test Each Question Type
+    cy.get('div').should('contain', 'Multiple choice - standard');
+    cy.get('div').contains('Multiple choice - standard').next().click();
+
+    cy.get('div').find('[contenteditable]').eq(0)
+      .clear()
+      .type('What is your favorite color?');
+
+    cy.get('div').find('[contenteditable]').eq(2)
+      .clear()
+      .type('White');
+
+    cy.get('div').find('[contenteditable]').eq(4)
+      .clear()
+      .type('Black');
+
+    cy.get('div').find('[contenteditable]').eq(6)
+      .clear()
+      .type('Blue');
+
+    cy.get('div').find('[contenteditable]').eq(6)
+      .trigger('mouseup');
+
+    // Add new choice
+
+    cy.contains('Add New Choice').should('be.visible');
+    cy.contains('Add New Choice').click();
+
+    // Go to the Preview Tab
+
+    cy.contains('PREVIEW').should('be.visible');
+    cy.contains('PREVIEW').click();
+
+    // Save Multiple Choice - standard
+
+    cy.contains('SAVE').should('be.visible');
+    cy.contains('SAVE').click();
+  });
+
+  it('Visit Item Detail Page', () => {
+    cy.visit('/author/items/5c094fe34be5af0d7fe9ba83/item-detail');
+    cy.wait(2000);
+    cy.contains('PREVIEW').should('be.visible');
+    cy.contains('PREVIEW').click();
+  });
+
+  it('Check Answers', () => {
+    cy.contains('Show Answers').should('be.visible');
+    cy.contains('Show Answers').click();
+  });
+});

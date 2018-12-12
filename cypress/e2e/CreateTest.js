b0cc556e647d25b559dbc3e33865d1133779831d
juli-jaiswal <43469423+juli-jaiswal@users.noreply.github.com>
2018-12-12 12:26:02 +0530

    fix(create-test):fixed create test issue (#376)

--- cypress/e2e/CreateTest.js
+++ cypress/e2e/CreateTest.js
@@ -12,54 +12,69 @@ describe('Check Create Test Flow', () => {
   });
 
   it('Visit Create Test Page', () => {
-    cy.visit('/author/tests/create');
-    cy.server();
-    cy.route('GET', '/api/testitem/limit=10&skip=0&data=true&validation=true')
-      .as('load');
-
     /* eslint-disable */
-    cy.wait(2000);
+    cy.wait(5000);
 
     // Add test items
+    cy.get('button')
+      .eq(4)
+      .find('span')
+      .should('contain', 'ADD');
+    cy.get('button')
+      .eq(4)
+      .click();
 
-    cy.get('button').eq(3).find('span').should('contain', 'ADD');
-    cy.get('button').eq(3).click();
+    cy.get('button')
+      .eq(5)
+      .find('span')
+      .should('contain', 'ADD');
+    cy.get('button')
+      .eq(5)
+      .click();
 
-    cy.get('button').eq(4).find('span').should('contain', 'ADD');
-    cy.get('button').eq(4).click();
-
-    cy.get('button').eq(5).find('span').should('contain', 'ADD');
-    cy.get('button').eq(5).click();
-
-    cy.get('button').eq(6).find('span').should('contain', 'ADD');
-    cy.get('button').eq(6).click();
+    cy.get('button')
+      .eq(6)
+      .find('span')
+      .should('contain', 'ADD');
+    cy.get('button')
+      .eq(6)
+      .click();
 
     // Summary Tab Test
 
-    cy.contains('Summary')
-      .click();
+    cy.contains('Summary').click();
 
-    cy.get('input').first().focus()
+    cy.get('input')
+      .first()
+      .focus()
       .clear()
       .type('Nice Product!');
 
-    cy.get('textarea').eq(0).focus()
+    cy.get('textarea')
+      .eq(0)
+      .focus()
       .clear()
       .type('This is a description for test!');
 
     // Review Tab Test
 
-    cy.contains('Review')
-      .click();
+    cy.contains('Review').click();
 
     // Review tab should contain expected buttons
 
-    cy.get('button').eq(3).find('span').should('contain', 'Remove Selected');
-    cy.get('button').eq(4).find('span').should('contain', 'Move to');
-    cy.get('button').eq(6).find('span').should('contain', 'Collapse Rows');
+    cy.get('button')
+      .eq(3)
+      .find('span')
+      .should('contain', 'Remove Selected');
+    cy.get('button')
+      .eq(4)
+      .find('span')
+      .should('contain', 'Move to');
+    cy.get('button')
+      .eq(6)
+      .find('span')
+      .should('contain', 'Collapse Rows');
 
-
-    cy.contains('Save changes')
-      .click();
+    cy.contains('Save changes').click();
   });
 });

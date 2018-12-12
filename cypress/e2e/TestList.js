fad97fc4dd152a2a29e3e681799f06b9256cecde
juli-jaiswal <43469423+juli-jaiswal@users.noreply.github.com>
2018-12-12 18:37:43 +0530

    fix(cypress-test-list):fixed tes list issue (#386)

--- cypress/e2e/TestList.js
+++ cypress/e2e/TestList.js
@@ -2,7 +2,9 @@ describe('Create a new test', () => {
   it('Visit Create Test Page', () => {
     cy.setToken().then(() => {
       cy.visit('/author/tests');
-      cy.contains('Create Test').should('be.visible');
+      cy.get('button')
+        .contains('Create Test')
+        .should('be.visible');
       cy.contains('Create Test').click();
     });
   });

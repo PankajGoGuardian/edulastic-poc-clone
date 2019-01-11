dccace38620ec97b94a1ed81016d617f2c55dc34
Aswin <aswin.raj@snapwiz.com>
2019-01-11 19:39:30 +0530

    feat(checkAnswer-student): add check answer for responsive student (#590)

--- src/client/assessment/src/themes/common/ToolbarModal.js
+++ src/client/assessment/src/themes/common/ToolbarModal.js
@@ -1,55 +1,57 @@
 import React from 'react';
 import Modal from 'react-responsive-modal';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Button } from 'antd';
+import { checkAnswerEvaluation } from '../../actions/checkanswer';
 
 class ToolbarModal extends React.Component {
   checkAnswer = () => {
-    const { onClose, checkanswer } = this.props;
-    checkanswer();
+    const { onClose, checkAnswerEvaluation } = this.props;
+    checkAnswerEvaluation();
     onClose();
-  }
+  };
 
   hint = () => {
     const { onClose } = this.props;
     onClose();
-  }
+  };
 
   bookmark = () => {
     const { onClose } = this.props;
     onClose();
-  }
+  };
 
   pointer = () => {
     const { onClose } = this.props;
     onClose();
-  }
+  };
 
   inchRuler = () => {
     const { onClose } = this.props;
     onClose();
-  }
+  };
 
   centimeterRuler = () => {
     const { onClose } = this.props;
     onClose();
-  }
+  };
 
   calculator = () => {
     const { onClose } = this.props;
     onClose();
-  }
+  };
 
   eliminationQuestion = () => {
     const { onClose } = this.props;
     onClose();
-  }
+  };
 
   procractorRuler = () => {
     const { onClose } = this.props;
     onClose();
-  }
+  };
 
   render() {
     const { isVisible, onClose } = this.props;
@@ -62,15 +64,27 @@ class ToolbarModal extends React.Component {
         center
       >
         <Container>
-          <StyledButton onClick={() => this.checkAnswer()}>Check Answer</StyledButton>
+          <StyledButton onClick={() => this.checkAnswer()}>
+            Check Answer
+          </StyledButton>
           <StyledButton onClick={() => this.hint()}>Hint</StyledButton>
           <StyledButton onClick={() => this.bookmark()}>Bookmark</StyledButton>
           <StyledButton onClick={() => this.pointer()}>Pointer</StyledButton>
-          <StyledButton onClick={() => this.inchRuler()}>Inch Ruler</StyledButton>
-          <StyledButton onClick={() => this.centimeterRuler()}>Centimeter Ruler</StyledButton>
-          <StyledButton onClick={() => this.calculator()}>Calculator</StyledButton>
-          <StyledButton onClick={() => this.eliminationQuestion()}>Elimination Question</StyledButton>
-          <StyledButton onClick={() => this.procractorRuler()}>Procractor Ruler</StyledButton>
+          <StyledButton onClick={() => this.inchRuler()}>
+            Inch Ruler
+          </StyledButton>
+          <StyledButton onClick={() => this.centimeterRuler()}>
+            Centimeter Ruler
+          </StyledButton>
+          <StyledButton onClick={() => this.calculator()}>
+            Calculator
+          </StyledButton>
+          <StyledButton onClick={() => this.eliminationQuestion()}>
+            Elimination Question
+          </StyledButton>
+          <StyledButton onClick={() => this.procractorRuler()}>
+            Procractor Ruler
+          </StyledButton>
         </Container>
       </Modal>
     );
@@ -83,7 +97,10 @@ ToolbarModal.propTypes = {
   checkanswer: PropTypes.func.isRequired
 };
 
-export default ToolbarModal;
+export default connect(
+  null,
+  { checkAnswerEvaluation }
+)(ToolbarModal);
 
 const Container = styled.div`
   display: flex;
@@ -103,7 +120,8 @@ const StyledButton = styled(Button)`
   font-size: 14px;
   font-weight: 600;
   letter-spacing: 0.3px;
-  &:active, &:focus {
+  &:active,
+  &:focus {
     border-color: #f3f3f3;
   }
 `;

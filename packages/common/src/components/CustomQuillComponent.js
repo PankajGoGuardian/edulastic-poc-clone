fb5bf4ac277ec820ac7bd81ece39b9f13a8a6d65
Batman Hit <30376020+batmanhit@users.noreply.github.com>
2019-01-16 14:35:47 +0400

    ev-597, ev-598, ev-599 (#605)
    Custom Quill component fixed

--- packages/common/src/components/CustomQuillComponent.js
+++ packages/common/src/components/CustomQuillComponent.js
@@ -13,7 +13,7 @@ class ResponseCmp extends Embed {
     const node = super.create();
     const responseCount = document.querySelectorAll('.response-btn').length;
     node.setAttribute('contenteditable', false);
-    node.innerHTML = `&nbsp;Quillspan class="index">${responseCount +
+    node.innerHTML = `&nbsp;<span class="index">${responseCount +
       1}</span><span class="text">Response</span>&nbsp;`;
     return node;
   }

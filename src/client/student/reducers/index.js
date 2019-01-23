2c657efec395f0ea4b4047ebe339ca38ff09ca89
Aswin <aswin.raj@snapwiz.com>
2019-01-23 11:57:59 +0530

    refactor(folder): remove unused folders(src/) (#657)

--- src/client/student/reducers/index.js
+++ src/client/student/reducers/index.js
@@ -2,20 +2,14 @@ import ui from './ui';
 //import assignments from './assignment';
 //import reports from './reports';
 import skillReport from '../skillReport/ducks';
-import testItemActivity from '../src/reducers/testItemActivity';
-import items from '../src/reducers/items';
 import { studentAssignmentReducer } from '../AssignmentModule';
 import { studentReportReducer } from '../ReportsModule';
 import user from './user';
 
 const studentReducer = {
   ui,
-  //reports,
   skillReport,
   user,
-  studentItems: items,
-  testItemActivity,
-  //assignments,
   studentReport: studentReportReducer,
   studentAssignment: studentAssignmentReducer
 };
--- src/client/student/src/reducers/testItemActivity.js
+++
@@ -1,9 +0,0 @@
-const initialState = {};
-
-// testItemActivity reducer
-const testItemActivity = (state = initialState, { type, payload }) => {
-  switch (type) {
-    default:
-      return state;
-  }
-};

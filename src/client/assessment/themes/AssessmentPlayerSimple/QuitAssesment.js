658b9b9bbc90fa20f73040c61f6b8aea36c0a7bc
Arun Sasidharan <43816559+arun-sasidharan@users.noreply.github.com>
2019-02-08 20:38:05 +0530

    fix(assessment player practice): quit functionality (#806)

--- src/client/assessment/themes/AssessmentPlayerSimple/QuitAssesment.js
+++ src/client/assessment/themes/AssessmentPlayerSimple/QuitAssesment.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import { withNamespaces } from '@edulastic/localization';
 
-const QuitAssesment = ({ t }) => <Quit>{t('pagination.quit')}</Quit>;
+const QuitAssesment = ({ t }) => (
+  <Quit to="/home/assignments">{t('pagination.quit')}</Quit>
+);
 
 QuitAssesment.propTypes = {
   t: PropTypes.func.isRequired
@@ -11,10 +14,13 @@ QuitAssesment.propTypes = {
 
 export default withNamespaces('common')(QuitAssesment);
 
-const Quit = styled.span`
+const Quit = styled(Link)`
   margin: 0px 42px !important;
   font-weight: bold;
-  font-size:  ${props => props.theme.quitAssessmentFontSize};
-  color:  ${props => props.theme.quitAssessmentTextColor};
+  font-size: ${props => props.theme.quitAssessmentFontSize};
+  color: ${props => props.theme.quitAssessmentTextColor};
   text-transform: uppercase;
+  &:hover {
+    color: ${props => props.theme.quitAssessmentTextColor};
+  }
 `;

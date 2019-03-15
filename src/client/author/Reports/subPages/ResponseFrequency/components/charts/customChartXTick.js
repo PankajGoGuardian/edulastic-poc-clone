644cfc6dce286566378808fb9f3406609691742f
subendra-snapwiz <48204535+subendra-snapwiz@users.noreply.github.com>
2019-03-15 15:16:10 +0530

    tooltip and chart number of question mismatch (#1073)
    * tooltip and chart number of question mismatch
    
    * as per advised method

--- src/client/author/Reports/subPages/ResponseFrequency/components/charts/customChartXTick.js
+++ src/client/author/Reports/subPages/ResponseFrequency/components/charts/customChartXTick.js
@@ -2,11 +2,23 @@ import React from "react";
 import { StyledAxisTickText } from "../styled";
 
 export const CustomChartXTick = props => {
-  const { x, y, payload, getDataByIndex } = props;
+  const { x, y, payload, data } = props;
+
+  const getDataByName = name => {
+    if (data) {
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].name === name) {
+          return data[i].qCount;
+        }
+      }
+    }
+    return "";
+  };
+
   return (
     <g transform={`translate(${x},${y})`}>
       <StyledAxisTickText textAnchor="middle" verticalAnchor="start" width={70}>
-        {payload.value + " (" + getDataByIndex(payload.index).qCount + ")"}
+        {payload.value + " (" + getDataByName(payload.value) + ")"}
       </StyledAxisTickText>
     </g>
   );

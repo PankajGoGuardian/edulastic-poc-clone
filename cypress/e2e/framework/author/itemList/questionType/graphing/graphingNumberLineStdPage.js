f8565a7d0e36b890bb67dde4c86a367fb40b950a
watasm <um.artem@gmail.com>
2019-03-21 16:31:58 +0400

    feat(graph): add hyperbola and ellipse tools (#1115)

--- cypress/e2e/framework/author/itemList/questionType/graphing/graphingNumberLineStdPage.js
+++ cypress/e2e/framework/author/itemList/questionType/graphing/graphingNumberLineStdPage.js
@@ -1,8 +1,8 @@
-import GraphingNumberLineStandardPage from "./graphingNumberLineStdPage";
+import GraphingStandardPage from "./graphingStandardPage";
 
-class GraphingNumberLineDragAndDropPage extends GraphingStandardPage {
-  constructor() {
-    super();
+class GraphingNumberLineStandardPage extends GraphingStandardPage {
+  constructor(svgWidth, svgHeight) {
+    super(svgWidth, svgHeight);
   }
 
   // elements ---------------------------------------------------------------
@@ -11,12 +11,32 @@ class GraphingNumberLineDragAndDropPage extends GraphingStandardPage {
     return cy.contains("section", "Possible responses");
   }
 
+  getToolbarContainer() {
+    return cy.contains("section", "Toolbar");
+  }
+
+  getLabelsContainer() {
+    return cy.contains("section", "Labels");
+  }
+
+  getTicksContainer() {
+    return cy.contains("section", "Ticks");
+  }
+
+  getResponsesAllowedParameter() {
+    return cy.get('input[name="responsesAllowed"]');
+  }
+
   getTitleParameter() {
     return cy.get('input[name="title"]');
   }
 
   getBoard() {
     return cy.get('[data-cy="axis-labels-container"]');
+  }
+
+  getBoards() {
+    return cy.get('[data-cy="axis-labels-container"] .jxgbox');
   }
 
   getVisibleTickLabelsOnBoard(containText = null) {
@@ -81,12 +101,28 @@ class GraphingNumberLineDragAndDropPage extends GraphingStandardPage {
     return cy.get('input[name="separationDistanceY"]');
   }
 
+  getLayoutStackResponsesSpacing() {
+    return cy.get('input[name="stackResponsesSpacing"]');
+  }
+
   getLabelsFrequency() {
     return cy.get('input[name="frequency"]');
   }
 
   getLabelsSpecificPoints() {
     return cy.get('input[name="specificPoints"]');
+  }
+
+  getTicksDistance() {
+    return cy.get('input[name="ticksDistance"]');
+  }
+
+  getMinorTicks() {
+    return cy.get('input[name="minorTicks"]');
+  }
+
+  getSegmentsToolbar() {
+    return cy.get('[data-cy="segmentsToolbar"]');
   }
 
   // actions ---------------------------------------------------------------
@@ -113,6 +149,16 @@ class GraphingNumberLineDragAndDropPage extends GraphingStandardPage {
     return this;
   }
 
+  clickOnShowMinArrow() {
+    cy.contains("Show min arrow").click();
+    return this;
+  }
+
+  clickOnShowMaxArrow() {
+    cy.contains("Show max arrow").click();
+    return this;
+  }
+
   clickOnShowMin() {
     cy.contains("Show min").click();
     return this;
@@ -122,5 +168,36 @@ class GraphingNumberLineDragAndDropPage extends GraphingStandardPage {
     cy.contains("Show max").click();
     return this;
   }
+
+  clickOnShowLabels() {
+    cy.contains("Show labels").click();
+    return this;
+  }
+
+  selectRenderingBase(option) {
+    cy.contains("div", "Rendering base")
+      .parent()
+      .find("select")
+      .select(option);
+    return this;
+  }
+
+  selectToolOnToolbar(index, option) {
+    this.getToolbarContainer()
+      .find('select[data-cy="selectStyle"]')
+      .eq(index)
+      .select(option);
+    return this;
+  }
+
+  clickOnDeleteToolOnToolbar(index) {
+    this.getToolbarContainer()
+      .find('select[data-cy="selectStyle"]')
+      .eq(index)
+      .parent()
+      .next()
+      .click();
+    return this;
+  }
 }
-export default GraphingNumberLineDragAndDropPage;
+export default GraphingNumberLineStandardPage;

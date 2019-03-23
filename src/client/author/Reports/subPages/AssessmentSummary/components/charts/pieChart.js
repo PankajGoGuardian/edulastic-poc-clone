319949ac2aca27e02bff85002e81ffbeacb84e6a
subendra-snapwiz <48204535+subendra-snapwiz@users.noreply.github.com>
2019-03-23 18:48:39 +0530

    EV-1356-3: pie chart tooltip fix (#1142)

--- src/client/author/Reports/subPages/AssessmentSummary/components/charts/pieChart.js
+++ src/client/author/Reports/subPages/AssessmentSummary/components/charts/pieChart.js
@@ -3,6 +3,7 @@ import { ResponsiveContainer, PieChart, Pie, Legend, Tooltip } from "recharts";
 import { fadedBlack } from "@edulastic/colors";
 import { StyledCustomChartTooltip } from "../styled";
 import { Row, Col } from "antd";
+import { sumBy } from "lodash";
 
 export const SimplePieChart = props => {
   const renderCustomizedLabel = args => {
@@ -21,11 +22,15 @@ export const SimplePieChart = props => {
   const chartData = useMemo(() => {
     let arr = [];
     if (props.data) {
+      const sum = sumBy(props.data, o => {
+        return o.bandPerf;
+      });
       for (let i = 0; i < props.data.length; i++) {
         arr.push({
           bandPerf: props.data[i].bandPerf,
           fill: props.data[i].color,
-          name: props.data[i].masteryName
+          name: props.data[i].masteryName,
+          sum: sum
         });
       }
     }
@@ -38,7 +43,7 @@ export const SimplePieChart = props => {
         <div>
           <Row type="flex" justify="start">
             <Col className="tooltip-key">{payload[0].name} : </Col>
-            <Col className="tooltip-value">{Math.round(payload[0].value)}%</Col>
+            <Col className="tooltip-value">{Math.round((payload[0].value / payload[0].payload.sum) * 100)}%</Col>
           </Row>
         </div>
       );

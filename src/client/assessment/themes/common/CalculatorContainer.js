93399d40ad404914171eca8300f7d90104697184
Senior Full Stack Developer <39874090+seniorapple@users.noreply.github.com>
2019-03-22 23:43:40 -0700

    feat(calculator) : create a calculator for scientific, Basic and Grap… (#1112)
    * feat(calculator) : create a calculator for scientific, Basic and Graphing (#1455)
    
    * fix(calculator) :  add basic calculator
    
    * fix(calculator) : reslove comments

---
+++ src/client/assessment/themes/common/CalculatorContainer.js
@@ -0,0 +1,214 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components";
+import Draggable from "react-draggable";
+import BasicCalculator from "./BasicCalculator";
+import { WithResources } from "@edulastic/common";
+
+class CalculatorContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      calculateMode: this.props.calculateMode,
+      changeMode: this.props.changeMode
+    };
+
+    this.desmosGraphingRef = React.createRef();
+    this.desmosBasicRef = React.createRef();
+    this.desmosScientificRef = React.createRef();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      calculateMode: nextProps.calculateMode,
+      changeMode: nextProps.changeMode
+    });
+  }
+
+  componentDidMount() {
+    let desmosGraphCalculator = Desmos.GraphingCalculator(ReactDOM.findDOMNode(this.desmosGraphingRef));
+    desmosGraphCalculator.setExpression({ dragMode: Desmos.DragModes.XY });
+
+    Desmos.FourFunctionCalculator(ReactDOM.findDOMNode(this.desmosBasicRef));
+    Desmos.ScientificCalculator(ReactDOM.findDOMNode(this.desmosScientificRef));
+
+    let geogebraGraphing = new GGBApplet(
+      {
+        id: "ggbAppletGraphing",
+        appName: "graphing",
+        width: 800,
+        height: 600,
+        showToolBar: true,
+        borderColor: null,
+        showMenuBar: true,
+        allowStyleBar: true,
+        showAlgebraInput: true,
+        enableLabelDrags: false,
+        enableShiftDragZoom: true,
+        capturingThreshold: null,
+        showToolBarHelp: false,
+        errorDialogsActive: true,
+        showTutorialLink: true,
+        showLogging: true,
+        useBrowserForJS: false
+      },
+      "5.0",
+      "geogebra-graphingculator"
+    );
+    geogebraGraphing.inject("geogebra-graphingculator");
+
+    let geogebraScientific = new GGBApplet(
+      {
+        id: "ggbAppletScientific",
+        appName: "scientific",
+        width: 800,
+        height: 600,
+        showToolBar: true,
+        borderColor: null,
+        showMenuBar: true,
+        allowStyleBar: true,
+        showAlgebraInput: true,
+        enableLabelDrags: false,
+        enableShiftDragZoom: true,
+        capturingThreshold: null,
+        showToolBarHelp: false,
+        errorDialogsActive: true,
+        showTutorialLink: true,
+        showLogging: true,
+        useBrowserForJS: false
+      },
+      "5.0",
+      "geogebra-scientificcalculator"
+    );
+    geogebraScientific.inject("geogebra-scientificcalculator");
+  }
+
+  render() {
+    const { calculateMode, changeMode } = this.state;
+    return (
+      <Container>
+        <StyledDraggable>
+          <StyledDiv visible={changeMode == 2 && calculateMode === 0}>
+            <StyledTitle>Desmos Graphing Calculator</StyledTitle>
+            <DesmosGraphingCalculator
+              id="demos-graphiccalculator"
+              ref={ref => {
+                this.desmosGraphingRef = ref;
+              }}
+            />
+          </StyledDiv>
+        </StyledDraggable>
+
+        <StyledDraggable>
+          <StyledDiv visible={changeMode == 2 && calculateMode === 1}>
+            <StyledTitle>Desmos Basic Calculator</StyledTitle>
+            <DesmosBasicCalculator
+              ref={ref => {
+                this.desmosBasicRef = ref;
+              }}
+            />
+          </StyledDiv>
+        </StyledDraggable>
+
+        <StyledDraggable>
+          <StyledDiv visible={changeMode == 2 && calculateMode === 2}>
+            <StyledTitle>Desmos Scientific Calculator</StyledTitle>
+            <DesmosScientificCalculator
+              ref={ref => {
+                this.desmosScientificRef = ref;
+              }}
+            />
+          </StyledDiv>
+        </StyledDraggable>
+
+        <StyledDraggableF>
+          <StyledDiv visible={changeMode == 2 && calculateMode === 3}>
+            <StyledTitle>GeoGebra Graphing Calculator</StyledTitle>
+            <GeoGebracalculator id="geogebra-graphingculator" />
+          </StyledDiv>
+        </StyledDraggableF>
+
+        <StyledDraggableF>
+          <StyledDiv visible={changeMode == 2 && calculateMode === 4}>
+            <StyledTitle>Basic Calculator</StyledTitle>
+            <BasicCalculator id="geogebra-basiccalculator" />
+          </StyledDiv>
+        </StyledDraggableF>
+
+        <StyledDraggableF>
+          <StyledDiv visible={changeMode == 2 && calculateMode === 5}>
+            <StyledTitle>GeoGebra Scientific Calculator</StyledTitle>
+            <GeoGebracalculator id="geogebra-scientificcalculator" />
+          </StyledDiv>
+        </StyledDraggableF>
+      </Container>
+    );
+  }
+}
+
+const Container = styled.div``;
+
+const StyledDraggable = styled(Draggable)`
+  position: absolute;
+`;
+
+const StyledDraggableF = styled(Draggable)`
+  position: absolute;
+  width: 800px;
+`;
+
+const StyledDiv = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 200px;
+  display: ${props => (props.visible ? "block" : "none")};
+`;
+
+const StyledTitle = styled.div`
+  width: 100%;
+  height: 35px;
+  background: #0288d1;
+  color: #ffffff;
+  font-size: 16px;
+  line-height: 35px;
+  padding: 0 12px;
+  font-weight: 600;
+  text-align: left;
+  cursor: move;
+`;
+
+const DesmosGraphingCalculator = styled.div`
+  width: 600px;
+  height: 400px;
+`;
+
+const DesmosBasicCalculator = styled.div`
+  width: 350px;
+  height: 500px;
+`;
+
+const DesmosScientificCalculator = styled.div`
+  width: 600px;
+  height: 500px;
+`;
+
+const GeoGebracalculator = styled.div`
+  width: 800px !important;
+  height: 600px !important;
+`;
+
+const CalculatorContainerWithResources = ({ ...props }) => (
+  <WithResources
+    resources={[
+      "https://www.desmos.com/api/v1.2/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6&ext.js",
+      "https://cdn.geogebra.org/apps/deployggb.js"
+    ]}
+    fallBack={<h2>Loading...</h2>}
+  >
+    <CalculatorContainer {...props} />
+  </WithResources>
+);
+
+export default CalculatorContainerWithResources;
+
+// export default CalculatorContainer;
--- src/client/assessment/themes/common/ToolBar.js
+++ src/client/assessment/themes/common/ToolBar.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import { Button } from "antd";
 import { white } from "@edulastic/colors";
+import CalculatorMenu from "./CalculatorMenu";
+
 import { IconCursor, IconInRuler, IconCalculator, IconClose, IconProtactor, IconScratchPad } from "@edulastic/icons";
 
 class ToolBar extends Component {
@@ -11,6 +13,9 @@ class ToolBar extends Component {
     this.state = {
       select: 0
     };
+
+    this.toolbarHandler = this.toolbarHandler.bind(this);
+    this.handleCalculateMode = this.handleCalculateMode.bind(this);
   }
 
   toolbarHandler = value => {
@@ -18,36 +23,46 @@ class ToolBar extends Component {
 
     this.setState({ select: value });
     if (value === 5) {
-      changeMode(true);
+      changeMode(true, value);
     } else {
-      changeMode(false);
+      changeMode(false, value);
     }
   };
+
+  handleCalculateMode = value => {
+    this.setState({ select: 2 });
+
+    const { changeCaculateMode } = this.props;
+    changeCaculateMode(value);
+  };
+
+  componentDidMount() {}
 
   render() {
     const { select } = this.state;
     return (
       <Container>
-        <StyledButton enable={select === 0 && true} onClick={() => this.toolbarHandler(0)}>
+        <StyledButton enable={select === 0} onClick={() => this.toolbarHandler(0)}>
           <CursorIcon />
         </StyledButton>
 
-        <StyledButton enable={select === 1 && true} onClick={() => this.toolbarHandler(1)}>
+        <StyledButton enable={select === 1} onClick={() => this.toolbarHandler(1)}>
           <InRulerIcon />
         </StyledButton>
 
-        <StyledButton enable={select === 2 && true} onClick={() => this.toolbarHandler(2)}>
+        <StyledButton enable={select === 2} onClick={() => this.toolbarHandler(2)}>
           <CaculatorIcon />
+          {select == 2 && <CalculatorMenu changeCaculateMode={this.handleCalculateMode} />}
         </StyledButton>
 
-        <StyledButton enable={select === 3 && true} onClick={() => this.toolbarHandler(3)}>
+        <StyledButton enable={select === 3} onClick={() => this.toolbarHandler(3)}>
           <CloseIcon />
         </StyledButton>
 
-        <StyledButton enable={select === 4 && true} onClick={() => this.toolbarHandler(4)}>
+        <StyledButton enable={select === 4} onClick={() => this.toolbarHandler(4)}>
           <ProtactorIcon />
         </StyledButton>
-        <StyledButton enable={select === 5 && true} onClick={() => this.toolbarHandler(5)}>
+        <StyledButton enable={select === 5} onClick={() => this.toolbarHandler(5)}>
           <ScratchPadIcon />
         </StyledButton>
       </Container>
@@ -56,7 +71,8 @@ class ToolBar extends Component {
 }
 
 ToolBar.propTypes = {
-  changeMode: PropTypes.func.isRequired
+  changeMode: PropTypes.func.isRequired,
+  changeCaculateMode: PropTypes.func.isRequired
 };
 
 export default ToolBar;

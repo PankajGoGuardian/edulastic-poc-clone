f54135a31e9fa9a3fc10c6ce6f5635ff5144158b
chpinan1128 <clearg1929@gmail.com>
2019-04-04 04:06:57 -0700

    fix(TestItemCol): use variable for media

--- src/client/assessment/components/TestItemPreview/containers/TestItemCol/styled/Container.js
+++ src/client/assessment/components/TestItemPreview/containers/TestItemCol/styled/Container.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
+import { mobileWidth } from "@edulastic/colors";
 
 export const Container = styled.div`
   width: ${({ width }) => width};
   display: flex;
   flex-direction: column;
   border-right-color: ${props => props.theme.testItemPreview.itemColBorderColor};
-  @media (max-width: 468px) {
+  @media (max-width: ${mobileWidth}) {
     padding-left: 10px;
     margin-right: ${props => !props.value && "20px"};
     margin-left: ${props => props.value && "20px"};

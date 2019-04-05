df1bfb38489268e6dd4c0a8233d8a2cc4be88cea
IgorMinailov <42372439+IgorMinailov@users.noreply.github.com>
2019-04-05 07:36:41 +0300

    feat(Passage): first try to do this (#1256)

--- src/client/assessment/widgets/Passage/PassageView.js
+++ src/client/assessment/widgets/Passage/PassageView.js
@@ -1,27 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { Pagination } from "antd";
-import { MathFormulaDisplay } from "@edulastic/common";
+import ReactQuill from "react-quill";
 
 import { InstructorStimulus } from "./styled/InstructorStimulus";
 import { Heading } from "./styled/Heading";
 
 const ContentsTitle = Heading;
 
-const PassageView = ({ item }) => {
+const PassageView = ({ item, preview }) => {
   const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    if (preview) {
+      document.getElementsByClassName("ql-editor")[0].contentEditable = false;
+    }
+  }, []);
 
   return (
     <div>
       <InstructorStimulus dangerouslySetInnerHTML={{ __html: item.instructor_stimulus }} />
       <Heading dangerouslySetInnerHTML={{ __html: item.heading }} />
       <ContentsTitle dangerouslySetInnerHTML={{ __html: item.contentsTitle }} />
+      <div
+        id="myToolbar"
+        style={{
+          display: "block",
+          position: "relative",
+          width: 50,
+          top: 0,
+          background: "transparent"
+        }}
+        className="toolbars"
+      >
+        <span className="ql-formats">
+          <select className="ql-background">
+            <option value="transparent" />
+            <option value="#99c2ff" />
+            <option value="#99ff99" />
+            <option value="#ff8533" />
+            <option value="#ffff80" />
+            <option value="#ff99bb" />
+          </select>
+        </span>
+      </div>
       {!item.paginated_content && item.content && (
-        <MathFormulaDisplay dangerouslySetInnerHTML={{ __html: item.content }} />
+        <ReactQuill id="mainContents" defaultValue={item.content} modules={PassageView.modules} />
       )}
       {item.paginated_content && item.pages && !!item.pages.length && (
         <div>
-          <MathFormulaDisplay dangerouslySetInnerHTML={{ __html: item.pages[page - 1] }} />
+          <ReactQuill id="paginatedContents" defaultValue={item.pages[page - 1]} modules={PassageView.modules} />
+
           <Pagination
             pageSize={1}
             simple
@@ -37,7 +66,20 @@ const PassageView = ({ item }) => {
 };
 
 PassageView.propTypes = {
-  item: PropTypes.object.isRequired
+  item: PropTypes.object.isRequired,
+  preview: PropTypes.bool
 };
+
+PassageView.defaultProps = {
+  preview: false
+};
+
+PassageView.modules = {
+  toolbar: {
+    container: "#myToolbar"
+  }
+};
+
+PassageView.formats = ["background"];
 
 export default PassageView;

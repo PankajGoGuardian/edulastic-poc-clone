6befb8aa7be282d8cf5d95034e00dc8377a5d959
Vadym <vadymdykyi90@gmail.com>
2019-04-05 10:46:28 +0300

    fix(Math Fill In The Blanks Page): uncaught:exception handler

--- packages/common/src/components/MathInput/StaticMath.js
+++ packages/common/src/components/MathInput/StaticMath.js
@@ -196,7 +196,7 @@ class StaticMath extends PureComponent {
     return (
       <MathInputStyles>
         <div ref={this.containerRef} className="input" onBlur={onBlur}>
-          <div className="input__math asd" style={style} data-cy="answer-math-input-style">
+          <div className="input__math" style={style} data-cy="answer-math-input-style">
             <span className="input__math__field" ref={this.mathFieldRef} data-cy="answer-math-input-field" />
           </div>
           <div className="input__keyboard">

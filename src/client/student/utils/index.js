efb6bd5677f4485fd5b6299be1a0f6af7a22b4aa
Arun Sasidharan <43816559+arun-sasidharan@users.noreply.github.com>
2019-04-12 21:02:13 +0530

    fix(assignment due/start date): fixes on the wrong formating (#1388)

--- src/client/student/utils/index.js
+++ src/client/student/utils/index.js
@@ -1,16 +1,4 @@
-/* eslint import/prefer-default-export: 0 */
-// import { assessmentApi } from '@edulastic/api';
 import moment from "moment";
-// import { loadQuestions } from '../actions/questions';
-// import { loadAssessment } from  '../actions/assessment';
-
-// export const loadJSON = async (assessmentId, dispatch) => {
-//   const json = await assessmentApi.getAssessment(assessmentId);
-//   const { _id, name } = json;
-//   const { questions } = json.questionsApiActivity;
-//   dispatch(loadAssessment(_id, name));
-//   dispatch(loadQuestions(questions));
-// };
 
 export const formatTime = time => {
   if (typeof time === "string") {
@@ -22,5 +10,5 @@ export const formatTime = time => {
   }
   return moment(time)
     .local()
-    .format("MMM, DD YYYY hh:MMA");
+    .format("MMM, DD YYYY hh:mm A");
 };

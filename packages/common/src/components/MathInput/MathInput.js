e41f0475221132aef9ae226e3c6ef52bdedbba2d
Batman Hit <30376020+batmanhit@users.noreply.github.com>
2019-04-27 02:02:14 +0800

    fix(mathquill): focus on the mathinput on mathModal (#1539)
    * CustomQuillComponent hack succeeded
    
    * yarn.lock updates
    
    * Issue with cursor pointing in math quill editor

---
+++ packages/common/src/components/MathInput/MathInput.js
@@ -0,0 +1,203 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { MathKeyboard } from "@edulastic/common";
+import { math } from "@edulastic/constants";
+
+import { MathInputStyles } from "./MathInputStyles";
+
+const { EMBED_RESPONSE } = math;
+
+class MathInput extends React.PureComponent {
+  state = {
+    mathField: null,
+    mathFieldFocus: false
+  };
+
+  containerRef = React.createRef();
+
+  mathFieldRef = React.createRef();
+
+  componentWillUnmount() {
+    // make sure you remove the listener when the component is destroyed
+    document.removeEventListener("click", this.handleClick, false);
+    document.removeEventListener("click", this.handleChangeField, false);
+    this.setState({ mathFieldFocus: false });
+  }
+
+  handleClick = e => {
+    const { onFocus } = this.props;
+
+    if (e.target.nodeName === "LI" && e.target.attributes[0].nodeValue === "option") {
+      return;
+    }
+    if (this.containerRef.current && !this.containerRef.current.contains(e.target)) {
+      onFocus(false);
+      this.setState({ mathFieldFocus: false });
+    }
+  };
+
+  componentWillReceiveProps(nextProps) {
+    const { mathField } = this.state;
+    if (mathField && mathField.latex() !== nextProps.value) {
+      mathField.latex(nextProps.value);
+    }
+  }
+
+  componentDidMount() {
+    const { value } = this.props;
+    const MQ = window.MathQuill.getInterface(2);
+
+    MQ.registerEmbed("response", () => ({
+      htmlString: `<span class="response-embed">
+        <span class="response-embed__char">R</span>
+        <span class="response-embed__text">Response</span>
+      </span>`,
+      text() {
+        return "custom_embed";
+      },
+      latex() {
+        return EMBED_RESPONSE;
+      }
+    }));
+
+    const mathField = MQ.MathField(this.mathFieldRef.current, window.MathQuill);
+    mathField.write(value);
+    mathField.focus();
+
+    this.setState(
+      () => ({ mathField }),
+      () => {
+        const textarea = mathField.el().querySelector(".mq-textarea textarea");
+        textarea.setAttribute("data-cy", `answer-input-math-textarea`);
+        textarea.addEventListener("keyup", this.handleChangeField);
+        document.addEventListener("click", this.handleClick, false);
+      }
+    );
+  }
+
+  handleChangeField = () => {
+    const { onInput } = this.props;
+    const { mathField } = this.state;
+
+    const text = mathField.latex();
+    onInput(text);
+  };
+
+  onInput = (key, command = "cmd") => {
+    const { mathField } = this.state;
+
+    if (!mathField) return;
+    if (key === "left_move") {
+      mathField.keystroke("Left");
+    } else if (key === "right_move") {
+      mathField.keystroke("Right");
+    } else if (key === "ln--") {
+      mathField.write("ln\\left(\\right)");
+    } else if (key === "leftright3") {
+      mathField.write("\\sqrt[3]{}");
+    } else if (key === "Backspace") {
+      mathField.keystroke("Backspace");
+    } else if (key === "leftright2") {
+      mathField.write("^2");
+    } else if (key === "down_move") {
+      mathField.keystroke("Down");
+    } else if (key === "up_move") {
+      mathField.keystroke("Up");
+    } else if (key === "\\embed{response}") {
+      mathField.write(key);
+    } else {
+      mathField[command](key);
+    }
+    mathField.focus();
+    this.handleChangeField();
+  };
+
+  onClose = () => {
+    this.setState({ mathFieldFocus: false });
+  };
+
+  focus = () => {
+    const { mathField } = this.state;
+    mathField.focus();
+  };
+
+  render() {
+    const { mathFieldFocus } = this.state;
+    const {
+      alwaysShowKeyboard,
+      showResponse,
+      style,
+      onFocus,
+      onBlur,
+      onKeyDown,
+      symbols,
+      numberPad,
+      fullWidth,
+      className
+    } = this.props;
+
+    return (
+      <MathInputStyles fullWidth={fullWidth} className={className}>
+        <div
+          ref={this.containerRef}
+          onFocus={() => {
+            onFocus(true);
+            this.setState({ mathFieldFocus: true });
+          }}
+          onBlur={() => {
+            onBlur();
+          }}
+          className="input"
+        >
+          <div onKeyDown={onKeyDown} className="input__math" style={style} data-cy="answer-math-input-field">
+            <span
+              className="input__math__field"
+              ref={this.mathFieldRef}
+              onClick={() => this.setState({ mathFieldFocus: true })}
+            />
+          </div>
+          <div className={alwaysShowKeyboard ? "input__keyboard" : "input__absolute__keyboard"}>
+            {(alwaysShowKeyboard || mathFieldFocus) && (
+              <MathKeyboard
+                symbols={symbols}
+                numberPad={numberPad}
+                showResponse={showResponse}
+                onInput={(key, command) => this.onInput(key, command)}
+              />
+            )}
+          </div>
+        </div>
+      </MathInputStyles>
+    );
+  }
+}
+
+MathInput.propTypes = {
+  alwaysShowKeyboard: PropTypes.bool,
+  onInput: PropTypes.func.isRequired,
+  symbols: PropTypes.array.isRequired,
+  numberPad: PropTypes.array.isRequired,
+  showResponse: PropTypes.bool,
+  value: PropTypes.string,
+  style: PropTypes.object,
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func,
+  onKeyDown: PropTypes.func,
+  fullWidth: PropTypes.bool,
+  className: PropTypes.string
+};
+
+MathInput.defaultProps = {
+  alwaysShowKeyboard: false,
+  value: "",
+  showResponse: false,
+  style: {},
+  onFocus: () => {},
+  onBlur: () => {},
+  onKeyDown: () => {},
+  fullWidth: false,
+  className: ""
+};
+
+export default MathInput;
--- packages/common/src/components/MathModal/index.js
+++ packages/common/src/components/MathModal/index.js
@@ -1,72 +1,56 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import { Modal } from "antd";
 import MathInput from "../MathInput";
 
-class MathModal extends React.Component {
-  state = {
-    latex: ""
-  };
+const MathModal = ({ value, symbols, numberPad, showResponse, show, onSave, onClose }) => {
+  const mathInputRef = useRef(null);
+  const [latex, setLatex] = useState(value || "");
 
-  static propTypes = {
-    show: PropTypes.bool,
-    symbols: PropTypes.array.isRequired,
-    numberPad: PropTypes.array.isRequired,
-    showResponse: PropTypes.bool,
-    value: PropTypes.string,
-    onSave: PropTypes.func,
-    onClose: PropTypes.func
-  };
-
-  static defaultProps = {
-    show: false,
-    value: "",
-    showResponse: false,
-    onSave: () => {},
-    onClose: () => {}
-  };
-
-  constructor(props) {
-    super(props);
-    this.state.latex = props.value;
-  }
-
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    const { show } = this.props;
-    if (!show && nextProps.show) {
-      this.setState({
-        latex: nextProps.value
-      });
+  useEffect(() => {
+    if (show) {
+      setLatex(value);
+      if (mathInputRef.current) {
+        mathInputRef.current.setFocus();
+      }
     }
-  }
+  }, [show]);
 
-  onInput(latex) {
-    this.setState({ latex });
-  }
+  const onInput = newLatex => {
+    setLatex(newLatex);
+  };
 
-  render() {
-    const { symbols, numberPad, showResponse, show, onSave, onClose } = this.props;
-    const { latex } = this.state;
+  return (
+    <Modal visible={show} title="Edit Math" maskClosable={false} onOk={() => onSave(latex)} onCancel={() => onClose()}>
+      <MathInput
+        ref={mathInputRef}
+        alwaysShowKeyboard
+        symbols={symbols}
+        numberPad={numberPad}
+        showResponse={showResponse}
+        value={latex}
+        onInput={newLatex => onInput(newLatex)}
+      />
+    </Modal>
+  );
+};
 
-    return (
-      <Modal
-        visible={show}
-        title="Edit Math"
-        maskClosable={false}
-        onOk={() => onSave(latex)}
-        onCancel={() => onClose()}
-      >
-        <MathInput
-          alwaysShowKeyboard
-          symbols={symbols}
-          numberPad={numberPad}
-          showResponse={showResponse}
-          value={latex}
-          onInput={newLatex => this.onInput(newLatex)}
-        />
-      </Modal>
-    );
-  }
-}
+MathModal.propTypes = {
+  show: PropTypes.bool,
+  symbols: PropTypes.array.isRequired,
+  numberPad: PropTypes.array.isRequired,
+  showResponse: PropTypes.bool,
+  value: PropTypes.string,
+  onSave: PropTypes.func,
+  onClose: PropTypes.func
+};
+
+MathModal.defaultProps = {
+  show: false,
+  value: "",
+  showResponse: false,
+  onSave: () => {},
+  onClose: () => {}
+};
 
 export default MathModal;

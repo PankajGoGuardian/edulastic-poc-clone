6ede1de2e547d944d37debc62396824227a7cd4a
Programcar <39874090+seniorapple@users.noreply.github.com>
2019-05-06 00:47:14 -0600

    fix(da-schools) : fix type issue(#2503) (#1659)

--- src/client/author/Shared/Components/CountrySelect/CountrySelect.js
+++ src/client/author/Shared/Components/CountrySelect/CountrySelect.js
@@ -6,18 +6,41 @@ const Option = Select.Option;
 import { StyledSelect } from "./styled";
 
 class CountrySelect extends React.Component {
+  state = {
+    value: ""
+  };
+
+  handleSearch = value => {
+    this.setState({
+      value
+    });
+  };
+
   render() {
     const { countryList } = this.props;
     const { getFieldDecorator } = this.props.form;
     const options = [];
     Object.entries(countryList).map(([key, value]) => {
-      options.push(<Option value={key}>{value}</Option>);
+      if (value.indexOf(this.state.value) >= 0) {
+        options.push(<Option value={key}>{value}</Option>);
+      }
     });
     return (
       <React.Fragment>
         {getFieldDecorator("country", {
           rules: [{ required: true, message: "Please select Country" }]
-        })(<StyledSelect placeholder="Select Country">{options}</StyledSelect>)}
+        })(
+          <StyledSelect
+            showSearch
+            placeholder="Select Country"
+            onSearch={this.handleSearch}
+            showArrow={false}
+            filterOption={false}
+            notFoundContent={null}
+          >
+            {options}
+          </StyledSelect>
+        )}
       </React.Fragment>
     );
   }

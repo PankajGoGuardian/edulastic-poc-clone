6bf7c318c5c1b6bbd1212e7caa96f400cdca1ad4
Arun Sasidharan <43816559+arun-sasidharan@users.noreply.github.com>
2019-05-07 17:22:12 +0530

    fix(math api): request URL protocol as per environment (#1696)

--- packages/evaluators/src/math.js
+++ packages/evaluators/src/math.js
@@ -2,7 +2,9 @@ import axios from "axios";
 import { omitBy } from "lodash";
 import { ScoringType } from "./const/scoring";
 
-const url = "https://edulastic-poc.snapwiz.net/math-api/evaluate";
+const url = `${
+  window && window.location ? window.location.protocol : "https:"
+}//edulastic-poc.snapwiz.net/math-api/evaluate`;
 
 const evaluate = data =>
   axios

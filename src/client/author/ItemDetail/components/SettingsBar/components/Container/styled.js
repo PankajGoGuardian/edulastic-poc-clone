f2c0d2fb1d0fe4ffd7236a91b434c1ebcb1a1ede
Alexander Boris <borisalex202@gmail.com>
2019-05-17 06:00:17 +0300

    Fix z-index of Layout pop-up and close pop-up by click outside (#1914)

--- src/client/author/ItemDetail/components/SettingsBar/components/Container/styled.js
+++ src/client/author/ItemDetail/components/SettingsBar/components/Container/styled.js
@@ -8,7 +8,7 @@ export const Content = styled.div`
   right: 0;
   top: 0;
   bottom: 0;
-  z-index: 1000;
+  z-index: 1030;
   padding: 30px;
   box-shadow: -3px 3px 6px 0 rgba(0, 0, 0, 0.16);
   overflow-y: auto;

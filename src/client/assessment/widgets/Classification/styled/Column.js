ba4a612c8a25401831a4f431811e0dc10b7e0e5c
IgorMinailov <42372439+IgorMinailov@users.noreply.github.com>
2019-05-21 13:14:34 +0300

    fix(Classification): fix of layout options UI (#1997)

--- src/client/assessment/widgets/Classification/styled/Column.js
+++ src/client/assessment/widgets/Classification/styled/Column.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Column = styled.td`
   padding: 0 16px 0 16px;
   word-break: break-word;
+  min-width: 90px;
   width: ${({ rowTitles, colCount }) =>
     rowTitles.length > 0 ? 100 / colCount - 100 / colCount / 5 / colCount : 100 / colCount}%;
 `;
--- src/client/assessment/widgets/Classification/styled/RowTitleCol.js
+++ src/client/assessment/widgets/Classification/styled/RowTitleCol.js
@@ -3,5 +3,6 @@ import styled from "styled-components";
 export const RowTitleCol = styled.td`
   padding: 0 16px 0 16px;
   word-break: break-word;
+  min-width: 90px;
   width: ${({ colCount }) => 100 / colCount / 5}%;
 `;

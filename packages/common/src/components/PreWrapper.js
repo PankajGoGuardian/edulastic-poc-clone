6fc45c0c2f13a6388da689ed89d36fcea1f4e9ad
Radosavljevic Slobodan <slobodanmeister@gmail.com>
2019-05-22 15:16:39 +0200

    ev-3370 (#2030)
    * fix(previewTemplateBoxLayout):  make normal whitespace for pre wrapper
    
    * fix(previewTemplateBoxLayout): make pre-wrap whitespace

--- packages/common/src/components/PreWrapper.js
+++ packages/common/src/components/PreWrapper.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const PreWrapper = styled.pre`
   font-family: "Open Sans", "Droid Sans", Arial, sans-serif;
+  white-space: pre-wrap;
 `;
 
 PreWrapper.displayName = "PreWrapper";

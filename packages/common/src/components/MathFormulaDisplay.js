265093e131f3c4ae04e146e29292a8e955038851
mgourav-snapwiz <50568503+mgourav-snapwiz@users.noreply.github.com>
2019-06-10 18:55:34 +0530

    fix(EV-4367):  aligning images properly within responses when migrated (#2590)

--- packages/common/src/components/MathFormulaDisplay.js
+++ packages/common/src/components/MathFormulaDisplay.js
@@ -5,7 +5,7 @@ const MathFormulaDisplay = withMathFormula(styled.div`
   img {
     max-width: ${({ stem, choice }) => (!stem ? (!choice ? null : "165px !important") : "200px !important")};
     max-height: ${({ stem, choice }) => (!stem ? (!choice ? null : "110px !important") : "120px !important")};
-    width: auto !important;
+    width: ${({ stem, choice }) => (stem || choice ? "auto !important" : null)};
   }
 `);
 

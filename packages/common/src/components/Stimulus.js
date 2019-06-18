efee6a75b15cbf8ad793ae6302101b18d7058907
snapwizshivam <38942634+snapwizshivam@users.noreply.github.com>
2019-06-18 15:48:55 +0530

    changing EV-3827  commit changes (#2859)

--- packages/common/src/components/Stimulus.js
+++ packages/common/src/components/Stimulus.js
@@ -3,7 +3,6 @@ import { withMathFormula } from "../HOC/withMathFormula";
 
 const Stimulus = withMathFormula(styled.div`
   margin-bottom: 15px;
-  line-height: 0.7;
 `);
 
 export default Stimulus;

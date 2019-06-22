45fa76ed3d94346429e2577b3877eec9902ab5ac
Avijit <avijit.rajendra@snapwiz.com>
2019-06-22 16:12:41 +0530

    fix(subject-dropdown): removed all subjects from dropdown while create test

--- src/client/author/TestPage/components/Review/components/ReviewSummary/ReviewSummary.js
+++ src/client/author/TestPage/components/Review/components/ReviewSummary/ReviewSummary.js
@@ -29,73 +29,77 @@ const ReviewSummary = ({
   onChangeSubjects,
   grades,
   subjects
-}) => (
-  <Container>
-    <Photo url={thumbnail} onChangeField={onChangeField} owner={owner} height={120} />
+}) => {
+  let subjectsList = [...selectsData.allSubjects];
+  subjectsList.splice(0, 1);
+  return (
+    <Container>
+      <Photo url={thumbnail} onChangeField={onChangeField} owner={owner} height={120} />
 
-    <MainTitle>Grade</MainTitle>
-    <SummarySelect
-      data-cy="gradesSelect"
-      mode="multiple"
-      size="large"
-      style={{ width: "100%" }}
-      disabled={!owner}
-      placeholder="Please select"
-      defaultValue={grades}
-      onChange={onChangeGrade}
-    >
-      {selectsData.allGrades.map(({ value, text }) => (
-        <Select.Option key={value} value={value}>
-          {text}
-        </Select.Option>
+      <MainTitle>Grade</MainTitle>
+      <SummarySelect
+        data-cy="gradesSelect"
+        mode="multiple"
+        size="large"
+        style={{ width: "100%" }}
+        disabled={!owner}
+        placeholder="Please select"
+        defaultValue={grades}
+        onChange={onChangeGrade}
+      >
+        {selectsData.allGrades.map(({ value, text }) => (
+          <Select.Option key={value} value={value}>
+            {text}
+          </Select.Option>
+        ))}
+      </SummarySelect>
+
+      <MainTitle>Subject</MainTitle>
+      <SummarySelect
+        data-cy="subjectSelect"
+        mode="multiple"
+        size="large"
+        disabled={!owner}
+        style={{ width: "100%" }}
+        placeholder="Please select"
+        defaultValue={subjects}
+        onChange={onChangeSubjects}
+      >
+        {subjectsList.map(({ value, text }) => (
+          <Select.Option key={value} value={value}>
+            {text}
+          </Select.Option>
+        ))}
+      </SummarySelect>
+
+      <MainTitle>Summary</MainTitle>
+      <FlexContainer justifyContent="space-between">
+        <SummaryInfoContainer>
+          <SummaryInfoNumber data-cy="question">{questionsCount}</SummaryInfoNumber>
+          <SummaryInfoTitle>Questions</SummaryInfoTitle>
+        </SummaryInfoContainer>
+        <SummaryInfoContainer>
+          <SummaryInfoNumber data-cy="points">{totalPoints}</SummaryInfoNumber>
+          <SummaryInfoTitle>Points</SummaryInfoTitle>
+        </SummaryInfoContainer>
+      </FlexContainer>
+      <Row>
+        <TableHeaderCol span={8}>Summary</TableHeaderCol>
+        <TableHeaderCol span={8}>Q's</TableHeaderCol>
+        <TableHeaderCol span={8}>Points</TableHeaderCol>
+      </Row>
+      {tableData.map(data => (
+        <TableBodyRow key={data.key}>
+          <TableBodyCol span={8}>
+            <Standard>{data.standard}</Standard>
+          </TableBodyCol>
+          <TableBodyCol span={8}>{data.qs}</TableBodyCol>
+          <TableBodyCol span={8}>{data.points}</TableBodyCol>
+        </TableBodyRow>
       ))}
-    </SummarySelect>
-
-    <MainTitle>Subject</MainTitle>
-    <SummarySelect
-      data-cy="subjectSelect"
-      mode="multiple"
-      size="large"
-      disabled={!owner}
-      style={{ width: "100%" }}
-      placeholder="Please select"
-      defaultValue={subjects}
-      onChange={onChangeSubjects}
-    >
-      {selectsData.allSubjects.map(({ value, text }) => (
-        <Select.Option key={value} value={value}>
-          {text}
-        </Select.Option>
-      ))}
-    </SummarySelect>
-
-    <MainTitle>Summary</MainTitle>
-    <FlexContainer justifyContent="space-between">
-      <SummaryInfoContainer>
-        <SummaryInfoNumber data-cy="question">{questionsCount}</SummaryInfoNumber>
-        <SummaryInfoTitle>Questions</SummaryInfoTitle>
-      </SummaryInfoContainer>
-      <SummaryInfoContainer>
-        <SummaryInfoNumber data-cy="points">{totalPoints}</SummaryInfoNumber>
-        <SummaryInfoTitle>Points</SummaryInfoTitle>
-      </SummaryInfoContainer>
-    </FlexContainer>
-    <Row>
-      <TableHeaderCol span={8}>Summary</TableHeaderCol>
-      <TableHeaderCol span={8}>Q's</TableHeaderCol>
-      <TableHeaderCol span={8}>Points</TableHeaderCol>
-    </Row>
-    {tableData.map(data => (
-      <TableBodyRow key={data.key}>
-        <TableBodyCol span={8}>
-          <Standard>{data.standard}</Standard>
-        </TableBodyCol>
-        <TableBodyCol span={8}>{data.qs}</TableBodyCol>
-        <TableBodyCol span={8}>{data.points}</TableBodyCol>
-      </TableBodyRow>
-    ))}
-  </Container>
-);
+    </Container>
+  );
+};
 
 ReviewSummary.propTypes = {
   totalPoints: PropTypes.number.isRequired,
--- src/client/author/TestPage/components/Summary/components/Sidebar/Sidebar.js
+++ src/client/author/TestPage/components/Summary/components/Sidebar/Sidebar.js
@@ -48,142 +48,146 @@ const Sidebar = ({
   isTextColorPickerVisible,
   isBackgroundColorPickerVisible,
   windowWidth
-}) => (
-  <FlexContainer flexDirection="column">
-    <Block>
-      <SummaryHeader
-        createdBy={createdBy}
-        thumbnail={thumbnail}
-        owner={owner}
-        windowWidth={windowWidth}
-        analytics={analytics}
-        onChangeField={onChangeField}
-      />
-      <MainTitle>{isPlaylist ? "Play List Name" : "Assessment Name"}</MainTitle>
-      <SummaryInput
-        value={title}
-        data-cy="testname"
-        onChange={e => onChangeField("title", e.target.value)}
-        size="large"
-        placeholder={isPlaylist ? `Enter a playlist name` : `Enter the test name`}
-      />
-      {!title && <ErrorWrapper>Test should have title</ErrorWrapper>}
-      <MainTitle>Description</MainTitle>
-      <SummaryTextArea
-        value={description}
-        onChange={e => onChangeField("description", e.target.value)}
-        size="large"
-        placeholder="Enter a description"
-        isplaylist={isPlaylist}
-      />
-      <MainTitle>Grade</MainTitle>
-      <SummarySelect
-        data-cy="gradeSelect"
-        mode="multiple"
-        size="large"
-        style={{ width: "100%" }}
-        placeholder="Please select"
-        defaultValue={grades}
-        onChange={onChangeGrade}
-      >
-        {selectsData.allGrades.map(({ value, text }) => (
-          <Select.Option key={value} value={value}>
-            {text}
-          </Select.Option>
-        ))}
-      </SummarySelect>
+}) => {
+  let subjectsList = [...selectsData.allSubjects];
+  subjectsList.splice(0, 1);
+  return (
+    <FlexContainer flexDirection="column">
+      <Block>
+        <SummaryHeader
+          createdBy={createdBy}
+          thumbnail={thumbnail}
+          owner={owner}
+          windowWidth={windowWidth}
+          analytics={analytics}
+          onChangeField={onChangeField}
+        />
+        <MainTitle>{isPlaylist ? "Play List Name" : "Assessment Name"}</MainTitle>
+        <SummaryInput
+          value={title}
+          data-cy="testname"
+          onChange={e => onChangeField("title", e.target.value)}
+          size="large"
+          placeholder={isPlaylist ? `Enter a playlist name` : `Enter the test name`}
+        />
+        {!title && <ErrorWrapper>Test should have title</ErrorWrapper>}
+        <MainTitle>Description</MainTitle>
+        <SummaryTextArea
+          value={description}
+          onChange={e => onChangeField("description", e.target.value)}
+          size="large"
+          placeholder="Enter a description"
+          isplaylist={isPlaylist}
+        />
+        <MainTitle>Grade</MainTitle>
+        <SummarySelect
+          data-cy="gradeSelect"
+          mode="multiple"
+          size="large"
+          style={{ width: "100%" }}
+          placeholder="Please select"
+          defaultValue={grades}
+          onChange={onChangeGrade}
+        >
+          {selectsData.allGrades.map(({ value, text }) => (
+            <Select.Option key={value} value={value}>
+              {text}
+            </Select.Option>
+          ))}
+        </SummarySelect>
 
-      <MainTitle>Subject</MainTitle>
-      <SummarySelect
-        data-cy="subjectSelect"
-        mode="multiple"
-        size="large"
-        style={{ width: "100%" }}
-        placeholder="Please select"
-        defaultValue={subjects}
-        onChange={onChangeSubjects}
-      >
-        {selectsData.allSubjects.map(({ value, text }) => (
-          <Select.Option key={value} value={value}>
-            {text}
-          </Select.Option>
-        ))}
-      </SummarySelect>
+        <MainTitle>Subject</MainTitle>
+        <SummarySelect
+          data-cy="subjectSelect"
+          mode="multiple"
+          size="large"
+          style={{ width: "100%" }}
+          placeholder="Please select"
+          defaultValue={subjects}
+          onChange={onChangeSubjects}
+        >
+          {subjectsList.map(({ value, text }) => (
+            <Select.Option key={value} value={value}>
+              {text}
+            </Select.Option>
+          ))}
+        </SummarySelect>
 
-      {isPlaylist && (
-        <div>
-          <Col span={windowWidth > 993 ? 12 : 24}>
-            <MainTitle>Text Color</MainTitle>
-            <SummaryDiv>
-              <ColorBox data-cy="image-text-box-color-picker" background={textColor} />
-              <SummaryButton onClick={() => onChangeColor("isTextColorPickerVisible", true)}>CHOOSE</SummaryButton>
-              {isTextColorPickerVisible && (
-                <ColorPickerContainer data-cy="image-text-box-color-panel">
-                  <ColorPickerWrapper onClick={() => onChangeColor("isTextColorPickerVisible", false)} />
-                  <ChromePicker color={textColor} onChangeComplete={color => onChangeColor("textColor", color.hex)} />
-                </ColorPickerContainer>
-              )}
-            </SummaryDiv>
-          </Col>
-          <Col span={windowWidth > 993 ? 12 : 24}>
-            <MainTitle>Background Color</MainTitle>
-            <SummaryDiv>
-              <ColorBox data-cy="image-text-box-color-picker" background={backgroundColor} />
-              <SummaryButton onClick={() => onChangeColor("isBackgroundColorPickerVisible", true)}>
-                CHOOSE
-              </SummaryButton>
-              {isBackgroundColorPickerVisible && (
-                <ColorPickerContainer data-cy="image-text-box-color-panel">
-                  <ColorPickerWrapper onClick={() => onChangeColor("isBackgroundColorPickerVisible", false)} />
-                  <ChromePicker
-                    color={backgroundColor}
-                    onChangeComplete={color => onChangeColor("backgroundColor", color.hex)}
-                  />
-                </ColorPickerContainer>
-              )}
-            </SummaryDiv>
-          </Col>
-        </div>
-      )}
-      <MainTitle>Tags</MainTitle>
-      <SummarySelect
-        data-cy="tagsSelect"
-        mode="tags"
-        size="large"
-        style={{ marginBottom: 0 }}
-        placeholder="Please select"
-        defaultValue={tags}
-        onChange={value => onChangeField("tags", value)}
-      >
-        {selectsData.allTags.map(({ value, text }) => (
-          <Select.Option key={value} value={value}>
-            {text}
-          </Select.Option>
-        ))}
-      </SummarySelect>
-      {/* to be done later */}
-      {false && (
-        <>
-          <MainTitle>Collection</MainTitle>
-          <SummarySelect
-            data-cy="CollectionSelect"
-            size="large"
-            style={{ width: "100%" }}
-            placeholder="Please select"
-            defaultValue={subjects}
-            onChange={""}
-          >
-            {selectsData.allCollections.map(({ value, text }) => (
-              <Select.Option key={value} value={value}>
-                {text}
-              </Select.Option>
-            ))}
-          </SummarySelect>
-        </>
-      )}
-    </Block>
-  </FlexContainer>
-);
+        {isPlaylist && (
+          <div>
+            <Col span={windowWidth > 993 ? 12 : 24}>
+              <MainTitle>Text Color</MainTitle>
+              <SummaryDiv>
+                <ColorBox data-cy="image-text-box-color-picker" background={textColor} />
+                <SummaryButton onClick={() => onChangeColor("isTextColorPickerVisible", true)}>CHOOSE</SummaryButton>
+                {isTextColorPickerVisible && (
+                  <ColorPickerContainer data-cy="image-text-box-color-panel">
+                    <ColorPickerWrapper onClick={() => onChangeColor("isTextColorPickerVisible", false)} />
+                    <ChromePicker color={textColor} onChangeComplete={color => onChangeColor("textColor", color.hex)} />
+                  </ColorPickerContainer>
+                )}
+              </SummaryDiv>
+            </Col>
+            <Col span={windowWidth > 993 ? 12 : 24}>
+              <MainTitle>Background Color</MainTitle>
+              <SummaryDiv>
+                <ColorBox data-cy="image-text-box-color-picker" background={backgroundColor} />
+                <SummaryButton onClick={() => onChangeColor("isBackgroundColorPickerVisible", true)}>
+                  CHOOSE
+                </SummaryButton>
+                {isBackgroundColorPickerVisible && (
+                  <ColorPickerContainer data-cy="image-text-box-color-panel">
+                    <ColorPickerWrapper onClick={() => onChangeColor("isBackgroundColorPickerVisible", false)} />
+                    <ChromePicker
+                      color={backgroundColor}
+                      onChangeComplete={color => onChangeColor("backgroundColor", color.hex)}
+                    />
+                  </ColorPickerContainer>
+                )}
+              </SummaryDiv>
+            </Col>
+          </div>
+        )}
+        <MainTitle>Tags</MainTitle>
+        <SummarySelect
+          data-cy="tagsSelect"
+          mode="tags"
+          size="large"
+          style={{ marginBottom: 0 }}
+          placeholder="Please select"
+          defaultValue={tags}
+          onChange={value => onChangeField("tags", value)}
+        >
+          {selectsData.allTags.map(({ value, text }) => (
+            <Select.Option key={value} value={value}>
+              {text}
+            </Select.Option>
+          ))}
+        </SummarySelect>
+        {/* to be done later */}
+        {false && (
+          <>
+            <MainTitle>Collection</MainTitle>
+            <SummarySelect
+              data-cy="CollectionSelect"
+              size="large"
+              style={{ width: "100%" }}
+              placeholder="Please select"
+              defaultValue={subjects}
+              onChange={""}
+            >
+              {selectsData.allCollections.map(({ value, text }) => (
+                <Select.Option key={value} value={value}>
+                  {text}
+                </Select.Option>
+              ))}
+            </SummarySelect>
+          </>
+        )}
+      </Block>
+    </FlexContainer>
+  );
+};
 
 Sidebar.propTypes = {
   title: PropTypes.string.isRequired,

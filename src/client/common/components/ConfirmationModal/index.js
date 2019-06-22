74d0cb09eeb9c0ba9e72951410c107b86e43076b
snapwizshivam <38942634+snapwizshivam@users.noreply.github.com>
2019-06-22 13:03:12 +0530

    Ev 4764 - fix evaluation for  numeric question (#2914)
    * fix(EV-4764): evaluation for numeric question with alternate ans fixed
    
    * fix(EV-4764): evaluation for numeric type fixed
    
    * refactor code
    
    * code cleanup
    
    * fix(multi-part): modify multipart evaluator for the same

--- src/client/common/components/ConfirmationModal/index.js
+++ src/client/common/components/ConfirmationModal/index.js
@@ -1,45 +1,59 @@
 import React from "react";
 import { Row, Col, Button, Modal } from "antd";
-import { StyledCol, StyledP, StyledInput, LightBlueSpan } from "./styled";
+import {
+  StyledCol,
+  StyledP,
+  StyledInput,
+  LightGreenSpan,
+  ModalWrapper,
+  InitOptions,
+  StyledButton,
+  ModalFooter
+} from "./styled";
 import PropTypes from "prop-types";
 
 const ConfirmationModal = ({ title, show, onOk, onCancel, inputVal, onInputChange, expectedVal, bodyText, okText }) => {
   return (
-    <Modal
+    <ModalWrapper
       centered
       visible={show}
+      width="750px"
       title={title}
       onCancel={onCancel}
       maskClosable={false}
       footer={[
-        <Button type="primary" key={"1"} onClick={onCancel}>
-          No, Cancel
-        </Button>,
-        <Button type="primary" key={"2"} onClick={onOk} disabled={expectedVal !== inputVal.toUpperCase()}>
-          {okText}
-        </Button>
+        <ModalFooter>
+          <StyledButton cancel={true} type="primary" key={"1"} onClick={onCancel}>
+            No, Cancel
+          </StyledButton>
+          <StyledButton type="primary" key={"2"} onClick={onOk} disabled={expectedVal !== inputVal.toUpperCase()}>
+            {okText}
+          </StyledButton>
+        </ModalFooter>
       ]}
     >
-      <Row>
-        <Col span={24}>
-          <StyledP>{bodyText}</StyledP>
-          <StyledP>
-            This action can NOT be undone. If you are sure, please type <LightBlueSpan>{expectedVal}</LightBlueSpan> in
-            the space below
-          </StyledP>
-        </Col>
-      </Row>
-      <Row>
-        <StyledCol span={24}>
-          <StyledInput
-            value={inputVal}
-            onChange={onInputChange}
-            // here paste is not allowed, and user has to manually type in inputVal
-            onPaste={e => e.preventDefault()}
-          />
-        </StyledCol>
-      </Row>
-    </Modal>
+      <InitOptions>
+        <Row>
+          <Col span={24}>
+            <StyledP>{bodyText}</StyledP>
+            <StyledP>
+              This action can NOT be undone. If you are sure, please type <LightGreenSpan>{expectedVal}</LightGreenSpan>{" "}
+              in the space below
+            </StyledP>
+          </Col>
+        </Row>
+        <Row>
+          <StyledCol span={24}>
+            <StyledInput
+              value={inputVal}
+              onChange={onInputChange}
+              // here paste is not allowed, and user has to manually type in inputVal
+              onPaste={e => e.preventDefault()}
+            />
+          </StyledCol>
+        </Row>
+      </InitOptions>
+    </ModalWrapper>
   );
 };
 

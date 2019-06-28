6b51807e9f809e2155433477ddc09b86335ae39e
Manoj Singh Negi <justanothermanoj@gmail.com>
2019-06-28 14:20:26 +0530

    #5079 refactor(single assessment) listen directly for action in reducer

--- src/client/author/Reports/ducks.js
+++ src/client/author/Reports/ducks.js
@@ -1,13 +1,11 @@
-import { takeEvery, put, all } from "redux-saga/effects";
 import { createSelector } from "reselect";
 import { createAction, createReducer } from "redux-starter-kit";
 
-const SET_PRINTING_STATE_REQUEST = "[reports] set printing state request";
 const SET_PRINTING_STATE = "[reports] set printing state";
 
 // -----|-----|-----|-----| ACTIONS BEGIN |-----|-----|-----|----- //
 
-export const setPrintingStateAction = createAction(SET_PRINTING_STATE_REQUEST);
+export const setPrintingStateAction = createAction(SET_PRINTING_STATE);
 
 // -----|-----|-----|-----| ACTIONS ENDED |-----|-----|-----|----- //
 
@@ -37,22 +35,3 @@ export const reportReducer = createReducer(initialState, {
     state.isPrinting = payload;
   }
 });
-
-// -----|-----|-----|-----| REDUCER BEGIN |-----|-----|-----|----- //
-
-// =====|=====|=====|=====| =============== |=====|=====|=====|===== //
-
-// -----|-----|-----|-----| SAGAS BEGIN |-----|-----|-----|----- //
-
-function* setPrintingStateWatcher({ payload }) {
-  yield put({
-    type: SET_PRINTING_STATE,
-    payload: payload
-  });
-}
-
-export function* reportSaga() {
-  yield all([yield takeEvery(SET_PRINTING_STATE_REQUEST, setPrintingStateWatcher)]);
-}
-
-// -----|-----|-----|-----| SAGAS ENDED |-----|-----|-----|----- //

8baaee82bf384559b1d8078f60f6c7cdabd1cf93
pintea andrei <clearg1929@gmail.com>
2019-06-28 13:01:45 +0200

    fix(EV-5181): fit image to draggable/droppable container

--- src/client/assessment/widgets/MatchList/styled/Wrapper.js
+++ src/client/assessment/widgets/MatchList/styled/Wrapper.js
@@ -6,8 +6,8 @@ export const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   img {
-    max-width: 220px;
-    max-height: 120px;
+    max-width: 220px !important;
+    max-height: 120px !important;
     width: auto !important;
   }
 `;

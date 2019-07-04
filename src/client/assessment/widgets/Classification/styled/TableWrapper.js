522292cd43c95035f4be83ff1de172e9657d56f7
Slobodan Radosavljevic <slobodanmeister@gmail.com>
2019-07-04 12:29:24 +0200

    WIP

--- src/client/assessment/widgets/Classification/styled/TableWrapper.js
+++ src/client/assessment/widgets/Classification/styled/TableWrapper.js
@@ -6,10 +6,9 @@ export const TableWrapper = styled.div`
   width: 700px;
   margin: auto;
   background: ${({ imageUrl }) => (imageUrl ? `url('${imageUrl}')` : "inherit")};
-  background-size: ${({ isBgImageMaximized }) => (isBgImageMaximized ? "100% 100%" : "auto")};
   background-repeat: no-repeat;
+  background-size: ${({ isBgImageMaximized, imageOptions = { width: 0, height: 0 } }) =>
+    false ? "100% 100%" : `${imageOptions.width}px ${imageOptions.height}px`};
   background-position: ${({ imageOptions }) => (imageOptions ? `${imageOptions.x}px ${imageOptions.y}px` : "inherit")};
-  background-size: ${({ imageOptions }) =>
-    imageOptions ? `${imageOptions.width}px ${imageOptions.height}px` : "inherit"};
   padding-bottom: 20px;
 `;

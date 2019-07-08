fc3d434a51a2493650c29e0646ded6ad3ff8618e
utkarshbharti <utkarsh.bharti@snapwiz.com>
2019-07-08 16:21:06 +0530

    fix(teacher dashboard) changed dashboard layout

--- src/client/author/Dashboard/components/Showcase/showcase.js
+++ src/client/author/Dashboard/components/Showcase/showcase.js
@@ -1,24 +1,12 @@
 import React from "react";
 import { Row, Col, Layout, Rate, Icon, Button } from "antd";
 import styled from "styled-components";
-import SideContent from "../SideContent/Sidecontent";
 import MyClasses from "./components/Myclasses/Myclass";
-import ShowcaseImage from "./components/ShowcaseImage/Showcaseimage";
-import { GreencardSection, InfoSection } from "./components/Cards/Cards";
 
 const { Content } = Layout;
 
 const MainContent = () => (
   <MainContainer>
-    <Row gutter={10}>
-      <Col span={17}>
-        <ShowcaseImage />
-      </Col>
-      <Col span={7}>
-        <GreencardSection />
-        <InfoSection />
-      </Col>
-    </Row>
     <Row>
       <MyClasses />
     </Row>
@@ -27,7 +15,6 @@ const MainContent = () => (
 
 const MainContainer = styled.div`
   background: #f3f3f8;
-  width: calc(100vw - 356px);
   padding: 1rem;
 `;
 export default MainContent;

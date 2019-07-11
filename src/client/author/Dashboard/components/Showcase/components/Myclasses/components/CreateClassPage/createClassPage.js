c9bbf517c93a80f26dfa72cc93520ed422058e4b
Avijit <avijit.rajendra@snapwiz.com>
2019-07-11 18:17:32 +0530

    fix(GC): google classroom in dashboard

--- src/client/author/Dashboard/components/Showcase/components/Myclasses/components/CreateClassPage/createClassPage.js
+++ src/client/author/Dashboard/components/Showcase/components/Myclasses/components/CreateClassPage/createClassPage.js
@@ -3,7 +3,29 @@ import { Icon } from "antd";
 import { Link } from "react-router-dom";
 import GoogleClassRoomImg from "../../../../../../assets/images/google-classroom.png";
 import { CreateCardBox, CreateClassButton, SyncClassDiv, SyncImg } from "./styled";
-const CreateClassPage = () => {
+import GoogleLogin from "react-google-login";
+const CreateClassPage = ({ allowGoogleLogin, isUserGoogleLoggedIn, fetchClassList }) => {
+  const scopes = [
+    "https://www.googleapis.com/auth/classroom.courses",
+    "https://www.googleapis.com/auth/classroom.rosters",
+    "https://www.googleapis.com/auth/classroom.coursework.me",
+    "https://www.googleapis.com/auth/classroom.coursework.students",
+    "https://www.googleapis.com/auth/classroom.announcements",
+    "https://www.googleapis.com/auth/classroom.guardianlinks.students",
+    "https://www.googleapis.com/auth/classroom.guardianlinks.me.readonly",
+    "https://www.googleapis.com/auth/classroom.profile.photos",
+    "https://www.googleapis.com/auth/classroom.profile.emails",
+    "https://www.googleapis.com/auth/userinfo.profile"
+  ].join(" ");
+
+  const handleLoginSucess = data => {
+    fetchClassList({ data });
+  };
+
+  const handleError = err => {
+    console.log("error", err);
+  };
+
   return (
     <CreateCardBox>
       <Link to={"/author/manageClass/createClass"}>
@@ -12,11 +34,30 @@ const CreateClassPage = () => {
           <p>create class</p>
         </CreateClassButton>
       </Link>
-      <p>or</p>
-      <SyncClassDiv>
-        <SyncImg src={GoogleClassRoomImg} width={35} />
-        <p>Sync with google classroom</p>
-      </SyncClassDiv>
+      {allowGoogleLogin !== false && (
+        <GoogleLogin
+          clientId={process.env.POI_APP_GOOGLE_CLIENT_ID}
+          render={renderProps => (
+            <>
+              <p>or</p>
+              <SyncClassDiv>
+                <i
+                  style={{ cursor: "pointer", marginLeft: "8px", display: "flex" }}
+                  title={"Sync with Google Classroom"}
+                  onClick={renderProps.onClick}
+                >
+                  <SyncImg src={GoogleClassRoomImg} width={35} />
+                </i>
+              </SyncClassDiv>
+            </>
+          )}
+          scope={scopes}
+          onSuccess={handleLoginSucess}
+          onFailure={handleError}
+          prompt={isUserGoogleLoggedIn ? "" : "consent"}
+          responseType="code"
+        />
+      )}
     </CreateCardBox>
   );
 };

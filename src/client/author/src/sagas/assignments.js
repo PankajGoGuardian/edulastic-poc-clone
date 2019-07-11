addfb33bb1c4973181c93141edf605e4564fbc92
Avijit <avijit.rajendra@snapwiz.com>
2019-07-11 11:36:04 +0530

    fix(assignments): da assignments view , removed condition for class and  put for user role

--- src/client/author/src/sagas/assignments.js
+++ src/client/author/src/sagas/assignments.js
@@ -18,7 +18,7 @@ import {
   RECEIVE_ASSIGNMENT_CLASS_LIST_SUCCESS,
   RECEIVE_ASSIGNMENT_CLASS_LIST_ERROR
 } from "../constants/actions";
-import { getClassIds } from "../../../student/Reports/ducks";
+import { getUserRole } from "../selectors/user";
 
 function* receiveAssignmentClassList({ payload = {} }) {
   try {
@@ -44,9 +44,8 @@ function* receiveAssignmentsSummary({ payload = {} }) {
       set(filters, "Subject", get(filters, "subject"));
       unset(filters, "subject");
     }
-
-    const classList = yield select(getClassIds);
-    if (classList && classList.length) {
+    const userRole = yield select(getUserRole);
+    if (userRole !== "teacher") {
       const entities = yield call(assignmentApi.fetchAssignmentsSummary, {
         districtId,
         filters: pickBy(filters, identity)
@@ -57,7 +56,7 @@ function* receiveAssignmentsSummary({ payload = {} }) {
       });
     }
   } catch (error) {
-    const errorMessage = "Receive tests is failing";
+    const errorMessage = "Receive Assignments Summary is failing";
     yield call(message.error, errorMessage);
     yield put({
       type: RECEIVE_ASSIGNMENTS_SUMMARY_ERROR,

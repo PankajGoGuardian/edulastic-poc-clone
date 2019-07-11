cd1edb17c661c6d87343af0f5dc7bb5dc1a05299
utkarshbharti <utkarsh.bharti@snapwiz.com>
2019-07-11 12:04:52 +0530

    fix, checking if class already exists in array

--- src/client/student/Assignments/components/index.js
+++ src/client/student/Assignments/components/index.js
@@ -2,25 +2,41 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
 import { Layout, Spin } from "antd";
-
+import { isEmpty } from "lodash";
 // components
 import Header from "../../sharedComponents/Header";
 import SubHeader from "./SubHeader";
 import AssignmentContainer from "./Container";
 import { getEnrollClassAction } from "../../ManageClass/ducks";
+import { changeClassAction } from "../../Login/ducks";
 
 const Wrapper = styled(Layout)`
   width: 100%;
 `;
 
-const Assignments = ({ allClasses, loadAllClasses, loading }) => {
+const Assignments = ({ activeClasses, loadAllClasses, changeClass, loading, location }) => {
   useEffect(() => {
     loadAllClasses();
   }, []);
   if (loading) return <Spin />;
+  const { classItem = {} } = location;
+  const activeEnrolledClasses = activeClasses.filter(c => c.status === "1");
+  if (!isEmpty(classItem)) {
+    const { _id } = classItem;
+    changeClass(_id);
+    const isExist = activeEnrolledClasses.find(item => item._id === classItem._id);
+    if (!isExist) {
+      activeEnrolledClasses.push(classItem);
+    }
+  }
   return (
     <Wrapper>
-      <Header titleText="common.dashboardTitle" classSelect={true} showActiveClass={false} classList={allClasses} />
+      <Header
+        titleText="common.dashboardTitle"
+        classSelect={true}
+        showActiveClass={false}
+        classList={activeEnrolledClasses}
+      />
       <SubHeader />
       <AssignmentContainer />
     </Wrapper>
@@ -29,10 +45,12 @@ const Assignments = ({ allClasses, loadAllClasses, loading }) => {
 
 export default connect(
   state => ({
-    allClasses: state.studentEnrollClassList.allClasses,
+    // allClasses: state.studentEnrollClassList.allClasses,
+    activeClasses: state.studentEnrollClassList.filteredClasses,
     loading: state.studentEnrollClassList.loading
   }),
   {
-    loadAllClasses: getEnrollClassAction
+    loadAllClasses: getEnrollClassAction,
+    changeClass: changeClassAction
   }
 )(Assignments);

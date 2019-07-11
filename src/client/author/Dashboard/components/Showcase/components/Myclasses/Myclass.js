33ba54358f74cd6befedea94739d9875f0938fd5
utkarshbharti <utkarsh.bharti@snapwiz.com>
2019-07-11 18:25:26 +0530

    fix(teacher dashboard) showing only active classes on dashboard

--- src/client/author/Dashboard/components/Showcase/components/Myclasses/Myclass.js
+++ src/client/author/Dashboard/components/Showcase/components/Myclasses/Myclass.js
@@ -24,16 +24,15 @@ const MyClasses = ({ getTeacherDashboard, classData, loading }) => {
   useEffect(() => {
     getTeacherDashboard();
   }, []);
-  const [showAllCards, setShowAllCards] = useState(false);
 
-  const sortableCards = classData
+  const sortableClasses = classData
     .filter(d => d.asgnStartDate !== null && d.asgnStartDate !== undefined)
     .sort((a, b) => b.asgnStartDate - a.asgnStartDate);
-  const unSortablecards = classData.filter(d => d.asgnStartDate === null || d.asgnStartDate === undefined);
+  const unSortableClasses = classData.filter(d => d.asgnStartDate === null || d.asgnStartDate === undefined);
 
-  const allCards = [...sortableCards, ...unSortablecards];
-
-  const ClassCards = allCards.map(item => (
+  const allClasses = [...sortableClasses, ...unSortableClasses];
+  const allActiveClasses = allClasses.filter(c => c.active === 1);
+  const ClassCards = allActiveClasses.map(item => (
     <Col xs={24} sm={24} md={12} lg={12} xl={8} key={item._id}>
       <Card data={item} />
     </Col>

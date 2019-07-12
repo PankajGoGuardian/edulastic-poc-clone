adb652b5f7e68c1a74b23c3157c6630b6b219d7e
utkarshbharti <utkarsh.bharti@snapwiz.com>
2019-07-12 10:16:26 +0530

    added loadigselector for active class groups

--- src/client/author/sharedDucks/groups.js
+++ src/client/author/sharedDucks/groups.js
@@ -30,13 +30,15 @@ const initialState = {
   groups: [],
   archiveGroups: [],
   students: [],
-  loadedGroups: []
+  loadedGroups: [],
+  activeGroupsLoading: true
 };
 
 // set groups
 const setGroups = (state, { payload }) => {
   state.groups = payload;
   state.isLoading = false;
+  state.activeGroupsLoading = false;
 };
 // set archive groups
 const setArchiveGroups = (state, { payload }) => {
@@ -79,6 +81,7 @@ export const getArchiveGroupsSelector = state => (state[module].isLoading ? [] :
 export const getStudentsSelector = state => (state[module].isLoading ? [] : state[module].students);
 
 export const getLoadedGroupsSelector = state => state[module].loadedGroups;
+export const getActiveGroupsLoadingSelector = state => state[module].activeGroupsLoading;
 
 // fetch groups of that user
 function* fetchGroups() {

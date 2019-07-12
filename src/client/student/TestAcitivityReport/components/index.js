d77f9d4e166f3e6bad7e47da30aa01798e1dca2f
AndreyYevsega <andrii.yevsega@gmail.com>
2019-07-12 12:14:53 +0300

    fix( reports review): show test name

--- src/client/student/TestAcitivityReport/components/index.js
+++ src/client/student/TestAcitivityReport/components/index.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
+import { get } from "lodash";
 //components
 import TestAcivityHeader from "../../sharedComponents/Header";
 import TestActivitySubHeader from "./SubHeader";
@@ -11,17 +12,37 @@ import MainContainer from "../../styled/mainContainer";
 //actions
 import { loadTestActivityReportAction } from "../ducks";
 import { setCurrentItemAction } from "../../sharedDucks/TestItem";
+import { getAssignmentsSelector } from "../../Reports/ducks";
 
-const ReportListContainer = ({ flag, match, location, loadTestActivityReport, setCurrentItem }) => {
+const ReportListContainer = ({
+  flag,
+  match,
+  location,
+  loadTestActivityReport,
+  setCurrentItem,
+  assignments,
+  testFeedback
+}) => {
+  const [assignmentItemTitle, setAssignmentItemTitle] = useState(null);
+
   useEffect(() => {
     loadTestActivityReport({ testActivityId: match.params.id, groupId: match.params.classId });
     setCurrentItem(0);
   }, []);
+
+  useEffect(() => {
+    if (!testFeedback) return;
+
+    const { assignmentId } = testFeedback[0];
+    const [assignmentItem] = assignments.filter(item => item._id === assignmentId);
+    setAssignmentItemTitle(assignmentItem.title);
+  }, [testFeedback]);
+
   return (
     <MainContainer flag={flag}>
       <TestAcivityHeader titleText="common.reportsTitle" />
-      <TestActivitySubHeader title={location.title} />
-      <ReportListContent title={location.title} reportId={match.params.id} />
+      <TestActivitySubHeader title={assignmentItemTitle} />
+      <ReportListContent title={assignmentItemTitle} reportId={match.params.id} />
     </MainContainer>
   );
 };
@@ -29,8 +50,10 @@ const ReportListContainer = ({ flag, match, location, loadTestActivityReport, se
 const enhance = compose(
   withRouter,
   connect(
-    ({ ui }) => ({
-      flag: ui.flag
+    state => ({
+      flag: state.ui.flag,
+      testFeedback: get(state, "testFeedback", null),
+      assignments: getAssignmentsSelector(state)
     }),
     {
       setCurrentItem: setCurrentItemAction,
@@ -43,5 +66,7 @@ export default enhance(ReportListContainer);
 
 ReportListContainer.propTypes = {
   flag: PropTypes.bool.isRequired,
-  location: PropTypes.object.isRequired
+  location: PropTypes.object.isRequired,
+  assignments: PropTypes.array.isRequired,
+  testFeedback: PropTypes.array.isRequired
 };

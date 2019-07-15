14903a938e5c438216b207146dd76dadc8e1ce96
Slobodan Radosavljevic <slobodanmeister@gmail.com>
2019-07-15 15:00:28 +0200

    WIP

--- src/client/assessment/widgets/MatchList/styled/ListItem.js
+++ src/client/assessment/widgets/MatchList/styled/ListItem.js
@@ -10,10 +10,11 @@ export const ListItem = styled.div`
   font-weight: ${props => props.theme.widgets.matchList.listItemFontWeight};
   color: ${props => props.theme.widgets.matchList.listItemColor};
   border: 1px solid ${props => props.theme.widgets.matchList.listItemBorderColor};
-
+  /* TODO: assign these on resize? */
   img {
     max-width: 200px !important;
     max-height: 120px !important;
     width: auto !important;
+    border: 2px solid red;
   }
 `;

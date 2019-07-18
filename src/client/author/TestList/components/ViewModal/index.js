2d3e7b1045ecd1221d330bbe5f814208f096cb4b
Avijit <avijit.rajendra@snapwiz.com>
2019-07-18 10:53:19 +0530

    fix(test-summary): change question to item

--- src/client/author/TestList/components/ViewModal/index.js
+++ src/client/author/TestList/components/ViewModal/index.js
@@ -141,9 +141,11 @@ class ViewModal extends React.Component {
               <SummaryCardContainer>
                 <SummaryCard>
                   <SummaryCardValue>
-                    {isPlaylist ? _source.modules && _source.modules.length : summary.totalQuestions}
+                    {isPlaylist
+                      ? _source.modules && _source.modules.length
+                      : summary.totalItems || summary.totalQuestions || 0}
                   </SummaryCardValue>
-                  <SummaryCardLabel>Questions</SummaryCardLabel>
+                  <SummaryCardLabel>Items</SummaryCardLabel>
                 </SummaryCard>
                 <SummaryCard>
                   <SummaryCardValue>{summary.totalPoints}</SummaryCardValue>

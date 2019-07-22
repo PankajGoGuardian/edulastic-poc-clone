91b8a1b0b618c4d3666fd8ab5fedb581c960603d
Avijit <avijit.rajendra@snapwiz.com>
2019-07-22 10:06:19 +0530

    fix(review-tab): UI changes for collapse mode

--- src/client/author/TestPage/components/Review/components/ReviewItemsTable/MetaInfoCell/MetaInfoCell.js
+++ src/client/author/TestPage/components/Review/components/ReviewItemsTable/MetaInfoCell/MetaInfoCell.js
@@ -13,12 +13,12 @@ import { AudioIcon } from "../../../../../../ItemList/components/Item/styled";
 import { MetaTag, ExtraInfo, DokStyled } from "./styled";
 
 const MetaInfoCell = ({
-  data: { item, type, by, id, shared, likes, audio = {}, isPremium = false, points = 0, dok },
-  itemTableView
+  data: { item, type, by, id, shared, likes, audio = {}, isPremium = false, points = 0, dok }
 }) => {
   return (
-    <FlexContainer justifyContent="space-between" style={{ width: "100%" }}>
+    <FlexContainer justifyContent="space-between" style={{ width: "100%", paddingTop: "5px" }}>
       <FlexContainer>
+        {isPremium && <PremiumTag />}
         {item && item.data && <Standards item={item} search={{ curriculumId: "" }} reviewpage={true} />}
         {type && (
           <FlexContainer>
@@ -28,28 +28,18 @@ const MetaInfoCell = ({
           </FlexContainer>
         )}
       </FlexContainer>
-      {itemTableView && (
-        <FlexContainer justifyContent="flex-end">
-          {isPremium && <PremiumTag />}
-          {dok && <DokStyled>{`DOK:${dok}`}</DokStyled>}
-          {renderAnalytics(by, IconUser)}
-          {renderAnalytics(id && id.substring(18), IconHash)}
-          {renderAnalytics(shared, IconShare)}
-          {renderAnalytics(likes, IconHeart)}
-          {audio && audio.hasOwnProperty("ttsSuccess") ? (
-            <AudioIcon className="fa fa-volume-up" success={audio.ttsSuccess} />
-          ) : (
-            ""
-          )}
-        </FlexContainer>
-      )}
-      {!itemTableView && (
-        <FlexContainer justifyContent="flex-end">
-          {renderAnalytics(id && id.substring(18), IconHash)}
-          <ExtraInfo> Points </ExtraInfo>
-          <ExtraInfo> {points}</ExtraInfo>
-        </FlexContainer>
-      )}
+      <FlexContainer justifyContent="flex-end">
+        {dok && <DokStyled>{`DOK:${dok}`}</DokStyled>}
+        {renderAnalytics(by, IconUser)}
+        {renderAnalytics(id && id.substring(18), IconHash)}
+        {renderAnalytics(shared, IconShare)}
+        {renderAnalytics(likes, IconHeart)}
+        {audio && audio.hasOwnProperty("ttsSuccess") ? (
+          <AudioIcon className="fa fa-volume-up" success={audio.ttsSuccess} />
+        ) : (
+          ""
+        )}
+      </FlexContainer>
     </FlexContainer>
   );
 };

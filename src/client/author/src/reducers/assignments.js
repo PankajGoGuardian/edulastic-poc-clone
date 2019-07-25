162cc87e0584d09fa2d36e876ae2b09074ffacf0
Avijit <avijit.rajendra@snapwiz.com>
2019-07-25 15:20:23 +0530

    fix(da-assingments): filtering set as false

--- src/client/author/src/reducers/assignments.js
+++ src/client/author/src/reducers/assignments.js
@@ -56,7 +56,7 @@ const reducer = (state = initialState, { type, payload }) => {
         loading: false,
         summaryEntities: entities,
         total,
-        filtering
+        filtering: false
       };
     }
     case RECEIVE_ASSIGNMENTS_SUMMARY_ERROR:

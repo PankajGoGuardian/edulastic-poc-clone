9345f3e0feb08d59d17110892dc451631b95a8fd
Radosavljevic Slobodan <slobodanmeister@gmail.com>
2019-07-29 14:37:04 +0200

    fix(showAnswerSaga): handle additional case (#3887)
    * fix(showAnswerSaga): handle additional case
    
    when item is removed from the test, it still neds to be evaluated (i.e. in modal)
    
    * fix(add-students): remove deactivated and not enrolled students

--- src/client/author/TestPage/ducks.js
+++ src/client/author/TestPage/ducks.js
@@ -651,8 +651,20 @@ function* showAnswerSaga({ payload }) {
   try {
     const testItems = yield select(state => get(state, ["tests", "entity", "testItems"], []));
     const testItem = testItems.find(x => x._id === payload.id) || {};
-    const questions = _keyBy(testItem.data && testItem.data.questions, "id");
     const answers = yield select(state => get(state, "answers", {}));
+    let questions = _keyBy(testItem.data && testItem.data.questions, "id");
+
+    // when item is removed from the test, we get the question from the payload (i.e modal case)
+    if (!questions || Object.keys(questions).length === 0) {
+      const { item } = payload;
+
+      // eslint-disable-next-line prefer-destructuring
+      questions = item.data.questions.reduce((acc, curr) => {
+        acc[curr.id] = curr;
+        return acc;
+      }, {});
+    }
+
     const evaluation = yield createShowAnswerData(questions, answers);
     yield put({
       type: ADD_ITEM_EVALUATION,

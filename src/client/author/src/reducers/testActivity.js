852f71025e5e0f8327bf68fe2888e0c7a7f1f46b
Nagarajan N <nagarajan.natarajan@snapwiz.com>
2019-07-30 11:11:40 +0530

    fix(LCB): minor issues

--- src/client/author/src/reducers/testActivity.js
+++ src/client/author/src/reducers/testActivity.js
@@ -134,6 +134,8 @@ const reducer = (state = initialState, { type, payload }) => {
                 _st.entities[entityIndex].questionActivities[itemIndex] = {
                   ...oldQAct,
                   ...questionItem,
+                  skipped: false,
+                  graded: true,
                   score,
                   maxScore
                 };

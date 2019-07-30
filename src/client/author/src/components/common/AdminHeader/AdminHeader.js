52b71f363175656772126ace2620aafd2590a9ab
utkarshbharti <utkarsh.bharti@snapwiz.com>
2019-07-30 13:54:11 +0530

    fix(da schools) added school count

--- src/client/author/src/components/common/AdminHeader/AdminHeader.js
+++ src/client/author/src/components/common/AdminHeader/AdminHeader.js
@@ -79,14 +79,14 @@ class AdminHeader extends Component {
   };
 
   render() {
-    const { title, active } = this.props;
-
+    const { title, active, count = 0 } = this.props;
+    const SchoolTabtext = count > 0 ? `Schools (${count})` : "Schools";
     return (
       <React.Fragment>
         <AdminHeaderContent>
           <StyledTabs type="card" defaultActiveKey={active.mainMenu} onTabClick={this.onHeaderTabClick}>
             <StyledTabPane tab="District Profile" key={"District Profile"} />
-            <StyledTabPane tab="Schools" key={"Schools"} />
+            <StyledTabPane tab={SchoolTabtext} key={"Schools"} />
             <StyledTabPane tab="Users" key={"Users"} />
             <StyledTabPane tab="Classes" key={"Classes"} />
             <StyledTabPane tab="Courses" key={"Courses"} />

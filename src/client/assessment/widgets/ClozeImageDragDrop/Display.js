8301ff1a8b41105f24060eb72775217fca953608
pintea andrei <clearg1929@gmail.com>
2019-08-01 05:03:32 +0200

    fix(EV-6632): clozeImageDragDrop wrap text to fit drop area

--- src/client/assessment/widgets/ClozeImageDragDrop/Display.js
+++ src/client/assessment/widgets/ClozeImageDragDrop/Display.js
@@ -486,7 +486,7 @@ class Display extends Component {
                     ...btnStyle,
                     borderStyle: smallSize ? "dashed" : "solid",
                     height: isWrapText ? "auto" : responseContainer.height || "auto", // responseContainer.height || "auto",
-                    width: isWrapText ? "auto" : responseContainer.width || "auto",
+                    width: responseContainer.width,
                     minHeight: responseContainer.height || "auto",
                     minWidth: responseContainer.width || "auto",
                     maxWidth: response.maxWidth

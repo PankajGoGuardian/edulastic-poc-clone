c4ce3563ed048f8bc8e7b6ddb20e836fc6c7bd9f
Avijit <avijit.rajendra@snapwiz.com>
2019-08-02 14:55:49 +0530

    fix(review-test): review test UI change

--- src/client/author/TestPage/components/Review/components/List/List.js
+++ src/client/author/TestPage/components/Review/components/List/List.js
@@ -32,135 +32,142 @@ const splitItems = (item, testItem) => {
   );
 };
 
-const SortableItem = SortableElement(
-  ({
-    indx,
-    selected,
-    item,
-    testItem,
-    onCheck,
-    isEditable = false,
-    points,
-    onChangePoints,
-    owner,
-    metaInfoData,
-    onPreview,
-    questions,
-    mobile
-  }) => {
-    const DragHandle = SortableHandle(() => <QuestionIndex>Q{indx + 1}</QuestionIndex>);
-    const handleCheck = e => onCheck(indx, e.target.checked);
-    /**
-     * @type {{item:Object,question:Object}[]}
-     */
-    const items = testItem.itemLevelScoring
-      ? [{ item, question: (testItem.data && testItem.data.questions[0]) || {} }]
-      : splitItems(item, testItem);
+export const SortableItem = ({
+  indx,
+  selected,
+  item,
+  testItem,
+  onCheck,
+  isEditable = false,
+  points,
+  onChangePoints,
+  owner,
+  collapseView = false,
+  metaInfoData,
+  onPreview,
+  questions,
+  mobile
+}) => {
+  // const DragHandle = SortableHandle(() => <QuestionIndex>Q{indx + 1}</QuestionIndex>);
+  const handleCheck = e => onCheck(indx, e.target.checked);
+  /**
+   * @type {{item:Object,question:Object}[]}
+   */
+  const items = testItem.itemLevelScoring
+    ? [{ item, question: (testItem.data && testItem.data.questions[0]) || {} }]
+    : splitItems(item, testItem);
 
-    return (
-      <TestItemWrapper data-cy={metaInfoData.id}>
-        {mobile ? (
-          <FlexContainer flexDirection="column" alignItems="flex-start">
-            <FlexContainer justifyContent="space-between" style={{ width: "100%", marginBottom: "15px" }}>
-              {isEditable && (
-                <FlexContainer flexDirection="column" justifyContent="center">
-                  <DragHandle />
-                  <QuestionCheckbox data-cy="queCheckbox" checked={selected.includes(indx)} onChange={handleCheck} />
-                </FlexContainer>
-              )}
+  return (
+    <TestItemWrapper data-cy={metaInfoData.id}>
+      {mobile ? (
+        <FlexContainer flexDirection="column" alignItems="flex-start">
+          <FlexContainer justifyContent="space-between" style={{ width: "100%", marginBottom: "15px" }}>
+            {isEditable && !collapseView && (
+              <FlexContainer flexDirection="column" justifyContent="center">
+                {/* <DragHandle /> */}
+                <QuestionCheckbox data-cy="queCheckbox" checked={selected.includes(indx)} onChange={handleCheck} />
+              </FlexContainer>
+            )}
 
-              <FlexContainer>
-                <PreviewButton data-cy="previewButton" onClick={() => onPreview(metaInfoData.id)}>
-                  Preview
-                </PreviewButton>
-                <FlexContainer flexDirection="column">
-                  <PointsLabel>Points</PointsLabel>
-                  <PointsInput
-                    data-cy="points"
-                    size="large"
-                    type="number"
-                    disabled={!owner || !isEditable}
-                    value={points}
-                    onChange={e => onChangePoints(metaInfoData.id, +e.target.value)}
-                  />
-                </FlexContainer>
+            <FlexContainer>
+              <PreviewButton data-cy="previewButton" onClick={() => onPreview(metaInfoData.id)}>
+                Preview
+              </PreviewButton>
+              <FlexContainer flexDirection="column">
+                <PointsLabel>Points</PointsLabel>
+                <PointsInput
+                  data-cy="points"
+                  size="large"
+                  type="number"
+                  disabled={!owner || !isEditable}
+                  value={points}
+                  onChange={e => onChangePoints(metaInfoData.id, +e.target.value)}
+                />
               </FlexContainer>
             </FlexContainer>
-            <FlexContainer>
-              <AnswerContext.Provider value={{ isAnswerModifiable: false }}>
-                <TestItemPreview
-                  style={{ marginTop: -10, padding: 0, boxShadow: "none", display: "flex" }}
-                  cols={item}
-                  metaData={metaInfoData.id}
-                  preview="show"
-                  verticalDivider={item.verticalDivider}
-                  disableResponse
-                  scrolling={item.scrolling}
-                  questions={questions}
-                  windowWidth="100%"
-                  isReviewTab
-                  testItem
-                />
-              </AnswerContext.Provider>
-            </FlexContainer>
           </FlexContainer>
-        ) : (
-          items.map(({ item: _item, question }, index) => {
-            return (
-              <FlexContainer justifyContent="space-between" alignItems="flex-start">
-                <FlexContainer alignItems="flex-start" style={{ width: "85%" }}>
+          <FlexContainer>
+            <AnswerContext.Provider value={{ isAnswerModifiable: false }}>
+              <TestItemPreview
+                style={{ marginTop: -10, padding: 0, boxShadow: "none", display: "flex" }}
+                cols={item}
+                metaData={metaInfoData.id}
+                preview="show"
+                verticalDivider={item.verticalDivider}
+                disableResponse
+                scrolling={item.scrolling}
+                questions={questions}
+                windowWidth="100%"
+                isReviewTab
+                testItem
+              />
+            </AnswerContext.Provider>
+          </FlexContainer>
+        </FlexContainer>
+      ) : (
+        items.map(({ item: _item, question }, index) => {
+          return (
+            <FlexContainer justifyContent="space-between" alignItems="flex-start">
+              <FlexContainer alignItems="flex-start" style={{ width: "85%" }}>
+                {!collapseView && (
                   <FlexContainer
                     style={{ marginTop: 30, visibility: index === 0 ? "visible" : "hidden", width: "5%" }}
                     flexDirection="column"
                     justifyContent="center"
                   >
-                    {index === 0 && <DragHandle />}
+                    {/* {index === 0 && <DragHandle />} */}
                     {isEditable && <QuestionCheckbox checked={selected.includes(indx)} onChange={handleCheck} />}
                   </FlexContainer>
-                  <AnswerContext.Provider value={{ isAnswerModifiable: false }}>
-                    <TestItemPreview
-                      style={{ marginTop: -10, padding: 0, boxShadow: "none", display: "flex", width: "95%" }}
-                      cols={_item}
-                      preview="show"
-                      metaData={metaInfoData.id}
-                      disableResponse
-                      verticalDivider={item.verticalDivider}
-                      scrolling={item.scrolling}
-                      questions={questions}
-                      windowWidth="100%"
-                      isReviewTab
-                      testItem
-                    />
-                  </AnswerContext.Provider>
-                </FlexContainer>
-                <FlexContainer style={{ width: "15%" }} flexDirection="column" alignItems="flex-end">
-                  <FlexContainer flexDirection="column" style={{ margin: 0 }}>
-                    <PointsLabel>Points</PointsLabel>
-                    <PointsInput
-                      size="large"
-                      type="number"
-                      disabled={!owner || !isEditable}
-                      value={
-                        testItem.itemLevelScoring
-                          ? testItem.itemLevelScore
-                          : get(question, ["validation", "valid_response", "score"], 0)
-                      }
-                      onChange={e => onChangePoints(metaInfoData.id, +e.target.value)}
-                    />
-                  </FlexContainer>
-                  {index === 0 && <PreviewButton onClick={() => onPreview(metaInfoData.id)}>Preview</PreviewButton>}
-                </FlexContainer>
+                )}
+                <AnswerContext.Provider value={{ isAnswerModifiable: false }}>
+                  <TestItemPreview
+                    style={{
+                      marginTop: -10,
+                      padding: 0,
+                      boxShadow: "none",
+                      display: "flex",
+                      width: collapseView ? "100%" : "95%"
+                    }}
+                    cols={_item}
+                    preview="show"
+                    metaData={metaInfoData.id}
+                    disableResponse
+                    verticalDivider={item.verticalDivider}
+                    scrolling={item.scrolling}
+                    questions={questions}
+                    windowWidth="100%"
+                    isReviewTab
+                    testItem
+                  />
+                </AnswerContext.Provider>
               </FlexContainer>
-            );
-          })
-        )}
-        <FlexContainer style={{ margin: "20px 0" }}>
-          <MetaInfoCell data={metaInfoData} itemTableView={true} />
-        </FlexContainer>
-      </TestItemWrapper>
-    );
-  }
-);
+              <FlexContainer style={{ width: "15%" }} flexDirection="column" alignItems="flex-end">
+                <FlexContainer flexDirection="column" style={{ margin: 0 }}>
+                  <PointsLabel>Points</PointsLabel>
+                  <PointsInput
+                    size="large"
+                    type="number"
+                    disabled={!owner || !isEditable}
+                    value={
+                      testItem.itemLevelScoring
+                        ? testItem.itemLevelScore
+                        : get(question, ["validation", "valid_response", "score"], 0)
+                    }
+                    onChange={e => onChangePoints(metaInfoData.id, +e.target.value)}
+                  />
+                </FlexContainer>
+                {index === 0 && <PreviewButton onClick={() => onPreview(metaInfoData.id)}>Preview</PreviewButton>}
+              </FlexContainer>
+            </FlexContainer>
+          );
+        })
+      )}
+      <FlexContainer style={{ margin: "20px 0" }}>
+        <MetaInfoCell data={metaInfoData} itemTableView={true} />
+      </FlexContainer>
+    </TestItemWrapper>
+  );
+};
 
 const List = SortableContainer(
   ({

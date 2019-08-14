bdaae4c38a58ae07bfbbde9d598a1ca5d661a955
prajwal-snapwiz <53730619+prajwal-snapwiz@users.noreply.github.com>
2019-08-14 12:27:58 +0530

    fix(EV-5344): prevent font-size override (#4260)

--- packages/common/src/components/Paper.js
+++ packages/common/src/components/Paper.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { white } from "@edulastic/colors";
 
 const Paper = styled.div`
+  font-size: ${props => props.fontSize};
   border-radius: ${props => (props.borderRadius ? props.borderRadius : "10px")};
   background: ${white};
   padding: ${props => (props.padding ? props.padding : "0px")};

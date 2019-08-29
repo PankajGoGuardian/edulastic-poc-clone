4dbc7c35a424d8c427dcc69ad40f74c5ab2698c4
mangaljain <mangal.jain@snapwiz.com>
2019-08-29 16:39:44 +0530

    fix(Mobile UI): Teacher dashboard done (#4568)

--- packages/common/index.js
+++ packages/common/index.js
@@ -51,3 +51,6 @@ export { default as AnswerContext } from "./src/contexts/AnswerContext";
 export { default as ScratchPadContext } from "./src/contexts/ScratchPadContext";
 // realtime hook
 export { default as useRealtimeV2 } from "./src/useRealtimeV2";
+
+// Mobile
+export { MenuIcon } from "./src/components/MenuIcon";
--- src/client/author/Dashboard/components/Header/Header.js
+++ src/client/author/Dashboard/components/Header/Header.js
@@ -1,19 +1,37 @@
 import React from "react";
+import { compose } from "redux";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { HeaderWrapper, TitleWrapper, ManageClassButton, IconPlus, ButtonText } from "./styled";
+import PropTypes from "prop-types";
 import { green } from "@edulastic/colors";
+import { FlexContainer, MenuIcon } from "@edulastic/common";
+import { HeaderWrapper, TitleWrapper, ManageClassButton, IconPlus, ButtonText } from "./styled";
+import { toggleSideBarAction } from "../../../src/actions/toggleMenu";
 
-function HeaderSection() {
-  return (
-    <HeaderWrapper>
+const HeaderSection = ({ toggleSideBar }) => (
+  <HeaderWrapper>
+    <FlexContainer style={{ pointerEvents: "none" }}>
+      <MenuIcon className="hamburger" onClick={() => toggleSideBar()} />
       <TitleWrapper>Dashboard</TitleWrapper>
-      <Link to={"/author/manageClass"}>
-        <ManageClassButton>
-          <IconPlus color={green} />
-          <ButtonText>Manage Class</ButtonText>
-        </ManageClassButton>
-      </Link>
-    </HeaderWrapper>
-  );
-}
-export default HeaderSection;
+    </FlexContainer>
+    <Link to="/author/manageClass">
+      <ManageClassButton>
+        <IconPlus color={green} />
+        <ButtonText>Manage Class</ButtonText>
+      </ManageClassButton>
+    </Link>
+  </HeaderWrapper>
+);
+
+HeaderSection.propTypes = {
+  toggleSideBar: PropTypes.func.isRequired
+};
+
+const enhance = compose(
+  connect(
+    null,
+    { toggleSideBar: toggleSideBarAction }
+  )
+);
+
+export default enhance(HeaderSection);

f2596b0859906867372aa932a3b0547a7d580412
Prajwal-Snapwiz <prajwal.venkatesh@gmail.com>
2019-09-03 18:19:44 +0530

    fix(style): update points field style

--- src/client/assessment/components/HOC/withPoints/styled/Container.js
+++ src/client/assessment/components/HOC/withPoints/styled/Container.js
@@ -1,8 +1,12 @@
 import styled from "styled-components";
+import { white, sectionBorder } from "@edulastic/colors";
 
 export const Container = styled.div`
   display: flex;
   align-items: center;
-  padding: 0;
   margin-bottom: 20px;
+  border: 1px solid ${sectionBorder};
+  padding: 15px;
+  border-radius: 3px;
+  background: ${white};
 `;

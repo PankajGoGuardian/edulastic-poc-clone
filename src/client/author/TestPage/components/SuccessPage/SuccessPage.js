03418e50f10b020fa850be6f47f584c3b0e7252b
arun-sasidharan <arun.sasidharan@snapwiz.com>
2019-09-03 12:16:28 +0530

    fix(regrade): success page is created for regrade flow

--- src/client/author/TestPage/components/SuccessPage/SuccessPage.js
+++ src/client/author/TestPage/components/SuccessPage/SuccessPage.js
@@ -75,9 +75,9 @@ class SuccessPage extends React.Component {
   }
 
   handleAssign = () => {
-    const { test, history, isAssignSuccess } = this.props;
+    const { test, history, isAssignSuccess, isRegradeSuccess } = this.props;
     const { _id } = test;
-    if (isAssignSuccess) {
+    if (isAssignSuccess || isRegradeSuccess) {
       history.push(`/author/assignments`);
     } else {
       history.push(`/author/assignments/${_id}`);
@@ -91,7 +91,7 @@ class SuccessPage extends React.Component {
   };
 
   renderHeaderButton = () => {
-    const { isAssignSuccess, isPlaylist } = this.props;
+    const { isAssignSuccess, isPlaylist, isRegradeSuccess } = this.props;
     return (
       !isPlaylist && (
         <AssignButton
@@ -101,7 +101,7 @@ class SuccessPage extends React.Component {
           variant="create"
           shadow="none"
         >
-          {isAssignSuccess ? "VIEW RESPONSE" : "ASSIGN"}
+          {isAssignSuccess || isRegradeSuccess ? "VIEW RESPONSE" : "ASSIGN"}
         </AssignButton>
       )
     );
@@ -122,7 +122,7 @@ class SuccessPage extends React.Component {
   }
 
   render() {
-    const { test, isPlaylist, playlist, isAssignSuccess, assignment = {}, userId } = this.props;
+    const { test, isPlaylist, playlist, isAssignSuccess, isRegradeSuccess, assignment = {}, userId } = this.props;
     const { isShareModalVisible } = this.state;
     const { title, _id, status, thumbnail, scoring = {}, grades, subjects, authors = [] } = isPlaylist
       ? playlist
@@ -183,8 +183,8 @@ class SuccessPage extends React.Component {
           <SecondHeader>
             <BreadCrumb data={isPlaylist ? playlistBreadCrumbData : breadCrumbData} style={{ position: "unset" }} />
           </SecondHeader>
-          <FlexContainerWrapper isAssignSuccess={isAssignSuccess}>
-            {isAssignSuccess && (
+          <FlexContainerWrapper isAssignSuccess={isAssignSuccess || isRegradeSuccess}>
+            {(isAssignSuccess || isRegradeSuccess) && (
               <FlexContainerWrapperLeft>
                 <ImageWrapper imgUrl={thumbnail} />
                 <FlexShareWithBox width={"100%"}>
@@ -192,7 +192,7 @@ class SuccessPage extends React.Component {
                 </FlexShareWithBox>
               </FlexContainerWrapperLeft>
             )}
-            <FlexContainerWrapperRight isAssignSuccess={isAssignSuccess}>
+            <FlexContainerWrapperRight isAssignSuccess={isAssignSuccess || isRegradeSuccess}>
               {isAssignSuccess && (
                 <>
                   <FlexTitle>Success!</FlexTitle>
@@ -203,6 +203,18 @@ class SuccessPage extends React.Component {
                     Your students can begin work on this assessment right away.You can monitor student progress and
                     responses by clicking on the &nbsp;
                     <span style={{ color: themeColor }}>View Response</span>&nbsp; button.
+                  </FlexText>
+                  <Divider />
+                </>
+              )}
+              {isRegradeSuccess && (
+                <>
+                  <FlexTitle>Success!</FlexTitle>
+                  <FlexTextWrapper>
+                    Regrade request for <b>{title}</b> is raised.
+                  </FlexTextWrapper>
+                  <FlexText>
+                    New changes will be reflecting in all selected assignment once the regrade process is completed.
                   </FlexText>
                   <Divider />
                 </>
@@ -276,6 +288,7 @@ export default enhance(SuccessPage);
 SuccessPage.propTypes = {
   match: PropTypes.object.isRequired,
   isAssignSuccess: PropTypes.bool,
+  isRegradeSuccess: PropTypes.bool,
   test: PropTypes.object,
   playlist: PropTypes.object,
   fetchPlaylistById: PropTypes.func.isRequired,

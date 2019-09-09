f7c0c96c7a6b90b36f765db736ecbd8e9af9f193
mgourav-snapwiz <gourav.mukherjee@snapwiz.com>
2019-09-09 11:34:13 +0530

    docs(assesment): adding comments

--- packages/common/src/components/Hints/index.js
+++ packages/common/src/components/Hints/index.js
@@ -6,10 +6,32 @@ import Text from "./Text";
 import { FlexContainer } from "@edulastic/common";
 
 const Hints = ({ questions = [] }) => {
-  // checking for empty hints;
+  /**
+   * input
+   * questions: [
+   * {
+   *  ...restProps,
+   *  hints: [{label: "", value: ""}]
+   * },
+   * {
+   *  ...restProps,
+   *  hints: [{label: "", value: ""}]
+   * }
+   * ]
+   *
+   * output: a number >= 0
+   *
+   * logic:
+   * for all questions, check if there are hints
+   * for all hints check if the label is not empty
+   * empty label is possible when a user entered something in the hint and then cleared it (obj is not removed)
+   *
+   * a number > 0 would indicate the current item has hints which have non empty label
+   */
+
+  //  TODO :  need to remove the object if the hint is cleared
   const validHints = questions.reduce((acc, question) => {
-    // checking if the hints have an empty label, if so filtering them
-    acc = question.hints.filter(hint => hint.label.length > 0).length;
+    acc += question.hints.filter(hint => hint.label.length > 0).length;
     return acc;
   }, 0);
   if (!validHints) return null;

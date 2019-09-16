a73d17a8ca42f5ccadfe2961b55fbb873e4e0c09
Prajwal-Snapwiz <prajwal.venkatesh@gmail.com>
2019-09-16 10:54:52 +0530

    fix(EV-2492): allow warning for both dot and comma as seperators

--- src/client/assessment/widgets/MathFormula/components/MathFormulaAnswerMethod/index.js
+++ src/client/assessment/widgets/MathFormula/components/MathFormulaAnswerMethod/index.js
@@ -335,7 +335,9 @@ const MathFormulaAnswerMethod = ({
   const restrictKeys = allowedVariables ? allowedVariables.split(",").map(segment => segment.trim()) : [];
   const customKeys = get(item, "customKeys", []);
   const isShowDropdown = item.isUnits && item.showDropdown;
-  const warningFlag =  options?.setThousandsSeparator?.[0] === "." && options?.setDecimalSeparator?.[0] === "."
+  const warningFlag =
+    options?.setThousandsSeparator?.[0] === options?.setDecimalSeparator?.[0] &&
+    options?.setDecimalSeparator?.[0] !== undefined;
   const studentTemplate = item.template && item.template.replace(/\\embed\{response\}/g, "\\MathQuillMathField{}");
   const innerValues = getInnerValuesForStatic(studentTemplate, value);
   const mathInputProps = {

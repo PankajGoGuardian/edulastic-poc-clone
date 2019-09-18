fcd32bbdf839faa70936798739a3c6cd697c6568
AnkarionTemios <pavel.emelyanov@webideal.pro>
2019-09-18 11:33:35 +0300

    fix(Charts): fix dot plot axis alignment

--- src/client/assessment/widgets/Charts/components/Circles.js
+++ src/client/assessment/widgets/Charts/components/Circles.js
@@ -35,7 +35,7 @@ const Circles = ({
 
   const getCenterX = index => step * index + 2;
 
-  const getCenterY = dot => convertUnitToPx(dot.y, { height: height / 2, margin, yAxisMax, yAxisMin, stepSize }) + 20;
+  const getCenterY = dot => convertUnitToPx(dot.y, { height: height, margin, yAxisMax, yAxisMin, stepSize }) + 20;
 
   const renderValidationIcons = index => (
     <g transform={`translate(${getCenterX(index) + step / 2 - 6},${getCenterY(bars[index]) - 30})`}>
@@ -51,13 +51,13 @@ const Circles = ({
 
   const getBarHeight = y =>
     Math.abs(
-      convertUnitToPx(yAxisMin, { height: height / 2, margin, yAxisMax, yAxisMin, stepSize }) -
-        convertUnitToPx(y, { height: height / 2, margin, yAxisMax, yAxisMin, stepSize })
+      convertUnitToPx(yAxisMin, { height: height, margin, yAxisMax, yAxisMin, stepSize }) -
+        convertUnitToPx(y, { height: height, margin, yAxisMax, yAxisMin, stepSize })
     );
 
   const getLength = y =>
     Math.floor(
-      (height / 2 - margin - convertUnitToPx(y, { height: height / 2, margin, yAxisMax, yAxisMin, stepSize })) /
+      (height - margin - convertUnitToPx(y, { height: height, margin, yAxisMax, yAxisMin, stepSize })) /
         (yAxisStep / 2.5)
     );
 
@@ -71,7 +71,7 @@ const Circles = ({
           {Array.from({ length: getLength(dot.y) }).map((a, ind) => (
             <Circle
               cx={getCenterX(index) + step / 2}
-              cy={height / 2 - margin - ind * (yAxisStep * 0.4) - yAxisStep / 2 + 30}
+              cy={height - margin - ind * yAxisStep - yAxisStep / 2 + 30}
               r={yAxisStep / 3.5 - 5}
             />
           ))}
@@ -108,7 +108,7 @@ const Circles = ({
               />
             </Fragment>
           )}
-          <Text textAnchor="middle" x={getCenterX(index) + step / 2} y={height / 2 + 20}>
+          <Text textAnchor="middle" x={getCenterX(index) + step / 2} y={height + 20}>
             {dot.x}
           </Text>
         </Fragment>

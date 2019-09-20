eb778de95b74bc0c91c456832897542fef10cd9b
utkarshbharti <utkarsh.bharti@snapwiz.com>
2019-09-20 11:17:00 +0530

    style : manage class details page ui change as per new mock up

--- src/client/author/ManageClass/components/ClassDetails/MainInfo.js
+++ src/client/author/ManageClass/components/ClassDetails/MainInfo.js
@@ -2,16 +2,38 @@ import React from "react";
 import PropTypes from "prop-types";
 import { find } from "lodash";
 import * as moment from "moment";
-import { MainContainer, LeftWrapper, MidWrapper, RightWrapper, Image, FieldValue, FieldLabel } from "./styled";
+import {
+  MainContainer,
+  LeftWrapper,
+  MidWrapper,
+  RightWrapper,
+  Image,
+  FieldValue,
+  FieldLabel,
+  StyledDivider,
+  ImageContainer,
+  ClassInfoContainer,
+  FlexDiv
+} from "./styled";
 import defaultImage from "../../../src/assets/manageclass/abstract.jpg";
 import selectsData from "../../../TestPage/components/common/selectsData";
 import FeaturesSwitch from "../../../../features/components/FeaturesSwitch";
-import { Tag } from "antd";
+import SubHeader from "./SubHeader";
+import { Tag, Row, Col } from "antd";
 import { themeColor, white } from "@edulastic/colors";
+import { fetchClassListAction } from "../../ducks";
 
 const { allGrades, allSubjects } = selectsData;
 
-const MainInfo = ({ entity = {} }) => {
+const MainInfo = ({
+  entity = {},
+  fetchClassList,
+  viewAssessmentHandler,
+  isUserGoogleLoggedIn,
+  allowGoogleLogin,
+  syncGCModal,
+  archiveClass
+}) => {
   // eslint-disable-next-line max-len
   const {
     thumbnail,
@@ -39,79 +61,86 @@ const MainInfo = ({ entity = {} }) => {
   const gradeSubject = { grades, subjects: subject };
   return (
     <MainContainer>
-      <LeftWrapper>
+      <ImageContainer>
         <Image src={thumbnail || defaultImage} alt="Class" />
-        <FieldValue>
-          <label>Tags :&nbsp;</label>
-          {tags &&
-            tags.map(({ _id, tagName }, index) => (
-              <Tag style={{ color: white, minWidth: "0px" }} color={themeColor} key={index}>
-                {tagName}
-              </Tag>
-            ))}
-        </FieldValue>
-      </LeftWrapper>
-      <MidWrapper>
-        <FieldValue>
-          <div>Grade :</div>
-          <span>{`${_grade}`}</span>
-        </FieldValue>
-        <FieldValue>
-          <div>Subject :</div>
-          <span>{_subject.text}</span>
-        </FieldValue>
-        <FieldValue>
-          <div>Standard :</div>
-          {standardSets && standardSets.length ? (
-            <span>{standardSets.map(({ name }) => name).join(", ")}</span>
-          ) : (
-            <span>Other</span>
-          )}
-        </FieldValue>
-        <FeaturesSwitch inputFeatures="selectCourse" actionOnInaccessible="hidden" key="selectCourse">
-          <FieldValue>
-            <div>Course :</div>
-            <span>{course && course.name}</span>
-          </FieldValue>
-        </FeaturesSwitch>
-        {coTeachers && coTeachers.length ? (
-          <FeaturesSwitch
-            inputFeatures="addCoTeacher"
-            actionOnInaccessible="hidden"
-            key="addCoTeacher"
-            gradeSubject={gradeSubject}
-          >
+      </ImageContainer>
+      <ClassInfoContainer>
+        <FlexDiv>
+          <SubHeader
+            {...entity}
+            fetchClassList={fetchClassList}
+            viewAssessmentHandler={viewAssessmentHandler}
+            isUserGoogleLoggedIn={isUserGoogleLoggedIn}
+            allowGoogleLogin={allowGoogleLogin}
+            syncGCModal={() => setOpenGCModal(true)}
+            archiveClass={archiveClass}
+          />
+        </FlexDiv>
+        <StyledDivider orientation="left" />
+        <FlexDiv>
+          <MidWrapper>
             <FieldValue>
-              <div>Co-Teachers :</div>
-              <span>{coTeachers}</span>
-            </FieldValue>
-          </FeaturesSwitch>
-        ) : (
-          ""
-        )}
-      </MidWrapper>
-      <RightWrapper>
-        <FieldValue>
-          <FieldLabel>Start Date :</FieldLabel>
-          <span>{moment(startDate).format("MMM DD, YYYY")}</span>
-        </FieldValue>
-        <FieldValue>
-          <FieldLabel>End Date :</FieldLabel>
-          <span>{moment(endDate).format("MMM DD, YYYY")}</span>
-        </FieldValue>
-        {!!googleId && (
-          <>
-            <FieldValue>
-              <FieldLabel>Google Class-code :</FieldLabel>
-              <span>{googleCode}</span>
+              <div>Grade</div>
+              <span>{`${_grade}`}</span>
             </FieldValue>
             <FieldValue>
-              <FieldLabel>Last Sync :</FieldLabel>
-              <span>{moment(lastSyncDate).format("MMM DD, YYYY")}</span>
+              <div>Subject</div>
+              <span>{_subject.text}</span>
             </FieldValue>
-          </>
-        )}
-      </RightWrapper>
+            <FieldValue>
+              <div>Standard</div>
+              {standardSets && standardSets.length ? (
+                <span>{standardSets.map(({ name }) => name).join(", ")}</span>
+              ) : (
+                <span>Other</span>
+              )}
+            </FieldValue>
+            <FeaturesSwitch inputFeatures="selectCourse" actionOnInaccessible="hidden" key="selectCourse">
+              <FieldValue>
+                <div>Course</div>
+                <span>{course && course.name}</span>
+              </FieldValue>
+            </FeaturesSwitch>
+            {coTeachers && coTeachers.length ? (
+              <FeaturesSwitch
+                inputFeatures="addCoTeacher"
+                actionOnInaccessible="hidden"
+                key="addCoTeacher"
+                gradeSubject={gradeSubject}
+              >
+                <FieldValue>
+                  <div>Co-Teachers</div>
+                  <span>{coTeachers}</span>
+                </FieldValue>
+              </FeaturesSwitch>
+            ) : (
+              ""
+            )}
+          </MidWrapper>
+          <RightWrapper>
+            <FieldValue>
+              <div>Start Date</div>
+              <span>{moment(startDate).format("MMM DD, YYYY")}</span>
+            </FieldValue>
+            <FieldValue>
+              <div>End Date</div>
+              <span>{moment(endDate).format("MMM DD, YYYY")}</span>
+            </FieldValue>
+            {!!googleId && (
+              <>
+                <FieldValue>
+                  <div>G. Class-code</div>
+                  <span>{googleCode}</span>
+                </FieldValue>
+                <FieldValue>
+                  <div>Last Sync</div>
+                  <span>{moment(lastSyncDate).format("MMM DD, YYYY")}</span>
+                </FieldValue>
+              </>
+            )}
+          </RightWrapper>
+        </FlexDiv>
+      </ClassInfoContainer>
     </MainContainer>
   );
 };

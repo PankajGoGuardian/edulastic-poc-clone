0aff2edbbcbf8da7fb0cc78f8a2da68f11de1155
gauravguptagit <gauravguptagit@users.noreply.github.com>
2019-09-25 09:22:46 +0530

    feat(EV-7338):added test authoring screens visual tests (#5048)

--- cypress/e2e/visual-regression-suites/authorTest.spec.js
+++ cypress/e2e/visual-regression-suites/authorTest.spec.js
@@ -1,7 +1,10 @@
 /* eslint-disable cypress/no-unnecessary-waiting */
 import FileHelper from "../framework/util/fileHelper";
+import TestLibrary from "../framework/author/tests/testLibraryPage";
 
-const SCREEN_SIZES = Cypress.config("SCREEN_SIZES");
+const SCREEN_SIZES = [[1600, 900], [1366, 768], [1024, 650]]; // Cypress.config("SCREEN_SIZES");
+const testUrl = "author/tests/5d8a008333ac9af288c03bce";
+const testLibraryPage = new TestLibrary();
 
 describe(`visual regression tests - ${FileHelper.getSpecName(Cypress.spec.name)}`, () => {
   before("set token", () => {
@@ -18,6 +21,30 @@ describe(`visual regression tests - ${FileHelper.getSpecName(Cypress.spec.name)}
         cy.setResolution(size);
         cy.visit(`/${pageURL}`);
         cy.contains("Create from Scratch").should("be.visible");
+        cy.matchImageSnapshot();
+      });
+
+      it(`test-description when resolution is - '${size}'`, () => {
+        cy.setResolution(size);
+        cy.visit(`/${testUrl}`);
+        cy.wait("@testdetail");
+        cy.wait(1000);
+        cy.matchImageSnapshot();
+      });
+
+      it(`test-additem when resolution is - '${size}'`, () => {
+        cy.setResolution(size);
+        testLibraryPage.header.clickOnAddItems();
+        testLibraryPage.searchFilters.clearAll();
+        testLibraryPage.testAddItem.authoredByMe();
+        cy.wait(1000);
+        cy.matchImageSnapshot();
+      });
+
+      it(`test-review when resolution is - '${size}'`, () => {
+        cy.setResolution(size);
+        testLibraryPage.header.clickOnReview();
+        cy.wait(1000);
         cy.matchImageSnapshot();
       });
     });

868eb4dd7bfd4ed5fcb67ae15e4f76f174e5e1e4
viraingarg-snapwiz <53215503+viraingarg-snapwiz@users.noreply.github.com>
2019-10-09 16:38:14 +0530

    Fix(EV-8543,EV-8540):Reports chart x-axis label for 1028 (#5342)
    -
    -
    -

--- src/client/author/Reports/common/components/charts/chartUtils/customChartXTick.js
+++ src/client/author/Reports/common/components/charts/chartUtils/customChartXTick.js
@@ -1,35 +1,21 @@
 import React from "react";
-import { StyledAxisTickText, StyledText } from "../../../styled";
-import { isMobileScreen } from "../../../util";
+import { StyledAxisTickText } from "../../../styled";
 
 export const CustomChartXTick = props => {
   const { x, y, payload, data, getXTickText } = props;
 
   let text;
-  let isMobile = false;
-  if (isMobileScreen()) {
-    isMobile = true;
-  }
   if (getXTickText) {
     text = getXTickText(payload, data);
   } else {
     text = payload.value;
   }
-  if (text.length > 12 && isMobile) {
-    text = text.substring(0, 12) + "...";
-  }
 
   return (
-    <g transform={`translate(${isMobile ? x + 25 : x},${y})`}>
-      {!isMobile ? (
-        <StyledAxisTickText textAnchor="middle" verticalAnchor="start" width={70}>
-          {text}
-        </StyledAxisTickText>
-      ) : (
-        <StyledText textAnchor="middle" width={70} transform="rotate(-45)" dx="-45">
-          {text}
-        </StyledText>
-      )}
+    <g transform={`translate(${x},${y})`}>
+      <StyledAxisTickText textAnchor="middle" verticalAnchor="start" width={70}>
+        {text}
+      </StyledAxisTickText>
     </g>
   );
 };

ffed90da7676eb966d922647eee5f2c3b546f34b
Manoj Singh Negi <justanothermanoj@gmail.com>
2019-10-11 11:05:55 +0530

    #8441 fix(student): fix content overflowing in math input (#5378)

--- src/client/assessment/widgets/MathFormula/styled/MathInputWrapper.js
+++ src/client/assessment/widgets/MathFormula/styled/MathInputWrapper.js
@@ -2,8 +2,12 @@ import styled from "styled-components";
 
 const MathInputWrapper = styled.div`
   position: relative;
-  max-width: 450px;
+  max-width: 100%;
   min-width: ${({ width }) => width || "auto"};
+
+  & > div {
+    max-width: 100%;
+  }
 `;
 
 export default MathInputWrapper;

e47a721efc12e55a95b8916d536bfb48d09ccd8e
viraingarg-snapwiz <53215503+viraingarg-snapwiz@users.noreply.github.com>
2019-10-14 15:11:55 +0530

    Fix(EV-8685):In SAP Subject field value should be bold (#5437)
    -
    -
    -

--- src/client/author/Reports/subPages/studentProfileReport/StudentAssessmentProfile/index.js
+++ src/client/author/Reports/subPages/studentProfileReport/StudentAssessmentProfile/index.js
@@ -82,7 +82,7 @@ const StudentAssessmentProfile = ({
       <StyledCard>
         <StyledH3>Assessment Performance Details of {studentName}</StyledH3>
         <p>
-          <b>Subject</b>: {studentInformation.standardSet}
+          <b>Subject : {studentInformation.standardSet}</b>
         </p>
         <AssessmentChart
           data={chartData}

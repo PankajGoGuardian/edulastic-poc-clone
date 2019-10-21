679f07655167bb620337f43ed8aa6ea1cba4c129
mangaljain <mangal.jain@snapwiz.com>
2019-10-21 08:31:45 +0530

    fix(UI): fix button width (#5580)

--- src/client/student/styled/AssignmentCardButton.js
+++ src/client/student/styled/AssignmentCardButton.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import { Button } from "antd";
-import { extraDesktopWidth, largeDesktopWidth, mobileWidthMax } from "@edulastic/colors";
+import { extraDesktopWidth, largeDesktopWidth, smallDesktopWidth, tabletWidth } from "@edulastic/colors";
 
 const StartAssignButton = styled(Button)`
   ${props => (props.theme.zoomLevel == "xs" ? "max-width: 200px; height: 40px;" : "max-width: 300px; height: auto;")}
-
+  width: 200px;
   border-radius: 4px;
   display: flex;
   align-items: center;
@@ -12,18 +12,10 @@ const StartAssignButton = styled(Button)`
   text-transform: uppercase;
   background: ${props => props.theme.assignment.cardDefaultBtnBgColor};
   border: solid 1px ${props => props.theme.assignment.cardDefaultBtnBgHoverColor};
-  width: 100%;
   padding: 5px 20px;
   cursor: pointer;
   float: right;
-  margin: 0px;
-
-  span {
-    color: ${props => props.theme.assignment.cardDefaultBtnTextColor};
-    font-size: ${props => props.theme.assignment.cardDefaultBtnFontSize};
-    font-weight: 600;
-    letter-spacing: 0.2px;
-  }
+  margin: 20px 0px 0px;
 
   &:hover {
     background-color: ${props => props.theme.assignment.cardDefaultBtnBgHoverColor};
@@ -33,26 +25,27 @@ const StartAssignButton = styled(Button)`
     }
   }
 
-  @media (max-width: ${extraDesktopWidth}) {
-    width: auto;
+  span {
+    color: ${props => props.theme.assignment.cardDefaultBtnTextColor};
+    font-size: ${props => props.theme.assignment.cardDefaultBtnFontSize};
+    font-weight: 600;
+    letter-spacing: 0.2px;
   }
 
+  @media (max-width: ${extraDesktopWidth}) {
+    width: 150px;
+  }
   @media (max-width: ${largeDesktopWidth}) {
     height: 36px;
-
     span {
       font-size: 9px;
     }
   }
-
-  @media screen and (min-width: 1025px) {
-    margin-right: 0px;
+  @media screen and (max-width: ${smallDesktopWidth}) {
+    width: 120px;
   }
-
-  @media screen and (max-width: 768px) {
-    margin: 20px auto 0px auto
-    width: 70%;
-    float: none;
+  @media screen and (max-width: ${tabletWidth}) {
+    margin-top: 10px;
   }
 
   ${({ theme }) =>

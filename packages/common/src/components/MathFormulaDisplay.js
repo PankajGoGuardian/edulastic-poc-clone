b13a8e6650cec9151a67dea11c88b300f8ad8732
Pintea Andrei <32391750+chpinan1128@users.noreply.github.com>
2019-10-24 18:00:10 +0300

    fix(EV-8760): fix height of image in correct answer block (#5642)

--- packages/common/src/components/MathFormulaDisplay.js
+++ packages/common/src/components/MathFormulaDisplay.js
@@ -16,12 +16,6 @@ const MathFormulaDisplay = withTheme(
     @media (max-width: ${desktopWidth}) {
       font-size: ${fonts.previewFontSizeMobile};
     }
-
-    img {
-      max-width: ${({ stem, choice }) => (!stem ? (!choice ? null : "165px !important") : "200px !important")};
-      max-height: ${({ stem, choice }) => (!stem ? (!choice ? null : "110px !important") : "120px !important")};
-      width: ${({ stem, choice }) => (stem || choice ? "auto !important" : null)};
-    }
   `)
 );
 

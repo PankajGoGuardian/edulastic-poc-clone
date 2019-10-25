ae8e90c9d857008d71d24e1d10329b42bc589951
snapwizshivam <38942634+snapwizshivam@users.noreply.github.com>
2019-10-25 12:29:32 +0530

    updaating routes for math api call (#5718)

--- packages/api/src/graphEvaluate.js
+++ packages/api/src/graphEvaluate.js
@@ -1,8 +1,7 @@
 import API from "./utils/API";
 
 const api = new API(`${process.env.POI_APP_API_URI}`);
-const convertLatex2Js = "/graph-api/convertLatex2Js";
-const graphEvaluate = "/graph-api/evaluate";
+const convertLatex2Js = "/math/convertLatex2Js";
 
 const convert = data =>
   api
@@ -13,18 +12,6 @@ const convert = data =>
     })
     .then(result => result.data);
 
-const evaluate = (data, type) =>
-  api
-    .callApi({
-      url: `${graphEvaluate}/${type}`,
-      method: "post",
-      data
-    })
-    .then(result => {
-      return result.data;
-    });
-
 export default {
-  convert,
-  evaluate
+  convert
 };

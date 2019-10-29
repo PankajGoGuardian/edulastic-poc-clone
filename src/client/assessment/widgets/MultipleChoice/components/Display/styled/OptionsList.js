dee5528130443b35cd2cc22f2c0f1fe8fcf24bdb
prajwal-snapwiz <53730619+prajwal-snapwiz@users.noreply.github.com>
2019-10-29 17:52:24 +0530

    fix(EV-9069): image goes out of box in MCQ & MSQ (#5768)

--- src/client/assessment/widgets/MultipleChoice/components/Display/styled/OptionsList.js
+++ src/client/assessment/widgets/MultipleChoice/components/Display/styled/OptionsList.js
@@ -3,7 +3,6 @@ import { mobileWidth } from "@edulastic/colors";
 
 export const OptionsList = styled.div`
   display: flex;
-  flex-wrap: wrap;
   justify-content: space-between;
   margin-left: ${props => (props.styleType === "primary" ? "0px" : "-25px")};
   flex-direction: column;

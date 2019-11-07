d6bca15018c218abeed08eddd16c47fd49dead47
AnkarionTemios <pavel.emelyanov@webideal.pro>
2019-11-07 16:34:45 +0300

    fix(ClozeMath): fix text alignment issue

--- src/client/assessment/widgets/ClozeMath/ClozeMathBlock/CheckedBlock.js
+++ src/client/assessment/widgets/ClozeMath/ClozeMathBlock/CheckedBlock.js
@@ -63,10 +63,7 @@ const CheckedBlock = ({ item, evaluation, userAnswer, id, type, isMath, width, h
     >
       <CheckBox className={checkBoxClass} key={`input_${index}`} onClick={onInnerClick} style={{ height, width }}>
         {showIndex && (
-          <span
-            className="index"
-            style={{ alignSelf: "stretch", height: "auto", textAlign: "left", paddingLeft: "11px" }}
-          >
+          <span className="index" style={{ alignSelf: "stretch", height: "auto" }}>
             {index + 1}
           </span>
         )}

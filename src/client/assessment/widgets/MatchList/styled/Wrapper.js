7043b0d05b2a107106fb75a205edcc10a6e401eb
mgourav-snapwiz <50568503+mgourav-snapwiz@users.noreply.github.com>
2019-11-22 12:11:19 +0530

    fix(matchList): aligning content to center inside responses (#6209)

--- src/client/assessment/widgets/MatchList/styled/Wrapper.js
+++ src/client/assessment/widgets/MatchList/styled/Wrapper.js
@@ -4,7 +4,7 @@ export const Wrapper = styled.div.attrs({
   className: "matchlist-wrapper"
 })`
   width: 100%;
-  height: 100%;
+  height: auto;
   display: flex;
   justify-content: center;
   align-items: center;

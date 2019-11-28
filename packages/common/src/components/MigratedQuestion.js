64287b45bd923b3a4faefdc6f4d4d84cebbda9d1
Pintea Andrei <32391750+chpinan1128@users.noreply.github.com>
2019-11-28 07:47:27 +0200

    fix(EV-9441): changed display option for migrated question (#6300)

--- packages/common/src/components/MigratedQuestion.js
+++ packages/common/src/components/MigratedQuestion.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const MigratedQuestion = styled.div`
-  display: flex;
-  width: 100%;
+  display: inline-flex;
+  /* width: 100%; */
   height: 100%;
   align-items: center;
 

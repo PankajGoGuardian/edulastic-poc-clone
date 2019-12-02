205122ab7d9c065a04e054390ebf6ed069c11655
Pintea Andrei <32391750+chpinan1128@users.noreply.github.com>
2019-12-02 15:47:23 +0200

    fix(EV-10080): wrap text and center alginment (#6367)

--- src/client/assessment/widgets/MatchList/styled/Wrapper.js
+++ src/client/assessment/widgets/MatchList/styled/Wrapper.js
@@ -3,13 +3,12 @@ import styled from "styled-components";
 export const Wrapper = styled.div.attrs({
   className: "matchlist-wrapper"
 })`
-  width: auto;
+  width: 100%;
   height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
   .answer-con.match.label-to-control {
-    width: max-content;
     max-width: 100%;
     margin: 0 auto;
   }

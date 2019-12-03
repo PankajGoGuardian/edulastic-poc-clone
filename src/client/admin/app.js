b3e438ad498f7af6f3304096d722458cff9ebb35
Arun Sasidharan <43816559+arun-sasidharan@users.noreply.github.com>
2019-12-03 13:16:38 +0530

    EV-10108  fix(ed-admin): admin login breaking due to district t… (#6387)
    * fix(ed-admin): admin login breaking due to district table is used
    
    * Revert "fix(ed-admin): admin login breaking due to district table is used"
    
    This reverts commit 0ab0c1b46a880eb9377151ec699fd0981a517ea3.
    
    * update to wrap it with theme provider

--- src/client/admin/app.js
+++ src/client/admin/app.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Layout, Menu, Icon } from "antd";
+import { ThemeProvider } from "styled-components";
 import { Route, Switch, Redirect } from "react-router-dom";
+import { themes } from "../theme";
 import Sider from "./Common/Sider";
 import { LogoCompact, Logo, Button, MainDiv } from "./Common/StyledComponents";
 import CleverSearch from "./Containers/CleverSearch";
@@ -30,49 +32,51 @@ const siderMenuData = [
 
 function Admin({ match, history, logoutAction, location }) {
   return (
-    <Layout style={{ minHeight: "100vh" }}>
-      <Logout logoutAction={logoutAction} />
-      <Sider role="navigation">
-        {([state, toggleState]) => {
-          return (
-            <div>
-              <Button
-                aria-label={`${state ? "Open" : "Close"} sidebar`}
-                onClick={() => toggleState(val => !val)}
-                noStyle
-                style={{ minHeight: "60px" }}
-              >
-                {state ? <LogoCompact /> : <Logo />}
-              </Button>
-              <Menu theme="light" defaultSelectedKeys={[location.pathname]} mode="inline">
-                {siderMenuData.map(item => (
-                  <Menu.Item
-                    onClick={() => {
-                      toggleState(false);
-                      if (item.href) {
-                        history.push(item.href);
-                      }
-                    }}
-                    key={item.href}
-                  >
-                    <Icon title={item.label} type={item.icon} />
-                    <span>{item.label}</span>
-                  </Menu.Item>
-                ))}
-              </Menu>
-            </div>
-          );
-        }}
-      </Sider>
-      <MainDiv>
-        <Switch>
-          <Redirect exact path={match.path} to={`${match.path}/search`} />
-          <Route path={`${match.path}/search`} component={CleverSearch} />
-          <Route path={`${match.path}/proxyUser`} component={ProxyUser} />
-          <Route path={`${match.path}/upgrade`} component={UpgradeUser} />
-        </Switch>
-      </MainDiv>
-    </Layout>
+    <ThemeProvider theme={themes.default}>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Logout logoutAction={logoutAction} />
+        <Sider role="navigation">
+          {([state, toggleState]) => {
+            return (
+              <div>
+                <Button
+                  aria-label={`${state ? "Open" : "Close"} sidebar`}
+                  onClick={() => toggleState(val => !val)}
+                  noStyle
+                  style={{ minHeight: "60px" }}
+                >
+                  {state ? <LogoCompact /> : <Logo />}
+                </Button>
+                <Menu theme="light" defaultSelectedKeys={[location.pathname]} mode="inline">
+                  {siderMenuData.map(item => (
+                    <Menu.Item
+                      onClick={() => {
+                        toggleState(false);
+                        if (item.href) {
+                          history.push(item.href);
+                        }
+                      }}
+                      key={item.href}
+                    >
+                      <Icon title={item.label} type={item.icon} />
+                      <span>{item.label}</span>
+                    </Menu.Item>
+                  ))}
+                </Menu>
+              </div>
+            );
+          }}
+        </Sider>
+        <MainDiv>
+          <Switch>
+            <Redirect exact path={match.path} to={`${match.path}/search`} />
+            <Route path={`${match.path}/search`} component={CleverSearch} />
+            <Route path={`${match.path}/proxyUser`} component={ProxyUser} />
+            <Route path={`${match.path}/upgrade`} component={UpgradeUser} />
+          </Switch>
+        </MainDiv>
+      </Layout>
+    </ThemeProvider>
   );
 }
 

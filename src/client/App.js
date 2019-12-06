7abad6e7626063c60b936579fa34afe62ac956ff
Yogesh VR <yogesh@snapwiz.com>
2019-12-06 15:21:58 +0530

    hiding signup button & prevent it from router (address bar)

--- src/client/App.js
+++ src/client/App.js
@@ -196,7 +196,7 @@ class App extends Component {
                 component={DistrictRoutes}
                 redirectPath={defaultRoute}
               />
-              <LoggedOutRoute path="/Signup" component={TeacherSignup} redirectPath={defaultRoute} />
+              {/* <LoggedOutRoute path="/Signup" component={TeacherSignup} redirectPath={defaultRoute} /> */}
               <LoggedOutRoute
                 exact
                 path="/partnerLogin/:partner/Signup"

8565afd5d946ffbb873f65bc6eb917efb7b442e4
Pintea Andrei <clearg1929@gmail.com>
2019-12-10 22:09:31 +0200

    fix(EV-10340) fix user is able to highlight multiple elements

--- src/client/assessment/widgets/Passage/index.js
+++ src/client/assessment/widgets/Passage/index.js
@@ -17,9 +17,12 @@ import PassageView from "./PassageView";
 import Details from "./Details";
 
 import { saveUserWorkAction, clearUserWorkAction } from "../../actions/userWork";
+
 const EmptyWrapper = styled.div``;
 
-const PassageWrapper = styled(Paper)`
+const PassageWrapper = styled(Paper).attrs(() => ({
+  id: "passage-wrapper"
+}))`
   border-radius: ${({ flowLayout }) => (flowLayout ? 0 : 10)}px;
   background: ${({ flowLayout }) => (flowLayout ? "transparent" : white)};
   box-shadow: ${({ flowLayout }) => (flowLayout ? "unset" : `0 3px 10px 0 ${boxShadowDefault}`)};
@@ -92,7 +95,7 @@ const enhance = compose(
   withNamespaces("assessment"),
   connect(
     (state, ownProps) => ({
-      userWork: get(state, `userWork.present[${ownProps.item.id}].resourceId`, [])
+      userWork: get(state, `userWork.present[${ownProps.item.id}].resourceId`, null)
     }),
     {
       setQuestionData: setQuestionDataAction,

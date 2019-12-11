1d9a9ee1cfd79ee8dbddd7a05f438d73b74b9d46
Prajwal-Snapwiz <prajwal.venkatesh@gmail.com>
2019-12-11 11:33:14 +0530

    fix(EV-10392): create from pdf not visible

--- src/client/author/AssessmentCreate/components/DropArea/styled.js
+++ src/client/author/AssessmentCreate/components/DropArea/styled.js
@@ -26,20 +26,22 @@ export const UploadDragger = styled(Dragger)`
   display: flex;
   width: 100%;
   height: 100%;
-  .ant-upload-list {
+  &.ant-upload-list {
     display: none;
   }
-  .ant-upload-drag-container {
-    cursor: auto;
+  &.ant-upload-drag-container {
+    /* cursor: auto; */
     padding: 0;
+    margin: 0;
   }
-  .ant-upload.ant-upload-drag {
+  &.ant-upload.ant-upload-drag {
     background: transparent;
     padding: 0;
     margin: 0;
-    height: auto;
+    height: 100%;
+    cursor: auto;
   }
-  .ant-upload.ant-upload-drag .ant-upload-btn {
+  &.ant-upload.ant-upload-drag .ant-upload-btn {
     width: 100%;
     height: 100%;
     padding: 0;

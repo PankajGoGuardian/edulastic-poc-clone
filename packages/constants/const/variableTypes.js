8b8823eb0315b67c89c144c47fc5c1d5d642a9e2
Pintea Andrei <32391750+pin1128@users.noreply.github.com>
2019-12-17 18:31:50 +0200

    fix(EV-10296): add sequence options in the dynamic variables (#6640)
    * fix(EV-10296): add sequence options in the dynamic variables
    
    * fix(dynamic parameters): creating unique combinations
    
    * fix(dynamic parameters): update combinations table

--- packages/constants/const/variableTypes.js
+++ packages/constants/const/variableTypes.js
@@ -1,11 +1,15 @@
 const NUMBER_RANGE = "Number Range";
 const NUMBER_SET = "Number Set";
+const NUMBER_SEQUENCE = "Number Sequence";
 const TEXT_SET = "Text Set";
+const TEXT_SEQUENCE = "Text Sequence";
 const FORMULA = "Formula";
 
 module.exports = {
   NUMBER_RANGE,
   NUMBER_SET,
+  NUMBER_SEQUENCE,
   TEXT_SET,
+  TEXT_SEQUENCE,
   FORMULA
 };

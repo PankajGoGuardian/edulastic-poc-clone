ee006fe799825e68182095544c78039e679eca42
mangaljain <mangal.jain@snapwiz.com>
2020-01-06 16:33:45 +0530

    fix(UI): removed extra space (#6935)

--- src/client/assessment/themes/common/QuestionSelectDropdown/index.js
+++ src/client/assessment/themes/common/QuestionSelectDropdown/index.js
@@ -32,7 +32,7 @@ const QuestionSelectDropdown = ({
       >
         {options.map((item, index) => (
           <Select.Option key={index} value={item}>
-            {`${t("common.layout.selectbox.question")} ${index + 1}/ ${options.length}`}
+            {`${t("common.layout.selectbox.question")} ${index + 1}/${options.length}`}
             {bookmarks[index] ? (
               <IconBookmark color="#f8c165" height={16} />
             ) : skipped[index] ? (

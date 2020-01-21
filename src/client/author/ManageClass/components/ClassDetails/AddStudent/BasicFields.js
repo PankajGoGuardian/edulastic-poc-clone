7769a3c2192807bd720dcb8145252a6869d8dd70
arpit-snapwiz <57706980+arpit-snapwiz@users.noreply.github.com>
2020-01-21 11:37:28 +0530

    fix(EV-11156): resetClassDetails is now having a default value (#7091)

--- src/client/author/ManageClass/components/ClassDetails/AddStudent/BasicFields.js
+++ src/client/author/ManageClass/components/ClassDetails/AddStudent/BasicFields.js
@@ -24,7 +24,7 @@ const BasicFields = ({
   modalClose,
   showClassCodeField,
   fetchClassDetailsUsingCode,
-  resetClassDetails,
+  resetClassDetails = () => {},
   validatedClassDetails,
   ...restProps
 }) => {

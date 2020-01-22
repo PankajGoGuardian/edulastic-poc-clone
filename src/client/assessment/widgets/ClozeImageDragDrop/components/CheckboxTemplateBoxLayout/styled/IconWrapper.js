abb5fd4999701297af27ca47943f7a7833c511c2
mgourav-snapwiz <50568503+mgourav-snapwiz@users.noreply.github.com>
2020-01-22 11:20:47 +0530

    fix(labelImgDragDrop): preventing overlap of evaluation icon inside response (#7129)

--- src/client/assessment/widgets/ClozeImageDragDrop/components/CheckboxTemplateBoxLayout/styled/IconWrapper.js
+++ src/client/assessment/widgets/ClozeImageDragDrop/components/CheckboxTemplateBoxLayout/styled/IconWrapper.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const IconWrapper = styled.div`
   position: absolute;
-  right: ${({ rightPosition }) => rightPosition || 20}px;
+  right: ${({ rightPosition }) => rightPosition || 10}px;
   display: flex;
   top: 50%;
   transform: translateY(-50%);

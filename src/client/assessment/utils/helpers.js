e99a7e9488786f235e012a38cad7d994bc585ed0
snapwizshivam <38942634+snapwizshivam@users.noreply.github.com>
2020-01-31 11:16:50 +0530

    fontsize changes  for question content (#7187)

--- src/client/assessment/utils/helpers.js
+++ src/client/assessment/utils/helpers.js
@@ -9,7 +9,7 @@ export const getFontSize = (fontSize, withRem = true) => {
     case "small":
       return withRem ? "0.6875rem" : "11px";
     case "normal":
-      return withRem ? (window.innerWidth <= 1024 ? "0.75rem" : "0.875rem") : "14px";
+      return withRem ? (window.innerWidth <= 1024 ? "0.875rem" : "1rem") : "16px";
     case "large":
       return withRem ? "1.0625rem" : "17px"; // 16PX = 1REM (BASE)
     case "xlarge":
@@ -17,7 +17,7 @@ export const getFontSize = (fontSize, withRem = true) => {
     case "xxlarge":
       return withRem ? "1.5rem" : "24px";
     default:
-      return window.innerWidth < 1366 ? (withRem ? "0.75rem" : "12px") : withRem ? "0.875rem" : "14px";
+      return window.innerWidth < 1366 ? (withRem ? "0.875rem" : "14px") : withRem ? "1rem" : "16px";
   }
 };
 

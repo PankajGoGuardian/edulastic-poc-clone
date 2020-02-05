ecfb110b623b3b01a4cf1ac4ac991d060f748624
viraingarg-snapwiz <53215503+viraingarg-snapwiz@users.noreply.github.com>
2020-02-05 16:58:16 +0530

    Fix(EV-11563): Removed Organization tab from content editor account. (#7283)

--- src/client/author/src/Sidebar/SideMenu.js
+++ src/client/author/src/Sidebar/SideMenu.js
@@ -127,9 +127,8 @@ class SideMenu extends Component {
     let _menuItems = cloneDeep(menuItems);
     if (features.isCurator || features.isPublisherAuthor) {
       _menuItems[0].path = "publisher/dashboard";
-      _menuItems[7].label = "Organization";
       const [item1, item2, item3, item4, item5] = _menuItems;
-      _menuItems = [item1, item3, item4, item5, _menuItems[7]];
+      _menuItems = [item1, item3, item4, item5];
     }
     if (features.isPublisherAuthor) {
       const [item1, ...rest] = _menuItems;

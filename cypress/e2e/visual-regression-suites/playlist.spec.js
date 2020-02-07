09a514a2ca8a7bdfba1130074c971a20685eb1f1
soumya597 <44015921+soumya597@users.noreply.github.com>
2020-02-07 13:15:25 +0530

    Fix(EV-7335) : author playlist screens and old data fixes (#7314)
    * Fix(EV-7335) : author playlist screens and old data fixes
    
    * Fix(EV-7335) : author playlist - manage modules pop up

--- cypress/e2e/visual-regression-suites/playlist.spec.js
+++ cypress/e2e/visual-regression-suites/playlist.spec.js
@@ -7,16 +7,16 @@ const search = new SearchFilters();
 const SCREEN_SIZES = Cypress.config("SCREEN_SIZES");
 const library = new TestLibrary();
 
-describe.skip(`${FileHelper.getSpecName(Cypress.spec.name)}`, () => {
+describe(`${FileHelper.getSpecName(Cypress.spec.name)}`, () => {
   before("set token", () => {
     cy.fixture("usersVisualRegression").then(allusers => {
-      const { username, password } = allusers.default.teacher;
+      const { username, password } = allusers.teacherPlaylist;
       cy.setToken(username, password); // setting auth token for teacher user
     });
   });
 
   context(`playlist use-this`, () => {
-    const pageURL = "author/playlists/5d931e74804ba10b22fdb3b0/use-this";
+    const pageURL = "/author/playlists/5e3bfb21b852990007419622/use-this";
 
     SCREEN_SIZES.forEach(size => {
       it(`private,when resolution is '${size}'`, () => {
@@ -71,9 +71,75 @@ describe.skip(`${FileHelper.getSpecName(Cypress.spec.name)}`, () => {
       it(`Tile view, when resolution is '${size}'`, () => {
         cy.setResolution(size); // set the screen resolution
         cy.get('[data-cy="tileView"]').click();
-        cy.contains("View").should("be.visible");
+        cy.contains("published").should("be.visible");
         search.scrollFiltersToTop();
         cy.matchImageSnapshotWithSize(); // take screenshot and compare
+      });
+    });
+  });
+
+  context(`Author Playlist`, () => {
+    const pageURL = "/author/playlists/5e3bf7f307c8b30007d02d0f/edit";
+    SCREEN_SIZES.forEach(size => {
+      it(`Summary page when resolution is '${size}'`, () => {
+        cy.setResolution(size);
+        cy.visit(`/${pageURL}`);
+        cy.contains("Play List Name").should("be.visible");
+        cy.matchImageSnapshotWithSize();
+        // for mobile
+        if (size[0] < screenSizes.MAX_TAB_WIDTH)
+          cy.isPageScrollPresent().then(({ hasScroll }) => {
+            if (hasScroll) cy.scrollPageAndMatchImageSnapshots(50);
+          });
+      });
+
+      it(`Add Test when resolution is '${size}'`, () => {
+        cy.setResolution(size);
+        cy.get('[data-cy="addTests"]').click({ force: true });
+        cy.wait("@searchTest");
+        cy.contains("Manage Modules").should("be.visible");
+        cy.matchImageSnapshotWithSize();
+        // for mobile
+        if (size[0] < screenSizes.MAX_TAB_WIDTH)
+          cy.isPageScrollPresent().then(({ hasScroll }) => {
+            if (hasScroll) cy.scrollPageAndMatchImageSnapshots(50);
+          });
+      });
+
+      it(`Manage Modules Pop up when resolution is '${size}'`, () => {
+        cy.setResolution(size);
+        cy.get('[data-cy="ManageModules"]').click();
+        cy.matchImageSnapshotWithSize();
+        // for mobile
+        if (size[0] < screenSizes.MAX_TAB_WIDTH)
+          cy.isPageScrollPresent().then(({ hasScroll }) => {
+            if (hasScroll) cy.scrollPageAndMatchImageSnapshots(50);
+          });
+        cy.get('[data-cy="manageModuleApply"]').click({ force: true });
+      });
+
+      it(`Review Page when resolution is '${size}'`, () => {
+        cy.setResolution(size);
+        cy.get('[data-cy="review"]').click();
+        cy.contains("Draft Playlist 1 ").should("be.visible");
+        cy.matchImageSnapshotWithSize();
+        // for mobile
+        if (size[0] < screenSizes.MAX_TAB_WIDTH)
+          cy.isPageScrollPresent().then(({ hasScroll }) => {
+            if (hasScroll) cy.scrollPageAndMatchImageSnapshots(50);
+          });
+      });
+
+      it(`Settings when resolution is '${size}'`, () => {
+        cy.setResolution(size);
+        cy.get('[data-cy="settings"]').click();
+        cy.contains("User Customization").should("be.visible");
+        cy.matchImageSnapshotWithSize();
+        // for mobile
+        if (size[0] < screenSizes.MAX_TAB_WIDTH)
+          cy.isPageScrollPresent().then(({ hasScroll }) => {
+            if (hasScroll) cy.scrollPageAndMatchImageSnapshots(50);
+          });
       });
     });
   });

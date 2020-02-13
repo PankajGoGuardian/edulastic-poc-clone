8aa760a72f5bf3896a4b2a11f926848019922558
arpit <arpit.chandrakar@snawpiz.com>
2020-02-13 11:33:20 +0530

    style(EV-11118): hiding other from the item bank menu

--- src/client/author/PickUpQuestionType/components/Container/Container.js
+++ src/client/author/PickUpQuestionType/components/Container/Container.js
@@ -328,10 +328,11 @@ class Container extends Component {
                     <IconRulerPencil />
                     Tools
                   </Menu.Item>
-                  <Menu.Item key="other">
+                  {/* implementation is in progress */}
+                  {/* <Menu.Item key="other">
                     <IconMore />
                     Other
-                  </Menu.Item>
+                  </Menu.Item> */}
                 </LeftMenuWrapper>
               </PerfectScrollbar>
             </AffixWrapper>
@@ -436,10 +437,11 @@ class Container extends Component {
                 <IconRulerPencil />
                 Tools
               </Menu.Item>
-              <Menu.Item key="other" onClick={this.toggleCategories}>
+              {/* implementation is in progress */}
+              {/* <Menu.Item key="other" onClick={this.toggleCategories}>
                 <IconMore />
                 Other
-              </Menu.Item>
+              </Menu.Item> */}
             </Menu>
           </StyledModalContainer>
         </StyledModal>

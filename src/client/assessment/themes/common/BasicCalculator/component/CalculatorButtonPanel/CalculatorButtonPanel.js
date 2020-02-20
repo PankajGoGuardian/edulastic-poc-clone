27087e543043ae9400427b2f2c5a5247984567a8
viraingarg-snapwiz <53215503+viraingarg-snapwiz@users.noreply.github.com>
2020-02-20 23:02:36 +0530

    Fix(EV-11816): Added keyboard control in basic calculator. (#7520)
    * Fix(EV-11816): Added keyboard control in basic calculator.
    
    * Update: Added support for keycode also

--- src/client/assessment/themes/common/BasicCalculator/component/CalculatorButtonPanel/CalculatorButtonPanel.js
+++ src/client/assessment/themes/common/BasicCalculator/component/CalculatorButtonPanel/CalculatorButtonPanel.js
@@ -1,12 +1,82 @@
-import CalculatorButton from "../CalculatorButton/CalculatorButton";
 import React from "react";
 import PropTypes from "prop-types";
+import CalculatorButton from "../CalculatorButton/CalculatorButton";
 
 import { StyledDiv, StyledDivF } from "./styled";
 
+const keyBoardKeysSupported = "1234567890/*+-=%.".split("");
+keyBoardKeysSupported.push("Enter");
+
+const keyCodesSupported = {
+  13: "Enter",
+  48: "0",
+  49: "1",
+  50: "2",
+  51: "3",
+  52: "4",
+  53: "5",
+  54: "6",
+  55: "7",
+  56: "8",
+  57: "9",
+  96: "0",
+  97: "1",
+  98: "2",
+  99: "3",
+  100: "4",
+  101: "5",
+  102: "6",
+  103: "7",
+  104: "8",
+  105: "9",
+  106: "*",
+  107: "+",
+  109: "-",
+  110: ".",
+  111: "/",
+  187: "=",
+  189: "-",
+  190: ".",
+  191: "/",
+  "shift+53": "%",
+  "shift+187": "+"
+};
+
 class CalculatorButtonPanel extends React.Component {
+  componentDidMount() {
+    window.addEventListener("keydown", this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.handleKeyPress);
+  }
+
+  handleKeyPress = e => {
+    const { key, shiftKey, keyCode } = e;
+    let keySymbol = "";
+    if (shiftKey) {
+      keySymbol = key || keyCodesSupported[`shift+${keyCode}`] || "";
+    } else {
+      keySymbol = key || keyCodesSupported[keyCode] || "";
+    }
+
+    if (keyBoardKeysSupported.includes(keySymbol)) {
+      const { clickHandler } = this.props;
+      let value = keySymbol;
+      if (keySymbol === "/") {
+        value = "÷";
+      } else if (keySymbol === "*") {
+        value = "x";
+      } else if (keySymbol === "Enter") {
+        value = "=";
+      }
+      clickHandler(value);
+    }
+  };
+
   handleClick = buttonName => {
-    this.props.clickHandler(buttonName);
+    const { clickHandler } = this.props;
+    clickHandler(buttonName);
   };
 
   render() {
@@ -46,6 +116,6 @@ class CalculatorButtonPanel extends React.Component {
   }
 }
 CalculatorButtonPanel.propTypes = {
-  clickHandler: PropTypes.func
+  clickHandler: PropTypes.func.isRequired
 };
 export default CalculatorButtonPanel;

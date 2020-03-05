1c4cab22ec92e745c1138f10587410dfb15d2c49
nagarajanSnapwiz <44694625+nagarajanSnapwiz@users.noreply.github.com>
2020-03-05 16:05:56 +0530

    hotfix:wrong import (#7779)

--- src/client/student/Profile/components/Header.js
+++ src/client/student/Profile/components/Header.js
@@ -1,7 +1,7 @@
 import React, { memo } from "react";
 import { MainHeader } from "@edulastic/common";
-import { withNamespaces } from "@edulastic/localization";
+import { withNamespaces } from "react-i18next";
 
 const ProfileHeader = ({ t }) => <MainHeader headingText={t("common.profileTitle")} />;
 
-export default memo(withNameSpaces("header")(ProfileHeader));
+export default memo(withNamespaces("header")(ProfileHeader));

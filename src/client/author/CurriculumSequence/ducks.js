8807fda49477956cdbd9f13ed528d122aa7f9169
Yogesh VR <yogesh@snapwiz.com>
2020-03-05 21:12:37 +0530

    temp fix to avoid firing the requests w/o required field

--- src/client/author/CurriculumSequence/ducks.js
+++ src/client/author/CurriculumSequence/ducks.js
@@ -653,6 +653,7 @@ function* fetchClassListByDistrictId({ payload }) {
 }
 
 function* fetchStudentListByGroupId({ payload }) {
+  if (payload.classId) return;
   try {
     const requestPayload = {
       districtId: payload.districtId,

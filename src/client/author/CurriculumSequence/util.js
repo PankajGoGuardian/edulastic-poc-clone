f31da34a03d526f6059e7d4a56ae65fbcbd06210
Amit Vishwakarma <35987339+amitsnapwiz@users.noreply.github.com>
2020-03-13 14:12:44 +0530

    fix(playlist): submitted percentage and time spent fix (#7898)

--- src/client/author/CurriculumSequence/util.js
+++ src/client/author/CurriculumSequence/util.js
@@ -39,7 +39,7 @@ export const getSummaryData = (modules, playlistMetrics, isStudent) => {
       }
     }
     const duration = moment.duration(tSpent);
-    const h = duration.hours();
+    const h = Math.floor(duration.asHours());
     let m = duration.minutes();
     const s = duration.seconds();
     if (s > 50) {
@@ -47,7 +47,6 @@ export const getSummaryData = (modules, playlistMetrics, isStudent) => {
     }
 
     const timeSpent = h > 0 ? `${h} H ${m} mins` : `${m} min`;
-
     return {
       title,
       name,
@@ -65,20 +64,17 @@ export const getSummaryData = (modules, playlistMetrics, isStudent) => {
 };
 
 export const getProgressData = (playlistMetrics, _id, contentId, assignments) => {
-  const data = playlistMetrics?.[_id]?.find(x => x.testId === contentId) || {};
-  const {
-    assignmentId = "",
-    gradedCount = 0,
-    totalAssigned = 0,
-    totalScore = 0,
-    maxScore = 0,
-    timeSpent: tSpent
-  } = data;
-  const submitted = assignmentId && totalAssigned ? round((gradedCount / totalAssigned) * 100, 0) : 0;
+  const data = playlistMetrics?.[_id]?.filter(x => x.testId === contentId) || [];
+  const totalScore = data.reduce((a, c) => a + parseInt(c.totalScore || 0), 0);
+  const maxScore = data.reduce((a, c) => a + parseInt(c.maxScore || 0), 0);
   const progress = maxScore ? round((totalScore / maxScore) * 100, 0) : 0;
   const classes = assignments.reduce((a, c) => a + (c?.["class"]?.length || 0), 0);
+  const tSpent = data.reduce((a, c) => a + parseInt(c.timeSpent || 0), 0);
+  const totalAssigned = data.reduce((a, c) => (c.assignmentId ? a + parseInt(c.totalAssigned || 0) : 0), 0);
+  const totalGraded = data.reduce((a, c) => (c.assignmentId ? a + parseInt(c.gradedCount || 0) : 0), 0);
+  const submitted = (totalAssigned && totalGraded && round((totalGraded / totalAssigned) * 100, 0)) || 0;
   const duration = moment.duration(parseInt(tSpent || 0, 10));
-  const h = duration.hours();
+  const h = Math.floor(duration.asHours());
   const m = duration.minutes();
   const s = duration.seconds();
 

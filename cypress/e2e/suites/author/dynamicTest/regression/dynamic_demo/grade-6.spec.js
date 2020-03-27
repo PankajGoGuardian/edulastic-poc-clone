1df4a0e5f5c606f77ccd710f1e1b0803ee4c6e64
sanjayupasi <sanjay.upasi@snapwiz.com>
2020-03-27 17:02:11 +0530

    Fix(dynamic-demo): Fixed

--- cypress/e2e/suites/author/dynamicTest/regression/dynamic_demo/grade-6.spec.js
+++ cypress/e2e/suites/author/dynamicTest/regression/dynamic_demo/grade-6.spec.js
@@ -18,45 +18,44 @@ describe(` >>item groups`, () => {
   before("Login and create new items", () => {
     cy.login("publisher", "author2.sparkmath@edulastic.com", "welcome");
   });
-  [testData1, testData2, testData3, testData4, testData5].forEach((testData, ind) => {
+  /* , testData2, testData3, testData4, testData5 */
+  [testData1].forEach((testData, ind) => {
     describe(`grade-${ind + 1}`, () => {
       const test_name = [];
       testData.forEach((data, index) => {
-        if (index === 13) {
-          context(`>autoselect-${index + 1}-${data.tags}`, () => {
-            before("create test", () => {
-              obj = {};
-              tets_id = "not_created";
-              testLibraryPage.createNewTestAndFillDetails({
-                name: data.name,
-                collections: "Spark Math - SparkMath for Playlist",
-                grade: data.grade,
-                subject: "Math",
-                tags: data.tags
-              });
-            });
-            it(`>create test`, () => {
-              testLibraryPage.testSummary.header.clickOnAddItems();
-              testLibraryPage.testAddItem.clickOnGroupItem();
-              cy.server();
-              cy.route("POST", "**api/test").as("createTest");
-              groupItemsPage.createDynamicTest(1, data);
-              testLibraryPage.header.clickOnReview();
-              cy.wait("@createTest").then(xhr => {
-                tets_id = xhr.response.body.result._id;
-              });
-            });
-            it(">save id and name", () => {
-              cy.wait(1).then(() => {
-                obj.tags = data.tags;
-                obj.name = data.name;
-                obj.id = tets_id;
-                obj.standard = data.standard.standardsToSelect;
-                test_name.push(obj);
-              });
+        context(`>autoselect-${index + 1}-${data.tags}`, () => {
+          before("create test", () => {
+            obj = {};
+            tets_id = "not_created";
+            testLibraryPage.createNewTestAndFillDetails({
+              name: data.name,
+              collections: "Spark Math - SparkMath for Playlist",
+              grade: data.grade,
+              subject: "Math",
+              tags: data.tags
             });
           });
-        }
+          it(`>create test`, () => {
+            testLibraryPage.testSummary.header.clickOnAddItems();
+            testLibraryPage.testAddItem.clickOnGroupItem();
+            cy.server();
+            cy.route("POST", "**api/test").as("createTest");
+            groupItemsPage.createDynamicTest(1, data);
+            testLibraryPage.header.clickOnReview();
+            cy.wait("@createTest").then(xhr => {
+              tets_id = xhr.response.body.result._id;
+            });
+          });
+          it(">save id and name", () => {
+            cy.wait(1).then(() => {
+              obj.tags = data.tags;
+              obj.name = data.name;
+              obj.id = tets_id;
+              obj.standard = data.standard.standardsToSelect;
+              test_name.push(obj);
+            });
+          });
+        });
       });
 
       context("Write", () => {
--- cypress/e2e/suites/author/temp/grade.spec.js
+++ cypress/e2e/suites/author/temp/grade.spec.js
@@ -1,26 +1,26 @@
 context("Algebra", () => {
   it("test", () => {
-    cy.fixture("/dynamic_demo/csv/Grade-8").then(sheet => {
-      let lineSplit = sheet.split("\n");
+    cy.fixture("/dynamic_demo/csv/Grade5").then(sheet => {
+      const lineSplit = sheet.split("\n");
       const dat = [];
       const requiredLine = lineSplit.slice(1);
       requiredLine.forEach(line => {
-        let obj = {};
-        let lineElements = line.split(",");
-        obj["tags"] = lineElements[0];
-        obj["collection"] = "Spark Math";
-        obj["name"] = `Grade-8-${lineElements[3]}`;
-        obj["deliveryCount"] = 10;
-        obj["standard"] = {};
-        obj.standard["standardsToSelect"] = [lineElements[2]];
-        obj.standard["grade"] = [`Grade ${lineElements[1]}`];
-        obj.standard["subject"] = "Mathematics";
-        obj.standard["standardSet"] = "Math - Common Core";
-        let tempArray = lineElements[2].split(".");
-        obj.standard["standardsGroup"] = `${tempArray[0]}.${tempArray[1]}`;
+        const obj = {};
+        const lineElements = line.split(",");
+        obj.tags = lineElements[0];
+        obj.collection = "Spark Math";
+        obj.name = `Grade-4-${lineElements.slice(2, lineElements.length - 1).join("")}`;
+        obj.deliveryCount = 10;
+        obj.standard = {};
+        obj.standard.standardsToSelect = [lineElements[1]];
+        obj.standard.grade = [`Grade ${lineElements[lineElements.length - 1].trim()}`];
+        obj.standard.subject = "Mathematics";
+        obj.standard.standardSet = "Math - Common Core";
+        const tempArray = lineElements[1].split(".");
+        obj.standard.standardsGroup = `${tempArray[0]}.${tempArray[1]}`;
         dat.push(obj);
       });
-      cy.writeFile("cypress/fixtures/dynamic_demo/data/Grade-8.json", dat);
+      cy.writeFile("cypress/fixtures/dynamic_demo/data/Grade5.json", dat);
       // cy.writeFile("dynsmic_updated",spit);
     });
   });

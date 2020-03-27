9a18e9cfa8a8c9e9720b379ff1571a35505fe2fd
sanjayupasi <sanjay.upasi@snapwiz.com>
2020-03-27 15:51:04 +0530

    Fix(dynaic-demo): Added data for new files

--- cypress/e2e/suites/author/dynamicTest/regression/dynamic_demo/grade-6.spec.js
+++ cypress/e2e/suites/author/dynamicTest/regression/dynamic_demo/grade-6.spec.js
@@ -2,63 +2,69 @@
 import TestLibrary from "../../../../../framework/author/tests/testLibraryPage";
 import GroupItemsPage from "../../../../../framework/author/tests/testDetail/groupItemsPage";
 
-const testData = require("../../../../../../fixtures/dynamic_demo/data/Grade-6");
+const testData1 = require("../../../../../../fixtures/dynamic_demo/data/Grade1");
+const testData2 = require("../../../../../../fixtures/dynamic_demo/data/Grade2");
+const testData3 = require("../../../../../../fixtures/dynamic_demo/data/Grade3");
+const testData4 = require("../../../../../../fixtures/dynamic_demo/data/Grade4");
+const testData5 = require("../../../../../../fixtures/dynamic_demo/data/Grade5");
 
 describe(` >>item groups`, () => {
   const testLibraryPage = new TestLibrary();
   const groupItemsPage = new GroupItemsPage();
 
   let tets_id;
-  let test_name = [];
   let obj = {};
 
   before("Login and create new items", () => {
     cy.login("publisher", "author2.sparkmath@edulastic.com", "welcome");
   });
-  testData.forEach((data, index) => {
-    if (index > 9) {
-      context(`>autoselect-${index + 1}-${data.tags}`, () => {
-        before("create test", () => {
-          obj = {};
-          tets_id = "not_created";
-          testLibraryPage.createNewTestAndFillDetails({
-            name: data.name,
-            collections: "Spark Math - SparkMath for Playlist",
-            grade: "Grade 10",
-            subject: "Math",
-            tags: data.tags
+  [testData1, testData2, testData3, testData4, testData5].forEach((testData, ind) => {
+    describe(`grade-${ind + 1}`, () => {
+      const test_name = [];
+      testData.forEach((data, index) => {
+        if (index === 13) {
+          context(`>autoselect-${index + 1}-${data.tags}`, () => {
+            before("create test", () => {
+              obj = {};
+              tets_id = "not_created";
+              testLibraryPage.createNewTestAndFillDetails({
+                name: data.name,
+                collections: "Spark Math - SparkMath for Playlist",
+                grade: data.grade,
+                subject: "Math",
+                tags: data.tags
+              });
+            });
+            it(`>create test`, () => {
+              testLibraryPage.testSummary.header.clickOnAddItems();
+              testLibraryPage.testAddItem.clickOnGroupItem();
+              cy.server();
+              cy.route("POST", "**api/test").as("createTest");
+              groupItemsPage.createDynamicTest(1, data);
+              testLibraryPage.header.clickOnReview();
+              cy.wait("@createTest").then(xhr => {
+                tets_id = xhr.response.body.result._id;
+              });
+            });
+            it(">save id and name", () => {
+              cy.wait(1).then(() => {
+                obj.tags = data.tags;
+                obj.name = data.name;
+                obj.id = tets_id;
+                obj.standard = data.standard.standardsToSelect;
+                test_name.push(obj);
+              });
+            });
           });
-        });
-        it(`>create test`, () => {
-          testLibraryPage.testSummary.header.clickOnAddItems();
-          testLibraryPage.testAddItem.clickOnGroupItem();
-          cy.server();
-          cy.route("POST", "**api/test").as("createTest");
-          groupItemsPage.createDynamicTest(1, data);
-          testLibraryPage.header.clickOnReview();
-          cy.wait("@createTest").then(xhr => {
-            // testLibraryPage.saveTestId(xhr);
-            tets_id = xhr.response.body.result._id;
-          });
-          // testLibraryPage.review.testheader.clickOnPublishButton();
-        });
-        it(">save id and name", () => {
-          cy.wait(1).then(() => {
-            obj["tags"] = data.tags;
-            obj["name"] = data.name;
-            obj["id"] = tets_id;
-            obj["standard"] = data.standard.standardsToSelect;
-            test_name.push(obj);
-          });
-        });
+        }
       });
-    }
-  });
 
-  context("Write", () => {
-    it("write", () => {
-      cy.wait(1).then(() => {
-        cy.writeFile("cypress/fixtures/dynamic_demo/results/Grade-6", test_name, { flag: "a+" });
+      context("Write", () => {
+        it("write", () => {
+          cy.wait(1).then(() => {
+            cy.writeFile(`cypress/fixtures/dynamic_demo/results/Grade${ind + 1}.json`, test_name);
+          });
+        });
       });
     });
   });

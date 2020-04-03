a19d6a5459e9948bc7e9437d47b355222e738dd8
pranjal-snapwiz <60252316+pranjal-snapwiz@users.noreply.github.com>
2020-04-03 14:40:52 +0530

    feature(TestReview): Clicking on question text shows preview (#8260)

--- src/client/author/TestPage/components/Review/components/ReviewItem/MainInfo/index.js
+++ src/client/author/TestPage/components/Review/components/ReviewItem/MainInfo/index.js
@@ -22,7 +22,7 @@ class MainInfo extends React.Component {
     const newHtml = helpers.sanitizeForReview(data.stimulus) || "";
     return (
       <FlexContainer data-cy-item-index={index} data-cy={data.id} style={{ justifyContent: "space-between" }}>
-        <Stimulus dangerouslySetInnerHTML={{ __html: newHtml }} />
+        <Stimulus dangerouslySetInnerHTML={{ __html: newHtml }} onClick={() => handlePreview(data.id)} />
         <FlexContainer
           style={{ width: "200px" }}
           flexDirection="column"

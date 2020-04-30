c7df40ab60c02ef2095cd52f3df08e955632e806
mgourav-snapwiz <50568503+mgourav-snapwiz@users.noreply.github.com>
2020-04-30 16:18:29 +0530

    fix(evaluator): fixing logic for penalty for choice matrix and evaluation score (#8653)
    * fix(evaluator): fixing exact match for choice matrix evaluator
    
    * fix[choiceMatrix]: fixing up exact match evaluator
    
    * fix[choice matrix]: refactoring partial match evaluator
    
    * feat[choice matrix]: adding penalty and rounding in evaluator
    
    * chore[choice matrix]: removing unused code
    
    * fix[choice matrix]: fixing scoring in no answer attempted exact match
    
    * feat[choice matrix]: added unit test cases for exact match evaluator
    
    * updated test cases to support multiple responses
    
    * refacor[choice matrix]: refactoring the partial match evaluator
    
    * removing commented code
    
    * updated tests for partial responses
    
    * update test data
    
    * adding lib code
    
    * fix(choice matrix): showing highlights for best possible match

--- packages/evaluators/src/choiceMatrix.js
+++ packages/evaluators/src/choiceMatrix.js
@@ -2,30 +2,7 @@ import { rounding as constantsForRoundingOff } from "./const/rounding";
 import { ScoringType } from "./const/scoring";
 
 const { ROUND_DOWN, NONE } = constantsForRoundingOff;
-const { PARTIAL_MATCH } = ScoringType;
-/**
- *
- * @param {Array} allAnswers
- * Group all the answers together
- * Input: [{ value: [ [0], [1] ] }, { value: [ [1], [1] ] }
- * Output: [ [0,1], [0,1] ]
- */
-const getAnswerSet = allAnswers => {
-  const answerSet = [];
-  allAnswers.forEach(answer => {
-    answer.value.forEach((row, rowIndex) => {
-      answerSet[rowIndex] = answerSet[rowIndex] || [];
-      if (row) {
-        row.forEach(ans => {
-          if (!answerSet[rowIndex].includes(ans)) {
-            answerSet[rowIndex].push(ans);
-          }
-        });
-      }
-    });
-  });
-  return answerSet;
-};
+const { PARTIAL_MATCH, EXACT_MATCH } = ScoringType;
 
 /**
  *
@@ -44,14 +21,17 @@ const getMaxScore = allAnswers => {
 
 /**
  *
- * @param {Array} userAnswers
- * transform the user answer for showing correct validation
- * put the value to the correct index
- * Input: [[0], [1], [0], [0]]
- * Output: [[0], [undefined, 1], [0]]
+ * @param {Array<Number[]>} arr
+ *
+ *  put the value to the correct index
+ *
+ * Input: [[0], [1], [0], [1]]
+ * Output: [[0], [empty, 1], [0], [empty, 1]]
+ *
+ * needed for determining the evaluation highlight for each cell
  */
-const transformUserAnswer = (userAnswers = []) =>
-  userAnswers.map(row => {
+const transformArray = (arr = []) =>
+  arr.map(row => {
     const _row = [];
     if (row) {
       row.forEach(col => {
@@ -63,69 +43,206 @@ const transformUserAnswer = (userAnswers = []) =>
 
 /**
  *
- * @param {Array} userAnswers
- * @param {Array} answerSet
+ * @param {Array<Number[]>} userResponse
+ * @param {Array<Object>} allAnswers
+ *
+ * validate the user response against all the answers
+ * get evaluations against all the answers
+ * also store the total correct, incorrect attempts against each answer set
  */
-const evaluateAnswers = (userAnswers, answerSet) => {
-  const transformedUserAnswers = transformUserAnswer(userAnswers);
-  const evaluation = transformedUserAnswers.map((row, rowIndex) => {
-    if (row) {
-      return row.map(ans => answerSet[rowIndex].includes(ans));
+const getEvaluationExactMatch = (userResponse, allAnswers) => {
+  const evaluations = allAnswers.map(answer => {
+    const { value, score: maxScoreForAllCorrect } = answer;
+    const actualCorrectAnswers = value.reduce((acc, curr) => acc.concat(curr)).length; // total correct answers set by user for current answer set
+    let correctAttempts = 0;
+    let incorrectAttempts = 0;
+    if (!userResponse.length) {
+      // user did not attempt, no score and evaluation highlights
+      return {
+        // allCorrect: false,
+        result: [],
+        maxScore: maxScoreForAllCorrect,
+        correctAttempts,
+        incorrectAttempts,
+        actualCorrectAnswers
+      };
     }
-    return row;
+    const transformedAnswer = transformArray(value);
+    // get the evaluation and score
+    const evaluation = userResponse.map((row, rowIndex) => {
+      let rowEvaluation = [];
+      if (Array.isArray(row)) {
+        rowEvaluation = row.map((col, columnIndex) => {
+          const correct = transformedAnswer[rowIndex][columnIndex] === col;
+          correct ? correctAttempts++ : incorrectAttempts++;
+          return correct;
+        });
+      }
+      return { evaluation: rowEvaluation };
+    });
+
+    return {
+      result: evaluation.map(obj => obj.evaluation),
+      correctAttempts,
+      incorrectAttempts,
+      maxScore: maxScoreForAllCorrect,
+      actualCorrectAnswers
+    };
   });
-  return evaluation;
+
+  return evaluations;
 };
 
 /**
  *
- * @param {Array.<number[]>} evaluation
+ * @param {Array<Number[]>} userResponse
+ * @param {Object} validation
+ *
+ * scoring is given on all or nothing basis
+ * highlights are shown according to best match from all answer sets
  */
-const getAnswerCount = evaluation => {
-  let correct = 0;
-  let incorrect = 0;
-  evaluation.forEach(row => {
-    if (row) {
-      row.forEach(ans => {
-        ans === true ? correct++ : incorrect++;
-      });
-    }
+const exactMatchEvaluator = (userResponse = [], validation = {}) => {
+  const { validResponse = {}, altResponses = [] } = validation;
+  const allAnswers = [{ ...validResponse }, ...altResponses];
+  let score = 0;
+  let maxScore = getMaxScore(allAnswers);
+  let evaluation = [];
+
+  if (!userResponse.length) {
+    // user did not attempt any answer, return back without evaluating anything
+    return { score, maxScore, evaluation };
+  }
+  const transformedUserAnswer = transformArray(userResponse);
+  const evaluations = getEvaluationExactMatch(transformedUserAnswer, allAnswers);
+
+  const allCorrectAnswerAttempt = evaluations.find(obj => {
+    const { correctAttempts, incorrectAttempts, actualCorrectAnswers } = obj;
+    return incorrectAttempts === 0 && correctAttempts === actualCorrectAnswers; // no incorrect answer, and exactly same answer as set by user
   });
-  return [correct, incorrect];
+
+  if (allCorrectAnswerAttempt) {
+    maxScore = allCorrectAnswerAttempt.maxScore;
+    score = maxScore;
+    evaluation = allCorrectAnswerAttempt.result;
+  } else {
+    /**
+     * show highlight against the best possible match
+     * in case of a tie,
+     * it will show the hightlights against the first, among the ones having a tie
+     *
+     * consider score of individual answer set when determining the best match
+     * sets having higher score will have higher priority than others
+     */
+    const bestMatch = evaluations.reduce((acc, curr) => {
+      const { correctAttempts, incorrectAttempts, maxScore: maxScoreForAllCorrect, actualCorrectAnswers } = acc;
+      const individualScore = maxScoreForAllCorrect / actualCorrectAnswers;
+      const accScore = (correctAttempts - incorrectAttempts) * individualScore;
+      const {
+        correctAttempts: currentCorrectAtttempts,
+        incorrectAttempts: currentIncorrectAttempts,
+        maxScore: currMaxScoreForAllCorrect,
+        actualCorrectAnswers: actualCorrectAnswersCurrent
+      } = curr;
+      const individualScoreCurrent = currMaxScoreForAllCorrect / actualCorrectAnswersCurrent;
+      const currScore = (currentCorrectAtttempts - currentIncorrectAttempts) * individualScoreCurrent;
+
+      if (currScore > accScore) {
+        return curr;
+      }
+      return acc;
+    });
+
+    evaluation = bestMatch.result;
+  }
+
+  return { score, maxScore, evaluation };
 };
+
+/**
+ *
+ * @param {Array<Number[]>} userResponse
+ * @param {Array<Object>} allAnswers
+ * @param {Number} penalty
+ *
+ * get the evaluation against all answers and score for each answer set
+ * score and penalty are calculated on individual option basis
+ */
+const getEvaluationPartialMatch = (userResponse, allAnswers, penalty) => {
+  const evaluations = allAnswers.map(answer => {
+    const { score: correctAnsMaxScore, value } = answer;
+    const actualCorrectAnswers = value.reduce((acc, curr) => acc.concat(curr)).length; // total correct answers set by user for current answer set
+    const scorePerCorrectAnswer = correctAnsMaxScore / actualCorrectAnswers;
+    const penaltyPerIncorrectAnswer = penalty / actualCorrectAnswers;
+    const transformedAnswer = transformArray(value); // move values in array to proper indexes
+    let currentScore = 0;
+    let correctAnswers = 0;
+    let incorrectAnswers = 0;
+    // get the evaluation for the current answer set
+    const currentEvaluation = userResponse.map((row, rowIndex) => {
+      if (Array.isArray(row)) {
+        return row.map((col, colIndex) => {
+          const isCorrect = transformedAnswer[rowIndex][colIndex] === col;
+          isCorrect ? correctAnswers++ : incorrectAnswers++;
+          return isCorrect;
+        });
+      }
+      return [];
+    });
+    const correctAnswerScore = correctAnswers * scorePerCorrectAnswer;
+    const penalisation = incorrectAnswers * penaltyPerIncorrectAnswer;
+    currentScore = Math.max(0, correctAnswerScore - penalisation); // score achieved for current answer set
+    return { evaluation: currentEvaluation, score: currentScore, maxScore: correctAnsMaxScore };
+  });
+
+  return evaluations;
+};
+
+const partialMatchEvaluator = (userResponse, validation) => {
+  const { validResponse = {}, altResponses = [], penalty = 0, rounding = NONE } = validation;
+  const allAnswers = [{ ...validResponse }, ...altResponses];
+  let score = 0;
+  let maxScore = getMaxScore(allAnswers);
+  let evaluation = [];
+  const transformedUserAnswer = transformArray(userResponse);
+
+  const evaluations = getEvaluationPartialMatch(transformedUserAnswer, allAnswers, penalty);
+
+  if (evaluations.length > 0) {
+    const maxByScore = evaluations.reduce((acc, curr) => {
+      if (curr.score > acc.score) {
+        return curr;
+      }
+      return acc;
+    });
+    // if rounding is selected, round down, otherwise keep the score as it is.
+    score = rounding === ROUND_DOWN ? Math.floor(maxByScore.score) : maxByScore.score;
+    maxScore = maxByScore.maxScore;
+    evaluation = maxByScore.evaluation;
+  }
+
+  return { score, maxScore, evaluation };
+};
+
 /**
  *
  * @param {userResponse} Object
  * @param {validation} Object
  */
 const evaluator = ({ userResponse = {}, validation = {} }) => {
-  const { value: userAnswers = [] } = userResponse;
-  const { validResponse, altResponses, scoringType, penalty = 0, rounding = NONE } = validation;
-  const allAnswers = [{ ...validResponse }, ...altResponses];
-  let score = 0; // initial score
-  const maxScore = getMaxScore(allAnswers);
-  if (!userAnswers.length) {
-    return { score, maxScore, evaluation: [] };
+  const { scoringType } = validation;
+  if (scoringType !== PARTIAL_MATCH && scoringType !== EXACT_MATCH) {
+    return { score: 0, maxScore: 0, evaluation: [] };
   }
-  const answerSet = getAnswerSet(allAnswers);
-  const evaluation = evaluateAnswers(userAnswers, answerSet);
-  const correctAnswerRows = evaluation.filter(arr => arr.length > 0 && arr.every(ans => ans === true));
-  const [correctAnswers, incorrectAnswers] = getAnswerCount(evaluation);
+  const { value = [] } = userResponse;
 
-  if (scoringType === PARTIAL_MATCH) {
-    const individualScore = maxScore / evaluation.length;
-    const correctAnswerScore = Math.min(correctAnswers * individualScore, maxScore);
-    const penalisation = incorrectAnswers * penalty;
-    score = Math.max(correctAnswerScore - penalisation, 0).toPrecision(2);
-    if (rounding === ROUND_DOWN) {
-      score = Math.floor(score);
-    }
-  } else if (correctAnswerRows.length === evaluation.length) {
-    // exact match with all answers correct
-    score = maxScore;
+  switch (scoringType) {
+    case EXACT_MATCH:
+      return exactMatchEvaluator(value, validation);
+    case PARTIAL_MATCH:
+      return partialMatchEvaluator(value, validation);
+    default:
+      return { score: 0, maxScore: 0, evaluation: [] };
   }
-  const evaluationObject = { score: parseFloat(score), maxScore, evaluation };
-  return evaluationObject;
 };
 
 export default evaluator;

edf4578a0e6aea96a8719f6334102d651acadcc0
Yogesh V R <yogesh@snapwiz.com>
2020-05-06 20:26:39 +0530

    chore (build): changes to post install script to use dist folder by t… (#8829)

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -2,10 +2,10 @@ set -e
 
 cd ~/edulastic-poc
 yarn install
+dir_name=build-$(date +"%d-%m-%y-%H-%M")
+sed -i '/PUBLIC_URL/c\'PUBLIC_URL=https://cdnedupoc.snapwiz.net/edulasticv2-development/JS/dist/"$dir_name"'' .env
 DESTINATION=~/poc_cdn_tmp yarn build
 #uploading assets to cloudfront/s3 cdn with different public path and directory
-aws s3 rm --recursive s3://edupoc/edulasticv2-development/JS/dist
-
-aws s3 sync ~/poc_cdn_tmp s3://edupoc/edulasticv2-development/JS/dist
+aws s3 sync ~/poc_cdn_tmp s3://edupoc/edulasticv2-development/JS/dist/$dir_name
 cp ~/poc_cdn_tmp/*.html ~/poc_dist/
 cp -r ~/poc_cdn_tmp/guidelines ~/poc_dist/

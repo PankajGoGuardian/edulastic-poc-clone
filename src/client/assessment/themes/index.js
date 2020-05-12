711b1544f7b423e0d5c774af5c651e313218fb98
sukanta-snapwiz <59563328+sukanta-snapwiz@users.noreply.github.com>
2020-05-12 10:20:41 +0530

    fix: not showing unanswered popup for matchlist/orderlist/sortist q type (#8899)

--- src/client/assessment/themes/index.js
+++ src/client/assessment/themes/index.js
@@ -156,7 +156,7 @@ const AssessmentContainer = ({
       const answers = answersById[q.id];
       switch (q.type) {
         case questionType.TOKEN_HIGHLIGHT:
-          return answersById[q.id].filter(token => token?.selected).length === 0;
+          return (answersById[q.id] || []).filter(token => token?.selected).length === 0;
         case questionType.LINE_CHART:
         case questionType.BAR_CHART:
         case questionType.HISTOGRAM:
@@ -167,10 +167,10 @@ const AssessmentContainer = ({
         }
         case questionType.SORT_LIST:
         case questionType.MATCH_LIST:
-          return answers.every(d => isNull(d));
+          return (answers || []).every(d => isNull(d));
         case questionType.ORDER_LIST:
           const prevOrder = [...Array(q.list.length).keys()];
-          return isEqual(prevOrder, answers);
+          return answers ? isEqual(prevOrder, answers) : true;
         case questionType.MATH:
           if (q.title === "Complete the Equation") {
             const ans = (answers || "").replace(/\\ /g, "");

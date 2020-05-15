ae773311f7b18beb9e5797c4ad7d455fd9ce05a5
Aditya Vyas <61740321+aditya-snapwiz@users.noreply.github.com>
2020-05-15 13:19:43 +0530

    fix(EV-14227):Enable Import Items to Test on PROD for two districts (#8978)
    * fix(EV-14227):Enable Import Items to Test on Production for two districts
    
    * fix: Changes suggested

--- src/client/author/AssessmentCreate/components/CreationOptions/CreationOptions.js
+++ src/client/author/AssessmentCreate/components/CreationOptions/CreationOptions.js
@@ -1,24 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { compose } from "redux";
 import OptionPDF from "../OptionPDF/OptionPDF";
 import OptionScratch from "../OptionScratch/OptionScratch";
 import BodyWrapper from "../../../AssignmentCreate/common/BodyWrapper";
 import FlexWrapper from "../../../AssignmentCreate/common/FlexWrapper";
 import OptionQti from "../OptionQTI/OptionQTI";
+import { QTI_DISTRICTS } from "../../../../config";
 
-const CreationOptions = ({ onUploadPDF }) => (
-  <BodyWrapper>
-    <FlexWrapper marginBottom="0px">
-      <OptionScratch />
-      <OptionPDF onClick={onUploadPDF} />
-      {/* <OptionQti /> */}
-    </FlexWrapper>
-  </BodyWrapper>
-);
+const CreationOptions = ({ onUploadPDF, isShowQTI }) => {
+  return (
+    <BodyWrapper>
+      <FlexWrapper marginBottom="0px">
+        <OptionScratch />
+        <OptionPDF onClick={onUploadPDF} />
+        {isShowQTI && <OptionQti />}
+      </FlexWrapper>
+    </BodyWrapper>
+  );
+};
 
 CreationOptions.propTypes = {
   onUploadPDF: PropTypes.func.isRequired
 };
 
-export default CreationOptions;
+const enhance = compose(
+  withRouter,
+  connect(state => ({
+    isShowQTI: QTI_DISTRICTS.includes(state?.user?.user?.districtId)
+  }))
+);
+
+export default enhance(CreationOptions);

b408cfbe1b447c0b10822fefaa98557a0b090818
Raghavendra <raghavendra.rao@snapwiz.com>
2020-05-15 20:42:39 +0530

    Revert "fix(EV-14227):Enable Import Items to Test on PROD for two districts (#8978)"
    This reverts commit ae773311f7b18beb9e5797c4ad7d455fd9ce05a5.

--- src/client/author/AssessmentCreate/components/CreationOptions/CreationOptions.js
+++ src/client/author/AssessmentCreate/components/CreationOptions/CreationOptions.js
@@ -1,36 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
-import { compose } from "redux";
+
 import OptionPDF from "../OptionPDF/OptionPDF";
 import OptionScratch from "../OptionScratch/OptionScratch";
 import BodyWrapper from "../../../AssignmentCreate/common/BodyWrapper";
 import FlexWrapper from "../../../AssignmentCreate/common/FlexWrapper";
 import OptionQti from "../OptionQTI/OptionQTI";
-import { QTI_DISTRICTS } from "../../../../config";
 
-const CreationOptions = ({ onUploadPDF, isShowQTI }) => {
-  return (
-    <BodyWrapper>
-      <FlexWrapper marginBottom="0px">
-        <OptionScratch />
-        <OptionPDF onClick={onUploadPDF} />
-        {isShowQTI && <OptionQti />}
-      </FlexWrapper>
-    </BodyWrapper>
-  );
-};
+const CreationOptions = ({ onUploadPDF }) => (
+  <BodyWrapper>
+    <FlexWrapper marginBottom="0px">
+      <OptionScratch />
+      <OptionPDF onClick={onUploadPDF} />
+      {/* <OptionQti /> */}
+    </FlexWrapper>
+  </BodyWrapper>
+);
 
 CreationOptions.propTypes = {
   onUploadPDF: PropTypes.func.isRequired
 };
 
-const enhance = compose(
-  withRouter,
-  connect(state => ({
-    isShowQTI: QTI_DISTRICTS.includes(state?.user?.user?.districtId)
-  }))
-);
-
-export default enhance(CreationOptions);
+export default CreationOptions;
--- src/client/config.js
+++ src/client/config.js
@@ -1,3 +1,2 @@
-export const s3ImageBucketPath = process.env.POI_APP_S3_IMAGE_PATH || "https://edureact-dev.s3.amazonaws.com";
-
-export const QTI_DISTRICTS = ["5e79ae5eda6952de6c21e279", "5ebc2adfda6952de6ce9ffb7"];
+export const s3ImageBucketPath =
+  process.env.POI_APP_S3_IMAGE_PATH || "https://edureact-dev.s3.amazonaws.com";

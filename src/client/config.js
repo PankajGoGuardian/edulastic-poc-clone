aa6478a5af12bc8172adfdadfc07d122d86ca2bb
Aditya Vyas <61740321+aditya-snapwiz@users.noreply.github.com>
2020-05-15 20:00:02 +0530

    fix: Added the PROD districtIds (#8987)
    * fix: Added the PROD districtIds
    
    * fix: added comment for QTI_DISTRICTS const

--- src/client/config.js
+++ src/client/config.js
@@ -1,3 +1,13 @@
 export const s3ImageBucketPath = process.env.POI_APP_S3_IMAGE_PATH || "https://edureact-dev.s3.amazonaws.com";
 
-export const QTI_DISTRICTS = ["5e79ae5eda6952de6c21e279", "5ebc2adfda6952de6ce9ffb7"];
+/*
+Districts having QTI IMPORT ACCESS
+1,2 - POC DISTRICT IDS FOR Barra USD & Corio District
+3,4 - PROD DISTRICT IDS FOR Barra USD & Corio District
+*/
+export const QTI_DISTRICTS = [
+  "5e79ae5eda6952de6c21e279",
+  "5ebc2adfda6952de6ce9ffb7",
+  "5e3d194d03b7ad09241ce6d6",
+  "5ebbbb3b03b7ad0924d19c46"
+];

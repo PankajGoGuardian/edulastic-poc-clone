a9203e18d5bb9b2bb186df95932dfe8538ff79a2
aman-snapwiz <38850135+aman-snapwiz@users.noreply.github.com>
2020-05-15 19:54:32 +0530

    fix: Added the PROD districtIds #8987

--- src/client/config.js
+++ src/client/config.js
@@ -1,3 +1,8 @@
 export const s3ImageBucketPath = process.env.POI_APP_S3_IMAGE_PATH || "https://edureact-dev.s3.amazonaws.com";
 
-export const QTI_DISTRICTS = ["5e79ae5eda6952de6c21e279", "5ebc2adfda6952de6ce9ffb7"];
+export const QTI_DISTRICTS = [
+  "5e79ae5eda6952de6c21e279",
+  "5ebc2adfda6952de6ce9ffb7",
+  "5e3d194d03b7ad09241ce6d6",
+  "5ebbbb3b03b7ad0924d19c46"
+];

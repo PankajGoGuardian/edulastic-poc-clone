74f6e81a08f4b879d8b098771714d957de2f0f00
Yogesh V R <yogesh@snapwiz.com>
2020-05-20 13:02:10 +0530

    chore (build-script): adds exit conditions on intermediate failures (#9052)

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -2,9 +2,20 @@ set -e
 
 cd ~/edulastic-poc
 yarn install
+if [ $? -ne 0 ]
+ then
+  echo "Yarn install failed...exiting script"
+  exit 1
+fi
+
 dir_name=build-$(date +"%d-%m-%y-%H-%M")
 sed -i '/PUBLIC_URL/c\'PUBLIC_URL=https://cdnedupoc.snapwiz.net/edulasticv2-development/JS/dist/"$dir_name"'' .env
 DESTINATION=~/poc_cdn_tmp yarn build
+if [ $? -ne 0 ]
+ then
+  echo "Build failed...exiting script"
+  exit 1
+fi
 #uploading assets to cloudfront/s3 cdn with different public path and directory
 aws s3 sync ~/poc_cdn_tmp s3://edupoc/edulasticv2-development/JS/dist/$dir_name
 cp ~/poc_cdn_tmp/*.html ~/poc_dist/

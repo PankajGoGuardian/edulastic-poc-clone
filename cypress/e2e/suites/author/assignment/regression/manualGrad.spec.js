0313cf347b6d13b045fa9cab0760e331925a561a
sanjayupasi <57387252+sanjayupasi@users.noreply.github.com>
2020-05-20 17:11:45 +0530

    Fix(EV-14351): Added que bar verification in  manual grade spec (#9064)

--- cypress/e2e/suites/author/assignment/regression/manualGrad.spec.js
+++ cypress/e2e/suites/author/assignment/regression/manualGrad.spec.js
@@ -8,8 +8,9 @@ import LiveClassboardPage from "../../../../framework/author/assignments/LiveCla
 import ExpressGraderPage from "../../../../framework/author/assignments/expressGraderPage";
 import ReportsPage from "../../../../framework/student/reportsPage";
 import QuestionResponsePage from "../../../../framework/author/assignments/QuestionResponsePage";
+import BarGraph from "../../../../framework/author/assignments/barGraphs";
 
-describe(`${FileHelper.getSpecName(Cypress.spec.name)}> Manual Grading`, () => {
+describe(`${FileHelper.getSpecName(Cypress.spec.name)}> manual grading`, () => {
   const testLibraryPage = new TestLibrary();
   const assignmentsPage = new AssignmentsPage();
   const testAssignPage = new TestAssignPage();
@@ -19,16 +20,17 @@ describe(`${FileHelper.getSpecName(Cypress.spec.name)}> Manual Grading`, () => {
   const grader = new ExpressGraderPage();
   const reportsPage = new ReportsPage();
   const qrp = new QuestionResponsePage();
+  const barGraph = new BarGraph();
 
   const TEST = "MANUAL_GRADE";
   const Teacher = {
     name: "Teacher1",
-    email: "Teacher.Manual@snapwiz.com",
+    email: "Teacher.ManualGrad@snapwiz.com",
     pass: "snapwiz"
   };
   const Student1Class1 = {
     name: "Student1",
-    email: "Student1.Manual@snapwiz.com",
+    email: "Student1.ManualGrad@snapwiz.com",
     pass: "snapwiz"
   };
 
@@ -42,16 +44,26 @@ describe(`${FileHelper.getSpecName(Cypress.spec.name)}> Manual Grading`, () => {
   };
   const attemptTypes = ["right", "wrong", "partialCorrect"];
 
+  const attemptTypeData = [
+    {
+      stuName: Student1Class1.name,
+      attempt: {
+        Q1: "manualGrade",
+        Q2: "manualGrade",
+        Q3: "manualGrade"
+      }
+    }
+  ];
   let OriginalTestId;
 
-  before("Login and create new items and test", () => {
+  before("> login and create new items and test", () => {
     cy.deleteAllAssignments(Student1Class1.email, Teacher.email);
     cy.login("teacher", Teacher.email, Teacher.pass);
     testLibraryPage.createTest(TEST).then(id => {
       OriginalTestId = id;
     });
   });
-  before("Assign Test", () => {
+  before("> assign test", () => {
     testLibraryPage.clickOnAssign();
     testAssignPage.selectClass("Class");
     testAssignPage.selectTestType("Class Assessment");
@@ -61,7 +73,7 @@ describe(`${FileHelper.getSpecName(Cypress.spec.name)}> Manual Grading`, () => {
       authorAssignmentPage.getStatus().should("have.length", 1);
     });
   });
-  before("Attempt Test", () => {
+  before("> attempt test", () => {
     cy.login("student", Student1Class1.email, Student1Class1.pass);
     assignmentsPage.verifyPresenceOfTest(OriginalTestId);
     assignmentsPage.getAssignmentByTestId(OriginalTestId).should("have.length", 1);
@@ -72,55 +84,68 @@ describe(`${FileHelper.getSpecName(Cypress.spec.name)}> Manual Grading`, () => {
     });
     studentTestPage.submitTest();
   });
-  after("Delete Assignments", () => {
+  after("> delete assignments", () => {
     cy.deleteAllAssignments(Student1Class1.email, Teacher.email);
   });
-  context("Grading", () => {
-    it("Verify Reports- Before Manual Evaluation", () => {
+  context("> grading", () => {
+    it("> verify reports- before manual evaluation", () => {
+      reportsPage.getPercentByTestId(OriginalTestId).should("have.text", `0%`);
       assignmentsPage.reviewSubmittedTestById(OriginalTestId);
       itemKeys.forEach((item, i) => {
         reportsPage.selectQuestion(`Q${i + 1}`);
         reportsPage.getAchievedScore().should("contain.text", ``);
       });
     });
-    it("Verify Author Side- Question View(Before Assigning)", () => {
+    it("> verify author side- question view(before assigning)", () => {
       cy.login("teacher", Teacher.email, Teacher.pass);
       testLibraryPage.sidebar.clickOnAssignment();
       authorAssignmentPage.clcikOnPresenatationIconByIndex(0);
+
       lcb.getStudentScoreByIndex(0).should("contain.text", "0 / 6");
+      barGraph.verifyQueBarAndToolTipBasedOnAttemptData(attemptTypeData, Cypress._.keys(attemptTypeData.attempt));
+
+      lcb.clickOnStudentsTab();
+      barGraph.verifyQueBarAndToolTipBasedOnAttemptData(attemptTypeData, Cypress._.keys(attemptTypeData.attempt));
+
       lcb.clickonQuestionsTab();
+      barGraph.verifyQueBarAndToolTipBasedOnAttemptData(attemptTypeData, Cypress._.keys(attemptTypeData.attempt), true);
     });
-    it("Assign Marks Manual Evaluation-In Question View", () => {
+    it("> assign marks manual evaluation-in question view", () => {
       itemKeys.forEach((item, i) => {
         lcb.questionResponsePage.selectQuestion(`Q${i + 1}`);
         lcb.questionResponsePage.getQuestionContainerByStudent(Student1Class1.name).as("studentQuesCard");
         lcb.questionResponsePage.getScoreInput(cy.get("@studentQuesCard")).should("have.attr", "value", ``);
         lcb.questionResponsePage.updateScoreAndFeedbackForStudent(Student1Class1.name, score[i]);
+        attemptTypeData[0].attempt[`Q${i + 1}`] = attemptTypes[i];
       });
+      barGraph.verifyQueBarAndToolTipBasedOnAttemptData(attemptTypeData, Cypress._.keys(attemptTypeData.attempt), true);
     });
-    it("Verify Author Side- Student view", () => {
+    it("> verify author side- student view", () => {
       lcb.clickOnStudentsTab();
       itemKeys.forEach((item, i) => {
         lcb.questionResponsePage.getQuestionContainer(i).as("studentQuesCard");
         lcb.questionResponsePage.getScoreInput(cy.get("@studentQuesCard")).should("have.attr", "value", `${score[i]}`);
       });
       qrp.verifyTotalScoreAndImprovement(3, 6);
+      barGraph.verifyQueBarAndToolTipBasedOnAttemptData(attemptTypeData, Cypress._.keys(attemptTypeData.attempt));
     });
-    it("Verify Author Side- Card View", () => {
+    it("> verify author side- card view", () => {
       lcb.clickOnCardViewTab();
       lcb.getStudentScoreByIndex(0).should("contain.text", "3 / 6");
       lcb.verifyQuestionCards(0, attemptTypes);
       lcb.getStudentPerformanceByIndex(0).should("contain", `50%`);
+      barGraph.verifyQueBarAndToolTipBasedOnAttemptData(attemptTypeData, Cypress._.keys(attemptTypeData.attempt));
     });
-    it("Verify Author Side-Express Grader", () => {
+    it("> verify author side-express grader", () => {
       lcb.header.clickOnExpressGraderTab();
       itemKeys.forEach((item, i) => {
         grader.verifyScoreAndPerformanceForQueNum(`Q${i + 1}`, `${Cypress._.round(score[i] / 2, 2)}/1`, percent[i]);
       });
     });
-    it("Verify Reports- After Manual Evaluation", () => {
+    it("> verify reports- after manual evaluation", () => {
       cy.login("student", Student1Class1.email, Student1Class1.pass);
       assignmentsPage.sidebar.clickOnGrades();
+      reportsPage.getPercentByTestId(OriginalTestId).should("have.text", `50%`);
       assignmentsPage.reviewSubmittedTestById(OriginalTestId);
       itemKeys.forEach((item, i) => {
         reportsPage.selectQuestion(`Q${i + 1}`);

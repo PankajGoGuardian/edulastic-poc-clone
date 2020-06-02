5012757ccef737be1369cc1cefc9f243517fbc60
mangaljain <mangal.jain@snapwiz.com>
2020-06-02 14:14:41 +0530

    fix(ui): added tooltip on mouse hover [EV-14727] (#9270)

--- src/client/author/TestPage/components/Review/components/ReviewItem/Actions/index.js
+++ src/client/author/TestPage/components/Review/components/ReviewItem/Actions/index.js
@@ -4,14 +4,14 @@ import { Container, ActionButton } from "./styled";
 
 export default ({ expanded, onPreview, onDelete, onCollapseExpandRow, isEditable, style }) => (
   <Container style={style}>
-    <ActionButton data-cy={`expand-${expanded}`} onClick={onCollapseExpandRow}>
+    <ActionButton title="Expand" data-cy={`expand-${expanded}`} onClick={onCollapseExpandRow}>
       {expanded ? <IconCollapse width={15} height={15} /> : <IconExpand width={15} height={15} />}
     </ActionButton>
-    <ActionButton data-cy="preview" onClick={onPreview}>
+    <ActionButton title="Preview" data-cy="preview" onClick={onPreview}>
       <IconEye width={18} height={18} />
     </ActionButton>
     {isEditable && (
-      <ActionButton data-cy="delete" onClick={onDelete}>
+      <ActionButton title="Remove" data-cy="delete" onClick={onDelete}>
         <IconTrash width={15} height={15} />
       </ActionButton>
     )}

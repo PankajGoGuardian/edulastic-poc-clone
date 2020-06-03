b6651f5a70a9fca7b0c79ec68f7786cbd7170ef9
Raghavendra Kulkarni <raghavendra-kulkarni@users.noreply.github.com>
2020-06-03 15:03:23 +0530

    fix: hide signup and `Others` in widget pane (#9339)
    Co-authored-by: Yogesh VR <yogesh@snapwiz.com>
    Co-authored-by: arpit <arpit.chandrakar@snawpiz.com>

--- src/client/author/PickUpQuestionType/components/Container/Container.js
+++ src/client/author/PickUpQuestionType/components/Container/Container.js
@@ -353,10 +353,11 @@ class Container extends Component {
                     <IconRulerPencil />
                     Tools
                   </Menu.Item>
-                  <Menu.Item key="other">
+                  {/* implementation is in progress */}
+                  {/* <Menu.Item key="other">
                     <IconMore />
                     Other
-                  </Menu.Item>
+                  </Menu.Item> */}
                 </LeftMenuWrapper>
               </PerfectScrollbar>
             </AffixWrapper>
@@ -463,10 +464,11 @@ class Container extends Component {
                 <IconRulerPencil />
                 Tools
               </Menu.Item>
-              <Menu.Item key="other" onClick={this.toggleCategories}>
+              {/* implementation is in progress */}
+              {/* <Menu.Item key="other" onClick={this.toggleCategories}>
                 <IconMore />
                 Other
-              </Menu.Item>
+              </Menu.Item> */}
             </Menu>
           </StyledModalContainer>
         </StyledModal>

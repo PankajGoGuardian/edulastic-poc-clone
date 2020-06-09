87e2e8f1449cce6cc2c62f4b19ce6995ece3c697
Himayan Debnath <63641486+himayan@users.noreply.github.com>
2020-06-09 22:20:44 +0530

    fix(ui): show content approver / author only for organisation DAs [EV-14426] (#9443)

--- src/client/author/src/components/common/AdminSubHeader/UserSubHeader.js
+++ src/client/author/src/components/common/AdminSubHeader/UserSubHeader.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { roleuser } from "@edulastic/constants";
+
 import { SubHeaderWrapper, StyledTabPane, StyledSubMenu } from "./styled";
 
-import { getUserRole } from "../../../selectors/user";
+import { getUserRole, isOrganizationDistrictSelector } from "../../../selectors/user";
 
 class AdminHeader extends Component {
   static propTypes = {
@@ -11,7 +13,7 @@ class AdminHeader extends Component {
     active: PropTypes.object.isRequired
   };
 
-  onSubTab = (key, e) => {
+  onSubTab = (key) => {
     const { history } = this.props;
     switch (key) {
       case "Student":
@@ -31,13 +33,11 @@ class AdminHeader extends Component {
         return;
       case "Content Approvers":
         history.push(`/author/users/content-approvers`);
-        
     }
   };
 
   render() {
-    const { active, role } = this.props;
-    console.log({ role });
+    const { active, role, isOrganizationDistrictAdmin } = this.props;
     return (
       <SubHeaderWrapper>
         {active.mainMenu === "Users" && (
@@ -46,17 +46,17 @@ class AdminHeader extends Component {
             defaultActiveKey={active.subMenu}
             onTabClick={this.onSubTab}
           >
-            {role === "district-admin" && (
+            {role === roleuser.DISTRICT_ADMIN && (
               <StyledTabPane tab="District Admin" key="District Admin" />
             )}
             <StyledTabPane tab="School Admin" key="School Admin" />
             <StyledTabPane tab="Teacher" key="Teacher" />
             <StyledTabPane tab="Student" key="Student" />
             {/* Below repeated conditions is bcz Fragment is not working here */}
-            {role === "district-admin" && (
+            {isOrganizationDistrictAdmin && (
               <StyledTabPane tab="Content Authors" key="Content Authors" />
             )}
-            {role === "district-admin" && (
+            {isOrganizationDistrictAdmin && (
               <StyledTabPane tab="Content Approvers" key="Content Approvers" />
             )}
           </StyledSubMenu>
@@ -68,7 +68,8 @@ class AdminHeader extends Component {
 
 export default connect(
   state => ({
-    role: getUserRole(state)
+    role: getUserRole(state),
+    isOrganizationDistrictAdmin: isOrganizationDistrictSelector(state)
   }),
   {}
 )(AdminHeader);

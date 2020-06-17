a0d9c5f81a936c22b7de772d0290df6fa8d7d255
mgourav-snapwiz <50568503+mgourav-snapwiz@users.noreply.github.com>
2020-06-17 16:34:18 +0530

    revert(poc): reverting #7056 [EV-14803] (#9558)

--- src/client/assessment/components/Graph/Display/PlacementContainer/DragDropValues.js
+++ src/client/assessment/components/Graph/Display/PlacementContainer/DragDropValues.js
@@ -1,99 +1,76 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Rnd } from "react-rnd";
-import { DragDropTitle, DragDropContainer } from "./styled";
-import { SortableContainer, SortableElement, arrayMove } from "react-sortable-hoc";
+import { DragDropValuesContainer, DragDropTitle, DragDropContainer } from "./styled";
 
 class DragDropValues extends Component {
   constructor(props) {
     super(props);
     this.container = React.createRef();
-    this.state = {
-      values: this.props.values
-    };
   }
 
-  onSortEnd = ({ oldIndex, newIndex }) => {
-    this.setState(({ values }) => ({
-      values: arrayMove(values, oldIndex, newIndex)
-    }));
-  };
-
-  handleDragDropValuePosition = (d, value, index) => {
-    const { onAddDragDropValue, width, valueHeight } = this.props;
-    onAddDragDropValue(value, d.x - 0.83 * width, d.y + 0.5 * valueHeight + index * valueHeight);
+  handleDragDropValuePosition = (d, value) => {
+    const { onAddDragDropValue, width, margin, valueHeight } = this.props;
+    const containerWidth = this.container?.current?.offsetWidth || width;
+    onAddDragDropValue(value, d.x - containerWidth - margin + width / 2, d.y - margin + valueHeight / 2);
     return false;
   };
 
-  handleDragDropValue = (d, value, index) => {
-    const { onDrawDragDropValue, width, valueHeight } = this.props;
-    onDrawDragDropValue(value, d.x - 0.83 * width, d.y + 0.5 * valueHeight + index * valueHeight);
+  handleDragDropValue = (d, value) => {
+    const { onDrawDragDropValue, width, margin, valueHeight } = this.props;
+    const containerWidth = this.container?.current?.offsetWidth || width;
+    onDrawDragDropValue(value, d.x - containerWidth - margin + width / 2, d.y - margin + valueHeight / 2);
   };
-
-  componentDidUpdate(oldProps) {
-    const newProps = this.props;
-    if (oldProps.values !== newProps.values) {
-      this.setState({ values: newProps.values });
-    }
-  }
 
   scrollHandler = e => e.preventDefault();
 
   render() {
-    const { values, width, valueHeight, dragDropBoundsClassName, scale } = this.props;
+    const { values, width, height, valueHeight, dragDropBoundsClassName, scale } = this.props;
     const containerStyle = {
       position: "relative",
-      height: 30 + values.length * valueHeight
+      width,
+      height: values.length * (valueHeight + 5)
     };
-    const size = { width: width, height: valueHeight };
-    const SortableItem = SortableElement(({ value, ind, position }) => (
-      <div style={{ backgroundColor: "rgba(230, 230, 230, 0.23)", width: width + "px" }}>
-        <Rnd
-          key={`item-${value.id}`}
-          position={position}
-          size={size}
-          index={ind}
-          onDragStop={(evt, d) => {
-            if (window.isIOS) {
-              document.body.removeEventListener("touchmove", this.scrollHandler);
-            }
-            this.handleDragDropValuePosition(d, value, ind);
-          }}
-          onDrag={(e, d) => {
-            if (window.isIOS) {
-              document.body.addEventListener("touchmove", this.scrollHandler, { passive: false });
-              document.body.scrollTop = 0;
-            }
-            this.handleDragDropValue(d, value, ind);
-          }}
-          style={{ zIndex: 10, position: "static" }}
-          disableDragging={false}
-          enableResizing={false}
-          bounds={dragDropBoundsClassName ? `.${dragDropBoundsClassName}` : ""}
-          className="drag-drop-value react-draggable"
-          scale={1}
-        >
-          <DragDropContainer style={{ margin: "14.5px 0" }} dangerouslySetInnerHTML={{ __html: value.text }} />
-        </Rnd>
-      </div>
-    ));
-
-    const SortableList = SortableContainer(({ items }) => {
-      const items2 = items.filter(item => item);
-      return (
-        <div>
-          {items2.map((value, index) => (
-            <SortableItem key={`item-${value.id}`} index={index} ind={index} value={value} position={{ x: 0, y: 0 }} />
-          ))}
-        </div>
-      );
-    });
 
     return (
-      <div style={containerStyle} className="drag-drop-values">
+      <DragDropValuesContainer ref={this.container} width={width} minHeight={height}>
         <DragDropTitle>DRAG DROP VALUES</DragDropTitle>
-        <SortableList items={this.state.values} onSortEnd={this.onSortEnd} />
-      </div>
+        <div style={containerStyle}>
+          {values.map((value, i) => {
+            const position = { x: 5, y: i * (valueHeight + 5) };
+            const size = { width: width - 10, height: valueHeight };
+
+            return (
+              <Rnd
+                key={value.id}
+                position={position}
+                size={size}
+                onDragStop={(evt, d) => {
+                  if (window.isIOS) {
+                    document.body.removeEventListener("touchmove", this.scrollHandler);
+                  }
+                  this.handleDragDropValuePosition(d, value);
+                }}
+                onDrag={(e, d) => {
+                  if (window.isIOS) {
+                    document.body.addEventListener("touchmove", this.scrollHandler, { passive: false });
+                    document.body.scrollTop = 0;
+                  }
+                  this.handleDragDropValue(d, value);
+                }}
+                style={{ zIndex: 10 }}
+                disableDragging={false}
+                enableResizing={false}
+                bounds={dragDropBoundsClassName ? `.${dragDropBoundsClassName}` : ""}
+                className="drag-drop-value"
+                scale={scale}
+              >
+                <DragDropContainer dangerouslySetInnerHTML={{ __html: value.text }} />
+              </Rnd>
+            );
+          })}
+        </div>
+      </DragDropValuesContainer>
     );
   }
 }

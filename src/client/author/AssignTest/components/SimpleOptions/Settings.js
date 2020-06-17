74651bb509e2b2f45890d7710f7458329a187d57
yeshwanthsnapwiz <62276813+yeshwanthsnapwiz@users.noreply.github.com>
2020-06-17 16:50:46 +0530

    fix(UI):placeholder rename [EV-15329] (#9611)

--- src/client/author/AssignTest/components/SimpleOptions/Settings.js
+++ src/client/author/AssignTest/components/SimpleOptions/Settings.js
@@ -351,7 +351,7 @@ const Settings = ({
                   size="large"
                   value={sebPassword}
                   type={showPassword ? "text" : "password"}
-                  placeholder="Password"
+                  placeholder="Quit Password"
                 />
               )}
             </Col>

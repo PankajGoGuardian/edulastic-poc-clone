cc8e0a1ed4c15571437afd2b9ce7965ea1ec1aab
sanjayupasi <57387252+sanjayupasi@users.noreply.github.com>
2020-06-26 19:22:16 +0530

    test(poc): removed import functions and refactored all files [EV-15465] (#9825)

--- cypress/e2e/suites/author/regrade/regression/editedItemRegradeUpdateAns.spec.js
+++ cypress/e2e/suites/author/regrade/regression/editedItemRegradeUpdateAns.spec.js
@@ -5,14 +5,15 @@ import FileHelper from "../../../../framework/util/fileHelper";
 import { regradeOptions, studentSide, releaseGradeTypes } from "../../../../framework/constants/assignmentStatus";
 import { attemptTypes } from "../../../../framework/constants/questionTypes";
 import MCQMultiplePage from "../../../../framework/author/itemList/questionType/mcq/mcqMultiplePage";
-import {
-  duplicateAndAssignTests,
-  verifyTeacherSide
-} from "../../../../framework/author/tests/regrade/editedItemRegradeCommonFunctions";
-import {
-  studentAttemptBasedOnStatus,
-  verifyStudentSide
-} from "../../../../framework/author/tests/regrade/editedItemRegradeCommonFunctions";
+import BarGraph from "../../../../framework/author/assignments/barGraphs";
+import ExpressGraderPage from "../../../../framework/author/assignments/expressGraderPage";
+import LiveClassboardPage from "../../../../framework/author/assignments/LiveClassboardPage";
+import AuthorAssignmentPage from "../../../../framework/author/assignments/AuthorAssignmentPage";
+import AssignmentsPage from "../../../../framework/student/assignmentsPage";
+import StudentTestPage from "../../../../framework/student/studentTestPage";
+import ReportsPage from "../../../../framework/student/reportsPage";
+import CypressHelper from "../../../../framework/util/cypressHelpers";
+import PreviewItemPopup from "../../../../framework/author/itemList/itemPreview";
 
 const { MCQ_MULTI } = require("./../../../../../fixtures/questionAuthoring");
 const { _ } = Cypress;
@@ -21,6 +22,14 @@ describe(`${FileHelper.getSpecName(Cypress.spec.name)}> test editing with applyi
   const testLibraryPage = new TestLibrary();
   const mcqMultiplePage = new MCQMultiplePage();
   const regrade = new Regrade();
+  const barGraphs = new BarGraph();
+  const itemPreview = new PreviewItemPopup();
+  const expressGrader = new ExpressGraderPage();
+  const lcb = new LiveClassboardPage();
+  const authorAssignmentPage = new AuthorAssignmentPage();
+  const assignmentsPage = new AssignmentsPage();
+  const studentTestPage = new StudentTestPage();
+  const reportsPage = new ReportsPage();
 
   const Teacher = {
     username: "teacher.regrade.edited.updateans@snapwiz.com",
@@ -91,29 +100,66 @@ describe(`${FileHelper.getSpecName(Cypress.spec.name)}> test editing with applyi
   const testname = "REGRADE_EDITED_ITEM";
   const oldCorrectAns = MCQ_MULTI["5"].setAns.correct;
   const newCorrectAns = MCQ_MULTI["6"].setAns.correct;
-  const attemptData = MCQ_MULTI["5"].attemptData;
-  const updatedAttempData = MCQ_MULTI["6"].attemptData;
+  const attemptsData = [];
+  const queCentric = {};
+
+  let attemptData = MCQ_MULTI["5"].attemptData;
+  let updatedAttempData = MCQ_MULTI["6"].attemptData;
   let itemId;
   let testid;
+  let updatedAttempt;
 
+  before("create tests", () => {
+    cy.deleteAllAssignments("", Teacher.username);
+    cy.login("teacher", Teacher.username, Teacher.password);
+
+    testLibraryPage.createTest(testname, false).then(id => {
+      [itemId] = testLibraryPage.items;
+      testid = id;
+      testLibraryPage.header.clickOnSettings();
+      testLibraryPage.testSettings.setRealeasePolicy(releaseGradeTypes.WITH_RESPONSE);
+      testLibraryPage.header.clickOnPublishButton();
+    });
+  });
   describe(`> regrade 'edited item' by 'updating correct ans at item level'`, () => {
-    before("create tests", () => {
-      cy.deleteAllAssignments("", Teacher.username);
-      cy.login("teacher", Teacher.username, Teacher.password);
+    before("> duplicate, assign and attempt the test", () => {
+      regrade_Options.forEach(() => {
+        assignedtestids.push([]);
+        versionedtestids.push([]);
+      });
 
-      testLibraryPage.createTest(testname, false).then(id => {
-        [itemId] = testLibraryPage.items;
-        testid = id;
-        testLibraryPage.header.clickOnSettings();
-        testLibraryPage.testSettings.setRealeasePolicy(releaseGradeTypes.WITH_RESPONSE);
-        testLibraryPage.header.clickOnPublishButton();
-
-        duplicateAndAssignTests({ regrade_Options, testid, assignedtestids, versionedtestids, attemptType });
+      regrade_Options.forEach((regOption, ind) => {
+        attemptType.forEach(attType => {
+          testLibraryPage.searchAndClickTestCardById(testid);
+          testLibraryPage.clickOnDuplicate();
+          testLibraryPage.testSummary.setName(`${regOption}-${attType}`);
+          testLibraryPage.header.clickOnPublishButton().then(id => {
+            assignedtestids[ind].push(id);
+            testLibraryPage.clickOnAssign();
+            testLibraryPage.assignPage.selectClass("Class");
+            testLibraryPage.assignPage.clickOnAssign();
+          });
+        });
       });
     });
+    before("> duplicate, assign and attempt the test", () => {
+      assignmentStatus.forEach((status, ind) => {
+        if (status !== studentSide.NOT_STARTED) {
+          cy.login("student", students[`Student${ind + 1}`].email, students[`Student${ind + 1}`].pass);
+          assignedtestids.forEach(testset => {
+            testset.forEach((id, ind) => {
+              assignmentsPage.sidebar.clickOnAssignment();
+              assignmentsPage.clickOnAssigmentByTestId(id);
 
-    before("> attempt by students based on status", () => {
-      studentAttemptBasedOnStatus({ assignedtestids, testid, attemptType, assignmentStatus, students, attemptData });
+              if (status === studentSide.SUBMITTED) {
+                studentTestPage.attemptQuestion("MCQ_MULTI", attemptType[ind], attemptData);
+                studentTestPage.clickOnNext(false, attemptType[ind] === attemptTypes.SKIP);
+                studentTestPage.submitTest();
+              } else studentTestPage.clickOnExitTest();
+            });
+          });
+        }
+      });
     });
 
     regrade_Options.forEach((option, optionsIndex) => {
@@ -135,6 +181,7 @@ describe(`${FileHelper.getSpecName(Cypress.spec.name)}> test editing with applyi
             testLibraryPage.review.previewItemPopUp.clickEditOnPreview();
             mcqMultiplePage.setCorrectAns(oldCorrectAns);
             mcqMultiplePage.setCorrectAns(newCorrectAns);
+            attemptData = updatedAttempData;
             mcqMultiplePage.header.saveAndgetId(true).then(itemversion => {
               cy.saveItemDetailToDelete(itemversion);
             });
@@ -146,14 +193,38 @@ describe(`${FileHelper.getSpecName(Cypress.spec.name)}> test editing with applyi
         });
 
         context("> verify student side", () => {
-          verifyStudentSide({
-            data,
-            assignmentStatus,
-            attemptType,
-            students,
-            versionedtestids,
-            option,
-            attemptData: updatedAttempData
+          assignmentStatus.forEach((status, statusIndex) => {
+            context(`> for Student${statusIndex + 1},'${status}'`, () => {
+              before(">login", () => {
+                optionsIndex = _.values(regradeOptions.edited).indexOf(option);
+                cy.login(
+                  "student",
+                  students[`Student${statusIndex + 1}`].email,
+                  students[`Student${statusIndex + 1}`].pass
+                );
+              });
+              attemptType.forEach((attempt, attempIndex) => {
+                const title = status === studentSide.SUBMITTED ? "" : "attempt and ";
+                it(`> ${title}verify edited question-'${attempt}'`, () => {
+                  if (status !== studentSide.SUBMITTED) {
+                    assignmentsPage.sidebar.clickOnAssignment();
+                    assignmentsPage.clickOnAssigmentByTestId(versionedtestids[optionsIndex][attempIndex]);
+                    studentTestPage.attemptQuestion("MCQ_MULTI", attempt, attemptData);
+                    studentTestPage.clickOnNext(false, attempt === attemptTypes.SKIP);
+                    studentTestPage.submitTest();
+                  } else assignmentsPage.sidebar.clickOnGrades();
+
+                  reportsPage.verifyPercentageOnTestCardByTestId(
+                    versionedtestids[optionsIndex][attempIndex],
+                    _.round((data.student[status][`${option}`][`${attempt}`] / data.points) * 100, 2)
+                  );
+
+                  reportsPage.clickOnReviewButtonButtonByTestId(versionedtestids[optionsIndex][attempIndex]);
+                  reportsPage.verifyAchievedScoreOfQueByIndex(0, data.student[status][`${option}`][`${attempt}`]);
+                  reportsPage.verifyMaxScoreOfQueByIndex(0, data.points);
+                });
+              });
+            });
           });
         });
 
@@ -161,15 +232,157 @@ describe(`${FileHelper.getSpecName(Cypress.spec.name)}> test editing with applyi
           before("> login and go to assignments", () => {
             cy.login("teacher", Teacher.username, Teacher.password);
           });
-          verifyTeacherSide({
-            data,
-            assignmentStatus,
-            attemptType,
-            students,
-            versionedtestids,
-            option,
-            attemptData: updatedAttempData,
-            updatedCorrectAns: true
+          assignmentStatus.forEach((sta, ind) => {
+            attemptsData.push({
+              stuName: students[`Student${ind + 1}`].name,
+              attempt: {},
+              status: studentSide.SUBMITTED
+            });
+          });
+          assignmentStatus.forEach((status, statusIndex) => {
+            context(`> for Student${statusIndex + 1},'${status}'`, () => {
+              attemptType.forEach((attempt, attemptIndex) => {
+                const title = status === studentSide.SUBMITTED ? "before" : "after";
+                context(`> verify edited question-'attempted as ${attempt}' ${title} regrade`, () => {
+                  before("> set attempt data", () => {
+                    assignmentStatus.forEach((sta, ind) => {
+                      attemptsData[ind].attempt.Q1 = attempt;
+                    });
+                    if (
+                      assignmentStatus.indexOf(studentSide.SUBMITTED) !== -1 &&
+                      option === regradeOptions.edited.MANUAL_POINTS
+                    ) {
+                      if (status === studentSide.SUBMITTED)
+                        attemptsData[assignmentStatus.indexOf(studentSide.SUBMITTED)].attempt.Q1 =
+                          attemptTypes.MANUAL_GRADE;
+                      else
+                        attemptsData[assignmentStatus.indexOf(studentSide.SUBMITTED)].attempt.Q1 =
+                          attemptTypes.PARTIAL_CORRECT;
+                    }
+                    updatedAttempt = attempt;
+
+                    if (status == studentSide.SUBMITTED) {
+                      if (attempt !== attemptTypes.PARTIAL_CORRECT && attempt !== attemptTypes.SKIP)
+                        updatedAttempt = attempt === attemptTypes.RIGHT ? attemptTypes.WRONG : attemptTypes.RIGHT;
+                    }
+                    optionsIndex = _.values(regradeOptions.edited).indexOf(option);
+                    testLibraryPage.sidebar.clickOnAssignment();
+                    authorAssignmentPage.clickOnLCBbyTestId(versionedtestids[optionsIndex][attemptIndex]);
+                  });
+
+                  it(`> verif lcb card view-`, () => {
+                    cy.wait(3000);
+                    lcb
+                      .getStudentScoreByIndex(statusIndex)
+                      .should("contain.text", `${data.teacher[status][`${option}`][`${attempt}`]} / ${data.points}`);
+
+                    lcb
+                      .getQuestionsByIndex(0)
+                      .find("div")
+                      .should("have.length", 1);
+
+                    barGraphs.verifyQueBarAndToolTipBasedOnAttemptData(attemptsData, ["Q1"]);
+
+                    lcb.verifyQuestionCards(statusIndex, [
+                      option === regradeOptions.edited.MANUAL_POINTS && status === studentSide.SUBMITTED
+                        ? attemptTypes.MANUAL_GRADE
+                        : attempt
+                    ]);
+                  });
+
+                  it("> verify student centric view", () => {
+                    lcb.clickOnStudentsTab();
+                    cy.wait(3000);
+                    lcb.questionResponsePage.selectStudent(students[`Student${statusIndex + 1}`].name);
+
+                    lcb.questionResponsePage
+                      .getTotalScore()
+                      .should("have.text", `${data.teacher[status][`${option}`][`${attempt}`]}`);
+
+                    lcb.questionResponsePage.getMaxScore().should("have.text", `${data.points}`);
+
+                    itemPreview.verifyQuestionResponseCard("MCQ_MULTI", attemptData, updatedAttempt);
+                    barGraphs.verifyQueBarAndToolTipBasedOnAttemptData(attemptsData[statusIndex], ["Q1"]);
+                  });
+
+                  it("> verify Question centric view", () => {
+                    lcb.clickonQuestionsTab();
+                    cy.wait(3000);
+                    lcb.questionResponsePage.getDropDown().click({ force: true });
+
+                    CypressHelper.getDropDownList().then(questions => {
+                      expect(questions).to.have.lengthOf(1);
+                    });
+
+                    lcb.questionResponsePage
+                      .getQuestionContainerByStudent(students[`Student${statusIndex + 1}`].name)
+                      .as("updatecard");
+
+                    itemPreview.verifyQuestionResponseCard(
+                      "MCQ_MULTI",
+                      attemptData,
+                      updatedAttempt,
+                      false,
+                      statusIndex
+                    );
+                    lcb.getQuestionCentricData(attemptsData, queCentric);
+                    barGraphs.verifyQueBarBasedOnQueAttemptData(queCentric.Q1);
+
+                    lcb.questionResponsePage
+                      .getScoreInput(cy.get("@updatecard"))
+                      .should("have.value", `${data.student[status][`${option}`][`${attempt}`]}`);
+                  });
+
+                  it("> verify express grader view", () => {
+                    lcb.header.clickOnExpressGraderTab();
+
+                    expressGrader.getGridRowByStudent(students[`Student${statusIndex + 1}`].name);
+                    expressGrader.verifyScoreAndPerformance(
+                      `${data.teacher[status][`${option}`][`${attempt}`]}/${data.points}`,
+                      _.round((data.teacher[status][`${option}`][`${attempt}`] / data.points) * 100, 2)
+                    );
+                    expressGrader.verifyNumberOfQuestions(1);
+                  });
+
+                  if (option === regradeOptions.edited.MANUAL_POINTS && status === studentSide.SUBMITTED) {
+                    context("> update and verify points in question centric view", () => {
+                      it("> upadate score and verify in question centric view", () => {
+                        lcb.header.clickOnLCBTab();
+                        lcb.clickonQuestionsTab();
+
+                        lcb.questionResponsePage.updateScoreAndFeedbackForStudent(
+                          students[`Student${statusIndex + 1}`].name,
+                          data.manualpoints
+                        );
+                        attemptsData[statusIndex].attempt.Q1 = attemptTypes.PARTIAL_CORRECT;
+                        cy.wait(3000);
+                        lcb.getQuestionCentricData(attemptsData, queCentric);
+                        barGraphs.verifyQueBarBasedOnQueAttemptData(queCentric.Q1);
+                      });
+
+                      it("> verify student centric view", () => {
+                        lcb.clickOnStudentsTab();
+                        cy.wait(3000);
+                        lcb.questionResponsePage.selectStudent(students[`Student${statusIndex + 1}`].name);
+                        lcb.questionResponsePage.getTotalScore().should("have.text", data.manualpoints.toString());
+
+                        barGraphs.verifyQueBarAndToolTipBasedOnAttemptData(attemptsData[statusIndex], ["Q1"]);
+                      });
+
+                      it(`> verify lcb card view-`, () => {
+                        lcb.clickOnCardViewTab();
+                        cy.wait(3000);
+                        lcb
+                          .getStudentScoreByIndex(statusIndex)
+                          .should("contain.text", `${data.manualpoints} / ${data.points}`);
+                        lcb.verifyQuestionCards(statusIndex, [attemptTypes.PARTIAL_CORRECT]);
+                        barGraphs.verifyQueBarAndToolTipBasedOnAttemptData(attemptsData, ["Q1"]);
+                      });
+                    });
+                  }
+                });
+              });
+            });
           });
         });
       });

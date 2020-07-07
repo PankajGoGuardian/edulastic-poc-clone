4a6a6684bb9178f69ed30bab4497f8be4ea5c905
Prithvi Raju M <prithvi.raju@snapwiz.com>
2020-07-07 17:36:27 +0530

    Chore/cherry pick changes (#10122)
    * refactor(evaluator): decoupling mcq evaluator from main evaluator (#9990)
    
    * refactor(evaluator): rewriting mcq evaluator
    
    * test(evaluator): adding tests for mcq evaluator
    
    * refactor(evaluator): adding round down functionality
    
    * build(evaluator): adding lib file for mcq
    
    * fix(poc): handling altResponses not being present (#10053)
    
    * fix(evaluator): fixing round down logic for mcq [EV-16058] (#10065)
    
    * fix(evaluator): fixing round down logic for mcq [EV-16058]
    
    * chore(evaluator): adding lib file [EV-16058]
    
    Co-authored-by: mgourav-snapwiz <50568503+mgourav-snapwiz@users.noreply.github.com>

--- packages/evaluators/test/mcq.js
+++ packages/evaluators/test/mcq.js
@@ -1,70 +1,84 @@
-import test from 'ava';
-import { multipleChoice as mcqEvaluator } from '../src/index';
+import test from "ava";
+import { multipleChoice as mcqEvaluator } from "../src/index";
 
-import { emObj1, emObj2, emObj3, pmObj1, pmObj2, pmObj3, attObj1, attObj2 } from './data/mcq';
+import { emObj1, emObj2, emObj3, emObj4, emObj5, pmObj1, pmObj2, pmObj3, pmObj4, pmObj5 } from "./data/mcq";
 
-test('#MCQ:exactMatch', async (t) => {
+test("#MCQ:exactMatch no user answer", async t => {
   // case 1
   const result = mcqEvaluator(emObj1);
-  t.is(result.score, 1, 'incorrect score');
-  t.is(result.maxScore, 1, 'incorrect maxScore');
-  t.deepEqual(result.evaluation, { 0: true, 1: true }, 'incorrect evaluation');
+  t.is(result.maxScore, 1, "incorrect maxScore");
+  t.is(result.score, 0, "incorrect score");
+  t.deepEqual(result.evaluation, {}, "incorrect evaluation");
+});
 
+test("#MCQ:exactMatch correct with alternate answer", async t => {
   // case 2
   const result2 = mcqEvaluator(emObj2);
-  t.is(result2.maxScore, 3, 'incorrect maxScore');
-  t.is(result2.score, 0, 'incorrect score');
-  t.deepEqual(result2.evaluation, { 1: false }, 'incorrect evaluation');
+  t.is(result2.maxScore, 3, "incorrect maxScore");
+  t.is(result2.score, 3, "incorrect score");
+  t.deepEqual(result2.evaluation, { 0: true }, "incorrect evaluation");
+});
 
+test("#MCQ:exactMatch partially correct with correct answer", async t => {
   // case 3
   const result3 = mcqEvaluator(emObj3);
-  t.is(result3.maxScore, 3, 'incorrect maxScore');
-  t.is(result3.score, 0, 'incorrect score');
-  t.deepEqual(
-    result3.evaluation,
-    { 1: false, 0: true },
-    'incorrect evaluation'
-  );
+  t.is(result3.maxScore, 2, "incorrect maxScore");
+  t.is(result3.score, 0, "incorrect score");
+  t.deepEqual(result3.evaluation, { 0: true }, "incorrect evaluation");
 });
 
-test('#MCQ:partialMatch', async (t) => {
-  // case 1
-  const result = mcqEvaluator(pmObj1);
-  t.is(result.score, 1, 'incorrect score');
-  t.is(result.maxScore, 1, 'incorrect maxscore');
-  t.deepEqual(
-    result.evaluation,
-    {
-      0: true,
-      1: true
-    },
-    'incorrect evaluation'
-  );
-
-  // case 2
-  const result1 = mcqEvaluator(pmObj2);
-  t.is(result1.score, 1, 'incorrect score');
-  t.is(result1.maxScore, 2, 'incorrect maxScore');
-  t.deepEqual(
-    result1.evaluation,
-    {
-      0: false,
-      1: true
-    },
-    'incorrect evaluation'
-  );
-
-  // case 3
-  const result2 = mcqEvaluator(pmObj3);
-  t.is(result2.score, 1.5, 'incorrect score');
-  t.is(result2.maxScore, 3, 'incorrect maxscore');
+test("#MCQ:exactMatch all correct with correct answer", async t => {
+  // case 4
+  const result4 = mcqEvaluator(emObj4);
+  t.is(result4.maxScore, 2, "incorrect maxScore");
+  t.is(result4.score, 2, "incorrect score");
+  t.deepEqual(result4.evaluation, { 0: true }, "incorrect evaluation");
 });
 
+test("#MCQ:exactMatch incorrect answer", async t => {
+  // case 5
+  const result5 = mcqEvaluator(emObj5);
+  t.is(result5.maxScore, 2, "incorrect maxScore");
+  t.is(result5.score, 0, "incorrect score");
+  t.deepEqual(result5.evaluation, { 0: false }, "incorrect evaluation");
+});
 
-test('#MCQ:attemptScore', async (t) => {
-  const result = mcqEvaluator(attObj1);
-  t.is(result.score, 4, 'incorrect score');
+test("#MCQ:partialMatch no answer", async t => {
+  // case 6
+  const result6 = mcqEvaluator(pmObj1);
+  t.is(result6.maxScore, 1, "incorrect maxScore");
+  t.is(result6.score, 0, "incorrect score");
+  t.deepEqual(result6.evaluation, {}, "incorrect evaluation");
+});
 
-  const result1 = mcqEvaluator(attObj2);
-  t.is(result1.score, 0, 'incorrect score');
+test("#MCQ:partialMatch partially correct answer no penalty", async t => {
+  // case 7
+  const result7 = mcqEvaluator(pmObj2);
+  t.is(result7.maxScore, 1, "incorrect maxScore");
+  t.is(result7.score, 0.5, "incorrect score");
+  t.deepEqual(result7.evaluation, { 0: true }, "incorrect evaluation");
+});
+
+test("#MCQ:partialMatch full match with correct answer", async t => {
+  // case 8
+  const result8 = mcqEvaluator(pmObj3);
+  t.is(result8.maxScore, 1, "incorrect maxScore");
+  t.is(result8.score, 1, "incorrect score");
+  t.deepEqual(result8.evaluation, { 0: true, 1: true }, "incorrect evaluation");
+});
+
+test("#MCQ:partialMatch full match with alternate answer", async t => {
+  // case 9
+  const result9 = mcqEvaluator(pmObj4);
+  t.is(result9.maxScore, 1, "incorrect maxScore");
+  t.is(result9.score, 1, "incorrect score");
+  t.deepEqual(result9.evaluation, { 0: true, 1: true }, "incorrect evaluation");
+});
+
+test("#MCQ:partialMatch partial match with correct answer with penalty", async t => {
+  // case 10
+  const result10 = mcqEvaluator(pmObj5);
+  t.is(result10.maxScore, 1, "incorrect maxScore");
+  t.is(result10.score, 0, "incorrect score");
+  t.deepEqual(result10.evaluation, { 0: true, 1: false }, "incorrect evaluation");
 });
--- src/client/author/src/utils/evaluators/index.js
+++ src/client/author/src/utils/evaluators/index.js
@@ -28,17 +28,17 @@ const mathEvaluate = async (data, type) => {
   // getting evaluation from backend (EV-7432)
   if (type === questionType.GRAPH) {
     const validationObj = data?.validation;
-    const correctAnswers = [
-      validationObj?.validResponse?.value,
-      ...validationObj?.altResponses?.map(i => i.value)
-    ].filter(i => i.length);
-    if (correctAnswers.length === 0) {
+    const correctAnswers = validationObj?.validResponse?.value || [];
+    const alternateAnswers = (validationObj?.altResponses || []).map(altResponse => altResponse.value);
+    const allAnswers = [correctAnswers, ...alternateAnswers].filter(i => i.length);
+    if (allAnswers.length === 0) {
       const error = new Error();
       error.message = "Questions should have answers set";
       throw error;
     }
   }
-  return await evaluateApi.evaluate(data, type);
+  const result = await evaluateApi.evaluate(data, type);
+  return result;
 };
 
 // clozeDropDown and ClozeText shares same logic

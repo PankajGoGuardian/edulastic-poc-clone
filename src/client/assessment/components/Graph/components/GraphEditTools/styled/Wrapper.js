33fc0bb4f9f7418d9f9074b627cb30419a7dff24
Aditya Vyas <61740321+aditya-snapwiz@users.noreply.github.com>
2020-07-14 13:18:54 +0530

    fix(client): remove extra margin if given from display (#10266)

--- src/client/assessment/components/Graph/components/GraphEditTools/styled/Wrapper.js
+++ src/client/assessment/components/Graph/components/GraphEditTools/styled/Wrapper.js
@@ -3,9 +3,9 @@ import styled from "styled-components";
 const padding = 19;
 
 export const Wrapper = styled.div`
-  top: ${props => `${props.margin.top + padding}px`};
+  top: ${props => `${Number(props.margin.top) + padding}px`};
   left: ${({ side, margin, width }) =>
-    `${side === "left" ? margin.left + padding : margin.left + width - 40 - padding}px`};
+    `${side === "left" ? Number(margin.left) + padding : Number(margin.left) + Number(width) - 40 - padding}px`};
   display: flex;
   flex-direction: column;
   position: absolute;

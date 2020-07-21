caba13a6ef2915e44082103bf3a2618249335520
Arun Sasidharan <43816559+arun-sasidharan@users.noreply.github.com>
2020-07-21 13:01:49 +0530

    feat(ui): for multiple item approval show confirmation popup [EV-10861] (#10422)
    * feat(ui): for multiple item approval show confirmation popup [EV-10861]
    
    * fix(ui): move state to redux for handling async close [EV-10861]

---
+++ src/client/author/ItemList/components/ApproveConfirmModal/index.js
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import styled from "styled-components";
+import { EduButton } from "@edulastic/common";
+import { lightGrey9 } from "@edulastic/colors";
+import { Table, Tooltip } from "antd";
+import { IconInfo } from "@edulastic/icons";
+import { ConfirmationModal } from "../../../src/components/common/ConfirmationModal";
+import { itemsDataTableSelector, approveOrRejectMultipleItem as approveOrRejectMultipleItemAction } from "../../ducks";
+import {
+  setApproveConfirmationOpenAction,
+  getShowApproveConfirmationSelector
+} from "../../../TestPage/components/AddItems/ducks";
+
+const padd = num => {
+  if (num < 10) {
+    return `0${num}`;
+  }
+  return num;
+};
+const ApproveConfirmModal = ({
+  showApproveConfirmation,
+  itemsDataTable,
+  approveOrRejectMultipleItem,
+  setApproveConfirmationOpen
+}) => {
+  const [isApproving, setIsApproving] = useState(false);
+  useEffect(() => {
+    setIsApproving(false);
+  }, [showApproveConfirmation]);
+
+  const columns = [
+    {
+      title: "Collection",
+      dataIndex: "name",
+      key: "collection",
+      render: (item, row) => {
+        if (item === "No Collection") {
+          return (
+            <div>
+              <span>{item}</span>
+              <Tooltip
+                title={`${row.count} ${
+                  row.count > 1 ? `Items` : `Item`
+                } will not be approved. Please select a collection to get approved`}
+              >
+                <IconInfo color={lightGrey9} style={{ cursor: "pointer", marginLeft: "10px" }} />
+              </Tooltip>
+            </div>
+          );
+        }
+        return item;
+      }
+    },
+    {
+      title: "Items",
+      dataIndex: "count",
+      key: "items",
+      render: count => `${padd(count)} ${count > 1 ? `Items` : `Item`}`
+    }
+  ];
+
+  const handleApprove = () => {
+    approveOrRejectMultipleItem({ status: "published" });
+    setIsApproving(true);
+  };
+
+  const handleCancel = () => {
+    setApproveConfirmationOpen(false);
+  };
+
+  return (
+    <StyledModal
+      title="Approve Items"
+      visible={showApproveConfirmation}
+      onCancel={handleCancel}
+      maskClosable={false}
+      centered
+      footer={[
+        <EduButton isGhost key="cancel" onClick={handleCancel}>
+          CANCEL
+        </EduButton>,
+        <EduButton data-cy="approve" key="submit" onClick={handleApprove} loading={isApproving}>
+          APPROVE
+        </EduButton>
+      ]}
+      destroyOnClose
+    >
+      <BodyStyled>
+        <Table pagination={false} dataSource={itemsDataTable} columns={columns} />;
+      </BodyStyled>
+    </StyledModal>
+  );
+};
+
+export default connect(
+  state => ({
+    itemsDataTable: itemsDataTableSelector(state),
+    showApproveConfirmation: getShowApproveConfirmationSelector(state)
+  }),
+  {
+    approveOrRejectMultipleItem: approveOrRejectMultipleItemAction,
+    setApproveConfirmationOpen: setApproveConfirmationOpenAction
+  }
+)(ApproveConfirmModal);
+
+const StyledModal = styled(ConfirmationModal)`
+  .ant-modal-content {
+    .ant-modal-body {
+      background: transparent;
+      box-shadow: none;
+      padding: 0;
+      min-height: 80px;
+    }
+  }
+`;
+
+const BodyStyled = styled.div`
+  text-align: left;
+  width: 100%;
+`;

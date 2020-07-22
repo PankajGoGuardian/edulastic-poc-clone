c2ae5c6616619aecd99b4bb39c95b9f7d4f6b8c7
sukanta-snapwiz <59563328+sukanta-snapwiz@users.noreply.github.com>
2020-07-22 15:56:46 +0530

    fix: failed to load demo player using public link [EV-16795] (#10473)

--- src/client/author/TestDemoPlayer.js
+++ src/client/author/TestDemoPlayer.js
@@ -3,14 +3,15 @@ import { withRouter } from "react-router";
 import PropTypes from "prop-types";
 import AssessmentPlayer from "../assessment";
 
-const DemoPlayer = ({ match }) => {
+const DemoPlayer = ({ match, isPublic }) => {
   const { id: testId } = match.params;
 
-  return <AssessmentPlayer testId={testId} preview demo />;
+  return <AssessmentPlayer testId={testId} preview demo sharedType={isPublic && "PUBLIC"} />;
 };
 
 DemoPlayer.propTypes = {
-  match: PropTypes.object.isRequired
+  match: PropTypes.object.isRequired,
+  isPublic: PropTypes.bool
 };
 
 export default withRouter(DemoPlayer);

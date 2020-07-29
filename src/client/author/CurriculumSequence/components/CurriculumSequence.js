105e4069885501559c028b8a8809f5d19e4288bd
sukanta-snapwiz <59563328+sukanta-snapwiz@users.noreply.github.com>
2020-07-29 18:07:31 +0530

    fix: show add modules button on customize enabled [EV-16600] (#10622)

--- src/client/author/CurriculumSequence/components/CurriculumSequence.js
+++ src/client/author/CurriculumSequence/components/CurriculumSequence.js
@@ -190,7 +190,7 @@ class CurriculumSequence extends Component {
         res.groupIds.push(ele.groupId);
         return res;
       });
-      if (!classId && mappedStudentPlaylists[playlistId] ?.groupIds.includes(classId)) {
+      if (!classId && mappedStudentPlaylists[playlistId]?.groupIds.includes(classId)) {
         mappedStudentPlaylists[playlistId].groupId = classId;
       }
     });
@@ -355,7 +355,7 @@ class CurriculumSequence extends Component {
 
   handleNavChange = value => () => {
     const { history, match } = this.props;
-    const url = `/author/playlists/${value}/${match ?.params ?.id}/use-this`;
+    const url = `/author/playlists/${value}/${match?.params?.id}/use-this`;
     // this.handleSave();
     history.push(url);
   };
@@ -373,7 +373,7 @@ class CurriculumSequence extends Component {
       checkPreviouslyCustomized
     } = this.props;
     const { authors } = destinationCurriculumSequence;
-    const canEdit = authors ?.find(x => x._id === currentUserId) || role === roleuser.EDULASTIC_CURATOR;
+    const canEdit = authors?.find(x => x._id === currentUserId) || role === roleuser.EDULASTIC_CURATOR;
 
     const isManageContentActive = activeRightPanel === "manageContent";
     setShowRightPanel(true);
@@ -416,7 +416,7 @@ class CurriculumSequence extends Component {
     try {
       const signedRequest = await curriculumSequencesApi.getSignedRequest({
         playlistId,
-        moduleId: module ?._id,
+        moduleId: module?._id,
         contentId,
         resource
       });
@@ -460,10 +460,10 @@ class CurriculumSequence extends Component {
   }
 
   checkWritePermission = () => {
-    const { destinationCurriculumSequence, currentUserId } = this.props;
+    const { destinationCurriculumSequence, currentUserId, customizeInDraft } = this.props;
     // Playlist is being authored - editFlow
-    if (!destinationCurriculumSequence.authors) return true;
-    return !!destinationCurriculumSequence.authors ?.find(x => x ?._id === currentUserId);
+    if (!destinationCurriculumSequence.authors || customizeInDraft) return true;
+    return !!destinationCurriculumSequence.authors?.find(x => x?._id === currentUserId);
   };
 
   // Duplicate the customised playlist
@@ -557,7 +557,7 @@ class CurriculumSequence extends Component {
     const isNotStudentOrParent = !(role === "student" || role === "parent");
 
     // figure out which tab contents to render || just render default playlist
-    const currentTab = match ?.params ?.currentTab || "playlist";
+    const currentTab = match?.params?.currentTab || "playlist";
 
     // get active classes for student playlists
     const playlistClassList = [...new Set(studentPlaylists.map(playlist => playlist.groupId))];
@@ -569,9 +569,9 @@ class CurriculumSequence extends Component {
     // Options for add unit
     const options1 = destinationCurriculumSequence.modules
       ? destinationCurriculumSequence.modules.map(module => ({
-        value: module.id,
-        label: module.name
-      }))
+          value: module.id,
+          label: module.name
+        }))
       : [];
 
     // TODO: change options2 to something more meaningful
@@ -579,11 +579,11 @@ class CurriculumSequence extends Component {
 
     const { status, customize = true, modules, collections: _playlistCollections = [] } = destinationCurriculumSequence;
     const sparkCollection = collections.find(c => c.name === "Spark Math" && c.owner === "Edulastic Corp") || {};
-    const isSparkMathPlaylist = _playlistCollections.some(item => item._id === sparkCollection ?._id);
+    const isSparkMathPlaylist = _playlistCollections.some(item => item._id === sparkCollection?._id);
 
     const getplaylistMetrics = () => {
       const temp = {};
-      modules ?.forEach(({ _id: moduleId }) => {
+      modules?.forEach(({ _id: moduleId }) => {
         temp[moduleId] = playlistMetricsList.filter(x => x.playlistModuleId === moduleId);
       });
       return temp;
@@ -596,28 +596,28 @@ class CurriculumSequence extends Component {
     // Module progress
     const modulesStatus = destinationCurriculumSequence.modules
       ? destinationCurriculumSequence.modules
-        .filter(m => {
-          if (m.data.length === 0) {
-            return false;
-          }
-          for (const test of m.data) {
-            if (!test.assignments || test.assignments.length === 0) {
+          .filter(m => {
+            if (m.data.length === 0) {
               return false;
             }
-            for (const assignment of test.assignments) {
-              if (!assignment.class || assignment.class.length === 0) {
+            for (const test of m.data) {
+              if (!test.assignments || test.assignments.length === 0) {
                 return false;
               }
-              for (const cs of assignment.class) {
-                if (cs.status !== "DONE") {
+              for (const assignment of test.assignments) {
+                if (!assignment.class || assignment.class.length === 0) {
                   return false;
+                }
+                for (const cs of assignment.class) {
+                  if (cs.status !== "DONE") {
+                    return false;
+                  }
                 }
               }
             }
-          }
-          return true;
-        })
-        .map(x => x._id)
+            return true;
+          })
+          .map(x => x._id)
       : [];
 
     const isAuthoringFlowReview = current === "review";
@@ -636,17 +636,15 @@ class CurriculumSequence extends Component {
     const isDesktop = windowWidth >= parseInt(smallDesktopWidth, 10);
     const isMobile = windowWidth <= parseInt(mobileWidthLarge, 10);
 
-    const isPlaylistDetailsPage = window.location ?.hash === "#review";
+    const isPlaylistDetailsPage = window.location?.hash === "#review";
     const showBreadCrumb = (currentTab === "playlist" || isPlaylistDetailsPage) && !urlHasUseThis;
     const canAllowDuplicate = allowDuplicateCheck(destinationCurriculumSequence.collections, collections, "playlist");
     const shouldHidCustomizeButton =
-      (isPlaylistDetailsPage || urlHasUseThis) &&
-      status === "published" &&
-      (!enableCustomize || !canAllowDuplicate);
+      (isPlaylistDetailsPage || urlHasUseThis) && status === "published" && (!enableCustomize || !canAllowDuplicate);
 
     const playlistsToSwitch = isStudent ? curatedStudentPlaylists : slicedRecentPlaylists;
     // should show useThis Notification only two times
-    const showUseThisNotification = location.state ?.fromUseThis && !loading && playlistsToSwitch ?.length <= 3;
+    const showUseThisNotification = location.state?.fromUseThis && !loading && playlistsToSwitch?.length <= 3;
 
     return (
       <>
@@ -828,7 +826,7 @@ const enhance = compose(
   connect(
     state => ({
       curriculumGuides: state.curriculumSequence.guides,
-      isManageModulesVisible: state.curriculumSequence ?.isManageModulesVisible,
+      isManageModulesVisible: state.curriculumSequence?.isManageModulesVisible,
       guide: state.curriculumSequence.selectedGuide,
       isContentExpanded: state.curriculumSequence.isContentExpanded,
       activeRightPanel: state.curriculumSequence.activeRightPanel,
@@ -842,15 +840,15 @@ const enhance = compose(
       isStudent: getUserRole(state) === "student",
       isTeacher: getUserRole(state) === "teacher",
       role: getUserRole(state),
-      playlistMetricsList: state ?.curriculumSequence ?.playlistMetrics,
-      studentPlaylists: state ?.studentPlaylist ?.playlists,
+      playlistMetricsList: state?.curriculumSequence?.playlistMetrics,
+      studentPlaylists: state?.studentPlaylist?.playlists,
       classId: getCurrentGroup(state),
       activeClasses: getFilteredClassesSelector(state),
       dateKeys: getDateKeysSelector(state),
-      currentUserId: state ?.user ?.user ?._id,
-      isVideoResourcePreviewModal: state.curriculumSequence ?.isVideoResourcePreviewModal,
-      showRightPanel: state.curriculumSequence ?.showRightPanel,
-      customizeInDraft: state.curriculumSequence ?.customizeInDraft
+      currentUserId: state?.user?.user?._id,
+      isVideoResourcePreviewModal: state.curriculumSequence?.isVideoResourcePreviewModal,
+      showRightPanel: state.curriculumSequence?.showRightPanel,
+      customizeInDraft: state.curriculumSequence?.customizeInDraft
     }),
     {
       onGuideChange: changeGuideAction,
@@ -958,14 +956,14 @@ export const ContentContainer = styled.div`
   @media (max-width: ${smallDesktopWidth}) {
     width: ${({ showRightPanel }) => (showRightPanel ? "calc(100% - 240px)" : "100%")};
     height: ${({ showBreadCrumb, isDifferentiationTab }) => {
-    if (isDifferentiationTab) {
-      return "calc(100vh - 175px)";
-    }
-    if (showBreadCrumb) {
-      return "calc(100vh - 138px)";
-    }
-    return "calc(100vh - 102px)";
-  }};
+      if (isDifferentiationTab) {
+        return "calc(100vh - 175px)";
+      }
+      if (showBreadCrumb) {
+        return "calc(100vh - 138px)";
+      }
+      return "calc(100vh - 102px)";
+    }};
   }
 
   @media (max-width: ${desktopWidth}) {

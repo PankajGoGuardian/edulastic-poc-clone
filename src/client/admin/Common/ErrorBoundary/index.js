76e6d1e3828f514c425cb64b548fb190abbd15a3
Prithvi Raju M <prithvi.raju@snapwiz.com>
2020-08-05 14:18:30 +0530

    chore: enhance sentry logs captured. (#10765)
    ---

--- src/client/admin/Common/ErrorBoundary/index.js
+++ src/client/admin/Common/ErrorBoundary/index.js
@@ -1,21 +1,23 @@
 import React from "react";
-import * as Sentry from '@sentry/browser';
+import * as Sentry from "@sentry/browser";
 
 class ErrorBoundary extends React.Component {
   state = { hasError: false };
 
-  static getDerivedStateFromError(error) {
+  static getDerivedStateFromError() {
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
 
   componentDidCatch(error, info) {
     // log the error to an error reporting service
-    console.error(error, info);
-    Sentry.withScope((scope) => {
+    Sentry.captureException(error);
+    Sentry.configureScope(scope => {
       scope.setExtras(info);
-      Sentry.captureException(error);
+      scope.setTag("error.type", "UnexpectedError");
     });
+    // log the error to an error reporting service
+    console.error(error, info);
   }
 
   render() {

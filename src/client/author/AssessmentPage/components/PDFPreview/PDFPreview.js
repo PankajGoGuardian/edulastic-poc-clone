314fbfa02e38bd4e1a143144f041ade6d5ee793b
prajwal-snapwiz <53730619+prajwal-snapwiz@users.noreply.github.com>
2020-08-12 17:25:54 +0530

    feat(client): replace scratchpad with pdf annotation tool [EV-15062] (#9577) (#10942)
    * feat(client): replace scratchpad with pdf annotation tool [EV-15062]
    
    * feat(client): code refactor [EV-15062]
    
    * feat(client): pdf-annotate.js update [EV-15062]
    
    * refactor(client): enhancements for image/vide/comment tools [EV-15062]
    
    * fix(client): fetch blank pdf on blank page add [EV-15062]
    
    * refactor(client): code cleanup + semantics enhancement [EV-15062]
    
    * refactor(code): storeAdapter code minimize [EV-15062]
    
    * refactor(client): added comments to pdf-annotate build [EV-15062]
    
    * fix(client): style updates + fix edge cases [EV-15062]
    
    * fix(client): node selection issue [EV-15062]
    
    * fix(client): comment + video+ image tool enhancement [EV-15062]
    
    * fix(client): missing closing braces [EV-15062]
    
    * fix(8078): prevent response box override with style fixes [EV-15062]
    
    * fix(client): feedback changes [EV-15062]
    
    * chore(client): lint ignore commit [EV-15062]
    
    * fix(client): access store internally through imports [EV-15062]
    
    * chore(client): refs not pushed [EV-15062]
    
    Co-authored-by: Prajwal-Snapwiz <prajwal.venkatesh@gmail.com>
    
    Co-authored-by: Prajwal-Snapwiz <prajwal.venkatesh@gmail.com>

--- src/client/author/AssessmentPage/components/PDFPreview/PDFPreview.js
+++ src/client/author/AssessmentPage/components/PDFPreview/PDFPreview.js
@@ -13,7 +13,7 @@ import { PDFPreviewWrapper, Preview, ZoomControlCotainer, PDFZoomControl } from 
 import { removeUserAnswerAction } from "../../../../assessment/actions/answers";
 import PDFViewer from "../PDFViewer";
 
-const handleDrop = (page, cb, zoom = 1) => ({ question }, e) => {
+const handleDrop = (page, cb, annotationContainer, zoom = 1) => ({ question }, e) => {
   const {
     nativeEvent: { offsetX, offsetY }
   } = e;
@@ -22,13 +22,19 @@ const handleDrop = (page, cb, zoom = 1) => ({ question }, e) => {
   const x = zoom != 0 ? offsetX / zoom : offsetX;
   const y = zoom != 0 ? offsetY / zoom : offsetY;
 
-  cb({
-    x,
-    y,
-    page,
-    questionId: data.id,
-    qIndex: data.index
-  });
+  const { offsetWidth, offsetHeight } = annotationContainer.current;
+
+  cb(
+    {
+      x,
+      y,
+      page,
+      questionId: data.id,
+      qIndex: data.index
+    },
+    offsetWidth,
+    offsetHeight
+  );
 };
 
 const getNumberStyles = (x, y, scale) => ({
@@ -125,7 +131,7 @@ const PDFPreview = ({
       <PerfectScrollbar ref={forwardedRef}>
         <Droppable
           types={["question"]}
-          onDrop={handleDrop(currentPage, onDropAnnotation, pdfScale)}
+          onDrop={handleDrop(currentPage, onDropAnnotation, annotationContainer, pdfScale)}
           style={{ top: 0, display: "block", width: "fit-content", margin: "auto" }}
         >
           {page.URL === "blank" && <Preview onClick={handleRemoveHighlight} ref={previewContainer} />}
@@ -142,7 +148,7 @@ const PDFPreview = ({
             />
           )}
 
-          <AnnotationsContainer className="annotations-container">
+          <AnnotationsContainer className="annotations-container" zoom={pdfScale} ref={annotationContainer}>
             {annotations
               .filter(item => item.toolbarMode === "question" && item.page === currentPage)
               .map(({ uuid, qIndex, x, y, questionId }) => (

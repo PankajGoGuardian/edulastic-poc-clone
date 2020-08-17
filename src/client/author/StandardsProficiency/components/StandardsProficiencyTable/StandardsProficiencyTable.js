c19abe3d1c7c018baf158ef5174432395724ad4c
Arun Sasidharan <43816559+arun-sasidharan@users.noreply.github.com>
2020-08-17 13:48:47 +0530

    fix(client): update standard proficiency should not fail [EV-17612] (#11007) (#11012)

--- src/client/author/StandardsProficiency/components/StandardsProficiencyTable/StandardsProficiencyTable.js
+++ src/client/author/StandardsProficiency/components/StandardsProficiencyTable/StandardsProficiencyTable.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Form, Icon, Radio, Button, message, Row } from "antd";
-import { EduButton,notification, RadioBtn } from "@edulastic/common";
+import { EduButton, notification, RadioBtn } from "@edulastic/common";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { get } from "lodash";
@@ -29,7 +29,6 @@ import {
   RadioWrap
 } from "./styled";
 import { ScoreColorSpan } from "./StandardsProficiencyEditableCell/styled";
-
 
 import {
   updateStandardsProficiencyAction,
@@ -101,7 +100,7 @@ class StandardsProficiencyTable extends React.Component {
   handleAdd = () => {
     const { data, editingKey } = this.state;
     if (data.length >= 5) {
-      notification({ messageKey:"maximumFiveLevels"});
+      notification({ messageKey: "maximumFiveLevels" });
       return;
     }
 
@@ -133,7 +132,7 @@ class StandardsProficiencyTable extends React.Component {
   handleDelete = key => {
     const data = [...this.state.data];
     if (data.length <= 3) {
-       notification({ messageKey:"minimumThreeLevel"});
+      notification({ messageKey: "minimumThreeLevel" });
       return;
     }
     const newData = data.filter(item => item.key !== key);
@@ -180,8 +179,8 @@ class StandardsProficiencyTable extends React.Component {
       updateData.decay = calcDecayingAttr;
     } else if (calcType === "MOVING_AVERAGE") {
       const { calcMovingAvrAttr } = this.state;
-      updateData.calcAttribute = calcMovingAvrAttr;
-      updateData.noOfAssessments = calcMovingAvrAttr;
+      updateData.calcAttribute = calcMovingAvrAttr || this.props.noOfAssessments;
+      updateData.noOfAssessments = calcMovingAvrAttr || this.props.noOfAssessments;
     } else {
       updateData.calcAttribute = 0;
     }

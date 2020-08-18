e98f330de61ed828585a37f5bcc7bbf63b99b545
Himayan Debnath <63641486+himayan@users.noreply.github.com>
2020-08-18 18:37:07 +0530

    fix(ui): reset username field when popup is closed [EV-17640] (#11043)

--- src/client/author/src/components/common/ShareModal/ShareModal.js
+++ src/client/author/src/components/common/ShareModal/ShareModal.js
@@ -254,12 +254,21 @@ class ShareModal extends React.Component {
     return `${data.email && data.email !== "null" ? ` (${data.email})` : ""}`;
   };
 
+  handleClose = () => {
+    const { onClose } = this.props;
+    this.setState({
+      currentUser: {},
+      searchString: "",
+      showWarning: false
+    });
+    onClose();
+  };
+
   render() {
     const { sharedType, permission, _permissionKeys, searchString, showWarning } = this.state;
     const {
       shareLabel,
       isVisible,
-      onClose,
       userList = [],
       fetching,
       sharedUsersList,
@@ -290,7 +299,7 @@ class ShareModal extends React.Component {
     if (sharedType === "DISTRICT") sharedTypeMessage = `Anyone in ${districtName}`;
     else if (sharedType === "SCHOOL") sharedTypeMessage = `Anyone in ${schools.map(s => s.name).join(", ")}`;
     return (
-      <SharingModal width="700px" footer={null} visible={isVisible} onCancel={onClose} centered>
+      <SharingModal width="700px" footer={null} visible={isVisible} onCancel={this.handleClose} centered>
         <ModalContainer>
           <h2 style={{ fontWeight: "bold", fontSize: 20 }}>Share with others</h2>
           <ShareBlock>
@@ -409,7 +418,7 @@ class ShareModal extends React.Component {
             </FlexContainer>
           </PeopleBlock>
           <DoneButtonContainer>
-            <EduButton height="32px" onClick={onClose} style={{ display: "inline-flex" }}>
+            <EduButton height="32px" onClick={this.handleClose} style={{ display: "inline-flex" }}>
               Cancel
             </EduButton>
             <EduButton

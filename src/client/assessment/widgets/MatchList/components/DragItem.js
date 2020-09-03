ca8008020c2bf8aa25f39cabd8513d0aa7ac937e
arpit-snapwiz <57706980+arpit-snapwiz@users.noreply.github.com>
2020-09-03 17:39:47 +0530

    fix(ui): Popover when text width is more than the available width [EV-18345] (#11372)
    * fix(ui): Popover when text width is more than the available width [EV-18345]
    
    * fix(ui): Adding touch support [EV-18345]
    
    * fix(ui): Removing ev listener and Moving popover to other component [EV-18345]
    
    Co-authored-by: arpit <arpit.chandrakar@snawpiz.com>

--- src/client/assessment/widgets/MatchList/components/DragItem.js
+++ src/client/assessment/widgets/MatchList/components/DragItem.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { DragSource } from "react-dnd";
-import { MathFormulaDisplay, isMobileDevice } from "@edulastic/common";
+import { Popover } from "antd";
+import { MathFormulaDisplay, isMobileDevice, measureText } from "@edulastic/common";
 import { Index } from "../styled/Index";
 import { IconClose } from "../styled/IconClose";
 import { IconCheck } from "../styled/IconCheck";
@@ -54,42 +55,98 @@ const DragItem = ({
   isPrintPreview,
   ...restProps
 }) => {
+  const currentActiveItem = useRef(null);
+
+  const [isActive, setActive] = useState(false);
+
+  const [boxWidth, setBoxWidth] = useState(0);
+  const { width: textWidth } = measureText(item?.label);
+
+  const showPopover = boxWidth ? textWidth > boxWidth : false;
+
   const itemView = (
     <Wrapper>
       <MathFormulaDisplay centerContent={centerContent} dangerouslySetInnerHTML={{ __html: item?.label || "" }} />
     </Wrapper>
   );
 
+  const handleOnHover = ev => {
+    ev.stopPropagation();
+    if (["mouseenter", "touchstart"].includes(ev?.type)) {
+      setActive(true);
+      return;
+    }
+    setActive(false);
+  };
+
+  // Cleanup
+  useEffect(
+    () => () => {
+      const currentRef = currentActiveItem.current;
+      if (!currentRef || !showPopover) return;
+      currentRef.removeEventListener("mouseenter", handleOnHover);
+      currentRef.removeEventListener("mouseleave", handleOnHover);
+      currentRef.removeEventListener("mousedown", handleOnHover);
+      currentRef.removeEventListener("touchstart", handleOnHover);
+      currentRef.removeEventListener("touchmove", handleOnHover);
+      currentRef.removeEventListener("touchend", handleOnHover);
+    },
+    []
+  );
+
+  useEffect(() => {
+    const currentRef = currentActiveItem.current;
+    if (!currentRef) return;
+    setBoxWidth(currentRef.clientWidth);
+  }, [currentActiveItem?.current?.clientWidth]);
+
+  useEffect(() => {
+    const currentRef = currentActiveItem.current;
+    if (!currentRef || !showPopover) return;
+    currentRef.addEventListener("mouseenter", handleOnHover);
+    currentRef.addEventListener("mouseleave", handleOnHover);
+    currentRef.addEventListener("mousedown", handleOnHover);
+    currentRef.addEventListener("touchstart", handleOnHover);
+    currentRef.addEventListener("touchmove", handleOnHover);
+    currentRef.addEventListener("touchend", handleOnHover);
+  }, [showPopover]);
+
+  const getContent = (
+    <div
+      className="drag-drop-item-match-list"
+      data-cy={`drag-drop-item-${renderIndex}`}
+      style={{
+        ...getStyles({ isDragging, flag, _preview: preview, correct, width })
+      }}
+    >
+      {isMobileDevice() && (
+        <DragPreview isDragging={isDragging} {...restProps}>
+          {itemView}
+        </DragPreview>
+      )}
+
+      {correct !== undefined && preview && showAnswer && (
+        <Index preview={preview} correct={correct}>
+          {displayIndex}
+        </Index>
+      )}
+      {itemView}
+      {correct !== undefined && (
+        <div style={{ marginRight: 15, opacity: preview ? "1" : "0" }}>
+          {correct && <IconCheck />}
+          {!correct && <IconClose />}
+        </div>
+      )}
+    </div>
+  );
+
   return (
     item &&
     connectDragSource(
-      <div className="__prevent-page-break">
-        <div
-          className="drag-drop-item-match-list"
-          data-cy={`drag-drop-item-${renderIndex}`}
-          style={{
-            ...getStyles({ isDragging, flag, _preview: preview, correct, width })
-          }}
-        >
-          {isMobileDevice() && (
-            <DragPreview isDragging={isDragging} {...restProps}>
-              {itemView}
-            </DragPreview>
-          )}
-
-          {correct !== undefined && preview && showAnswer && (
-            <Index preview={preview} correct={correct}>
-              {displayIndex}
-            </Index>
-          )}
-          {itemView}
-          {correct !== undefined && (
-            <div style={{ marginRight: 15, opacity: preview ? "1" : "0" }}>
-              {correct && <IconCheck />}
-              {!correct && <IconClose />}
-            </div>
-          )}
-        </div>
+      <div className="__prevent-page-break" ref={currentActiveItem} style={{ maxWidth: "100%" }}>
+        <Popover visible={showPopover && !!isActive && !isDragging} content={getContent}>
+          {getContent}
+        </Popover>
       </div>
     )
   );

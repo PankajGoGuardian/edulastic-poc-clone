43301b0195d457fd524a00feacff7e132ff34c95
pin1128 <32391750+pin1128@users.noreply.github.com>
2020-09-06 02:39:20 +0800

    fix(client): iPad issues at student side (#11422)
    * fix(client): implemented number pad for basic and intermediate [EV-16281]
    
    * fix(client): updated basic, itermediate keypad [EV-16281]
    
    * fix(client): use same svg for degree symbol in math keyboard [EV-16281]
    
    * fix(client): clean up keypad [EV-16281]
    
    * fix(client): update basic, intermediate keypad button images [EV-16281]
    
    * fix(client): test item view issues on ipad [EV-18677]
    
    - implemented header keys
    - show only one panel on ipad
    - save & exit button size issue on ipad
    
    * fix(client): blank screen issue on scrolling on the iPad safari [EV-18514]
    
    * fix(client): hide redo and undo button in the mathkeypad
    
    * fix(client): remove header keys in mathkeyboard

---
+++ packages/common/src/components/MathKeyboard/components/HeaderKeyboard.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+import { HEADER_BUTTONS } from "../constants/headerButtons";
+import { Label } from "./styled";
+
+const HeaderKeyboard = ({ onInput }) => {
+  const handleClick = (handler, command, numToMove) => () => {
+    if (handler && command) {
+      onInput(handler, command, numToMove);
+    }
+  };
+
+  return (
+    <HeaderKeyboardContainer>
+      {HEADER_BUTTONS.map(({ label, handler, command = "cmd", numToMove }, i) => (
+        <Button key={i} onClick={handleClick(handler, command, numToMove)} data-cy={`header-keyboard-${handler}`}>
+          <Label>{label}</Label>
+        </Button>
+      ))}
+    </HeaderKeyboardContainer>
+  );
+};
+
+export default HeaderKeyboard;
+
+const HeaderKeyboardContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  padding: 8px 24px 0px;
+`;
+
+const Button = styled.div`
+  padding: 8px;
+  padding-bottom: 0px;
+  cursor: pointer;
+  user-select: none;
+`;
--- src/client/assessment/components/TestItemPreview/containers/TestItemCol/styled/Container.js
+++ src/client/assessment/components/TestItemPreview/containers/TestItemCol/styled/Container.js
@@ -1,6 +1,13 @@
 import styled from "styled-components";
 import { mobileWidth } from "@edulastic/colors";
 
+/**
+ * do not provide overflow here;
+ * with overflow property, the page is getting blank on scrolling on the iPad Safari.
+ * Also, don't provide transform: translate3d(0, 0, 0); here as well
+ * if we use translate3d, the drag & drop not working properly.
+ * @see https://snapwiz.atlassian.net/browse/EV-15752
+ */
 export const Container = styled.div`
   width: ${({ width }) => width};
   display: flex;
@@ -10,7 +17,6 @@ export const Container = styled.div`
   border-radius: ${props => props.hasCollapseButtons && "10px"};
   min-height: ${props => props.hasCollapseButtons && "calc(100vh - 122px)"};
   padding-top: ${props => props.hasCollapseButtons && "15px"};
-  overflow-y: ${props => props.isScrollable && "auto"};
 
   ${({ height }) => height && `height: ${height}`};
   @media (max-width: ${mobileWidth}) {

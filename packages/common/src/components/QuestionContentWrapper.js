3cc2e922388f07091f5798ccda62f86440c0b5fb
arpit-snapwiz <57706980+arpit-snapwiz@users.noreply.github.com>
2020-09-09 12:15:42 +0530

    fix(ui): Reducing space of question label [EV-18644] (#11476)
    Co-authored-by: arpit <arpit.chandrakar@snawpiz.com>

--- packages/common/src/components/QuestionContentWrapper.js
+++ packages/common/src/components/QuestionContentWrapper.js
@@ -2,7 +2,13 @@ import React from "react";
 import { FlexContainer } from "@edulastic/common";
 
 const QuestionContentWrapper = ({ children }) => (
-  <FlexContainer alignItems="flex-start" flexDirection="column" width="100%" className="question-content-wrapper __print_question-content-wrapper">
+  <FlexContainer
+    alignItems="flex-start"
+    flexDirection="column"
+    width="calc(100% - 51px)" // current question label width
+    marginLeft="auto"
+    className="question-content-wrapper __print_question-content-wrapper"
+  >
     {children}
   </FlexContainer>
 );

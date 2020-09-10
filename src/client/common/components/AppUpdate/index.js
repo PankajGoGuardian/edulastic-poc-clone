6ba0c273fe1e3c191efce7e558c2d31b727207e9
yeshwanthsnapwiz <62276813+yeshwanthsnapwiz@users.noreply.github.com>
2020-09-10 16:40:20 +0530

    fix(ui): app update ui change [EV-18914] (#11493)
    * fix(ui): app update ui change [EV-18914]
    
    * fix(ui): update [EV-18914]
    
    * fix(ui): time change [EV-18914]
    
    * refactor(ui): refactor code [EV-18914]
    
    * refactor(ui): refactor code [EV-18914]
    
    * refactor(ui): refactor code [EV-18914]

---
+++ src/client/common/components/AppUpdate/index.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import { notification } from "@edulastic/common";
+import "./notification.scss";
+
+const AppUpdate = ({ visible }) => {
+  const handleOk = () => {
+    setTimeout(() => {
+      window.location.reload(true);
+    }, 100);
+  };
+
+  const btn = (
+    <span style={{ whiteSpace: "nowrap" }}>
+      A New update is available &nbsp;<a onClick={() => handleOk()}>Refresh</a>
+    </span>
+  );
+
+  useEffect(() => {
+    if (visible) {
+      notification({ msg: btn, placement: "bottomRight", duration: 0, className: "notification" });
+    }
+  }, [visible]);
+
+  return null;
+};
+
+export default AppUpdate;

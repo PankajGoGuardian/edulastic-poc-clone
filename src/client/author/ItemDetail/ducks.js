26d93259611070f0eab0fb89e06b78636e8c3e7b
jitesh-kumar <jitesh.kumar@snapwiz.com>
2020-09-12 10:39:32 +0530

    chore: cherry-pick commit 6a02e9a [EV-17317] --- #11519 and #11517

--- src/client/author/ItemDetail/ducks.js
+++ src/client/author/ItemDetail/ducks.js
@@ -1071,11 +1071,13 @@ export function* updateItemSaga({ payload }) {
     }
     const { redirect = true } = payload; // added for doc based assesment, where redirection is not required.
     if (redirect && item._id !== payload.id) {
+      const {isTestFlow,previousTestId} = yield select(state => get(state, "router.location.state"), {});
       yield put(
         replace(
           payload.testId
             ? `/author/items/${item._id}/item-detail/test/${payload.testId}`
-            : `/author/items/${item._id}/item-detail`
+            : `/author/items/${item._id}/item-detail`,
+            {isTestFlow,previousTestId}
         )
       );
     }
--- src/client/author/Reports/subPages/singleAssessmentReport/index.js
+++ src/client/author/Reports/subPages/singleAssessmentReport/index.js
@@ -58,6 +58,9 @@ const SingleAssessmentReportContainer = props => {
   const [isCliUser, setCliUser] = useState(cliUser || queryString.parse(location.search).cliUser);
   const [ddfilter, setDdFilter] = useState({ ...INITIAL_DD_FILTERS });
   const [selectedExtras, setSelectedExtras] = useState({ ...INITIAL_DD_FILTERS });
+  const [customStudentUserId, setCustomStudentUserId] = useState(
+    queryString.parse(location.search).customStudentUserId
+  );
 
   useEffect(() => {
     if (isCliUser) {
@@ -66,7 +69,7 @@ const SingleAssessmentReportContainer = props => {
     return () => {
       console.log("Single Assessment Reports Component Unmount");
       resetAllReports();
-    }
+    };
   }, []);
 
   useEffect(() => {
@@ -95,7 +98,7 @@ const SingleAssessmentReportContainer = props => {
   useEffect(() => {
     if (settings.selectedTest.key) {
       const arr = Object.keys(settings.requestFilters);
-      
+
       const obj = {};
       // eslint-disable-next-line array-callback-return
       arr.map(item => {
@@ -105,11 +108,18 @@ const SingleAssessmentReportContainer = props => {
       if (isCliUser) {
         obj.cliUser = true;
       }
+      if (customStudentUserId) {
+        obj.customStudentUserId = customStudentUserId;
+      }
       const path = `${settings.selectedTest.key}?${qs.stringify(obj)}`;
       history.push(path);
     }
 
-    const navigationItems = computeChartNavigationLinks(settings.selectedTest, settings.requestFilters, settings.cliUser);
+    const navigationItems = computeChartNavigationLinks(
+      settings.selectedTest,
+      settings.requestFilters,
+      settings.cliUser
+    );
     updateNavigation(navigationItems);
   }, [settings]);
 
@@ -155,17 +165,14 @@ const SingleAssessmentReportContainer = props => {
         <FilterLabel>{item.title}</FilterLabel>
         <ControlDropDown selectCB={updateCB} data={item.data} comData={item.key} by={item.data[0]} />
       </SearchField>
-    ))
+      ))
     : [];
 
   return (
     <FeaturesSwitch inputFeatures="singleAssessmentReport" actionOnInaccessible="hidden">
       <>
         {firstLoad && <Spin size="large" />}
-        <FlexContainer
-          alignItems="flex-start"
-          display={firstLoad ? "none" : "flex"}
-        >
+        <FlexContainer alignItems="flex-start" display={firstLoad ? "none" : "flex"}>
           <SingleAssessmentReportFilters
             onGoClick={onGoClick}
             loc={loc}
@@ -187,9 +194,11 @@ const SingleAssessmentReportContainer = props => {
             firstLoad={firstLoad}
             setFirstLoad={setFirstLoad}
           />
-          {!isCliUser && <FilterButton showFilter={showFilter} onClick={toggleFilter}>
-            <IconFilter />
-          </FilterButton>}
+          {!isCliUser && (
+            <FilterButton showFilter={showFilter} onClick={toggleFilter}>
+              <IconFilter />
+            </FilterButton>
+          )}
           <ReportContaner showFilter={showFilter}>
             <Route
               exact
@@ -228,6 +237,7 @@ const SingleAssessmentReportContainer = props => {
                   settings={settings}
                   pageTitle={loc}
                   filters={ddfilter}
+                  customStudentUserId={customStudentUserId}
                 />
               )}
             />

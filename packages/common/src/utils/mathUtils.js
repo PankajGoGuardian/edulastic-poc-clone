d33512600fa3ca39bcf521670c2567a973027b69
arpit-snapwiz <57706980+arpit-snapwiz@users.noreply.github.com>
2020-09-14 10:23:33 +0530

    fix(client): Handling exponent issue for complete the equation [EV-19090] (#11568)
    Co-authored-by: arpit <arpit.chandrakar@snawpiz.com>

--- packages/common/src/utils/mathUtils.js
+++ packages/common/src/utils/mathUtils.js
@@ -186,10 +186,12 @@ export const replaceMathHtmlWithLatexes = val => {
 
 export const getInnerValuesForStatic = (studentTemplate, userAnswer) => {
   const escapeRegExp = string => string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&").replace(/&amp;/g, "&");
-  const regexTemplate = new RegExp(
-    escapeRegExp(studentTemplate || "").replace(/\\\\MathQuillMathField\\\{\\\}/g, "(.*)"),
-    "g"
-  );
+
+  studentTemplate = escapeRegExp(studentTemplate || "").replace(/\\\\MathQuillMathField\\\{\\\}/g, "(.*)");
+  // To fix issue with exponent value 2^{responseBox}
+  studentTemplate = studentTemplate.replace(/\\{\(\.\*\)\\}/g, "(.*)");
+
+  const regexTemplate = new RegExp(studentTemplate, "g");
 
   if (userAnswer && userAnswer.length > 0) {
     const userInnerValues = regexTemplate.exec(userAnswer);

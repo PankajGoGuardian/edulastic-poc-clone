d5359b5843a110a29f41072355c2f010c4a0842b
Gourav Mukherjee <50568503+mgourav-snapwiz@users.noreply.github.com>
2020-09-16 19:36:47 +0530

    chore: cherry-pick 3f070f9 into 12.22 from master ---

--- src/client/author/ItemDetail/ducks.js
+++ src/client/author/ItemDetail/ducks.js
@@ -758,7 +758,7 @@ export function reducer(state = initialState, { type, payload }) {
           multipartItem: true,
           isPassageWithQuestions: true,
           canAddMultipleItems: !!payload.canAddMultipleItems,
-          itemLevelScoring:false
+          itemLevelScoring: false
         }
       };
     case ADD_PASSAGE: {
@@ -900,7 +900,7 @@ export function* deleteItemSaga({ payload }) {
     if (isItemPrevew) return;
 
     if (isTestFlow) {
-      yield put(push(`/author/tests/${testId}/editItem/${redirectId}`));
+      yield put(push(`/author/items/${redirectId}/item-detail/test/${testId}`));
       return;
     }
     if (redirectId) {
@@ -1071,14 +1071,13 @@ export function* updateItemSaga({ payload }) {
     }
     const { redirect = true } = payload; // added for doc based assesment, where redirection is not required.
     if (redirect && item._id !== payload.id) {
-      const {isTestFlow,previousTestId} = yield select(state => get(state, "router.location.state"), {});
-      //`/author/tests/${tId}/editItem/${item?._id}`
+      const { isTestFlow, previousTestId } = yield select(state => get(state, "router.location.state", {}));
       yield put(
         replace(
           payload.testId
-            ? `/author/tests/${payload.testId}/editItem/${item._id}`
+            ? `/author/items/${item._id}/item-detail/test/${payload.testId}`
             : `/author/items/${item._id}/item-detail`,
-            {isTestFlow,previousTestId}
+          { isTestFlow, previousTestId }
         )
       );
     }

5bcbe03e72ffa8fa5e59521f147562ef7c8a0341
Gourav Mukherjee <50568503+mgourav-snapwiz@users.noreply.github.com>
2020-09-17 20:44:35 +0530

    Revert "fix(client): enable scroll for student side passage view [EV-19268]  (#11620)" (#11665)
    This reverts commit c90f64e241a834418316786eca15a1a7d3b6e6e2.

--- src/client/assessment/components/TestItemPreview/containers/TestItemCol/styled/Container.js
+++ src/client/assessment/components/TestItemPreview/containers/TestItemCol/styled/Container.js
@@ -1,6 +1,13 @@
 import styled from "styled-components";
 import { mobileWidth } from "@edulastic/colors";
 
+/**
+ * do not provide overflow here;
+ * with overflow property, the page is getting blank on scrolling on the iPad Safari.
+ * Also, don't provide transform: translate3d(0, 0, 0); here as well
+ * if we use translate3d, the drag & drop not working properly.
+ * @see https://snapwiz.atlassian.net/browse/EV-15752
+ */
 export const Container = styled.div`
   width: ${({ width }) => width};
   display: flex;
@@ -9,9 +16,8 @@ export const Container = styled.div`
   background-color: ${props => props.hasCollapseButtons && "#fff"};
   border-radius: ${props => props.hasCollapseButtons && "10px"};
   min-height: ${props => props.hasCollapseButtons && "calc(100vh - 122px)"};
-  max-height: ${props => props.hasCollapseButtons && "calc(100vh - 122px)"};
   padding-top: ${props => props.hasCollapseButtons && "15px"};
-  overflow: ${props => props.hasCollapseButtons && "auto"};
+
   ${({ height }) => height && `height: ${height}`};
   @media (max-width: ${mobileWidth}) {
     padding-left: 0px;

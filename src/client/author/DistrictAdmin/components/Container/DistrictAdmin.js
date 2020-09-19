28bcc824c5067501eea1734f6f8ab4831441453e
Himayan Debnath <63641486+himayan@users.noreply.github.com>
2020-09-19 15:16:15 +0530

    fix(ui): show loader while fetching user data [EV-19327] (#11695)
    * fix(ui): show loader while fetching user data [EV-19327]
    
    * fix(ui): use studentReducer for multiple addition [EV-19327]
    
    * fix(ui): add pagination to admin tables in users tab [EV-19327]
    
    Co-authored-by: Himayan <himayand21@gmail.com>

--- src/client/author/DistrictAdmin/components/Container/DistrictAdmin.js
+++ src/client/author/DistrictAdmin/components/Container/DistrictAdmin.js
@@ -33,7 +33,7 @@ class DistrictAdmin extends Component {
         <StyledContent>
           <StyledLayout loading={showSpin ? "true" : "false"}>
             {showSpin && (
-              <SpinContainer>
+              <SpinContainer blur>
                 <StyledSpin size="large" />
               </SpinContainer>
             )}
@@ -47,10 +47,10 @@ class DistrictAdmin extends Component {
 
 const enhance = compose(
   connect(state => ({
-    loading: get(state, ["districtAdminReducer", "loading"], false),
-    updating: get(state, ["districtAdminReducer", "updating"], false),
-    creating: get(state, ["districtAdminReducer", "creating"], false),
-    deleting: get(state, ["districtAdminReducer", "deleting"], false),
+    loading: get(state, ["schoolAdminReducer", "loading"], false),
+    updating: get(state, ["schoolAdminReducer", "updating"], false),
+    creating: get(state, ["schoolAdminReducer", "creating"], false),
+    deleting: get(state, ["schoolAdminReducer", "deleting"], false),
     routeKey: get(state, ["router", "location", "key"])
   }))
 );

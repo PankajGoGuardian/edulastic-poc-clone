98d8a69bc1b7a4fc29b6f9547a8f81a52ebf3d8e
pin1128 <32391750+pin1128@users.noreply.github.com>
2020-09-23 03:37:13 -0700

    fix(client): improve doc based [EV-19541] (#11723)

--- src/client/author/AssessmentPage/components/PDFPreview/PDFPreview.js
+++ src/client/author/AssessmentPage/components/PDFPreview/PDFPreview.js
@@ -140,7 +140,7 @@ const PDFPreview = ({
       ref={previewContainer}
     >
       <PerfectScrollbar ref={forwardedRef} option={{ wheelSpeed: 0.6 }}>
-        <Droppable drop={handleDropQuestion}>
+        <Droppable drop={handleDropQuestion} noBorder={docLoading}>
           {page.URL === "blank" && <Preview onClick={handleRemoveHighlight} />}
 
           {page.URL !== "blank" && (

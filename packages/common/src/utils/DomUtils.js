1fcb1a91549cc67b3ac717b68b712ba9e2452dec
nagarajanSnapwiz <44694625+nagarajanSnapwiz@users.noreply.github.com>
2020-09-25 13:59:02 +0530

    fix(client): handle scrollElement missing in offset [EV-19782] (#11793)
    * fix(client): handle scrollElement missing in offset [EV-19782]
    
    * Update DomUtils.js

--- packages/common/src/utils/DomUtils.js
+++ packages/common/src/utils/DomUtils.js
@@ -1,11 +1,11 @@
-export function offset(el,scrollElement) {
+export function offset(el, scrollElement) {
   if (!el) {
     return;
   }
   const rect = el.getBoundingClientRect();
   let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  if(scrollElement != window){
+  if (scrollElement && scrollElement != window) {
     scrollTop = scrollElement.scrollTop;
     scrollLeft = scrollElement.scrollLeft;
   }
@@ -17,14 +17,13 @@ const isSmoothScrollSupported = "scrollBehavior" in document.documentElement.sty
 /**
  * @param {Element} el
  */
-export function scrollTo(el, subtractScroll = 0,scrollEl) {
-  if(!scrollEl){
+export function scrollTo(el, subtractScroll = 0, scrollEl) {
+  if (!scrollEl) {
     scrollEl = window;
   }
-  const { top = 0 } = offset(el,scrollEl) || {};
+  const { top = 0 } = offset(el, scrollEl) || {};
   if (isSmoothScrollSupported) {
     // behavior:auto|smooth|initial|inherit
-    console.log('top',top);
     scrollEl.scrollTo({ top: top - subtractScroll, left: 0, behavior: "auto" });
   } else {
     scrollEl.scrollTo(0, top - subtractScroll);

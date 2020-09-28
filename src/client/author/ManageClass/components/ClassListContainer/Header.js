38e70d4748c65e14fc0fcc51e5f0f367f4057acc
mangaljain <mangal.jain@snapwiz.com>
2020-09-28 14:59:23 +0530

    fix(ui): hiding bulk sync for clever lib from manage class list page [EV-18889] (#11829)

--- src/client/author/ManageClass/components/ClassListContainer/Header.js
+++ src/client/author/ManageClass/components/ClassListContainer/Header.js
@@ -4,23 +4,16 @@ import { Link } from "react-router-dom";
 import { withNamespaces } from "react-i18next";
 import { GoogleLogin } from "react-google-login";
 import * as Sentry from "@sentry/browser";
-import { Select } from "antd";
 
 // components
 import { EduButton, MainHeader, HeaderTabs, notification } from "@edulastic/common";
 import { canvasApi } from "@edulastic/api";
 import { IconGoogleClassroom, IconManage, IconPlusCircle, IconClass, IconGroup, IconClever } from "@edulastic/icons";
 import { StyledTabs } from "@edulastic/common/src/components/HeaderTabs";
-import { ButtonsWrapper, SelectStyled, OptionWrapper } from "./styled";
+import { ButtonsWrapper } from "./styled";
 import FeaturesSwitch from "../../../../features/components/FeaturesSwitch";
 import authorizeCanvas from "../../../../common/utils/CanavsAuthorizationModule";
 import { scopes } from "./ClassCreatePage";
-
-const Option = Select.Option;
-
-const CANVAS = "canvas";
-const GOOGLE = "google";
-const CLEVER = "clever";
 
 const Header = ({
   fetchGoogleClassList,
@@ -88,21 +81,6 @@ const Header = ({
     }
   ];
 
-  const enableGoogleSync = googleAllowedInstitutions.length > 0;
-  const enableCanvasSync = canvasAllowedInstitution.length > 0;
-
-  const options = {
-    [CLEVER]: enableCleverSync,
-    [GOOGLE]: enableGoogleSync,
-    [CANVAS]: enableCanvasSync
-  };
-
-  Object.keys(options).forEach(o => {
-    if (!options[o]) delete options[o];
-  });
-
-  const showDropDown = Object.values(options).filter(o => o).length > 1;
-
   return (
     <MainHeader Icon={IconManage} headingText={t("common.manageClassTitle")}>
       <FeaturesSwitch inputFeatures="studentGroups" actionOnInaccessible="hidden">
@@ -121,97 +99,42 @@ const Header = ({
         </StyledTabs>
       </FeaturesSwitch>
       <ButtonsWrapper>
-        {showDropDown ? (
-          <SelectStyled
-            data-cy="sync-options-dropdown"
-            minWidth="200px"
-            dropdownStyle={{ zIndex: 1000 }}
-            value="Bulk Sync Classes"
-          >
-            {Object.keys(options).map((option, index) => {
-              if (option === CLEVER) {
-                return (
-                  <Option key={index} data-cy={`sync-option-${index}`} onClick={() => setShowCleverSyncModal(true)}>
-                    <span>Sync with Clever</span>
-                    <IconClever width={18} height={18} />
-                  </Option>
-                );
-              }
-              if (option === GOOGLE) {
-                return (
-                  <Option key={index} data-cy={`sync-option-${index}`}>
-                    <GoogleLogin
-                      clientId={process.env.POI_APP_GOOGLE_CLIENT_ID}
-                      buttonText="Sync with Google Classroom"
-                      render={renderProps => (
-                        <OptionWrapper onClick={renderProps.onClick}>
-                          <span>Sync with Google Classroom</span>
-                          <IconGoogleClassroom />
-                        </OptionWrapper>
-                      )}
-                      scope={scopes}
-                      onSuccess={handleLoginSucess}
-                      onFailure={handleError}
-                      prompt={isUserGoogleLoggedIn ? "" : "consent"}
-                      responseType="code"
-                    />
-                  </Option>
-                );
-              }
-              if (option === CANVAS) {
-                return (
-                  <Option key={index} data-cy={`sync-option-${index}`} onClick={handleSyncWithCanvas}>
-                    <span>Sync with Canvas</span>
-                    <img
-                      alt="Canvas"
-                      src="https://cdn.edulastic.com/JS/webresources/images/as/canvas.png"
-                      width={18}
-                      height={18}
-                    />
-                  </Option>
-                );
-              }
-              return null;
-            })}
-          </SelectStyled>
-        ) : (
-          <>
-            {enableCleverSync && (
-              <EduButton isBlue isGhost onClick={() => setShowCleverSyncModal(true)}>
-                <IconClever width={18} height={18} />
-                <span>SYNC NOW WITH CLEVER</span>
-              </EduButton>
-            )}
-            {googleAllowedInstitutions?.length > 0 && !isCleverDistrict && (
-              <GoogleLogin
-                clientId={process.env.POI_APP_GOOGLE_CLIENT_ID}
-                buttonText="Sync with Google Classroom"
-                render={renderProps => (
-                  <EduButton isBlue isGhost onClick={renderProps.onClick}>
-                    <IconGoogleClassroom />
-                    <span>SYNC WITH GOOGLE CLASSROOM</span>
-                  </EduButton>
-                )}
-                scope={scopes}
-                onSuccess={handleLoginSucess}
-                onFailure={handleError}
-                prompt={isUserGoogleLoggedIn ? "" : "consent"}
-                responseType="code"
+        <>
+          {enableCleverSync && (
+            <EduButton isBlue isGhost onClick={() => setShowCleverSyncModal(true)}>
+              <IconClever width={18} height={18} />
+              <span>SYNC NOW WITH CLEVER</span>
+            </EduButton>
+          )}
+          {googleAllowedInstitutions?.length > 0 && !isCleverDistrict && !enableCleverSync && (
+            <GoogleLogin
+              clientId={process.env.POI_APP_GOOGLE_CLIENT_ID}
+              buttonText="Sync with Google Classroom"
+              render={renderProps => (
+                <EduButton isBlue isGhost onClick={renderProps.onClick}>
+                  <IconGoogleClassroom />
+                  <span>SYNC WITH GOOGLE CLASSROOM</span>
+                </EduButton>
+              )}
+              scope={scopes}
+              onSuccess={handleLoginSucess}
+              onFailure={handleError}
+              prompt={isUserGoogleLoggedIn ? "" : "consent"}
+              responseType="code"
+            />
+          )}
+          {canvasAllowedInstitution?.length > 0 && !isCleverDistrict && !enableCleverSync && (
+            <EduButton isBlue isGhost onClick={handleSyncWithCanvas}>
+              <img
+                alt="Canvas"
+                src="https://cdn.edulastic.com/JS/webresources/images/as/canvas.png"
+                width={18}
+                height={18}
               />
-            )}
-            {canvasAllowedInstitution?.length > 0 && !isCleverDistrict && (
-              <EduButton isBlue isGhost onClick={handleSyncWithCanvas}>
-                <img
-                  alt="Canvas"
-                  src="https://cdn.edulastic.com/JS/webresources/images/as/canvas.png"
-                  width={18}
-                  height={18}
-                />
-                <span>SYNC WITH CANVAS</span>
-              </EduButton>
-            )}
-          </>
-        )}
+              <span>SYNC WITH CANVAS</span>
+            </EduButton>
+          )}
+        </>
         <Link to={{ pathname: "/author/manageClass/createClass", state: { type: currentTab } }} data-cy="createClass">
           <EduButton isBlue>
             <IconPlusCircle />

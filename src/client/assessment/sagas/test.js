a2617943736471693d47a1e20c50fe5c2c085a98
prajwal-snapwiz <53730619+prajwal-snapwiz@users.noreply.github.com>
2020-10-06 11:31:13 +0530

    fix(client): prevent student from landing on test if paused [EV-20251] (#11977)
    Co-authored-by: Prajwal-Snapwiz <prajwal.venkatesh@gmail.com>

--- src/client/assessment/sagas/test.js
+++ src/client/assessment/sagas/test.js
@@ -545,6 +545,8 @@ function* loadTest({ payload }) {
     if (err.status) {
       if (err.status === 400) {
         messageKey = 'invalidAction'
+      } else if (err.status === 302) {
+        messageKey = 'testPausedByTeacher'
       }
     }
     if (userRole === roleuser.STUDENT) {

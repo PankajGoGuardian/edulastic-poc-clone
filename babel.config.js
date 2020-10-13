f928a58db39742ee4a4fe9534ca00272509e1ab0
Prithvi Raju M <aga5tya@yahoo.com>
2020-10-13 17:33:45 +0530

    chore: reset babel config [EV-20584] ---

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,19 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-
-let babelPreset = require('babel-preset-react-app')({
-  flow: false,
-  typescript: false,
-})
-
-babelPreset = { ...babelPreset }
-
-babelPreset.plugins.push.apply(babelPreset.plugins, [
-  [
-    'import',
-    {
-      libraryName: 'antd',
-    },
-    'antd',
+module.exports = {
+  presets: [['@babel/preset-react']],
+  plugins: [
+    [
+      'import',
+      {
+        libraryName: 'antd',
+      },
+      'antd',
+    ],
+    'lodash',
+    [
+      'styled-components',
+      {
+        useDisplayName: false,
+      },
+    ],
   ],
-  'lodash',
-  [
-    'styled-components',
-    {
-      useDisplayName: false,
-    },
-  ],
-])
-
-module.exports = babelPreset
+}

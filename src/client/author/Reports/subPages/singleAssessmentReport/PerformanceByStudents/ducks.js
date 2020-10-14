aaa933248f3351a9163e439934ca17771e1462c8
Jitesh Singh <jitesh.kumar@snapwiz.com>
2020-10-14 22:40:06 +0530

    Revert "[EV-20158] Remove orgData and replace filters with ES Autocomplete (#12127)"
    This reverts commit 6409b6b69ca17528824748ed8f711b49b3989e9d.

--- src/client/author/Reports/subPages/singleAssessmentReport/PerformanceByStudents/ducks.js
+++ src/client/author/Reports/subPages/singleAssessmentReport/PerformanceByStudents/ducks.js
@@ -6,6 +6,7 @@ import { notification } from '@edulastic/common'
 import { createAction, createReducer } from 'redux-starter-kit'
 
 import { RESET_ALL_REPORTS } from '../../../common/reportsRedux'
+import { getOrgDataFromSARFilter } from '../common/filterDataDucks'
 
 const GET_REPORTS_PERFORMANCE_BY_STUDENTS_REQUEST =
   '[reports] get reports performance by students request'
@@ -29,10 +30,15 @@ export const getPerformanceByStudentsRequestAction = createAction(
 export const stateSelector = (state) =>
   state.reportReducer.reportPerformanceByStudentsReducer
 
-export const getReportsPerformanceByStudents = createSelector(
+const _getReportsPerformanceByStudents = createSelector(
   stateSelector,
   (state) => state.performanceByStudents
 )
+
+export const getReportsPerformanceByStudents = (state) => ({
+  ..._getReportsPerformanceByStudents(state),
+  metaInfo: getOrgDataFromSARFilter(state),
+})
 
 export const getReportsPerformanceByStudentsLoader = createSelector(
   stateSelector,
@@ -60,8 +66,8 @@ const initialState = {
 }
 
 export const reportPerformanceByStudentsReducer = createReducer(initialState, {
-  [RESET_ALL_REPORTS]: (state) => (state = initialState),
-  [GET_REPORTS_PERFORMANCE_BY_STUDENTS_REQUEST]: (state) => {
+  [RESET_ALL_REPORTS]: (state, { payload }) => (state = initialState),
+  [GET_REPORTS_PERFORMANCE_BY_STUDENTS_REQUEST]: (state, { payload }) => {
     state.loading = true
   },
   [GET_REPORTS_PERFORMANCE_BY_STUDENTS_REQUEST_SUCCESS]: (

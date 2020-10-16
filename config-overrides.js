0b57725658b5d77a9eb57be188e9a8bf255cd260
Prithvi Raju M <aga5tya@yahoo.com>
2020-10-16 13:38:02 +0530

    chore: try with custom optimizers [EV-20584] ---

--- config-overrides.js
+++ config-overrides.js
@@ -13,6 +13,9 @@ const path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
 const ProgressBarPlugin = require('simple-progress-webpack-plugin')
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const safePostCssParser = require('postcss-safe-parser')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const packageJson = require('./package.json')
 
 /** Uncomment to have a copy of files written on disk */
@@ -21,6 +24,66 @@ const packageJson = require('./package.json')
 /** Uncomment to have a copy of files written on disk */
 
 // const rootNodeModDir = path.resolve(__dirname, 'node_modules')
+
+const getOptimizers = (sourceMapEnabled = true) => [
+  new TerserPlugin({
+    terserOptions: {
+      parse: {
+        // we want uglify-js to parse ecma 8 code. However, we don't want it
+        // to apply any minfication steps that turns valid ecma 5 code
+        // into invalid ecma 5 code. This is why the 'compress' and 'output'
+        // sections only apply transformations that are ecma 5 safe
+        // https://github.com/facebook/create-react-app/pull/4234
+        ecma: 8,
+      },
+      compress: {
+        ecma: 5,
+        warnings: false,
+        // Disabled because of an issue with Uglify breaking seemingly valid code:
+        // https://github.com/facebook/create-react-app/issues/2376
+        // Pending further investigation:
+        // https://github.com/mishoo/UglifyJS2/issues/2011
+        comparisons: false,
+        // Disabled because of an issue with Terser breaking valid code:
+        // https://github.com/facebook/create-react-app/issues/5250
+        // Pending futher investigation:
+        // https://github.com/terser-js/terser/issues/120
+        inline: 2,
+      },
+      mangle: {
+        safari10: true,
+      },
+      output: {
+        ecma: 5,
+        comments: false,
+        // Turned on because emoji and regex is not minified properly using default
+        // https://github.com/facebook/create-react-app/issues/2488
+        ascii_only: true,
+      },
+    },
+    // Use multi-process parallel running to improve the build speed
+    // Default number of concurrent runs: os.cpus().length - 1
+    parallel: true,
+    // Enable file caching
+    cache: true,
+    // @todo add flag for sourcemaps
+    sourceMap: sourceMapEnabled,
+  }),
+  new OptimizeCSSAssetsPlugin({
+    cssProcessorOptions: {
+      parser: safePostCssParser,
+      // @todo add flag for sourcemaps
+      map: {
+        // `inline: false` forces the sourcemap to be output into a
+        // separate file
+        inline: false,
+        // `annotation: true` appends the sourceMappingURL to the end of
+        // the css file, helping the browser find the sourcemap
+        annotation: true,
+      },
+    },
+  }),
+]
 
 module.exports = override(
   disableEsLint(),
@@ -232,6 +295,7 @@ module.exports = override(
             },
           },
         },
+        minimizer: getOptimizers(),
       }
     }
 

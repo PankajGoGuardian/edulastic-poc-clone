4b5f76a75ee0096fac651bfd54b84d115e626676
Jitesh Singh <jitesh.kumar@snapwiz.com>
2020-10-18 18:01:44 +0530

    Revert "fix(client): asssignment action summary report redirection fix [EV-20689] (#12196)"
    This reverts commit 3ccca0072310183e41d2e00552f5ad54387eb11d.

--- src/client/author/Assignments/components/ActionMenu/ActionMenu.js
+++ src/client/author/Assignments/components/ActionMenu/ActionMenu.js
@@ -199,14 +199,10 @@ const ActionMenu = ({
             Release Scores
           </StyledLink>
         </Menu.Item>
-        <Menu.Item
-          data-cy="summary-grades"
-          key="summary-report"
-          disabled={
-            !(currentAssignment.gradedCount + currentAssignment.submittedCount)
-          }
-        >
-          <Link to={`/author/reports/assessment-summary/test/${currentTestId}`}>
+        <Menu.Item data-cy="summary-grades" key="summary-report">
+          <Link
+            to={`/author/reports/performance-by-students/test/${currentTestId}`}
+          >
             <IconBarChart />
             <SpaceElement />
             View Summary Report

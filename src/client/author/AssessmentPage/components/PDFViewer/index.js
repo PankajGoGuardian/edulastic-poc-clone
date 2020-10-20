49773a198f9d347d55e2ccdcecf22c2f7d7c5149
Prithvi Raju M <aga5tya@yahoo.com>
2020-10-20 19:56:16 +0530

    chore: another one [EV-20584] ---

--- src/client/author/AssessmentPage/components/PDFViewer/index.js
+++ src/client/author/AssessmentPage/components/PDFViewer/index.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { Spin } from 'antd'
 import loadable from '@loadable/component'
-import pdfjs from 'pdfjs-dist/webpack'
+import pdfjs from 'pdfjs-dist/es5/build/pdf'
 // eslint-disable-next-line
 import { BLANK_URL } from '../Worksheet/Worksheet'
 import PdfStoreAdapter from './PdfStoreAdapter'
+
+pdfjs.GlobalWorkerOptions.workerSrc =
+  'https://cdn.jsdelivr.net/npm/pdfjs-dist@2.4.456/build/pdf.worker.min.js'
 
 const PDFJSANNOTATE = loadable.lib(() =>
   import('@edulastic/ext-libs/src/pdf-annotate')
@@ -100,9 +103,6 @@ const PDFViewer = ({
     }
 
     // if (!pdfjs) return
-
-    // pdfjs.GlobalWorkerOptions.workerSrc =
-    // 'https://cdn.jsdelivr.net/npm/pdfjs-dist@2.4.456/build/pdf.worker.min.js'
 
     const loadingTask = pdfjs.getDocument(URL)
     loadingTask.promise

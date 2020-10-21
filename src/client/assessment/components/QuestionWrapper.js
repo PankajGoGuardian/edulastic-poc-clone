9fa1a7b6a865c918a7d2b54f283e8d42c26138a5
pin1128 <32391750+pin1128@users.noreply.github.com>
2020-10-20 23:46:58 -0700

    fix(client): show scratchpad data at first time for drawing response in LCB [EV-21034] (#12270)

--- src/client/assessment/components/QuestionWrapper.js
+++ src/client/assessment/components/QuestionWrapper.js
@@ -311,14 +311,15 @@ const getQuestion = (type) => {
 const { TEACHER, SCHOOL_ADMIN, DISTRICT_ADMIN } = roleuser
 
 class QuestionWrapper extends Component {
-  static contextType = ItemDetailContext
-
-  state = {
-    main: [],
-    advanced: [],
-    activeTab: 0,
-    shuffledOptsOrder: [],
-    page: 1,
+  constructor(props) {
+    super(props)
+    this.state = {
+      main: [],
+      advanced: [],
+      activeTab: 0,
+      shuffledOptsOrder: [],
+      page: 1,
+    }
   }
 
   setPage = (page) => this.setState({ page })
@@ -381,6 +382,7 @@ class QuestionWrapper extends Component {
       data: prevData,
       windowWidth: prevWindowWidth,
       windowHeight: prevWindowHeight,
+      userWork: prevUserWork,
     } = prevProps
     const {
       data,
@@ -388,11 +390,14 @@ class QuestionWrapper extends Component {
       isExpressGrader,
       windowWidth,
       windowHeight,
+      userWork,
     } = this.props
+
     if (
       isLCBView &&
       !isExpressGrader &&
       data?.activity &&
+      isEqual(prevUserWork, userWork) &&
       isEqual(prevData?.activity, data?.activity) &&
       prevWindowHeight === windowHeight &&
       prevWindowWidth === windowWidth
@@ -724,6 +729,8 @@ class QuestionWrapper extends Component {
     )
   }
 }
+
+QuestionWrapper.contextType = ItemDetailContext
 
 QuestionWrapper.propTypes = {
   setQuestionData: PropTypes.func.isRequired,

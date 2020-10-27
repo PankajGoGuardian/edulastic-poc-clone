85d791efee4de79fad0a81d5f6d0fd898ab94407
pin1128 <32391750+pin1128@users.noreply.github.com>
2020-10-27 01:44:56 -0700

    fix(client): annotation move delete issue in doc based [EV-20624] (#12289)
    * fix(client): annotaition move delete issue in doc based [EV-20624]
    
    * fix(client): change  z index of response index in doc basedz [EV-20624]

--- packages/ext-libs/src/pdf-annotate.js
+++ packages/ext-libs/src/pdf-annotate.js
@@ -1138,7 +1138,7 @@ const { themeColor } = require('@edulastic/colors')
           if (!svg) {
             return
           }
-          var elements = svg.querySelectorAll('svg[data-pdf-annotate-type]')
+          var elements = svg.querySelectorAll('[data-pdf-annotate-type]')
 
           // Find a target element within SVG
           for (var i = 0, l = elements.length; i < l; i++) {
@@ -2027,10 +2027,11 @@ const { themeColor } = require('@edulastic/colors')
             var width = parseInt(node.getAttribute('width'), 10)
             var height = parseInt(node.getAttribute('height'), 10)
             var path = node.querySelector('path')
+            var rect = node.querySelector('rect')
             var svg = path.parentNode
 
             // Scale width/height
-            ;[node, svg, path].forEach(function (n) {
+            ;[node, path, rect].forEach(function (n) {
               n.setAttribute(
                 'width',
                 parseInt(n.getAttribute('width'), 10) * viewport.scale
@@ -2041,11 +2042,7 @@ const { themeColor } = require('@edulastic/colors')
               )
             })
 
-            // Transform path but keep scale at 100% since it will be handled natively
-            transform(
-              path,
-              (0, _objectAssign2.default)({}, viewport, { scale: 1 })
-            )
+            transform(path, viewport)
 
             switch (viewport.rotation % 360) {
               case 90:
@@ -2375,25 +2372,42 @@ const { themeColor } = require('@edulastic/colors')
         }
 
         var SIZE = 25
-        var commentSvg = `<svg class="svg-icon" id="comment" width="25" height="25">
-          <rect id="svg_2" height="25.34537" width="25.31416" y="-0.17269" x="-0.15708" stroke-width="0" stroke="#EFA12C" fill="#EFA12C"/>
-          <path stroke="#ffffff" id="svg_1" stroke-width="0.5" fill="#ffffff" d="m5.96968,15.58726a7.20087,7.20087 0 0 1 -0.30932,-0.76781l-0.01549,0a7.2261,7.2261 0 0 1 6.53852,-9.5306l0,0a7.21843,7.21843 0 1 1 0.34661,14.42916a7.14054,7.14054 0 0 1 -3.07121,-0.69102c-3.76223,0.74368 -3.46937,0.69102 -3.55492,0.69102a0.61863,0.61863 0 0 1 -0.60656,-0.74037l0.67237,-3.39039z"/>
-        </svg>`
+        var D =
+          'm5.96968,15.58726a7.20087,7.20087 0 0 1 -0.30932,-0.76781l-0.01549,0a7.2261,7.2261 0 0 1 6.53852,-9.5306l0,0a7.21843,7.21843 0 1 1 0.34661,14.42916a7.14054,7.14054 0 0 1 -3.07121,-0.69102c-3.76223,0.74368 -3.46937,0.69102 -3.55492,0.69102a0.61863,0.61863 0 0 1 -0.60656,-0.74037l0.67237,-3.39039z'
 
         function renderPoint(a) {
-          var div = document.createElement('div')
-          div.innerHTML = commentSvg.trim()
+          var commentSvg = document.createElementNS(
+            'http://www.w3.org/2000/svg',
+            'svg'
+          )
+          commentSvg.setAttribute('width', SIZE)
+          commentSvg.setAttribute('height', SIZE)
+          commentSvg.setAttribute('x', a.x)
+          commentSvg.setAttribute('y', a.y)
+          commentSvg.setAttribute('id', 'comment')
 
-          div.firstChild.setAttribute('x', a.x)
-          div.firstChild.setAttribute('y', a.y)
-          div.firstChild.setAttribute('stroke-width', 1)
-          div.firstChild.setAttribute('stroke', '#ff0')
+          var rect = document.createElementNS(
+            'http://www.w3.org/2000/svg',
+            'rect'
+          )
+          rect.setAttribute('width', SIZE)
+          rect.setAttribute('height', SIZE)
+          rect.setAttribute('stroke', '#EFA12C')
+          rect.setAttribute('fill', '#EFA12C')
+          rect.setAttribute('stroke-width', 0)
 
-          div.setAttribute('width', 100)
-          div.setAttribute('height', 100)
-          div.style.background = '#ff0'
+          var path = document.createElementNS(
+            'http://www.w3.org/2000/svg',
+            'path'
+          )
+          path.setAttribute('d', D)
+          path.setAttribute('stroke-width', 0.5)
+          path.setAttribute('stroke', '#ffffff')
+          path.setAttribute('fill', '#ffffff')
 
-          return div.firstChild
+          commentSvg.appendChild(rect)
+          commentSvg.appendChild(path)
+          return commentSvg
         }
         module.exports = exports['default']
 
@@ -3815,7 +3829,11 @@ const { themeColor } = require('@edulastic/colors')
                           viewY += annotation.size
                         }
 
-                        if (type === 'point') {
+                        if (
+                          type === 'point' ||
+                          type === 'video' ||
+                          type === 'image'
+                        ) {
                           viewY = (0, _utils.scaleUp)(svg, { viewY: viewY })
                             .viewY
                         }
@@ -3831,7 +3849,11 @@ const { themeColor } = require('@edulastic/colors')
                         var modelX = parseInt(t.getAttribute('x'), 10) + deltaX
                         var viewX = modelX
 
-                        if (type === 'point') {
+                        if (
+                          type === 'point' ||
+                          type === 'video' ||
+                          type === 'image'
+                        ) {
                           viewX = (0, _utils.scaleUp)(svg, { viewX: viewX })
                             .viewX
                         }
@@ -4719,6 +4741,7 @@ const { themeColor } = require('@edulastic/colors')
           input.style.top = e.clientY + 'px'
           input.style.left = e.clientX + 'px'
           input.style.fontSize = _textSize + 'px'
+          input.style.zIndex = 1
 
           input.addEventListener('blur', handleInputBlur)
           input.addEventListener('keyup', handleInputKeyup)

d7ef664146a389895054b03137969f595fc0d3e1
Prithvi Raju M <aga5tya@yahoo.com>
2020-10-30 16:14:19 +0530

    chore: fix the service worker endpoint on prod build ---

--- src/service-worker-registration.js
+++ src/service-worker-registration.js
@@ -97,7 +97,7 @@ function checkValidServiceWorker(swUrl, config) {
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
+      const swUrl = `${window.location.origin}/service-worker.js`
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.

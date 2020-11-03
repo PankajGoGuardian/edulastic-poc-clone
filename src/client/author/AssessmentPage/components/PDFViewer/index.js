22d063867f72707ad9ddf96e37b4eab8b311ff0b
snapwizshivam <38942634+snapwizshivam@users.noreply.github.com>
2020-11-03 12:46:41 +0530

    fix(client): annotation renders on multiple  pages fix (#12456)

--- src/client/author/AssessmentPage/components/PDFViewer/index.js
+++ src/client/author/AssessmentPage/components/PDFViewer/index.js
@@ -148,7 +148,7 @@ const PDFViewer = ({
       viewerRef.current.appendChild(_page)
       // TODO: dont rely on currentPage number as documentId !!!
       const RENDER_OPTIONS = {
-        documentId: pageNumber,
+        documentId: currentPage,
         pdfDocument,
         scale: pdfScale || 1.33,
         rotate: rotate || 0,

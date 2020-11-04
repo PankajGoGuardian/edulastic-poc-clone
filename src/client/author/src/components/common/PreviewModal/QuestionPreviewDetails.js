baa3cd63b294e2d922970bab45aa55fdf32ab6eb
sanjayupasi <57387252+sanjayupasi@users.noreply.github.com>
2020-11-04 17:25:42 +0530

    test(poc): added new data-cy attr in test/item review (#12410)
    * test(poc): added new data-cy attr in test/item review
    
    * test(poc): added new data-cy attr in test/item review
    
    * test(poc): added/changed data-cy attr in test/item revie
    
    * test(poc): added/changed data-cy attr in test/item revie
    
    * test(poc): chaged parm name

--- src/client/author/src/components/common/PreviewModal/QuestionPreviewDetails.js
+++ src/client/author/src/components/common/PreviewModal/QuestionPreviewDetails.js
@@ -16,43 +16,45 @@ const QuestionPreviewDetails = ({
     <FlexWrap>
       <DetailRow font={11}>
         <label>ID: </label>
-        <span>{id?.length > 6 ? id.substr(id.length - 6) : id || '--'}</span>
+        <span data-cy="item-id-on-preview">
+          {id?.length > 6 ? id.substr(id.length - 6) : id || '--'}
+        </span>
       </DetailRow>
       <DetailRow>
         <label>Owner: </label>
-        <span>{createdBy.name || '--'}</span>
+        <span data-cy="teacher-name-on-preview">{createdBy.name || '--'}</span>
       </DetailRow>
       <DetailRow font={11}>
         <label>Points: </label>
-        <span>{maxScore || '--'}</span>
+        <span data-cy="points-on-preview">{maxScore || '--'}</span>
       </DetailRow>
     </FlexWrap>
 
     <FlexWrap border="none" justify="flex-start">
       <DetailRow direction="column">
         <label>Depth of Knowledge</label>
-        <span>{depthOfKnowledge || '--'}</span>
+        <span data-cy="dok-on-preview">{depthOfKnowledge || '--'}</span>
       </DetailRow>
       <DetailRow direction="column">
         <label>Difficulty Level</label>
-        <span>{authorDifficulty || '--'}</span>
+        <span data-cy="diff-on-preview">{authorDifficulty || '--'}</span>
       </DetailRow>
       <DetailRow direction="column">
         <label>Bloom’s Taxonomy</label>
-        <span>{bloomsTaxonomy || '--'}</span>
+        <span data-cy="tax-on-preview">{bloomsTaxonomy || '--'}</span>
       </DetailRow>
     </FlexWrap>
 
     <FlexWrap border="none" direction="column" align="flex-start">
       <DetailRow className="standards">
         <label>Standard: </label>
-        <div>
+        <div data-cy="standards-on-preview">
           <Tags tags={standards} show={7} />
         </div>
       </DetailRow>
       <DetailRow className="tags">
         <label>Tags: </label>
-        <div>
+        <div data-cy="tags-on-preview">
           {(tags &&
             tags.length &&
             tags.map((tag) => <span>{tag.tagName}</span>)) ||

a5a4340cda0671b911bc5fcd86d1c2b3ce76f88a
viraingarg-snapwiz <53215503+viraingarg-snapwiz@users.noreply.github.com>
2020-11-05 19:13:58 +0530

    fix: added pagination in advanced assignments [EV-21469] (#12453) (#12495)

--- src/client/author/src/reducers/assignments.js
+++ src/client/author/src/reducers/assignments.js
@@ -43,6 +43,8 @@ const initialState = {
   isAdvancedView: false,
   syncWithGoogleClassroomInProgress: false,
   bulkActionInprogress: false,
+  totalAssignmentsClasses: 0,
+  assignmentStatusCounts: { notOpen: 0, inProgress: 0, inGrading: 0, done: 0 },
 }
 
 const reducer = (state = initialState, { type, payload }) => {
@@ -77,13 +79,25 @@ const reducer = (state = initialState, { type, payload }) => {
       return { ...state, loading: false, error: payload.error }
     case RECEIVE_ASSIGNMENT_CLASS_LIST_REQUEST:
       return { ...state, loading: true }
-    case RECEIVE_ASSIGNMENT_CLASS_LIST_SUCCESS:
+    case RECEIVE_ASSIGNMENT_CLASS_LIST_SUCCESS: {
+      const {
+        notOpen = 0,
+        inProgress = 0,
+        inGrading = 0,
+        done = 0,
+        total = 0,
+        assignments = [],
+        test = {},
+      } = payload?.entities || {}
       return {
         ...state,
         loading: false,
-        assignmentClassList: payload.entities.assignments,
-        currentTest: payload.entities.test,
+        assignmentClassList: assignments,
+        currentTest: test,
+        totalAssignmentsClasses: total,
+        assignmentStatusCounts: { notOpen, inProgress, inGrading, done },
       }
+    }
     case RECEIVE_ASSIGNMENT_CLASS_LIST_ERROR:
       return {
         ...state,

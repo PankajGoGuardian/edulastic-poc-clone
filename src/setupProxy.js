b26d83b910322e2ba1a7375f5f378905b8b42294
Prajwal-Snapwiz <prajwal.venkatesh@gmail.com>
2020-11-09 01:43:10 +0530

    chore: handle EA user proxy route + additional routes [EV-20862]

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,9 +4,20 @@ const express = require('express')
 const rootPath = path.join(__dirname, '..', 'dist', 'login', 'index.html')
 const router = express.Router()
 
-router.get('/', (_, res) => {
-  res.set('cache-control', 'no-store')
-  res.sendFile(rootPath)
+router.get('/', (req, res, next) => {
+  const query = new URLSearchParams(req.query)
+  if (
+    query.has('userId') ||
+    query.has('role') ||
+    query.has('token') ||
+    query.has('itemBank') ||
+    query.has('addAccount')
+  ) {
+    next()
+  } else {
+    res.set('cache-control', 'no-store')
+    res.sendFile(rootPath)
+  }
 })
 
 router.get('/resetPassword', (_, res) => {
@@ -14,12 +25,47 @@ router.get('/resetPassword', (_, res) => {
   res.sendFile(rootPath)
 })
 
-router.get('/districtLogin', (_, res) => {
+router.get('/districtLogin/:shortName', (_, res) => {
   res.set('cache-control', 'no-store')
   res.sendFile(rootPath)
 })
 
 router.get('/district/:shortName', (_, res) => {
+  res.set('cache-control', 'no-store')
+  res.sendFile(rootPath)
+})
+
+router.get('/school/:shortName', (_, res) => {
+  res.set('cache-control', 'no-store')
+  res.sendFile(rootPath)
+})
+
+router.get('/partnerLogin/:partner/Signup', (_, res) => {
+  res.set('cache-control', 'no-store')
+  res.sendFile(rootPath)
+})
+
+router.get('/partnerLogin/:partner', (_, res) => {
+  res.set('cache-control', 'no-store')
+  res.sendFile(rootPath)
+})
+
+router.get('/partnerLogin/:partner/GetStarted', (_, res) => {
+  res.set('cache-control', 'no-store')
+  res.sendFile(rootPath)
+})
+
+router.get('/AdminSignup', (_, res) => {
+  res.set('cache-control', 'no-store')
+  res.sendFile(rootPath)
+})
+
+router.get('/partnerLogin/:partner/AdminSignup', (_, res) => {
+  res.set('cache-control', 'no-store')
+  res.sendFile(rootPath)
+})
+
+router.get('/partnerLogin/:partner/StudentSignup', (_, res) => {
   res.set('cache-control', 'no-store')
   res.sendFile(rootPath)
 })

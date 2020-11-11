49a6101dfbd830f1e843f9744214566948f8d92c
Neeraj Negi <70655322+neerajnegisnapwiz@users.noreply.github.com>
2020-11-11 19:36:46 +0530

    fix(client): MAR peer-progress-analysis report unnecessary api call fix [EV-20119] (#12572)

--- src/client/author/Reports/subPages/multipleAssessmentReport/PeerProgressAnalysis/index.js
+++ src/client/author/Reports/subPages/multipleAssessmentReport/PeerProgressAnalysis/index.js
@@ -74,7 +74,7 @@ const usefetchProgressHook = (
         schoolIds,
       })
     }
-  }, [settings, compareBy.key, ddfilter])
+  }, [settings, compareBy.key])
 }
 
 const PeerProgressAnalysis = ({

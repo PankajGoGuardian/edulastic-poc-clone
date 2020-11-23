dda9d7ecb8bd94c06560a08af54956f050b7b2d8
madhur-vyas <72852437+madhur-vyas@users.noreply.github.com>
2020-11-23 19:13:17 +0530

    fix(client): update perfect scrollbar on x-scroll to fix library bug [EV-21941] (#12641)
    Co-authored-by: Madhur <madhurvyas001@gmail.com>

--- src/client/assessment/components/TestItemPreview/components/StudentWork.js
+++ src/client/assessment/components/TestItemPreview/components/StudentWork.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import PropTypes from 'prop-types'
 import { FlexContainer } from '@edulastic/common'
 import PerfectScrollbar from 'react-perfect-scrollbar'
 import styled from 'styled-components'
 
-const StyleImage = styled.img`
+const StyledImage = styled.img`
   max-height: 600px;
 `
 const FlexItem = styled.div`
@@ -11,26 +12,33 @@ const FlexItem = styled.div`
 `
 
 const StudentWork = ({ imageAttachments }) => {
+  const ScrollbarRef = useRef(null)
+
+  // This is needed for fixing a known bug in perfect-scrollbar library where
+  // scrollbar exceeds the content length on some screen sizes
+  // https://github.com/mdbootstrap/perfect-scrollbar/issues/920
+  const handleScroll = () => ScrollbarRef.current?.updateScroll()
+
   return (
-    <PerfectScrollbar>
+    <PerfectScrollbar ref={ScrollbarRef} onScrollX={handleScroll}>
       <FlexContainer
         alignItems="flex-end"
         flexDirection="row"
         flexWrap="no-wrap"
         justifyContent="space-between"
       >
-        {imageAttachments && imageAttachments.length > 0 && (
-          <>
-            {imageAttachments.map((imageFile) => (
-              <FlexItem>
-                <StyleImage src={imageFile.source} alt={imageFile.name} />
-              </FlexItem>
-            ))}
-          </>
-        )}
+        {imageAttachments.map((imageFile) => (
+          <FlexItem>
+            <StyledImage src={imageFile.source} alt={imageFile.name} />
+          </FlexItem>
+        ))}
       </FlexContainer>
     </PerfectScrollbar>
   )
+}
+
+StudentWork.propTypes = {
+  imageAttachments: PropTypes.array.isRequired,
 }
 
 export default StudentWork

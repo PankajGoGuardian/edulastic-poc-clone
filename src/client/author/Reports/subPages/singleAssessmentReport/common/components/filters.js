437dd9dec2ffe4e3ebf826efe9cdef9527ac5b01
Saraansh Baranwal <saraansh@users.noreply.github.com>
2020-12-03 17:36:27 +0530

    [fix] assessmentTypes condition fix [EV-22503] (#12900)

--- src/client/author/Reports/subPages/singleAssessmentReport/common/components/filters.js
+++ src/client/author/Reports/subPages/singleAssessmentReport/common/components/filters.js
@@ -309,9 +309,9 @@ const SingleAssessmentReportFilters = ({
                 updateFilterDropdownCB(e.join(','), 'assessmentTypes', true)
               }
               value={
-                !(filters.assessmentTypes || filters.assessmentTypes === 'All')
-                  ? []
-                  : filters.assessmentTypes.split(',')
+                filters.assessmentTypes && filters.assessmentTypes !== 'All'
+                  ? filters.assessmentTypes.split(',')
+                  : []
               }
               options={staticDropDownData.assessmentType.filter(
                 (a) => a.key !== 'All'

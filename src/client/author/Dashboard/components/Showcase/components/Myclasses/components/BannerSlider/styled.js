a245feee73bea7f9f7c561db7c8db1555c1eb7c1
mangaljain <mangal.jain@snapwiz.com>
2020-12-22 15:40:03 +0530

    Update styled.js

--- src/client/author/Dashboard/components/Showcase/components/Myclasses/components/BannerSlider/styled.js
+++ src/client/author/Dashboard/components/Showcase/components/Myclasses/components/BannerSlider/styled.js
@@ -104,26 +104,3 @@ export const SlideDescription = styled.span`
     cursor: pointer;
   }
 `
-
-export const BundleContainer = styled.div`
-  width: 241px;
-  height: 169px;
-  display: flex;
-  align-items: flex-end;
-  margin: 0px 10px 10px 0px;
-  border-radius: 10px;
-  padding: 12px 20px;
-  color: ${white};
-  cursor: pointer;
-  background-image: ${(props) => `url(${props.bgImage})`};
-  background-size: 100% 100%;
-  background-position: top left;
-  background-repeat: no-repeat;
-`
-
-export const Bottom = styled.div`
-  font-size: 13px;
-  height: 40px;
-  overflow: hidden;
-  font-weight: 600;
-`

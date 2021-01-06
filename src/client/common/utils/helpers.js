a8186df7104b6322157eaa9c98fd494f98778f6e
viraingarg-snapwiz <53215503+viraingarg-snapwiz@users.noreply.github.com>
2021-01-06 13:07:04 +0530

    fix: student name can have - and ' [EV-23572] (#13293)

--- src/client/common/utils/helpers.js
+++ src/client/common/utils/helpers.js
@@ -294,8 +294,9 @@ export const nameValidator = (name) => {
   // should contain at least three char (eg: stu, st1 s01)
   // should contain only one space between name/word
   // can contain number after initial alphabet
+  // can contain special characters ' and - after initial alphabet
 
-  const namePattern = /^(?!\d)[a-zA-Z\d]{2,}[a-zA-Z\d]+(?: [a-zA-z\d]+)*$/
+  const namePattern = /^(?!\d)(?!-)(?!')[a-zA-Z\d-']{2,}(?: [a-zA-z\d-']+)*$/
   if (!trimmedName || !namePattern.test(trimmedName)) {
     return false
   }

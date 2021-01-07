2738b0a82962e0fd03b52100e680b42dd465c322
pin1128 <32391750+pin1128@users.noreply.github.com>
2021-01-07 07:29:42 +0200

    fix(client): add utils for student attempt [EV-23146] (#13309)
    * fix(client): add utils for student attempt [EV-23146]
    
    * fix(client): remove alert [EV-23146]

--- src/studentEntry.js
+++ src/studentEntry.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import i18n, { I18nextProvider } from '@edulastic/localization'
 import { ConnectedRouter } from 'connected-react-router'
-import  Spin  from 'antd/es/spin'
+import Spin from 'antd/es/spin'
 
 import 'font-awesome/css/font-awesome.css'
 import 'antd/dist/antd.css'
@@ -12,41 +12,45 @@ import { init as SentryInit } from '@sentry/browser'
 import { Integrations } from '@sentry/tracing'
 import './client/index.css'
 import { updateSentryScope } from '@edulastic/api/src/utils/Storage'
-import AppStudent from './client/AppStudent';
+import AppStudent from './client/AppStudent'
 import configureStore, { history } from './client/configureStoreStudents'
 import AppConfig from './app-config'
-import { studentReducers } from './client/studentReducers';
-import { studentsSagas } from './client/studentSagas';
+import { studentReducers } from './client/studentReducers'
+import { studentsSagas } from './client/studentSagas'
 import { fetchUserAction } from './client/student/Login/ducks'
+import { isMobileDevice, isIOS } from './client/platform'
 
 if (AppConfig.sentryURI) {
-	SentryInit({
-	  whitelistUrls: [AppConfig.sentryWhiteListURLRegex],
-	  dsn: AppConfig.sentryURI,
-	  release: AppConfig.appVersion,
-	  environment: AppConfig.appStage || 'development',
-	  maxValueLength: 600, // defaults to 250 chars, we will need more info recorded.
-	  ignoreErrors: AppConfig.sentryIgnoreErrors,
-	  integrations: [new Integrations.BrowserTracing()],
-	  tracesSampleRate: 0.1, // we sample only 10% of the data from clients.
-	})
-	updateSentryScope()
-  }
+  SentryInit({
+    whitelistUrls: [AppConfig.sentryWhiteListURLRegex],
+    dsn: AppConfig.sentryURI,
+    release: AppConfig.appVersion,
+    environment: AppConfig.appStage || 'development',
+    maxValueLength: 600, // defaults to 250 chars, we will need more info recorded.
+    ignoreErrors: AppConfig.sentryIgnoreErrors,
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 0.1, // we sample only 10% of the data from clients.
+  })
+  updateSentryScope()
+}
 
-  // login - redux store
+window.isMobileDevice = isMobileDevice()
+window.isIOS = isIOS()
+
+// login - redux store
 const { store } = configureStore({}, studentReducers, studentsSagas)
 store.dispatch(fetchUserAction())
 
 const RootComp = () => (
-	<I18nextProvider i18n={i18n}>
-	  <Provider store={store}>
-		<ConnectedRouter history={history}>
-		  <Suspense fallback={<Spin />}>
-			<AppStudent />
-		  </Suspense>
-		</ConnectedRouter>
-	  </Provider>
-	</I18nextProvider>
-  )
-  
-  ReactDOM.render(<RootComp />, document.getElementById('app-students'))
+  <I18nextProvider i18n={i18n}>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Suspense fallback={<Spin />}>
+          <AppStudent />
+        </Suspense>
+      </ConnectedRouter>
+    </Provider>
+  </I18nextProvider>
+)
+
+ReactDOM.render(<RootComp />, document.getElementById('app-students'))

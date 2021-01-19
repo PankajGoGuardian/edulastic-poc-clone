b76be77dd1b99648379aee4e8146e1075dd6197c
mangaljain <mangal.jain@snapwiz.com>
2021-01-19 12:06:46 +0530

    feat(ui): used isItemBankTrialUsed flag [EV-23922]

---
+++ src/client/author/Dashboard/components/Showcase/components/Myclasses/components/FeaturedContentBundle/ItemBankTrialUsedModal/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { CustomModalStyled, EduButton } from '@edulastic/common'
+
+const Footer = ({ itemBankNotUsed, handleCloseModal }) => {
+  return (
+    <>
+      {itemBankNotUsed ? (
+        <>
+          <EduButton isGhost isBlue onClick={handleCloseModal}>
+            Cancle
+          </EduButton>
+          <EduButton isBlue>Upgrade</EduButton>
+        </>
+      ) : (
+        <>
+          <EduButton isGhost isBlue onClick={handleCloseModal}>
+            Cancle
+          </EduButton>
+          <EduButton isBlue>Purchase</EduButton>
+        </>
+      )}
+    </>
+  )
+}
+
+const ItemBankTrialUsedModal = ({
+  title,
+  isVisible,
+  handleCloseModal,
+  itemBankNotUsed,
+}) => {
+  return (
+    <CustomModalStyled
+      centered
+      title=""
+      footer={
+        <Footer
+          itemBankNotUsed={itemBankNotUsed}
+          handleCloseModal={handleCloseModal}
+        />
+      }
+      visible={isVisible}
+      onCancel={handleCloseModal}
+    >
+      {itemBankNotUsed ? (
+        <p>
+          Kindly Upgrade to premium version of app for trying any other item
+          bank.
+        </p>
+      ) : (
+        <p>
+          Free trail for <b> {title} </b> is already utilized. Kindly purchase
+          to access the content.
+        </p>
+      )}
+    </CustomModalStyled>
+  )
+}
+
+ItemBankTrialUsedModal.propTypes = {
+  title: PropTypes.string.isRequired,
+  isVisible: PropTypes.bool.isRequired,
+}
+
+export default ItemBankTrialUsedModal

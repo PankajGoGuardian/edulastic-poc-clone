d0d36cfdf9051873a462695d6d9338bfdf8212c1
Arun Sasidharan <43816559+arun-sasidharan@users.noreply.github.com>
2021-01-21 17:32:37 +0530

    fix(client): regrade actions disable and enable based on changes [EV-… (#13481)
    * fix(client): regrade actions disable and enable based on changes [EV-23982]
    
    * Update ducks.js
    
    Co-authored-by: nagarajanSnapwiz <44694625+nagarajanSnapwiz@users.noreply.github.com>

--- src/client/author/Regrade/MainContent.js
+++ src/client/author/Regrade/MainContent.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Radio, Row } from 'antd'
+import { Radio, Row, Spin } from 'antd'
 import { Container, InputsWrapper, OptionTitle } from './styled'
 import AssignmentsTable from './Table'
 
@@ -11,90 +11,111 @@ const ACTIONS = {
   DISCARD: 'DISCARD',
 }
 
-const MainContent = ({ regradeSettings, onUpdateSettings }) => {
+const MainContent = ({
+  regradeSettings,
+  onUpdateSettings,
+  isLoadRegradeSettings,
+  availableRegradeSettings,
+}) => {
   const {
     editedQuestion,
     addedQuestion,
     testSettings,
   } = regradeSettings.options
+  if (isLoadRegradeSettings) {
+    return <Spin />
+  }
+  const showAdd = availableRegradeSettings.includes('ADD')
+  const showEdit = availableRegradeSettings.includes('EDIT')
+  const showRemove = availableRegradeSettings.includes('REMOVE')
+  const showSettings = availableRegradeSettings.includes('SETTINGS')
   return (
     <Container>
       <h2>
         Following assignments will be updated and re-scored if applicable.
       </h2>
       <AssignmentsTable />
-      <InputsWrapper>
-        <Row>
-          <OptionTitle>Added Items</OptionTitle>
-        </Row>
-        <Group
-          style={{ marginLeft: '20px' }}
-          defaultValue={addedQuestion}
-          onChange={(e) => onUpdateSettings('addedQuestion', e.target.value)}
-        >
-          <Row key="addedQuestion">
-            <Radio data-cy="no-points" value={ACTIONS.SKIP}>
-              Give ZERO Points
-            </Radio>
-            <Radio data-cy="full-points" value={ACTIONS.SCORE}>
-              Give FULL Points
-            </Radio>
-            <Radio data-cy="manual-points" value={ACTIONS.MANUAL}>
-              Manually Grade
-            </Radio>
+      {showAdd && (
+        <InputsWrapper>
+          <Row>
+            <OptionTitle>Added Items</OptionTitle>
           </Row>
-        </Group>
-      </InputsWrapper>
-      <InputsWrapper>
-        <Row>
-          <OptionTitle>Edit Items</OptionTitle>
-        </Row>
-        <Group
-          style={{ marginLeft: '20px' }}
-          defaultValue={editedQuestion}
-          onChange={(e) => onUpdateSettings('editedQuestion', e.target.value)}
-        >
-          <Row key="editedQuestion">
-            <Radio data-cy="skip-grading" value={ACTIONS.SKIP}>
-              Skip rescoring
-            </Radio>
-            <Radio data-cy="restore-grading" value={ACTIONS.SCORE}>
-              Rescore automatically
-            </Radio>
-            <Radio data-cy="manual-grading" value={ACTIONS.MANUAL}>
-              Mark for manual grading
-            </Radio>
-          </Row>
-        </Group>
-      </InputsWrapper>
-      <InputsWrapper>
-        <Row>
-          <OptionTitle>Add, Apply updated settings options </OptionTitle>
-        </Row>
-        <Row>
           <Group
             style={{ marginLeft: '20px' }}
-            defaultValue={testSettings}
-            onChange={(e) => onUpdateSettings('testSettings', e.target.value)}
+            defaultValue={addedQuestion}
+            onChange={(e) => onUpdateSettings('addedQuestion', e.target.value)}
           >
-            <Row>
-              <Radio data-cy="choose-all" value="ALL">
-                Choose all assignments
+            <Row key="addedQuestion">
+              <Radio data-cy="no-points" value={ACTIONS.SKIP}>
+                Give ZERO Points
               </Radio>
-              <Radio data-cy="exclude-overidden" value="EXCLUDE">
-                Exclude assignments where overridden
+              <Radio data-cy="full-points" value={ACTIONS.SCORE}>
+                Give FULL Points
+              </Radio>
+              <Radio data-cy="manual-points" value={ACTIONS.MANUAL}>
+                Manually Grade
               </Radio>
             </Row>
           </Group>
-        </Row>
-      </InputsWrapper>
-      <InputsWrapper>
-        <Row>
-          <OptionTitle>
-            Removed items will be discard from assignment
-          </OptionTitle>
-        </Row>
-      </InputsWrapper>
+        </InputsWrapper>
+      )}
+
+      {showEdit && (
+        <InputsWrapper>
+          <Row>
+            <OptionTitle>Edit Items</OptionTitle>
+          </Row>
+          <Group
+            style={{ marginLeft: '20px' }}
+            defaultValue={editedQuestion}
+            onChange={(e) => onUpdateSettings('editedQuestion', e.target.value)}
+          >
+            <Row key="editedQuestion">
+              <Radio data-cy="skip-grading" value={ACTIONS.SKIP}>
+                Skip rescoring
+              </Radio>
+              <Radio data-cy="restore-grading" value={ACTIONS.SCORE}>
+                Rescore automatically
+              </Radio>
+              <Radio data-cy="manual-grading" value={ACTIONS.MANUAL}>
+                Mark for manual grading
+              </Radio>
+            </Row>
+          </Group>
+        </InputsWrapper>
+      )}
+      {showSettings && (
+        <InputsWrapper>
+          <Row>
+            <OptionTitle>Add, Apply updated settings options </OptionTitle>
+          </Row>
+          <Row>
+            <Group
+              style={{ marginLeft: '20px' }}
+              defaultValue={testSettings}
+              onChange={(e) => onUpdateSettings('testSettings', e.target.value)}
+            >
+              <Row>
+                <Radio data-cy="choose-all" value="ALL">
+                  Choose all assignments
+                </Radio>
+                <Radio data-cy="exclude-overidden" value="EXCLUDE">
+                  Exclude assignments where overridden
+                </Radio>
+              </Row>
+            </Group>
+          </Row>
+        </InputsWrapper>
+      )}
+      {showRemove && (
+        <InputsWrapper>
+          <Row>
+            <OptionTitle>
+              Removed items will be discard from assignment
+            </OptionTitle>
+          </Row>
+        </InputsWrapper>
+      )}
     </Container>
   )
 }

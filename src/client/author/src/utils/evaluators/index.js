02a261eb9a534c8c37fc15b4544fb345fbc04f35
Gourav Mukherjee <50568503+mgourav-snapwiz@users.noreply.github.com>
2021-01-22 10:54:51 +0530

    fix(client): allowing to save question for graph if it contains latex and points [EV-24038] (#13529)
    * fix(client): allowing to save question for graph if it contains latex and points [EV-24038]
    
    * fix(client): allowing to check answer for graph [EV-24038]

--- src/client/author/src/utils/evaluators/index.js
+++ src/client/author/src/utils/evaluators/index.js
@@ -27,7 +27,7 @@ import {
 const mathEvaluate = async (data, type) => {
   // getting evaluation from backend (EV-7432)
   if (type === questionType.GRAPH) {
-    const validationObj = data?.validation
+    const validationObj = data?.validation || {}
     const correctAnswers = validationObj?.validResponse?.value || []
     const alternateAnswers = (validationObj?.altResponses || []).map(
       (altResponse) => altResponse.value
@@ -35,13 +35,16 @@ const mathEvaluate = async (data, type) => {
     const allAnswers = [correctAnswers, ...alternateAnswers].filter(
       (i) => i.length
     )
-    if (allAnswers.length === 0) {
+    const { points, latex } = validationObj
+
+    if (allAnswers.length === 0 && !points && !latex) {
       const error = new Error()
       error.message = 'Questions should have answers set'
       throw error
     }
   }
   const result = await evaluateApi.evaluate(data, type)
+
   return result
 }
 

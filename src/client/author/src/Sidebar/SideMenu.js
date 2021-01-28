11ed51ddff13ccaa1901c9009ad236f8dae377e5
mangaljain <mangal.jain@snapwiz.com>
2021-01-28 21:23:34 +0530

    fix: removed the duplicate data-cy attr [EV-24065] (#13624)

--- src/client/author/src/Sidebar/SideMenu.js
+++ src/client/author/src/Sidebar/SideMenu.js
@@ -425,11 +425,7 @@ class SideMenu extends Component {
             </Link>
           </Menu.Item>
           {!isPublisher && (
-            <Menu.Item
-              data-cy="subscription"
-              key="2"
-              className="removeSelectedBorder"
-            >
+            <Menu.Item key="2" className="removeSelectedBorder">
               <Link to="/author/subscription">
                 <IconSubscriptionHighlight />{' '}
                 <span data-cy="subscription">

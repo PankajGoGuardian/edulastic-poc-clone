b6372de3a9fc4abc45b668fa61cf4e85bff4b695
aniket-snapwiz <75801936+aniket-snapwiz@users.noreply.github.com>
2021-02-02 10:32:09 +0530

    fix(ui): make transparent possible response box option functional [EV… (#13680)
    * fix(ui): make transparent possible response box option functional [EV-20369]
    
    * refactor: add transparent as background color instead of empty string [EV-20369]

--- src/client/assessment/components/ChoiceItem.js
+++ src/client/assessment/components/ChoiceItem.js
@@ -2,16 +2,20 @@ import styled from 'styled-components'
 import { greyThemeLight, themeColorHoverBlue } from '@edulastic/colors'
 
 export const ChoiceItem = styled.div.attrs(() => ({
-  className: 'draggable_box',
+  className: (props) =>
+    `${props.className ? props.className : 'draggable_box'}`,
 }))`
   padding: 6px;
   min-width: 100px;
   max-width: 400px;
-  border: 1px solid ${greyThemeLight};
+  border: ${(props) =>
+    props.transparentResponses ? 'none' : `1px solid ${greyThemeLight}`};
   border-radius: 4px;
   transform: translate3d(0px, 0px, 0px);
   background-color: ${(props) =>
-    props.theme.widgets.clozeDragDrop.responseBoxBgColor};
+    props.transparentResponses
+      ? 'transparent'
+      : props.theme.widgets.clozeDragDrop.responseBoxBgColor};
   margin-right: 5px;
   margin-bottom: 5px;
   &:hover {

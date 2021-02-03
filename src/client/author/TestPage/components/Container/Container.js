5a7d6c31f0a0214fcfbb99233f4c9e2656662919
viraingarg-snapwiz <53215503+viraingarg-snapwiz@users.noreply.github.com>
2021-02-03 21:36:41 +0530

    fix: enabled sharing options in test with EC, EngageNY and SmartStart collection items [EV-23763] (#13772)

--- src/client/author/TestPage/components/Container/Container.js
+++ src/client/author/TestPage/components/Container/Container.js
@@ -13,7 +13,11 @@ import {
   Progress,
   CustomPrompt,
 } from '@edulastic/common'
-import { test as testContants, roleuser } from '@edulastic/constants'
+import {
+  test as testContants,
+  roleuser,
+  collections as collectionsConstant,
+} from '@edulastic/constants'
 import { testsApi } from '@edulastic/api'
 import { themeColor } from '@edulastic/colors'
 import {
@@ -114,6 +118,7 @@ const {
   ITEM_GROUP_TYPES,
   ITEM_GROUP_DELIVERY_TYPES,
 } = testContants
+const { nonPremiumCollectionsToShareContent } = collectionsConstant
 
 class Container extends PureComponent {
   constructor() {
@@ -1044,11 +1049,16 @@ class Container extends PureComponent {
       !showEditButton &&
       !showDuplicateButton &&
       (testStatus === 'draft' || editEnable)
+
+    const premiumOrgCollections = collections.filter(
+      ({ _id }) =>
+        !Object.keys(nonPremiumCollectionsToShareContent).includes(_id)
+    )
     const testItems = (itemGroups || []).flatMap(
       (itemGroup) => itemGroup.items || []
     )
     const hasPremiumQuestion = !!testItems.find((i) =>
-      hasUserGotAccessToPremiumItem(i.collections, collections)
+      hasUserGotAccessToPremiumItem(i.collections, premiumOrgCollections)
     )
 
     const gradeSubject = { grades, subjects }

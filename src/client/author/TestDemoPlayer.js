370abeb3f5a8e296867822b9b177ff486abcf351
viraingarg-snapwiz <53215503+viraingarg-snapwiz@users.noreply.github.com>
2021-02-03 16:11:23 +0530

    fix: exit button not working in demo assessment player [EV-24197] (#13754)

--- src/client/author/TestDemoPlayer.js
+++ src/client/author/TestDemoPlayer.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import { withRouter } from 'react-router'
 import PropTypes from 'prop-types'
-import AssessmentPlayer from '../assessment'
-import { WithResources } from '@edulastic/common/src/HOC/withResources'
-import AppConfig from '../../app-config'
 import { Spin } from 'antd'
+import { WithResources } from '@edulastic/common/src/HOC/withResources'
+import AssessmentPlayer from '../assessment'
+import AppConfig from '../../app-config'
 
 const DemoPlayer = ({ match }) => {
   const { id: testId } = match.params
-/**
- * Assessment player need to be wrapped with jquery loading
- */
-  return <WithResources
-  resources={[`${AppConfig.jqueryPath}/jquery.min.js`]}
-  fallBack={<Spin />}
->
-    <AssessmentPlayer testId={testId} preview demo />
-  </WithResources>
+  /**
+   * Assessment player need to be wrapped with jquery loading
+   */
+  const closeTestPreviewModal = () => {
+    window.location.href = '/'
+  }
+  return (
+    <WithResources
+      resources={[`${AppConfig.jqueryPath}/jquery.min.js`]}
+      fallBack={<Spin />}
+    >
+      <AssessmentPlayer
+        testId={testId}
+        preview
+        demo
+        closeTestPreviewModal={closeTestPreviewModal}
+      />
+    </WithResources>
+  )
 }
 
 DemoPlayer.propTypes = {

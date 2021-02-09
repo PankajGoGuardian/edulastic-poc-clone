92933e9f33890bdabf34da5ad81aec60ee64e1e4
pint1128 <clearg1929@gmail.com>
2021-02-09 05:23:12 +0200

    fix(client): refactor language reducer [EV-24772]

--- src/client/assessment/widgets/MultipleChoice/helpers.js
+++
@@ -1,6 +0,0 @@
-export const changeDataToPreferredLanguage = (questionData, languageData) => {
-  const languageDataKeys = Object.keys(languageData || {})
-  languageDataKeys.forEach((key) => {
-    questionData[key] = languageData[key]
-  })
-}

743ed41ee281f3d8aa4198d4dd0d85a2cb93c562
mangaljain <mangal.jain@snapwiz.com>
2021-03-01 20:29:50 +0530

    fix(ui): multiple purchase modal ui changes [EV-25505]

--- src/client/author/src/components/common/PurchaseModals/SubscriptionAddonModal/index.js
+++ src/client/author/src/components/common/PurchaseModals/SubscriptionAddonModal/index.js
@@ -1,11 +1,26 @@
-import { camelCase } from 'lodash'
-import React, { useState } from 'react'
+import { camelCase, isNumber } from 'lodash'
+import React, { useMemo, useState } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { CheckboxLabel, CustomModalStyled, EduButton } from '@edulastic/common'
+import {
+  CheckboxLabel,
+  CustomModalStyled,
+  EduButton,
+  FieldLabel,
+  notification,
+  NumberInputStyled,
+  TextInputStyled,
+} from '@edulastic/common'
 import { Tooltip } from 'antd'
 import { title } from '@edulastic/colors'
-import { ModalBody, AddonList, FlexRow, Total } from './styled'
+import {
+  ModalBody,
+  AddonList,
+  FlexRow,
+  Total,
+  EmailWrapper,
+  NumberInputWrapper,
+} from './styled'
 
 const getInitialSelectedProductIds = ({
   defaultSelectedProductIds,
@@ -45,7 +60,17 @@ const SubscriptionAddonModal = ({
   teacherPremium,
   itemBankPremium,
   showRenewalOptions,
+  showMultiplePurchaseModal,
+  setEmailIds,
+  setProductsCart,
+  products,
 }) => {
+  const [emailValues, setEmailValues] = useState('')
+  const [quantities, setQuantities] = useState({})
+
+  const emailsArray =
+    (emailValues && emailValues.replace(/\s/g, '').split(/,|\n/)) || []
+
   const [selectedProductIds, setSelectedProductIds] = useState(
     getInitialSelectedProductIds({
       defaultSelectedProductIds,
@@ -64,9 +89,36 @@ const SubscriptionAddonModal = ({
     })
   )
 
+  const _totalPrice = useMemo(() => {
+    return itemBankPremium.reduce((a, c) => {
+      if (selectedProductIds.includes(c.id)) {
+        return a + c.price * (isNumber(quantities[c.id]) ? quantities[c.id] : 1)
+      }
+      return a
+    }, teacherPremium.price * (quantities[premiumProductId] || 1))
+  }, [itemBankPremium, quantities, selectedProductIds, teacherPremium])
+
   const handleClick = () => {
-    setAddOnProductIds(selectedProductIds)
-    setTotalPurchaseAmount(totalPrice)
+    if (showMultiplePurchaseModal) {
+      if (emailsArray.length > quantities[premiumProductId]) {
+        notification({
+          type: 'info',
+          msg: 'Email count(s) can not be more than Premium count(s)',
+        })
+        return
+      }
+      const setProductQuantity = products.map((product) => ({
+        ...product,
+        quantity: quantities[product.id],
+      }))
+
+      setProductsCart(setProductQuantity)
+      setEmailIds(emailsArray)
+    } else {
+      setAddOnProductIds(selectedProductIds)
+    }
+
+    setTotalPurchaseAmount(_totalPrice)
     handleCloseModal(false)
     setShowUpgradeModal(true)
   }
@@ -74,18 +126,47 @@ const SubscriptionAddonModal = ({
   const handleOnChange = (e, id) => {
     const productPrice = e.target.value
     if (e.target.checked) {
+      const _quantities = {
+        ...quantities,
+        [id]: 1,
+      }
+      setQuantities(_quantities)
       setSelectedProductIds((x) => x.concat(id))
       setTotalPrice((currentPrice) => currentPrice + productPrice)
     } else {
+      const _quantities = {
+        ...quantities,
+        [id]: undefined,
+      }
+      setQuantities(_quantities)
       setSelectedProductIds((x) => x.filter((y) => y !== id))
       setTotalPrice((currentPrice) => currentPrice - productPrice)
     }
   }
 
+  const handleQuantityChange = (itemId) => (value) => {
+    const _quantities = {
+      ...quantities,
+      [itemId]: value,
+    }
+    setQuantities(_quantities)
+  }
+
+  const handleInputEmailAddress = (ele) => {
+    const value = ele.target.value
+    setEmailValues(value)
+  }
+
+  const modalTitle = showMultiplePurchaseModal
+    ? 'Upgrade multiple accounts'
+    : showRenewalOptions
+    ? 'Renew Subscription'
+    : 'Select Add-ons'
+
   return (
     <CustomModalStyled
       centered
-      title={showRenewalOptions ? 'Renew Subscription' : 'Select Add-ons'}
+      title={modalTitle}
       footer={[
         <EduButton
           data-cy="proceedPayment"
@@ -94,7 +175,7 @@ const SubscriptionAddonModal = ({
           width="220px"
           height="45px"
         >
-          PROCEED WITH PAYMENT
+          PROCEED WITH PAYMENT {showMultiplePurchaseModal && `$${_totalPrice}`}
         </EduButton>,
       ]}
       visible={isVisible}
@@ -119,7 +200,9 @@ const SubscriptionAddonModal = ({
         </p>
         <p> These add-ons need the premium or enterprise subscription.</p>
         <AddonList>
-          {(showRenewalOptions || !isPaidPremium) && (
+          {(showRenewalOptions ||
+            !isPaidPremium ||
+            showMultiplePurchaseModal) && (
             <FlexRow>
               <Tooltip title="Premium subscription is mandatory for Spark content">
                 <StyledCheckbox
@@ -130,25 +213,53 @@ const SubscriptionAddonModal = ({
                   {teacherPremium.name}
                 </StyledCheckbox>
               </Tooltip>
-
-              <span className="priceCol">${teacherPremium.price}</span>
+              {showMultiplePurchaseModal && (
+                <NumberInputWrapper>
+                  <NumberInputStyled
+                    value={quantities[premiumProductId] || 1}
+                    onChange={handleQuantityChange(premiumProductId)}
+                    height="28px"
+                    width="80px"
+                    data-cy="selectNumberOfPremiumSubs"
+                    min={1}
+                  />
+                </NumberInputWrapper>
+              )}
+              <span className="priceCol">
+                ${teacherPremium.price * (quantities[premiumProductId] || 1)}
+              </span>
             </FlexRow>
           )}
           {itemBankPremium.map((item) => (
             <FlexRow key={item.id}>
-              <CheckboxLabel
+              <StyledCheckbox
                 data-cy={`${camelCase(item.name)}Checkbox`}
                 value={item.price}
                 onChange={(e) => handleOnChange(e, item.id)}
                 checked={selectedProductIds.includes(item.id)}
               >
                 {item.name}
-              </CheckboxLabel>
+              </StyledCheckbox>
+              {showMultiplePurchaseModal && (
+                <NumberInputWrapper>
+                  <NumberInputStyled
+                    value={quantities[item.id]}
+                    onChange={handleQuantityChange(item.id)}
+                    height="28px"
+                    width="80px"
+                    data-cy={item.type}
+                    min={1}
+                    max={quantities[premiumProductId] || 1}
+                  />
+                </NumberInputWrapper>
+              )}
               <span
                 className="priceCol"
                 data-cy={`${camelCase(item.name)}Price`}
               >
-                ${item.price}
+                $
+                {item.price *
+                  (isNumber(quantities[item.id]) ? quantities[item.id] : 1)}
               </span>
             </FlexRow>
           ))}
@@ -157,10 +268,21 @@ const SubscriptionAddonModal = ({
           <FlexRow>
             <label>Total</label>
             <span data-cy="TotalPrice" className="priceCol">
-              ${totalPrice}
+              ${_totalPrice}
             </span>
           </FlexRow>
         </Total>
+        {showMultiplePurchaseModal && (
+          <EmailWrapper>
+            <FieldLabel>Bookkeeper Email</FieldLabel>
+            <TextInputStyled
+              value={emailValues}
+              onChange={handleInputEmailAddress}
+              placeholder="Type the emails"
+              height="40px"
+            />
+          </EmailWrapper>
+        )}
       </ModalBody>
     </CustomModalStyled>
   )
@@ -173,6 +295,7 @@ SubscriptionAddonModal.propTypes = {
 export default SubscriptionAddonModal
 
 const StyledCheckbox = styled(CheckboxLabel)`
+  width: 150px;
   &.ant-checkbox-wrapper .ant-checkbox + span {
     color: ${title};
   }
--- src/client/author/src/components/common/PurchaseModals/SubscriptionAddonModal/styled.js
+++ src/client/author/src/components/common/PurchaseModals/SubscriptionAddonModal/styled.js
@@ -14,6 +14,8 @@ export const ModalBody = styled.div`
   .priceCol {
     color: ${title};
     font-weight: 600;
+    width: 70px;
+    text-align: right;
   }
 `
 export const AddonList = styled.div`
@@ -24,7 +26,7 @@ export const FlexRow = styled.div`
   justify-content: space-between;
   margin-bottom: 5px;
   .ant-checkbox-wrapper:not(.ant-checkbox-wrapper-checked) {
-    & + .priceCol {
+    & ~ .priceCol {
       text-decoration: line-through;
       color: ${title};
       opacity: 50%;
@@ -35,4 +37,45 @@ export const Total = styled.div`
   border-top: 1px solid #dddddd;
   margin-top: 15px;
   padding-top: 15px;
+`
+export const EmailWrapper = styled.div`
+  margin-top: 25px;
+  margin-bottom: 15px;
+`
+export const NumberInputWrapper = styled.div`
+  .ant-input-number-input {
+    text-align: center;
+  }
+  .ant-input-number-handler-wrap {
+    background: none;
+    border: none;
+    opacity: 1;
+    right: -30px;
+    font-size: 25px;
+    & .ant-input-number-handler-up-inner,
+    & .ant-input-number-handler-down-inner {
+      display: none;
+    }
+    .ant-input-number-handler-up,
+    .ant-input-number-handler-down {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 25px !important;
+      position: absolute;
+      border: none;
+      font-weight: 500 !important;
+    }
+    .ant-input-number-handler-up {
+      margin-left: -120px;
+      &:before {
+        content: '+';
+      }
+    }
+    .ant-input-number-handler-down {
+      &:after {
+        content: '-';
+      }
+    }
+  }
 `

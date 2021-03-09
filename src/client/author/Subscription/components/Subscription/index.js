8181a5a566bd0db5e9b7a36508fe78d922dce050
madhur-vyas <72852437+madhur-vyas@users.noreply.github.com>
2021-03-09 21:10:12 +0530

    fix(ui): fix button not showing in trial confirmation modal [EV-25866] (#14366)

--- src/client/author/Subscription/components/Subscription/index.js
+++ src/client/author/Subscription/components/Subscription/index.js
@@ -337,7 +337,10 @@ const Subscription = (props) => {
     itemBankSubscriptions &&
     itemBankSubscriptions?.length > 0 &&
     itemBankSubscriptions?.filter((x) => {
-      return x.itemBankId === productData?.itemBankId && x.isTrial
+      return (
+        x.itemBankId ===
+          currentItemBank?.config?.subscriptionData?.itemBankId && x.isTrial
+      )
     })?.length > 0
 
   const setShowSubscriptionAddonModalWithId = () => {
@@ -465,7 +468,7 @@ const Subscription = (props) => {
           showTrialConfirmationMessage={showTrialConfirmationMessage}
           isTrialItemBank={isTrialItemBank}
           isBlocked={currentItemBank?.isBlocked}
-          title={productData?.productName}
+          title={currentItemBank?.config?.subscriptionData?.productName}
           handleGoToCollectionClick={handleGoToCollectionClick}
           history={history}
         />

05ae90aa5b6d3596c27ca9bdeb8b7a174de23cee
snapwizshivam <shivam.yadav@snapwiz.com>
2021-04-01 13:18:43 +0530

    fix(client): unscored usecase changes update

--- src/client/author/TestPage/utils.js
+++ src/client/author/TestPage/utils.js
@@ -109,10 +109,10 @@ const createItemsSummaryData = (items = [], scoring, isLimitedDeliveryType) => {
     } else {
       summary.noStandards.totalQuestions += questions.length
       summary.noStandards.totalPoints = roundOff(
-        (summary.noStandards.totalPoints + sumBy(
+        summary.noStandards.totalPoints + sumBy(
           questions,
           ({ id }) => questionWisePoints[id]
-        ))
+        )
       )
     }
     summary.totalPoints = roundOff(

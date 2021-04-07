f6f37448b73da43f12632c7987f59855deb9808f
mangaljain <mangal.jain@snapwiz.com>
2021-04-07 17:45:53 +0530

    fix(ui): showing message from 90 days left for TP subscription [EV-25960] (#14819)

--- src/client/author/src/components/common/PurchaseModals/SubscriptionAddonModal/index.js
+++ src/client/author/src/components/common/PurchaseModals/SubscriptionAddonModal/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { CustomModalStyled } from '@edulastic/common'
 import PropTypes from 'prop-types'
+import moment from 'moment'
 import { ModalBody } from './styled'
+
+const ONE_YEAR = 365 * 24 * 60 * 60 * 1000
 
 const SubscriptionAddonModal = ({
   isVisible,
@@ -10,7 +13,11 @@ const SubscriptionAddonModal = ({
   modalDescription,
   children,
   footer = null,
+  shouldProrate = true,
+  subEndDate = Date.now(),
 }) => {
+  const newExtendingExpiry =
+    !shouldProrate && moment(subEndDate + ONE_YEAR).format('DD MMM, YYYY')
   return (
     <CustomModalStyled
       centered
@@ -41,6 +48,17 @@ const SubscriptionAddonModal = ({
               </a>{' '}
               to learn more.
             </p>
+            {!shouldProrate && (
+              <p>
+                Since you have less than 3 months left in your premium
+                subscription, you will need to renew the premium subscription to
+                make this purchase. As a bonus, you will get to use your add-on
+                product(s) for the additional time until expiration for free.
+                Both the premium and the add-on subscription will expire on
+                &nbsp; &nbsp;
+                {newExtendingExpiry}
+              </p>
+            )}
             <p> These add-ons need the premium or enterprise subscription.</p>
           </>
         )}

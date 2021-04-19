f2ac6aae5340c849ea11579efad8fc765bd4453a
pint1128 <clearg1929@gmail.com>
2021-04-19 19:56:44 +0300

    fix(client): remove response box index and cross mark from popover content [EV-26958]

--- src/client/assessment/widgets/ClozeImageDragDrop/components/CheckboxTemplateBoxLayout/PopoverContent.js
+++ src/client/assessment/widgets/ClozeImageDragDrop/components/CheckboxTemplateBoxLayout/PopoverContent.js
@@ -3,46 +3,23 @@ import PropTypes from 'prop-types'
 import { response } from '@edulastic/constants'
 import { MathSpan } from '@edulastic/common'
 
-import { IconWrapper } from './styled/IconWrapper'
-import { RightIcon } from './styled/RightIcon'
-import { WrongIcon } from './styled/WrongIcon'
-
-export function PopoverContent({
-  fontSize,
-  answer,
-  status,
-  className,
-  checkAnswer,
-  indexStr,
-}) {
+export function PopoverContent({ fontSize, answer, className }) {
   return (
     <div
       fontSize={fontSize}
       className={className}
       style={{
-        display: 'flex',
         position: 'relative',
         'max-width': response.maxWidth,
       }}
     >
       <div
-        className="index index-box"
-        style={{
-          display: checkAnswer ? 'none' : 'flex',
-          alignSelf: 'stretch',
-          height: 'auto',
-        }}
+        className="text container"
+        style={{ overflow: 'unset', width: '100%' }}
       >
-        {indexStr}
-      </div>
-      <div className="text container" style={{ overflow: 'unset' }}>
         <div>
           <MathSpan dangerouslySetInnerHTML={{ __html: answer }} />
         </div>
-        <IconWrapper rightPosition={10}>
-          {answer && status === 'right' && <RightIcon />}
-          {answer && status === 'wrong' && <WrongIcon />}
-        </IconWrapper>
       </div>
     </div>
   )
@@ -50,10 +27,8 @@ export function PopoverContent({
 
 PopoverContent.propTypes = {
   fontSize: PropTypes.number,
-  status: PropTypes.string.isRequired,
   className: PropTypes.string.isRequired,
   answer: PropTypes.any.isRequired,
-  checkAnswer: PropTypes.bool.isRequired,
 }
 
 PopoverContent.defaultProps = {

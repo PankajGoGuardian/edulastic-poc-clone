f0a35116df0557aa3bdc388d6d5ce85f3972cba0
Gourav Mukherjee <50568503+mgourav-snapwiz@users.noreply.github.com>
2021-04-19 13:18:01 +0530

    fix(client): update evaluation colors in cloze types [EV-23942] (#15024)
    * fix(client): update evaluation colors in cloze types [EV-23942]
    
    * fix(client): refactor [EV-23942]
    
    Co-authored-by: pint1128 <clearg1929@gmail.com>

--- src/client/assessment/widgets/ClozeImageDragDrop/components/CheckboxTemplateBoxLayout/styled/AnswerBox.js
+++ src/client/assessment/widgets/ClozeImageDragDrop/components/CheckboxTemplateBoxLayout/styled/AnswerBox.js
@@ -2,38 +2,9 @@ import styled, { css } from 'styled-components'
 import { white } from '@edulastic/colors'
 
 const boxBgColor = css`
-  background: ${({ theme, checked, correct, isPrintPreview }) => {
+  background: ${({ fillColor, isPrintPreview }) => {
     if (isPrintPreview) return white
-    if (checked === undefined && correct === undefined) {
-      return theme.checkbox.boxBgColor
-    }
-    if (checked === false) {
-      return theme.checkbox.noAnswerBgColor
-    }
-    if (checked && !correct) {
-      return theme.checkbox.wrongBgColor
-    }
-    if (checked && correct) {
-      return theme.checkbox.rightBgColor
-    }
-  }};
-`
-
-const indexBoxBgColor = css`
-  background: ${({ theme, checked, correct, isPrintPreview }) => {
-    if (isPrintPreview) return white
-    if (checked === undefined && correct === undefined) {
-      return theme.checkbox.boxBgColor
-    }
-    if (checked === false) {
-      return theme.checkbox.noAnswerIconColor
-    }
-    if (checked && !correct) {
-      return theme.checkbox.wrongIconColor
-    }
-    if (checked && correct) {
-      return theme.checkbox.rightIconColor
-    }
+    return fillColor
   }};
 `
 
@@ -43,21 +14,6 @@ export const AnswerBox = styled.div`
   border-radius: 4px;
   height: 100%;
   ${boxBgColor};
-
-  .index {
-    width: 32px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    align-self: stretch;
-    flex-shrink: 0;
-    color: ${white};
-    border-top-left-radius: 4px;
-    border-bottom-left-radius: 4px;
-    font-weight: ${({ theme }) => theme.semiBold};
-    font-size: ${({ theme }) => theme.titleSectionFontSize};
-    ${indexBoxBgColor};
-  }
 
   .text {
     padding-left: 8px;

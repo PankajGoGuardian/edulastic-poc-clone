edaf696d065a610bad83da01989e52e3ce3bd6ac
Neeraj Negi <70655322+neerajnegisnapwiz@users.noreply.github.com>
2021-05-07 12:39:11 +0530

    fix(client): school name to show '-' in table fix [EV-27642] (#15257)

--- src/client/author/Reports/subPages/singleAssessmentReport/PeerPerformance/util/transformers.js
+++ src/client/author/Reports/subPages/singleAssessmentReport/PeerPerformance/util/transformers.js
@@ -154,7 +154,9 @@ const analyseByRawScore = (rawData, groupedData, compareBy) => {
     absent += statusCounts[testActivityStatus.UN_ASSIGNED] || 0
     absent += statusCounts[testActivityStatus.UN_ENROLLED] || 0
     const schoolName = uniq(
-      groupedData[data].map((o) => o.schoolName).filter((txt) => txt)
+      groupedData[data]
+        .map((o) => (isEmpty(o.schoolName) ? ' - ' : o.schoolName))
+        .filter((txt) => txt)
     ).join(', ')
 
     item = {
@@ -225,7 +227,9 @@ const analyseByAboveBelowStandard = (rawData, groupedData, compareBy) => {
     absent += statusCounts[testActivityStatus.UN_ASSIGNED] || 0
     absent += statusCounts[testActivityStatus.UN_ENROLLED] || 0
     const schoolName = uniq(
-      groupedData[data].map((o) => o.schoolName).filter((txt) => txt)
+      groupedData[data]
+        .map((o) => (isEmpty(o.schoolName) ? ' - ' : o.schoolName))
+        .filter((txt) => txt)
     ).join(', ')
 
     item = {
@@ -306,7 +310,9 @@ const analyseByProficiencyBand = (rawData, groupedData, compareBy) => {
     absent += statusCounts[testActivityStatus.UN_ASSIGNED] || 0
     absent += statusCounts[testActivityStatus.UN_ENROLLED] || 0
     const schoolName = uniq(
-      groupedData[data].map((o) => o.schoolName).filter((txt) => txt)
+      groupedData[data]
+        .map((o) => (isEmpty(o.schoolName) ? ' - ' : o.schoolName))
+        .filter((txt) => txt)
     ).join(', ')
 
     item = {

46ce06f600505a91a71b782c98b304a16b6a6495
Avinash Thakur <80avin@users.noreply.github.com>
2021-05-18 13:42:32 +0530

    support for Sub Group Performance automation [EV-27303] (#15153)
    * support for Sub Group Performance automation [EV-27303]
    
    * use data-cy as selector for automation [EV-27303]
    
    * add selectors for ddFilters and barCharts [EV-27303]
    
    * use className for non-unique cypress selector [EV-27303]
    
    * fix: filter popup closes randomly during filling [EV-27303]

--- src/client/author/Reports/subPages/singleAssessmentReport/common/components/filters.js
+++ src/client/author/Reports/subPages/singleAssessmentReport/common/components/filters.js
@@ -447,6 +447,7 @@ const SingleAssessmentReportFilters = ({
           isGhost={!showFilter}
           onClick={toggleFilter}
           style={{ height: '24px' }}
+          aria-pressed={(!!showFilter).toString()}
         >
           <IconFilter width={15} height={15} />
           FILTERS

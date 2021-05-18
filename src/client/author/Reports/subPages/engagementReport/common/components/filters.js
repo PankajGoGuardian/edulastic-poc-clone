3d673ef34df9497f41f9f880568c0a9b1146f47e
Hareesh Bandari <hareesh.bandari@snapwiz.com>
2021-05-18 19:49:35 +0530

    fix: missing attribute for automation in other reports[EV-27302] (#15393)

--- src/client/author/Reports/subPages/engagementReport/common/components/filters.js
+++ src/client/author/Reports/subPages/engagementReport/common/components/filters.js
@@ -180,6 +180,7 @@ const EngagementReportFilters = ({
           isGhost={!showFilter}
           onClick={toggleFilter}
           style={{ height: '24px' }}
+          aria-pressed={(!!showFilter).toString()}
         >
           <IconFilter width={15} height={15} />
           FILTERS

c098a443b9b6bfe269a9c21c5e4a088b96541cac
Saraansh Baranwal <saraansh@users.noreply.github.com>
2021-05-19 18:45:10 +0530

    [EV-27988_1] fix(report): SPR - standard filter update condition (#15408)

--- src/client/author/Reports/subPages/studentProfileReport/common/components/filter/filters.js
+++ src/client/author/Reports/subPages/studentProfileReport/common/components/filter/filters.js
@@ -214,7 +214,7 @@ const StudentProfileReportFilters = ({
   }, [loc, showFilter])
 
   useEffect(() => {
-    if (standardFiltersRequired && !firstLoad) {
+    if (!standardFiltersRequired && !firstLoad) {
       setFilters({
         ...filters,
         domainId: '',

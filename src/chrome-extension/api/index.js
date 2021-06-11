0a3f9ea68c80cf669a8e8ce3695a83b0050e1b4a
Yogesh VR <yogesh@snapwiz.com>
2021-06-11 12:22:09 +0530

    removes chrome extension folder

--- src/chrome-extension/api/index.js
+++
@@ -1,2 +0,0 @@
-export { default as userApi } from './userApi'
-export { default as classApi } from './classApi'
--- src/chrome-extension/reducers/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, combineReducers } from 'redux'
-import meetingsReducer from './ducks/messages'
-import edulasticReducer from './ducks/edulastic'
-
-const rootReducer = combineReducers({ edulasticReducer, meetingsReducer })
-const store = createStore(rootReducer)
-
-export default store

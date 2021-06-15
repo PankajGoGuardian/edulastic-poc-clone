adea1f8ab75080a0d7ecdc0ccb9ae12334500835
saraansh <saraansh.baranwal@snapwiz.com>
2021-06-15 11:38:16 +0530

    fix scanned (code smell) bugs - part 6

--- src/client/assessment/reducers/shuffledOptions.js
+++ src/client/assessment/reducers/shuffledOptions.js
@@ -3,9 +3,7 @@ import { SET_SHUFFLED_OPTIONS } from '../constants/actions'
 
 const initialState = {}
 
-const setAnswers = (state, { payload }) => {
-  state = payload
-}
+const setAnswers = (state, { payload }) => ({ ...payload })
 
 export default createReducer(initialState, {
   [SET_SHUFFLED_OPTIONS]: setAnswers,

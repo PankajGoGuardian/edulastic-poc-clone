9de68a5c0e6be174d617cc49ee60bf3d6bd35418
viraingarg-snapwiz <53215503+viraingarg-snapwiz@users.noreply.github.com>
2021-06-28 13:20:57 +0530

    Revert "fix: alignment issue of hint and solution button [EV-28841] (#15778)"
    This reverts commit 7cad06022ee23066d49bc950643e12acb1a0f845.

--- src/client/assessment/components/QuestionWrapper.js
+++ src/client/assessment/components/QuestionWrapper.js
@@ -646,18 +646,21 @@ class QuestionWrapper extends Component {
                       />
                     </RubricTableWrapper>
                   )}
-                  {view === 'preview' && !isPrintPreview && !showFeedback && (
-                    <Hints
-                      question={data}
-                      enableMagnifier={enableMagnifier}
-                      saveHintUsage={saveHintUsage}
-                      isStudent={userRole === 'student'}
-                      itemIndex={itemIndex}
-                      isLCBView={isLCBView}
-                      isExpressGrader={isExpressGrader}
-                      isStudentReport={isStudentReport}
-                    />
-                  )}
+                  {view === 'preview' &&
+                    !isLCBView &&
+                    !isPrintPreview &&
+                    !isExpressGrader && (
+                      <Hints
+                        question={data}
+                        enableMagnifier={enableMagnifier}
+                        saveHintUsage={saveHintUsage}
+                        isStudent={userRole === 'student'}
+                        itemIndex={itemIndex}
+                        isLCBView={isLCBView}
+                        isExpressGrader={isExpressGrader}
+                        isStudentReport={isStudentReport}
+                      />
+                    )}
                 </StyledFlexContainer>
               </PaperWrapper>
             </QuestionContainer>

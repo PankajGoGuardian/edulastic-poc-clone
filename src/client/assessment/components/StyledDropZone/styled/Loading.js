17e099b2240dd15bbcdcf7227f21764d1b5293ef
saraansh <saraansh.baranwal@snapwiz.com>
2021-06-29 14:08:05 +0530

    refactor and add styles for StyledDropZone [EV-28962] [EV-28961]

--- src/client/assessment/components/StyledDropZone/styled/Loading.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components'
-import { Icon } from 'antd'
-
-export const Loading = styled(Icon)`
-  font-size: ${(props) => props.theme.styledDropZone.loadingIconFontSize};
-`

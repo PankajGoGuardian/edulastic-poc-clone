100414e68eefe6aacc2f4a1c44486b31f4dce617
jitesh-kumar <jitesh.kumar@snapwiz.com>
2021-07-06 13:06:58 +0530

    feat(client): UI changes for lms class mapping [EV-29080]

--- src/client/admin/MergeSyncTable/ApproveMergeModal.js
+++ src/client/admin/MergeSyncTable/ApproveMergeModal.js
@@ -20,6 +20,115 @@ const ApproveMergeModal = ({
   const [formattedData, setFormattedData] = useState([])
   const [mappedResult, setMappedResult] = useState({})
 
+  const setInitialMapping = (cleverId, edulasticId) => {
+    mappedResult[cleverId] = edulasticId
+  }
+
+  const getCustomClassNameAndScore = (classData) => {
+    let customName = classData.name
+    let score = 0
+    const gm = classData?.gm || 0
+    const sm = classData?.sm || 0
+    const scm = classData?.scm || 0
+    const cIdMatch = classData.cIdMatch || 0
+    // if cleverId matched, disable the dropdown with existing clever class
+    // G: match | S: match | Count: match
+    // It will be an exact match which is explained above.
+    // G: match | S: match | Count: no match
+    // We need to look for students present and if it matches 60% or more then merge
+    // G: match | S: no match | Count: match
+    // We need to look for students present and if it matches 80% or more then merge
+    // G: no match | S: match | Count: match
+    // We need to look for students present and if it matches 80% or more then merge
+    // G: no match | S: no match | Count: match
+    // We need to look for students present and if it matches 80% or more then merge
+    // G: no match | S: no match | Count: no match
+    // ignore and let it be blank which will result in the creation of a new class during sync
+    if (cIdMatch === 100) {
+      score = 0
+    } else if (gm === 100 && sm === 100 && scm === 100) {
+      customName += `Grade(Matched) | Subject(Matched) | Student count(${scm}%)`
+      score = 1
+    } else if (gm === 100 && sm === 100 && scm >= 60) {
+      customName += `Grade(Matched) | Subject(Matched) | Student count(${scm}%)`
+      score = 2
+    } else if (gm === 100 && scm >= 80) {
+      customName += `Grade(Matched) | Subject(Not Matched) | Student count(${scm}%)`
+      score = 3
+    } else if (sm === 100 && scm >= 80) {
+      customName += `Grade(Not Matched) | Subject(Matched) | Student count(${scm}%)`
+      score = 4
+    } else if (scm >= 80) {
+      customName += `Student count(${scm}%)`
+      score = 4
+    } else {
+      score = 5
+    }
+    return {
+      customName,
+      score,
+    }
+  }
+  const compareClass = (a, b) => {
+    return a.score - b.score
+  }
+  const formatClassData = (dataSet) => {
+    const resultDataSet = []
+    for (const key of Object.keys(dataSet)) {
+      const data = dataSet[key]
+      const matchedClasses = data.matchedClass
+      const cId = key
+      const cName = data.name
+      if (
+        matchedClasses?.length === 1 &&
+        matchedClasses?.[0]?.cIdMatch === 100
+      ) {
+        const { score, customName } = getCustomClassNameAndScore(
+          matchedClasses?.[0]
+        )
+        resultDataSet.push({
+          cId,
+          cName,
+          data: [
+            {
+              eId: matchedClasses?.[0]?._id,
+              eName: customName,
+              cIdMatch: 100,
+              score,
+            },
+          ],
+        })
+      } else {
+        const mappedEduClassDetails = {}
+        if (matchedClasses?.length > 0) {
+          for (const matchedClass of matchedClasses) {
+            mappedEduClassDetails[matchedClass._id] = matchedClass
+          }
+        }
+        resultDataSet.push({
+          cId,
+          cName,
+          data: data.eduClasses.map((_class) => {
+            const { score, customName } = getCustomClassNameAndScore({
+              ..._class,
+              ...(mappedEduClassDetails[_class._id] || {}),
+            })
+            return {
+              eId: _class._id,
+              eName: customName,
+              score,
+            }
+          }),
+        })
+      }
+    }
+    resultDataSet.forEach((_class) => {
+      _class.data.sort(compareClass)
+      setInitialMapping(_class.cId, _class.data?.[0]?.eId)
+    })
+    return resultDataSet
+  }
+
   const getCustomName = (obj) => {
     const isZipMatched = obj.zipMatch === 100
     const nameMatched = obj.nameMatch ? obj.nameMatch : ''
@@ -47,11 +156,6 @@ const ApproveMergeModal = ({
       (b.nameMatch || 0) - (a.nameMatch || 0)
     )
   }
-
-  const setInitialMapping = (cleverId, edulasticId) => {
-    mappedResult[cleverId] = edulasticId
-  }
-
   const formatData = (dataSet, masterDataSet) => {
     const resultDataSet = []
     const cleverIdMasterDataMap = masterDataSet.reduce((acc, data) => {
@@ -151,7 +255,7 @@ const ApproveMergeModal = ({
       setFormattedData(data)
     }
     if (mapperFieldName === 'Classes') {
-      setFormattedData(formatData(mappedClasses, edulasticClasses))
+      setFormattedData(formatClassData(mappedClasses))
     }
   }, [])
 
@@ -222,6 +326,7 @@ const ApproveMergeModal = ({
                 return (
                   <Select
                     showSearch
+                    allowClear
                     defaultValue={mappedResult[record.cId]}
                     dropdownStyle={{ zIndex: 2000 }}
                     style={{ width: '90%' }}

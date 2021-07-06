88f601c7f2d6b1e9dae618b5d02742f4bbada0e6
Avinash Thakur <avinash.thakur@snapwiz.com>
2021-07-06 16:35:43 +0530

    ui cleaning & minor fixes
    1. prevent duplicate tokenKeys in localStorage.tokens array
    2. fix crashes in switching
    3. switching dialog box update : show & disable current user/district
    4. use lastUsedDistrictId to get current district

--- src/client/common/components/SwtichUserModal/SwitchUserModal.js
+++ src/client/common/components/SwtichUserModal/SwitchUserModal.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { Modal } from 'antd'
 import styled from 'styled-components'
 import { EduButton } from '@edulastic/common'
-import { IconPlusCircle } from '@edulastic/icons'
+import { IconPlusCircle, IconCircleCheck, IconCheck } from '@edulastic/icons'
 
 const Button = styled(EduButton)`
   border: none;
@@ -21,6 +21,7 @@ const roles = {
   student: 'Student',
   parent: 'Parent',
 }
+const roleOrder = Object.fromEntries(Object.entries(roles).map(([key, v], i) => [key, i]))
 
 const color = {
   student: '#D0A20D',
@@ -30,7 +31,10 @@ const color = {
   parent: '#D0A20D',
 }
 
-const StyledDiv = styled.div`
+const StyledDiv = styled.div.attrs((props) => ({
+  ...props,
+  onClick: props.selected ? () => {} : props.onClick,
+}))`
   height: 70px;
   border: 1px solid ${(props) => color[props.role]};
   border-radius: 10px;
@@ -40,12 +44,19 @@ const StyledDiv = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  cursor: ${({ selected }) => (selected ? 'not-allowed' : 'pointer')};
   &: hover {
     background: ${(props) => color[props.role]};
     color: #fff;
   }
   & > div:first-child {
     text-align: left;
+  }
+  & > div:last-child {
+    flex: 0 0 38px;
+    & svg {
+      display: ${({ selected }) => (selected ? 'inline' : 'none')};
+    }
   }
 `
 
@@ -77,47 +88,50 @@ const SwitchUserModal = ({
     <div>
       <p>Select the role you want to switch</p>
       <div style={{ 'margin-top': '16px' }}>
-        {Object.keys(roles).map((role) => {
-          const users = otherAccounts.filter((acc) => acc.role === role)
-          return (
-            !!users.length &&
-            users.map((user) => (
-              <StyledDiv
-                key={`${user._id}_${user.role}_${user.district?._id}`}
-                role={role}
-                onClick={() =>
-                  switchUser(user, { _id: userId, personId, orgId })
-                }
-              >
-                <div style={{ 'font-size': '16px', 'font-weight': '600' }}>
-                  <p>{roles[user.role]}</p>
+        {otherAccounts
+          .filter((acc) => Object.keys(roles).includes(acc.role))
+          .sort((a, b) => {
+            if (a._id === userId && b._id !== userId) return -1
+            if (a._id !== userId && b._id === userId) return 1
+            return roleOrder[a.role] - roleOrder[b.role]
+          })
+          .map((user) => (
+            <StyledDiv
+              key={`${user._id}_${user.role}_${user.district?._id}`}
+              role={user.role}
+              selected={user._id === userId && user.district._id === orgId}
+              onClick={() => switchUser(user, { _id: userId, personId, orgId })}
+            >
+              <div style={{ 'font-size': '16px', 'font-weight': '600' }}>
+                <p>{roles[user.role]}</p>
+              </div>
+              <div style={{ marginLeft: '15px', minWidth: '50%' }}>
+                <div>
+                  <p>{user.username}</p>{' '}
                 </div>
-                <div style={{ marginLeft: '15px', minWidth: '50%' }}>
-                  <div>
-                    <p>{user.username}</p>{' '}
-                  </div>
-                  <div
-                    style={{
-                      maxWidth: '100%',
-                      overflow: 'hidden',
-                      margin: '0 2px',
-                    }}
-                  >
-                    <NoWrapPara titlePrefix="Districts">
-                      {(user.district.name ? [user.district] : user.districts)
-                        .map((i) => i.name)
-                        .join(', ')}
-                      {user.districts.length && user.institutions.length
-                        ? ', '
-                        : ''}
-                      {user.institutions.map((i) => i.name).join(', ')}
-                    </NoWrapPara>
-                  </div>
+                <div
+                  style={{
+                    maxWidth: '100%',
+                    overflow: 'hidden',
+                    margin: '0 2px',
+                  }}
+                >
+                  <NoWrapPara titlePrefix="Districts">
+                    {(user.district.name ? [user.district] : user.districts)
+                      .map((i) => i.name)
+                      .join(', ')}
+                    {user.districts.length && user.institutions.length
+                      ? ', '
+                      : ''}
+                    {user.institutions.map((i) => i.name).join(', ')}
+                  </NoWrapPara>
                 </div>
-              </StyledDiv>
-            ))
-          )
-        })}
+              </div>
+              <div>
+                <IconCircleCheck />
+              </div>
+            </StyledDiv>
+          ))}
       </div>
     </div>
     {userRole === 'edulastic-admin' ||

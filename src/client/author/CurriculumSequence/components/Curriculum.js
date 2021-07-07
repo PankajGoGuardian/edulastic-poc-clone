58b29e0b1e88de6f9460bade3c3b18cd9e65b562
mangaljain <mangal.jain@snapwiz.com>
2021-07-07 16:33:13 +0530

    fix(ui): delete button was getting hidden [EV-29406] (#15957)

--- src/client/author/CurriculumSequence/components/Curriculum.js
+++ src/client/author/CurriculumSequence/components/Curriculum.js
@@ -76,7 +76,7 @@ const SortableItem = sortableElement((props) => {
       {hasModuleSort && <SortableTestsHandle />}
       <DropContainer
         theme={themes.default}
-        width={isReview ? 'calc(100% - 40px)' : '100%'}
+        width={hasModuleSort ? 'calc(100% - 40px)' : '100%'}
         key={`drop-${id}-${moduleItem._id}`}
         drop={(arg1, item) => {
           onDrop(id, item)

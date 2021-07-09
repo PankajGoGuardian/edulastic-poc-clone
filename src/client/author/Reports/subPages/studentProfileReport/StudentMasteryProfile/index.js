440057887769a6f593ecbd80ca02f449d74c5bc4
hareeshsnapwiz <hareesh.bandari@snapwiz.com>
2021-07-09 22:16:07 +0530

    fix: minor change

--- src/client/author/Reports/subPages/studentProfileReport/StudentMasteryProfile/index.js
+++ src/client/author/Reports/subPages/studentProfileReport/StudentMasteryProfile/index.js
@@ -327,7 +327,7 @@ const StudentMasteryProfile = ({
             onClick={() => setExpandRows(!expandRows)}
             data-cy="expand-rows"
           >
-            <IconCollapse2 color={themeColor} width={15} height={16} />
+            <IconCollapse2 color={themeColor} width={12} height={13} />
             <span className="button-label">
               {expandRows ? 'COLLAPSE' : 'EXPAND'} ROWS
             </span>

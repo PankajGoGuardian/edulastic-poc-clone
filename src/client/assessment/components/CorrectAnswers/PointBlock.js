a07588994ec6ea8fd1df88e72015b7b94d299bcf
pin1128 <32391750+pin1128@users.noreply.github.com>
2021-08-03 15:32:37 +0800

    fix(client): hide alt scoring block in multipart question [EV-29328] (#16268)

--- src/client/assessment/components/CorrectAnswers/PointBlock.js
+++ src/client/assessment/components/CorrectAnswers/PointBlock.js
@@ -1,10 +1,6 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import UnScored from '@edulastic/common/src/components/Unscored'
-import {
-  FlexContainer,
-  ItemLevelContext,
-  getFormattedAttrId,
-} from '@edulastic/common'
+import { FlexContainer, getFormattedAttrId } from '@edulastic/common'
 import { withNamespaces } from '@edulastic/localization'
 import { Label } from '../../styled/WidgetOptions/Label'
 import { PointsInput } from '../../styled/CorrectAnswerHeader'
@@ -18,32 +14,27 @@ const PointBlock = ({
   correctAnsScore,
   questionType,
   unscored,
-}) => {
-  const itemLevelScoring = useContext(ItemLevelContext)
-  return (
-    itemLevelScoring || (
-      <FlexContainer flexDirection="column" mt="8px">
-        <Label>{t('component.correctanswers.points')}</Label>
-        {!unscored ? (
-          <PointsInput
-            type="number"
-            data-cy="points"
-            value={points}
-            width={width}
-            onChange={onChangePoints}
-            id={getFormattedAttrId(
-              `${questionType}-${t('component.correctanswers.points')}`
-            )}
-            max={!isCorrectAnsTab ? correctAnsScore : Number.MAX_SAFE_INTEGER}
-            min={0}
-            step={0.5}
-          />
-        ) : (
-          <UnScored text="Z" height="50px" width={width} />
+}) => (
+  <FlexContainer flexDirection="column" mt="8px">
+    <Label>{t('component.correctanswers.points')}</Label>
+    {!unscored ? (
+      <PointsInput
+        type="number"
+        data-cy="points"
+        value={points}
+        width={width}
+        onChange={onChangePoints}
+        id={getFormattedAttrId(
+          `${questionType}-${t('component.correctanswers.points')}`
         )}
-      </FlexContainer>
-    )
-  )
-}
+        max={!isCorrectAnsTab ? correctAnsScore : Number.MAX_SAFE_INTEGER}
+        min={0}
+        step={0.5}
+      />
+    ) : (
+      <UnScored text="Z" height="50px" width={width} />
+    )}
+  </FlexContainer>
+)
 
 export default withNamespaces('assessment')(PointBlock)

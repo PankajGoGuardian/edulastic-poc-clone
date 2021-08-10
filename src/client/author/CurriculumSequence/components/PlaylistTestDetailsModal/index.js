801698c7d813946c600b739da44a31e732e87c52
Amit Vishwakarma <amit.vishwakarma@snapwiz.com>
2021-08-10 13:47:27 +0530

    fix: playlist show edit test button for authors only [EV-30229]

--- src/client/author/CurriculumSequence/components/PlaylistTestDetailsModal/index.js
+++ src/client/author/CurriculumSequence/components/PlaylistTestDetailsModal/index.js
@@ -53,6 +53,11 @@ const PlaylistTestDetailsModal = ({
     onClose(false)
   }
 
+  const { authors = [] } = test
+
+  const showEditButton =
+    !isTestLoading && authors.some(({ _id }) => _id === userId)
+
   return (
     <ModalWrapper
       key={`playlist-review-${currentTestId}`}
@@ -70,10 +75,12 @@ const PlaylistTestDetailsModal = ({
           </i>{' '}
           back to playlist
         </BreadCrumb>
-        <ActionsWrapper>
-          <ActionBtn data-cy="editTest" onClick={handleEditTest}>
-            Edit test
-          </ActionBtn>
+        <ActionsWrapper showEditButton={showEditButton}>
+          {showEditButton && (
+            <ActionBtn data-cy="editTest" onClick={handleEditTest}>
+              Edit test
+            </ActionBtn>
+          )}
           <ActionBtn
             data-cy="viewAsStudent"
             onClick={() => viewAsStudent(currentTestId)}

b74bd681a38a4b3bdf8608a833b7f8ef03359c03
pin1128 <32391750+pin1128@users.noreply.github.com>
2021-08-17 15:54:14 +0800

    fix(client): added validation for dp number set, sequence [EV-27042] (#16399)
    * fix(client): added validation for dp number set, sequence [EV-27042]
    
    * fix(client): added comments [EV-27042]
    
    * fix(client): fixed comments [EV-27042]

---
+++ src/client/assessment/containers/WidgetOptions/components/Variables/components/validations.js
@@ -0,0 +1,62 @@
+const allowedNumbersRegex = new RegExp('[0-9]+')
+
+export default {
+  decimal: (evt) => {
+    const pressedKey = String.fromCharCode(
+      !evt.charCode ? evt.which : evt.charCode
+    )
+    const allowedKeys = [8, 9, 37, 38, 39, 40] // to allow arrow keys, backspace and tab
+    if (
+      !(
+        allowedKeys.includes(evt?.which || evt?.charCode) ||
+        allowedNumbersRegex?.test(pressedKey)
+      )
+    ) {
+      evt.stopPropagation()
+      evt.preventDefault()
+      return
+    }
+    return pressedKey
+  },
+  step: (evt) => {
+    const pressedKey = String.fromCharCode(
+      !evt.charCode ? evt.which : evt.charCode
+    )
+    // allow arrow keys, backspace, tab, and decimal
+    const allowedKeys = [8, 9, 37, 38, 39, 40, 110, 190]
+    if (
+      !(
+        allowedKeys.includes(evt?.which || evt?.charCode) ||
+        allowedNumbersRegex?.test(pressedKey)
+      )
+    ) {
+      evt.stopPropagation()
+      evt.preventDefault()
+      return
+    }
+    return pressedKey
+  },
+  numSequence: (evt) => {
+    if (evt.shiftKey) {
+      evt.stopPropagation()
+      evt.preventDefault()
+      return
+    }
+    const pressedKey = String.fromCharCode(
+      !evt.charCode ? evt.which : evt.charCode
+    )
+    // allow arrow keys, backspace, tab, and comma
+    const allowedKeys = [8, 9, 37, 38, 39, 40, 188]
+    if (
+      !(
+        allowedKeys.includes(evt?.which || evt?.charCode) ||
+        allowedNumbersRegex?.test(pressedKey)
+      )
+    ) {
+      evt.stopPropagation()
+      evt.preventDefault()
+      return
+    }
+    return pressedKey
+  },
+}

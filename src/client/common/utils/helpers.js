0c21f6a0a662cf2bb4dc506d372f2a15d32021ae
ganeshdubey07 <81735498+ganeshdubey07@users.noreply.github.com>
2021-09-20 17:14:39 +0530

    fix: allow comma for username but not for email id [EV-24645] (#16719)
    Co-authored-by: ganesh <imganeshdubey@gmail.com>

--- src/client/common/utils/helpers.js
+++ src/client/common/utils/helpers.js
@@ -13,9 +13,8 @@ import { Partners } from './static/partnerData'
 import { smallestZoomLevel } from './static/zoom'
 import { breakpoints } from '../../student/zoomTheme'
 
-// eslint-disable-next-line no-control-regex
-export const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
-
+// eslint-disable-next-line no-useless-escape
+export const emailRegex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
 const api = new API()
 
 export const getWordsInURLPathName = (pathname) => {
@@ -163,7 +162,7 @@ export const isDistrictPolicyAvailable = (isSignupUsingDaURL, districtPolicy) =>
   isSignupUsingDaURL && typeof districtPolicy === 'object'
 
 export const isEmailValid = (rule, value, callback, checks, message) => {
-  const userNameRegExp = new RegExp(`^[A-Za-z0-9._ \\-\\+\\'\\"]+$`)
+  const userNameRegExp = new RegExp(`^[a-zA-Z0-9_,-]{3,100}$`)
 
   let flag = false
 

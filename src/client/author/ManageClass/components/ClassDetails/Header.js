2bd6c1b69c145198eaac23ffc13cb6fcc329ad04
Nitesh377 <86227678+Nitesh377@users.noreply.github.com>
2021-10-21 09:54:07 +0530

    fix(client): Auto refresh is not happening when we click on resync class [EV-32374] (#17058)

--- src/client/author/ManageClass/components/ClassDetails/Header.js
+++ src/client/author/ManageClass/components/ClassDetails/Header.js
@@ -86,6 +86,7 @@ const Header = ({
   setUpdateCoTeacherModal,
   orgId,
   syncClassWithAtlas,
+  setSyncClassLoading,
   isCleverDistrict,
 }) => {
   const handleLoginSuccess = (data) => {
@@ -195,6 +196,7 @@ const Header = ({
 
   const handleCleverSync = () => {
     const classList = [{ ...selectedClass, course: selectedClass?.course?.id }]
+    setSyncClassLoading(true)
     syncClassesWithClever({ classList })
   }
 
@@ -272,6 +274,7 @@ const Header = ({
   }
 
   const handleAtlasSync = () => {
+    setSyncClassLoading(true)
     syncClassWithAtlas({ groupIds: [selectedClass._id] })
   }
 

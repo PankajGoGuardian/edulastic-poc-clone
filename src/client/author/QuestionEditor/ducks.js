91fc4f88447450dbe99ae9164b72e7396439371c
Gourav Mukherjee <50568503+mgourav-snapwiz@users.noreply.github.com>
2021-10-21 10:07:25 +0530

    fix(client): prevent routing to previous test id [EV-32106] [EV-32126] (#17056)

--- src/client/author/QuestionEditor/ducks.js
+++ src/client/author/QuestionEditor/ducks.js
@@ -738,12 +738,7 @@ function* saveQuestionSaga({
       const currentRouterState = yield select(
         (state) => state.router.location.state
       )
-      if (currentRouterState) {
-        const routerTestId = currentRouterState.previousTestId
-        if (routerTestId) {
-          tId = routerTestId
-        }
-      }
+
       if (isTestFlow) {
         if (!tId || tId === 'undefined') {
           const { __v, ...passageData } =

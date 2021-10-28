a8fec0d940582faa9310838e53ebd37c40f4c609
Gourav <gourav.mukherjee@snapwiz.com>
2021-10-28 14:34:58 +0530

    fix(client): prevent routing to previous test id [EV-32624]

--- src/client/author/ItemDetail/ducks.js
+++ src/client/author/ItemDetail/ducks.js
@@ -2083,12 +2083,6 @@ function* savePassage({ payload }) {
     if (isTestFlow) {
       // testId = yield select((state) => state.tests?.entity?._id)
 
-      if (currentRouterState) {
-        const routerTestId = currentRouterState.previousTestId
-        if (routerTestId) {
-          testId = routerTestId
-        }
-      }
       if (!testId || testId === 'undefined') {
         let passageItems = []
         if (updatedPassage?._id && updatedPassage?.testItems?.length > 1) {

273ec29dbea9fc6937eb3203c839324044928b9b
nagarajanSnapwiz <44694625+nagarajanSnapwiz@users.noreply.github.com>
2021-11-03 15:07:58 +0530

    Bubble sheet 21.0 cherry (#17186)
    * fix(client): handle webcam upload validations [EV-32300]
    
    * fix(client):  21.1 enhancements
    
    * fix(client): text changes requested
    
    * fix(client): text changes requested
    
    Co-authored-by: Nagarajan <nagarajan.natarajangoguardian.com@Nagarajan-Natarajans-MacBook-Pro.local>

--- src/client/scanScore/scanAnswerSheets/index.js
+++ src/client/scanScore/scanAnswerSheets/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import qs from 'qs'
 import { connect } from 'react-redux'
 import { compose } from 'redux'
-import { Progress, Row, Select } from 'antd'
+import { Progress, Row, Select, Tooltip } from 'antd'
 import { OpenCvProvider, useOpenCv } from 'opencv-react'
 import { withRouter } from 'react-router-dom'
 import { round } from 'lodash'
@@ -185,7 +185,11 @@ const ScanAnswerSheetsInner = ({
       } else {
         cv.imshow('canvasOutput', matSrc)
         matSrc.delete()
-        requestAnimationFrame(() => processVideo(vc))
+        requestAnimationFrame(() =>
+          processVideo(vc).catch(() => {
+            console.log('process video opencv error')
+          })
+        )
         return
       }
       if (answersList) {
@@ -199,7 +203,11 @@ const ScanAnswerSheetsInner = ({
         )
       }
       matSrc.delete()
-      requestAnimationFrame(() => processVideo(vc))
+      requestAnimationFrame(() =>
+        processVideo(vc).catch(() => {
+          console.log('process video opencv error')
+        })
+      )
       if (!result) {
         return
       }
@@ -231,7 +239,7 @@ const ScanAnswerSheetsInner = ({
                 arrAnswersRef.current.push(result)
                 notification({
                   type: 'success',
-                  msg: 'Form detected. You can now scan the next one.',
+                  msg: 'Form successfully scanned. Please scan the next one.',
                 })
                 audioRef.play()
 
@@ -301,7 +309,7 @@ const ScanAnswerSheetsInner = ({
           console.log(`Error While accessing camera: ${err}`)
         })
     }
-  }, [])
+  }, [selectedCamera])
 
   useEffect(() => {
     if (isCameraLoaded && isOpencvLoaded) {
@@ -340,7 +348,9 @@ const ScanAnswerSheetsInner = ({
                 videoRef.current.setAttribute('height', videoHeight)
                 // Start Video Processing
                 requestAnimationFrame(() =>
-                  processVideo(new cv.VideoCapture(videoRef.current))
+                  processVideo(
+                    new cv.VideoCapture(videoRef.current)
+                  ).catch(() => console.warn('opencv error'))
                 )
               }
             },
@@ -456,11 +466,11 @@ const ScanAnswerSheetsInner = ({
       ) : (
         <CameraUploaderWrapper>
           <Title>
-            Put your bubble sheets in front of the camera{' '}
+            Put your bubble sheet forms in front of the camera{' '}
             <HelpIcon onClick={openHelpModal}>?</HelpIcon>
           </Title>
           <SubTitle>
-            Ensure the sheets are fully visible and wait for the Form detected
+            Ensure the forms are fully visible and wait for the Form detected
             message with a beeper sound.
           </SubTitle>
           <FlexContainer width={`${videoSetting.width}px`}>
@@ -523,7 +533,7 @@ const ScanAnswerSheetsInner = ({
             onClick={triggerCompleteConfirmation}
             style={{ marginTop: 15 }}
           >
-            PROCEED TO NEXT STEP
+            Proceed to Scan Results
           </EduButton>
         </CameraUploaderWrapper>
       )}
@@ -533,7 +543,7 @@ const ScanAnswerSheetsInner = ({
           title="Scan Finished Confirmation"
           description="Have you scanned all Bubble Sheet Forms?"
           buttonText="YES, PROCEED"
-          cancelText="NO, LET ME FINISH"
+          cancelText="No, I have more to scan"
           onCancel={closeScanConfirmationModal}
           onProceed={handleScanComplete}
         />
@@ -550,7 +560,7 @@ const ScanAnswerSheetsInner = ({
         <Row>
           <FieldLabel>SELECT CAMERA</FieldLabel>
           <SelectInputStyled
-            disabled={cameraList.length > 1}
+            disabled={cameraList.length === 1}
             value={cameraList.length === 1 ? cameraList[0].id : selectedCamera}
             onChange={(v) => {
               setSelectedCamera(v)
@@ -564,13 +574,15 @@ const ScanAnswerSheetsInner = ({
         </Row>
         <br />
         <Row>
-          <Checkbox
-            checked={isFrontFacing}
-            onChange={() => {
-              setIsFrontFacing((x) => !x)
-            }}
-            label="CAMERA IS FACING ME"
-          />
+          <Tooltip title="Check this option if the camera is facing you. This will mirror the image horizontally so that you can scan more easily.">
+            <Checkbox
+              checked={isFrontFacing}
+              onChange={() => {
+                setIsFrontFacing((x) => !x)
+              }}
+              label="CAMERA IS FACING ME"
+            />
+          </Tooltip>
         </Row>
       </CustomModalStyled>
       {isHelpModalVisible && (

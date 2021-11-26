101c28d14f6f22c682903a3eaa2f60696f0e454d
Prajwal S Venkatesh <53730619+Prajwal-S-Venkatesh@users.noreply.github.com>
2021-11-26 18:14:23 +0530

    refactor: update user class list on clever dashboard class sync [EV-33261] (#17393) [EV-33282] (#17398)
    * fix: update user class list on clever dashboard class  sync [EV-33261]
    
    * refactor: update user class list on clever dashboard class sync [EV-33261]
    
    Co-authored-by: Prajwal S Venkatesh <prajwal.venkateshgoguardian.com@Prajwal-S-Venkateshs-MacBook-Pro.local>
    
    Co-authored-by: Prajwal S Venkatesh <prajwal.venkateshgoguardian.com@Prajwal-S-Venkateshs-MacBook-Pro.local>

--- src/client/author/ManageClass/ducks.js
+++ src/client/author/ManageClass/ducks.js
@@ -1104,7 +1104,7 @@ function* syncClassListWithCleverSaga({ payload }) {
     }
     switch (refreshPage) {
       case 'dashboard':
-        yield put(receiveTeacherDashboardAction())
+        yield put(receiveTeacherDashboardAction({ updateUserClassList: true }))
         break
       case 'manageClass':
         yield put(fetchGroupsAction())

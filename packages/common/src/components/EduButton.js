835ceae3e47cee010653cb80d3b53021013c8e22
Mangal Jain <mangal.jain@snapwiz.com>
2021-12-09 17:55:49 +0530

    fix(ui): keyboard tab focus was not working for windows systems [EV-31645] (#17433)

--- packages/common/src/components/EduButton.js
+++ packages/common/src/components/EduButton.js
@@ -119,9 +119,10 @@ const StyledButton = styled(Button)`
     background-color: ${getBgColor};
     border-color: ${getBorderColor};
     color: ${getColor};
-    &.focus-visible {
+    &.focus-visible,
+    &:focus {
       outline: 0;
-      box-shadow: 0 0 0 1.5px ${themeColorBlue};
+      box-shadow: 0 0 0 1px ${themeColorBlue} !important;
     }
   }
 

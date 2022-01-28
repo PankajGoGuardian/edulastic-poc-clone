9a1e66f1ff4905f4119ce0517f0adf1917ffae6c
Gourav Mukherjee <50568503+mgourav-snapwiz@users.noreply.github.com>
2022-01-28 13:47:05 +0530

    Revert "fix(ui): show spinner while test is loading [EV-33184] [EV-33184] (#17590)" (#17701)
    This reverts commit 50441f6a852b3832fc138c31bcb8dea598cc6847.
    
    Co-authored-by: Gourav Mukherjee <gourav.mukherjee@goguardian.com>

--- src/client/author/TestPage/components/Container/Container.js
+++ src/client/author/TestPage/components/Container/Container.js
@@ -35,7 +35,7 @@ import {
   fetchAssignmentsByTestAction,
 } from '../../../../publicTest/ducks'
 
-import { Content, ContentBackDrop, SpinnerContainer } from './styled'
+import { Content, ContentBackDrop } from './styled'
 import TestPageHeader from '../TestPageHeader/TestPageHeader'
 import {
   defaultImage,
@@ -787,11 +787,7 @@ class Container extends PureComponent {
     }
 
     if (isTestLoading && !test._id) {
-      return (
-        <SpinnerContainer>
-          <Spin />
-        </SpinnerContainer>
-      )
+      return <Spin />
     }
 
     switch (current) {

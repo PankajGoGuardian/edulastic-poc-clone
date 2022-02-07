a2af375088287caa29ba56c361b3a5b66a84e3f1
Gourav Mukherjee <gourav.mukherjee@goguardian.com>
2022-02-07 12:19:16 +0530

    fix(client): removing unwanted style manipulations [EV-34615]

--- src/client/assessment/widgets/ClozeMath/ClozeMathBlock/ClozeDropDown.js
+++ src/client/assessment/widgets/ClozeMath/ClozeMathBlock/ClozeDropDown.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState, useMemo } from 'react'
+import React, { useRef, useMemo } from 'react'
 import PropTypes from 'prop-types'
 import { find, indexOf } from 'lodash'
 import styled from 'styled-components'
@@ -69,10 +69,6 @@ const ClozeDropDown = ({ resprops = {}, id }) => {
   }, [item])
 
   const dropDownWrapper = useRef(null)
-  const [menuStyle, setMenuStyle] = useState({
-    top: `${dropdownStyle.height} !important`,
-    left: `0px !important`,
-  })
 
   const getPopupContainer = (node) => {
     return node.parentNode
@@ -95,36 +91,6 @@ const ClozeDropDown = ({ resprops = {}, id }) => {
     )
     val = getStemNumeration('lowercase', itemIndex)
   }
-  // TODO
-  // make a generic component for dropdown and use it in all questions
-  // so that we can have control all dropdowns just by changing one place
-  useEffect(() => {
-    // recalculate the dimensions whenever the element is mounted
-    if (dropDownWrapper.current) {
-      const wrapper = dropDownWrapper.current
-      const { height: wrapperHeight, top } = wrapper.getBoundingClientRect()
-      const optionCount = options?.length || 0
-      const heightOfEachOption = 40
-      const totalOptionsHeight = Math.max(optionCount * heightOfEachOption, 120)
-      if (window.innerHeight - (wrapperHeight + top) < totalOptionsHeight) {
-        // dropdown near to the bottom of the screen
-        // or dropdown has many options wihch cannot fit at the bottom
-
-        // show options above the dropdown
-        setMenuStyle({
-          top: `auto !important`,
-          left: `0px !important`,
-          bottom: `100%`,
-        })
-      } else {
-        // show options below the dropdown
-        setMenuStyle({
-          top: `${wrapper.clientHeight}px !important`,
-          left: `0px !important`,
-        })
-      }
-    }
-  }, [dropDownWrapper.current])
 
   return checked ? (
     <CheckedBlock
@@ -142,11 +108,7 @@ const ClozeDropDown = ({ resprops = {}, id }) => {
       allCorrects={allCorrects}
     />
   ) : (
-    <DropdownWrapper
-      ref={dropDownWrapper}
-      menuStyle={menuStyle}
-      isPrintPreview={isPrintPreview}
-    >
+    <DropdownWrapper ref={dropDownWrapper} isPrintPreview={isPrintPreview}>
       <Dropdown
         data-cy="textDropDown"
         disabled={disableResponse}
@@ -195,9 +157,6 @@ const DropdownWrapper = styled.div`
     }
   }
 
-  .ant-select-dropdown {
-    ${({ menuStyle }) => menuStyle};
-  }
   text-indent: 0;
 `
 

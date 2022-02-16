c72f339b3b694fe233017f3d3c7274bb4ea564b0
Andrei Pintea <87871557+antrusha0223@users.noreply.github.com>
2022-02-16 13:11:51 +0200

    fix(client): graph tools helper tooltip position issue first time [EV-30236] (#17725)

--- src/client/assessment/components/Graph/common/HelpTooltip.js
+++ src/client/assessment/components/Graph/common/HelpTooltip.js
@@ -1,7 +1,8 @@
 import React, { useMemo } from 'react'
 import { Popover } from 'antd'
 import styled from 'styled-components'
-import { MathSpan, replaceLatexTemplate } from '@edulastic/common'
+import { replaceLatexTemplate } from '@edulastic/common'
+import { replaceLatexesWithMathHtml } from '@edulastic/common/src/utils/mathUtils'
 import { withNamespaces } from '@edulastic/localization'
 
 const HelperToolTip = ({ toolName, children, t }) => {
@@ -9,7 +10,12 @@ const HelperToolTip = ({ toolName, children, t }) => {
     const text = replaceLatexTemplate(
       t(`component.graphing.helperText.${toolName}`)
     )
-    return <HelperText text={text} />
+
+    return (
+      <HelperText
+        dangerouslySetInnerHTML={{ __html: replaceLatexesWithMathHtml(text) }}
+      />
+    )
   }, [toolName])
 
   return (
@@ -26,12 +32,10 @@ const HelperToolTip = ({ toolName, children, t }) => {
 
 export default withNamespaces('assessment')(HelperToolTip)
 
-const HelperText = styled(MathSpan).attrs(({ text }) => ({
-  dangerouslySetInnerHTML: { __html: text },
-  fontSize: 11,
-  color: 'inherit',
-}))`
+const HelperText = styled.span`
   font-weight: 700;
+  color: inherit;
+  font-size: 11px;
 
   & .input__math {
     font-size: 13px;

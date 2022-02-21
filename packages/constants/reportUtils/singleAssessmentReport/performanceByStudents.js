3c535efa68f52f9191555d1688725b78e980a03e
Saraansh Baranwal <saraansh@users.noreply.github.com>
2022-02-21 12:19:18 +0530

    fix(client): Performance by Students report - update column name [EV-34797] (#17813)

--- packages/constants/reportUtils/singleAssessmentReport/performanceByStudents.js
+++ packages/constants/reportUtils/singleAssessmentReport/performanceByStudents.js
@@ -67,7 +67,7 @@ const tableColumns = [
     visibleOn: ['csv'],
   },
   {
-    title: 'Ethnicity',
+    title: 'Hispanic Ethnicity',
     dataIndex: 'hispanicEthnicity',
     key: 'hispanicEthnicity',
     visibleOn: ['csv'],

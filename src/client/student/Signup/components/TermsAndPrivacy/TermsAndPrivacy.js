cca87189828b59a26b9fdc55602effb4071ae1f2
mangaljain <mangal.jain@snapwiz.com>
2022-02-25 12:36:57 +0530

    fix: sign-up privacy policy text changes [EV-35060]

--- src/client/student/Signup/components/TermsAndPrivacy/TermsAndPrivacy.js
+++ src/client/student/Signup/components/TermsAndPrivacy/TermsAndPrivacy.js
@@ -5,23 +5,18 @@ import { TermsPrivacy } from '../../styled/index'
 const TermsAndPrivacy = ({ signIn }) => {
   return (
     !signIn && (
-      <>
-        <TermsPrivacy align="left">By signing up:</TermsPrivacy>
-
-        <TermsPrivacy align="left" minWidth="330px">
-          You are agreeing that you are authorized to act for your school and
-          you consent to Edulastic&apos;s collection of student data in the
-          {'  '}
-          <a
-            onClick={() => window.open(COPPADISCLOSURE)}
-            target="_blank"
-            rel="noreferrer"
-          >
-            {' '}
-            COPPA Disclosure
-          </a>
-        </TermsPrivacy>
-      </>
+      <TermsPrivacy align="left" minWidth="330px">
+        By signing up, you are agreeing that you are authorized to act for your
+        school and you consent to Edulastic&apos;s collection of student data in
+        the{' '}
+        <a
+          onClick={() => window.open(COPPADISCLOSURE)}
+          target="_blank"
+          rel="noreferrer"
+        >
+          COPPA Disclosure.
+        </a>
+      </TermsPrivacy>
     )
   )
 }

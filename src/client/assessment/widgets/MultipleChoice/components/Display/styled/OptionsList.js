b44d6f41c181d2f4fbd5c3335e44933504ac03cd
Aniket Hatti <75801936+aniket-snapwiz@users.noreply.github.com>
2022-04-07 15:12:28 +0530

    fix(ui): align mcq options [EV-35020] (#17942)
    Co-authored-by: aniket-snapwiz <aniket.hatti@goguardian.com>

--- src/client/assessment/widgets/MultipleChoice/components/Display/styled/OptionsList.js
+++ src/client/assessment/widgets/MultipleChoice/components/Display/styled/OptionsList.js
@@ -4,7 +4,8 @@ import { mobileWidth } from '@edulastic/colors'
 export const OptionsList = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: flex-start;
+  align-items: ${({ uiStyleType }) =>
+    uiStyleType === 'radioBelow' ? 'flex-end' : 'flex-start'};
   margin-left: ${(props) => (props.styleType === 'primary' ? '0px' : '-25px')};
   width: ${({ width }) => width || 'auto'};
   @media (max-width: ${mobileWidth}) {

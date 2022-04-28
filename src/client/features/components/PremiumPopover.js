633b6516909eb5099313b204a807a9eb4edd98cc
Saraansh Baranwal <saraansh@users.noreply.github.com>
2022-04-28 12:26:14 +0530

    fix(client): text change for bubble sheet premium popover [EV-35550] (#18046)

--- src/client/features/components/PremiumPopover.js
+++ src/client/features/components/PremiumPopover.js
@@ -43,7 +43,7 @@ const descriptions = {
   default:
     'Get additional reports, options to assist students, collaborate with colleagues, anti-cheating tools and more.',
   bubble:
-    'Get bubble sheet feature to take OMR exams, also gain access to additional reports, options to assist students, anti-cheating tools and many more.',
+    'Experience the additional features of Edulastic Teacher Premium for 14 days: OMR exams, read-loud for students, extra test security settings, easier collaboration, in-depth reports and more.',
 }
 
 const getContent = ({

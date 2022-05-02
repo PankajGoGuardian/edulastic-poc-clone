364b1df460eb6648dad63ea259b5b2a103e37586
Yeshwanth R <yeshwanth.rgoguardian.com@Yeshwanths-MacBook-Pro.local>
2022-05-02 12:30:26 +0530

    refactor(ui): refactor test code

--- src/client/privacyPolicy/privacyPolicy.spec.js
+++ src/client/privacyPolicy/privacyPolicy.spec.js
@@ -10,12 +10,63 @@ import PrivacyPolicyModal from './index'
 const mockStore = configureMockStore()
 const store = mockStore({})
 
+jest.mock('../../../packages/api/src/utils/API')
+
 jest.spyOn(userApi, 'eulaPolicyStatusUpdate')
 
 jest.spyOn(segmentApi, 'genericEventTrack')
 segmentApi.genericEventTrack = jest.fn()
 
-describe('Privacy Policy Modal Component', () => {
+const componentsVisibility = () => {
+  const moduleTitleElement = screen.getByText(
+    'End User License Agreement and Product Privacy Policy'
+  )
+  expect(moduleTitleElement).toBeInTheDocument()
+  const checkBoxTextElement = screen.getByText(
+    'By checking the box and clicking “Accept”, I agree to the Terms of Service and End User License Agreement and Privacy Policy of the Product'
+  )
+  const eulaPolicyContent = screen.getByTestId('eulaPolicyContent')
+  expect(eulaPolicyContent).toBeInTheDocument()
+  const productPrivacyPolicy = screen.getByTestId('productPrivacyPolicy')
+  expect(productPrivacyPolicy).toBeInTheDocument()
+  expect(checkBoxTextElement).toBeInTheDocument()
+  const acceptButton = screen.getByTestId('acceptButton')
+  expect(acceptButton).toBeDisabled()
+  const checkbox = screen.getByTestId('check')
+  expect(checkbox).toBeEnabled()
+}
+
+describe('Privacy Policy Modal Non EEA USER', () => {
+  beforeEach(() => {
+    jest
+      .spyOn(userApi, 'getUserLocation')
+      .mockImplementation(() =>
+        Promise.resolve({ result: { isEEAUser: false } })
+      )
+  })
+  test('test modal components visibility ', async () => {
+    await waitFor(() => {
+      render(<PrivacyPolicyModal store={store} />)
+    })
+    componentsVisibility()
+    const eeaPolicyContent = screen.queryByTestId('eeaPolicyContent')
+    expect(eeaPolicyContent).toBeNull()
+  })
+  test('click checkbox and accept policy', async () => {
+    userApi.eulaPolicyStatusUpdate = jest
+      .fn()
+      .mockReturnValue(Promise.resolve({}))
+    render(<PrivacyPolicyModal store={store} />)
+    const acceptButton = screen.getByTestId('acceptButton')
+    expect(acceptButton).toBeDisabled()
+    const checkbox = screen.getByTestId('check')
+    await userEvent.click(checkbox)
+    await userEvent.click(acceptButton)
+    expect(userApi.eulaPolicyStatusUpdate).toBeCalled()
+  })
+})
+
+describe('privacy Policy Modal EEA USER', () => {
   beforeEach(() => {
     jest
       .spyOn(userApi, 'getUserLocation')
@@ -23,38 +74,11 @@ describe('Privacy Policy Modal Component', () => {
         Promise.resolve({ result: { isEEAUser: true } })
       )
   })
-  test('test modal components visibility ', async () => {
-    const result = render(<PrivacyPolicyModal store={store} />)
-    const moduleTitleElement = screen.getByText(
-      'End User License Agreement and Product Privacy Policy'
-    )
-    expect(moduleTitleElement).toBeInTheDocument()
-    const checkBoxTextElement = screen.getByText(
-      'By checking the box and clicking “Accept”, I agree to the Terms of Service and End User License Agreement and Privacy Policy of the Product'
-    )
-    const eulaPolicyContent = result.getByTestId('eulaPolicyContent')
-    expect(eulaPolicyContent).toBeInTheDocument()
-    const productPrivacyPolicy = result.getByTestId('productPrivacyPolicy')
-    expect(productPrivacyPolicy).toBeInTheDocument()
-    expect(checkBoxTextElement).toBeInTheDocument()
-    const acceptButton = screen.getByTestId('acceptButton')
-    expect(acceptButton).toBeDisabled()
-    const checkbox = screen.getByTestId('check')
-    expect(checkbox).toBeEnabled()
+  test('test modal components visibility with eeaPolicyContent ', async () => {
+    render(<PrivacyPolicyModal store={store} />)
+    componentsVisibility()
     await waitFor(() => {
       expect(screen.getByTestId('eeaPolicyContent')).toBeInTheDocument()
     })
-  })
-  test('click checkbox and accept policy', async () => {
-    userApi.eulaPolicyStatusUpdate = jest
-      .fn()
-      .mockReturnValue(Promise.resolve({}))
-    const result = render(<PrivacyPolicyModal store={store} />)
-    const acceptButton = result.getByTestId('acceptButton')
-    expect(acceptButton).toBeDisabled()
-    const checkbox = result.getByTestId('check')
-    await userEvent.click(checkbox)
-    await userEvent.click(acceptButton)
-    expect(userApi.eulaPolicyStatusUpdate).toBeCalled()
   })
 })

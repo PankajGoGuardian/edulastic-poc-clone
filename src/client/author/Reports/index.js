04e5c157d038d42b113f2548138273a002c5dcf4
neeraj.negi <neeraj.negi@snapwiz.com>
2022-05-03 17:48:53 +0530

    added hide data warehouse flag [EV-35441]

--- src/client/author/Reports/index.js
+++ src/client/author/Reports/index.js
@@ -259,6 +259,7 @@ const Container = (props) => {
                   isCliUser={isCliUser}
                   breadcrumbData={headerSettings.breadcrumbData}
                   setDynamicBreadcrumb={setDynamicBreadcrumb}
+                  showDataWarehouse
                 />
               )
             }}

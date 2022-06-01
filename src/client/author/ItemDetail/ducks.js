b967c0327448091ea4ee14abaa64e68c6613ab8b
Arpit Chandrakar <arpit.chandrakar@goguardian.com>
2022-06-01 19:26:34 +0530

    fix(client): Able to save passage wihout question but publish test [EV-35804]

--- src/client/author/ItemDetail/ducks.js
+++ src/client/author/ItemDetail/ducks.js
@@ -1276,12 +1276,6 @@ export function* updateItemSaga({ payload }) {
       resourceTypes.includes(item.type)
     )
 
-    if (isPassageWithQuestions && !questions.length) {
-      notification({ messageKey: 'CannotSaveWithoutQuestions' })
-      yield put(itemUpdateCompletedAction())
-      return null
-    }
-
     const _widgets = rows
       .flatMap(({ widgets }) => widgets)
       .filter((widget) => widget.widgetType === 'question')

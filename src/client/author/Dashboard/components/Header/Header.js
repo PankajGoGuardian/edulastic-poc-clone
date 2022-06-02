e46e462dc84e7bc8abbd27c8dc8104c504417e6a
Amalraj R <43946598+AmalrajR@users.noreply.github.com>
2022-06-02 19:29:30 +0530

    test(poc): added data cy on renew subscription [EV-35818] (#18151)
    Co-authored-by: Amalraj R <amalraj.ramachandrangoguardian.com@Amalraj-Rs-MacBook-Pro.local>

--- src/client/author/Dashboard/components/Header/Header.js
+++ src/client/author/Dashboard/components/Header/Header.js
@@ -363,6 +363,7 @@ const HeaderSection = ({
             >
               {needsRenewal ? (
                 <EduButton
+                  data-cy="renewSubscription"
                   type="primary"
                   isBlue
                   style={{

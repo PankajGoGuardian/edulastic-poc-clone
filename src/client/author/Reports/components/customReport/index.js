7475d9a0bebbd78f448209626218f86c835bfd08
neeraj.negi <neeraj.negi@snapwiz.com>
2022-06-22 16:34:10 +0530

    nit fixes [EV-35601]

--- src/client/author/Reports/components/customReport/index.js
+++ src/client/author/Reports/components/customReport/index.js
@@ -5,7 +5,6 @@ import styled from 'styled-components'
 import { IconUpload } from '@edulastic/icons'
 import { connect } from 'react-redux'
 import { compose } from 'redux'
-import { get } from 'lodash'
 
 import { EduButton, FlexContainer } from '@edulastic/common'
 import { StyledContainer } from '../../common/styled'

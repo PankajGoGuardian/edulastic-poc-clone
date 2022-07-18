7e3f78a2eb62ee8e7c0da280c78d29e0fc0130f2
vishnu-nagar <107404195+vishnu-nagar@users.noreply.github.com>
2022-07-18 11:34:28 +0530

    style(poc): [CR] Data warehouse | text change for DA roles (#18256)
    * style(poc): text change for DA roles [EV-36080]
    
    * style(poc): text change from "District data Admin" to "District Data Admin"

--- src/client/author/DistrictAdmin/components/DistrictAdminTable/helpers.js
+++ src/client/author/DistrictAdmin/components/DistrictAdminTable/helpers.js
@@ -2,19 +2,19 @@ import { userPermissions } from '@edulastic/constants'
 
 export const daRoleList = [
   {
-    label: 'District Admin only',
+    label: 'DA',
     value: '-',
     tooltipTitle:
       'This district admin user cannot access data warehouse feature.',
   },
   {
-    label: 'Data Warehouse upload only',
+    label: 'Data Manager',
     value: userPermissions.DATA_OPS_ONLY,
     tooltipTitle:
       'This district admin can only upload data to data warehouse feature. No access is given to district menu and insights.',
   },
   {
-    label: 'District Admin and Data Warehouse insights user',
+    label: 'District Data Admin',
     value: userPermissions.DATA_OPS,
     tooltipTitle:
       'This district admin user has full access to data warehouse upload and insight feature.',

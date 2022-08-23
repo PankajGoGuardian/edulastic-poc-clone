PankajGoGuardian/edulastic-poc-clone

commit 582510b7434d5971a9e19fe96e50a4daab07ef3b

fix(poc): set difficulty to `All Levels` and remove `DoK` filter [EV-36424] (#18391)
* fix(poc): set difficulty to `All Levels` and remove `DoK` filter [EV-36424]

set difficulty to `All Levels` in AutoSelect section of Dynamic Test

remove Depth of Knowledge filter from AutoSelect section of Dynamic Test

* chore(poc): review changes [EV-36424]

* chore(poc): review changes [EV-36424]

src/client/author/TestPage/ducks.js
@@ -105,6 +105,7 @@ import { answersByQId } from '../../assessment/selectors/test'
 import { multiFind } from '../../common/utils/main'
 import { hasValidResponse } from '../questionUtils'
 import { getProfileKey } from '../../common/utils/testTypeUtils'
+import { selectsData } from './components/common'
 
 const {
   ITEM_GROUP_TYPES,
@@ -139,6 +140,7 @@ export const NewGroupAutoselect = {
   items: [],
   deliveryType: ITEM_GROUP_DELIVERY_TYPES.ALL_RANDOM,
   index: 0,
+  difficulty: selectsData.allAuthorDifficulty[0].value,
 }
 export const createWidget = ({ id, type, title }) => ({
   widgetType: type === 'sectionLabel' ? 'resource' : 'question',

src/client/author/TestPage/components/GroupItems/index.js
@@ -755,7 +755,8 @@ const GroupItems = ({
                           disabled={currentGroupIndex !== index}
                         />
                       </SelectWrapper>
-                      <SelectWrapper width="200px">
+                      {/* TODO DOK will be re-introduced in future, hence, commented the code */}
+                      {/* <SelectWrapper width="200px">
                         <Label>Depth of knowledge</Label>
                         <Select
                           data-cy={`selectDOK-${itemGroup.groupName}`}
@@ -782,7 +783,7 @@ const GroupItems = ({
                             </Select.Option>
                           ))}
                         </Select>
-                      </SelectWrapper>
+                      </SelectWrapper> */}
                       <SelectWrapper width="200px">
                         <Label>Difficulty</Label>
                         <Select
@@ -801,6 +802,9 @@ const GroupItems = ({
                             triggerNode.parentNode
                           }
                           disabled={currentGroupIndex !== index}
+                          defaultValue={
+                            selectsData.allAuthorDifficulty[0].value
+                          }
                         >
                           {selectsData.allAuthorDifficulty.map((el) => (
                             <Select.Option

src/client/author/TestPage/components/common/selectsData.js
@@ -45,6 +45,7 @@ const allDepthOfKnowledge = [
   { value: 'Extended Thinking', text: 'Extended Thinking' },
 ]
 
+// First value is the default value.
 const allAuthorDifficulty = [
   { value: '', text: 'All Levels' },
   { value: 'Easy', text: 'Easy' },

commit 8b1f0e1ec39d65754f9d0584e8647027d279f234

fix(poc): fix circular dependency error caused by #18391 [EV-36424] (#18393)

src/client/author/TestPage/ducks.js
@@ -105,7 +105,7 @@ import { answersByQId } from '../../assessment/selectors/test'
 import { multiFind } from '../../common/utils/main'
 import { hasValidResponse } from '../questionUtils'
 import { getProfileKey } from '../../common/utils/testTypeUtils'
-import { selectsData } from './components/common'
+import selectsData from './components/common/selectsData'
 
 const {
   ITEM_GROUP_TYPES,
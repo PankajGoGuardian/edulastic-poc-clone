1ca6c9099bac4da211737d080f68d25eb4058363
Andrei Pintea <87871557+antrusha0223@users.noreply.github.com>
2023-01-16 20:39:30 -0800

    fix(client): adding proper label to pie graph in lcb [EV-37162] (#18745)
    * fix(client): adding proper label to pie graph in lcb [EV-37162]
    
    * fix(client): refactoring [EV-37162]

--- src/client/author/ClassBoard/components/ProgressGraph/ProgressGraph.js
+++ src/client/author/ClassBoard/components/ProgressGraph/ProgressGraph.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { sumBy, round } from 'lodash'
+import { round } from 'lodash'
 import { Tooltip } from 'antd'
 import { lightGreen4 } from '@edulastic/colors'
+import { EduIf, WithResources } from '@edulastic/common'
 import BarGraph from '../BarGraph/BarGraph'
 import {
   StyledProgress,
@@ -13,6 +14,7 @@ import {
   ProgressBarContainer,
   AssignmentTitle,
 } from './styled'
+import AppConfig from '../../../../../app-config'
 
 export default class Graph extends Component {
   static propTypes = {
@@ -21,17 +23,24 @@ export default class Graph extends Component {
     onClickHandler: PropTypes.func.isRequired,
   }
 
-  calculateAvgScore = () => {
-    const { testActivity: students } = this.props
-
-    if (!students.length) {
-      return 0
+  componentDidUpdate() {
+    // WEB ACCESSIBILITY FOR PIE CHART.
+    if (window.$) {
+      const { gradebook } = this.props
+      const percentage = round(gradebook.avgScore * 100)
+      const jq = window.$
+      jq('svg.ant-progress-circle').attr('aria-hidden', false)
+      jq('svg.ant-progress-circle').attr(
+        'aria-label',
+        'average score pie chart'
+      )
+      jq('path.ant-progress-circle-trail').attr('aria-hidden', true)
+      jq('path.ant-progress-circle-path').attr('aria-hidden', false)
+      jq('path.ant-progress-circle-path').attr(
+        'aria-label',
+        `average score is ${percentage}%`
+      )
     }
-    const totalScore = sumBy(students, (student) => {
-      const { score, maxScore } = student
-      return ((score || 0) / (maxScore || 1)) * 100
-    })
-    return round(totalScore / students.length, 2)
   }
 
   render() {
@@ -42,54 +51,60 @@ export default class Graph extends Component {
       testActivity,
       title = '',
       isBoth = false,
+      isLoading,
     } = this.props
     const absentNumber = (testActivity || []).filter(
       (x) => x.status === 'absent'
     ).length
     const percentage = round(gradebook.avgScore * 100)
     return (
-      <StyledDiv>
-        <ProgressBarContainer>
-          <StyledProgressDiv>
-            {
-              // TODO: need to implement gradient stoke color
-            }
-            {!!title && (
-              <Tooltip title={title}>
-                <AssignmentTitle color={lightGreen4}>{title}</AssignmentTitle>
-              </Tooltip>
-            )}
-            <GraphDescription margin="3px 0 0 0" top={title ? '32%' : '25%'}>
-              average score
-            </GraphDescription>
-            <StyledProgress
-              marginTop={1}
-              className="getProgress"
-              strokeLinecap="square"
-              type="circle"
-              percent={percentage}
-              width={167}
-              strokeWidth={8}
-              strokeColor="#2B7FF0"
-              format={(percent) => `${percent}%`}
-            />
-            <GraphDescription margin="3px 0 0 0" top="67%">
-              MEDIAN {gradebook.median}%
-            </GraphDescription>
-          </StyledProgressDiv>
-          <GraphInfo data-cy="submittedSummary">
-            {gradebook.submittedNumber} out of {gradebook.total} Submitted
-            {absentNumber > 0 && <p>({absentNumber} absent)</p>}
-          </GraphInfo>
-        </ProgressBarContainer>
-        <BarGraph
-          gradebook={gradebook}
-          testQuestionActivities={testQuestionActivities}
-          onClickHandler={onClickHandler}
-          isBoth={isBoth}
-          isLoading={this.props.isLoading}
-        />
-      </StyledDiv>
+      <WithResources
+        resources={[`${AppConfig.jqueryPath}/jquery.min.js`]}
+        fallBack={<span />}
+      >
+        <StyledDiv>
+          <ProgressBarContainer>
+            <StyledProgressDiv>
+              {
+                // TODO: need to implement gradient stoke color
+              }
+              <EduIf condition={!!title}>
+                <Tooltip title={title}>
+                  <AssignmentTitle color={lightGreen4}>{title}</AssignmentTitle>
+                </Tooltip>
+              </EduIf>
+              <GraphDescription margin="3px 0 0 0" top={title ? '32%' : '25%'}>
+                average score
+              </GraphDescription>
+              <StyledProgress
+                marginTop={1}
+                className="getProgress"
+                strokeLinecap="square"
+                type="circle"
+                percent={percentage}
+                width={167}
+                strokeWidth={8}
+                strokeColor="#2B7FF0"
+                format={(percent) => `${percent}%`}
+              />
+              <GraphDescription margin="3px 0 0 0" top="67%">
+                MEDIAN {gradebook.median}%
+              </GraphDescription>
+            </StyledProgressDiv>
+            <GraphInfo data-cy="submittedSummary">
+              {gradebook.submittedNumber} out of {gradebook.total} Submitted
+              {absentNumber > 0 && <p>({absentNumber} absent)</p>}
+            </GraphInfo>
+          </ProgressBarContainer>
+          <BarGraph
+            gradebook={gradebook}
+            testQuestionActivities={testQuestionActivities}
+            onClickHandler={onClickHandler}
+            isBoth={isBoth}
+            isLoading={isLoading}
+          />
+        </StyledDiv>
+      </WithResources>
     )
   }
 }

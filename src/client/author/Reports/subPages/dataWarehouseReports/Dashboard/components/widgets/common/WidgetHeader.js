2b1a1dbdbfddf42e83ab02508de55e5a2370c005
neeraj.negi <neeraj.negi@snapwiz.com>
2023-03-24 11:02:31 +0530

    attendance summary chart link fix [EV-37785]

--- src/client/author/Reports/subPages/dataWarehouseReports/Dashboard/components/widgets/common/WidgetHeader.js
+++ src/client/author/Reports/subPages/dataWarehouseReports/Dashboard/components/widgets/common/WidgetHeader.js
@@ -1,17 +1,18 @@
 import { IconExternalLink } from '@edulastic/icons'
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { DW_DASHBOARD_URL } from '../../../../../../common/constants/dataWarehouseReports'
 
-const WidgetHeader = ({ title, url = { DW_DASHBOARD_URL } }) => {
+const WidgetHeader = ({ title, url }) => {
   return (
     <div>
       <span className="title">{title}</span>
-      <span className="external-link">
-        <Link to={url} target="_blank">
-          <IconExternalLink />
-        </Link>
-      </span>
+      {url && (
+        <span className="external-link">
+          <Link to={url} target="_blank">
+            <IconExternalLink />
+          </Link>
+        </span>
+      )}
     </div>
   )
 }

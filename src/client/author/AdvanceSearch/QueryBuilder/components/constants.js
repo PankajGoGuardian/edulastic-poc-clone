b3730a48d2253485d2e2cbc2dae8c64d4f5fb34e
Nitesh Kumar Sharma <86227678+Nitesh377@users.noreply.github.com>
2023-03-27 12:29:08 +0530

    fix(client): iframe src should be a embedded url in advancedsearch help video [EV-37991] (#19125)
    Co-authored-by: Nitesh377 <nitesh.sharma@goguardian.com>

--- src/client/author/AdvanceSearch/QueryBuilder/components/constants.js
+++ src/client/author/AdvanceSearch/QueryBuilder/components/constants.js
@@ -1,4 +1,4 @@
 export const advancedSearchHelpArtical =
   'https://support.goguardian.com/s/article/How-to-use-Advanced-Search-in-Assignments'
 export const advancedSearchHelpVideo =
-  'https://swvideo.wistia.com/medias/e3fsthjb3q'
+  '//fast.wistia.net/embed/iframe/e3fsthjb3q'

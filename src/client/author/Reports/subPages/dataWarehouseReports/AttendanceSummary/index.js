663aef021a838431da04c18823756eb23c0419fe
Navneet Chaudhary <navneet.chaudhary@goguardian.com>
2023-03-27 13:36:48 +0530

    fix(ui): adding hard coded data for the table [EV-37847]

--- src/client/author/Reports/subPages/dataWarehouseReports/AttendanceSummary/index.js
+++ src/client/author/Reports/subPages/dataWarehouseReports/AttendanceSummary/index.js
@@ -20,6 +20,145 @@ import PerformanceTable, { compareByOptions } from './Performance'
 import AttendanceSummaryChart from './WeeklyAttendaceChart/AttendanceSummaryChart'
 import Tardies from './Tardies'
 
+const hardCodedAttendenceData = [
+    {
+    dimension: {
+      _id: "1",
+      name: "Class 1"
+    },
+    avgAttendance: 85,
+    tardyEventCount: 2,
+    attendanceDistribution: [
+      {
+        _id: "p",
+        name: "Present",
+        color: "#00ff00",
+        value: 75
+      },
+      {
+        _id: "a",
+        name: "Absent",
+        color: "#ff0000",
+        value: 10
+      },
+      {
+        _id: "l",
+        name: "Late",
+        color: "#ffff00",
+        value: 10
+      },
+      {
+        _id: "e",
+        name: "Excused",
+        color: "#888888",
+        value: 5
+      }
+    ]
+  },
+  {
+    dimension: {
+      _id: "2",
+      name: "Class 2"
+    },
+    avgAttendance: 90,
+    tardyEventCount: 1,
+    attendanceDistribution: [
+      {
+        _id: "p",
+        name: "Present",
+        color: "#00ff00",
+        value: 85
+      },
+      {
+        _id: "a",
+        name: "Absent",
+        color: "#ff0000",
+        value: 5
+      },
+      {
+        _id: "l",
+        name: "Late",
+        color: "#ffff00",
+        value: 5
+      },
+      {
+        _id: "e",
+        name: "Excused",
+        color: "#888888",
+        value: 5
+      }
+    ]
+  },
+  {
+    dimension: {
+      _id: "3",
+      name: "Class 3"
+    },
+    avgAttendance: 75,
+    tardyEventCount: 3,
+    attendanceDistribution: [
+      {
+        _id: "p",
+        name: "Present",
+        color: "#00ff00",
+        value: 60
+      },
+      {
+        _id: "a",
+        name: "Absent",
+        color: "#ff0000",
+        value: 10
+      },
+      {
+        _id: "l",
+        name: "Late",
+        color: "#ffff00",
+        value: 5
+      },
+      {
+        _id: "e",
+        name: "Excused",
+        color: "#888888",
+        value: 5
+      },
+      {
+        _id: "t",
+        name: "Tardy",
+        color: "#ff8800",
+        value: 20
+      }
+    ]
+  },
+  {
+    dimension: {
+      _id: "4",
+      name: "Class 4"
+    },
+    avgAttendance: 95,
+    tardyEventCount: 0,
+    attendanceDistribution: [
+      {
+        _id: "p",
+        name: "Present",
+        color: "#00ff00",
+        value: 95
+      },
+      {
+        _id: "a",
+        name: "Absent",
+        color: "#ff0000",
+        value: 0
+      },
+      {
+        _id: "e",
+        name: "Excused",
+        color: "#888888",
+        value: 5
+      }
+    ]
+  },
+]
+
 // TODO move this action to parent.
 const useLegacyReportActions = (filters, props, reportId) => {
   useEffect(() => {

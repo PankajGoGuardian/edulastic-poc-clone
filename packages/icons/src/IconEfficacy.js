587f8eb1c696906b396224a1a97beb3e5f863bf5
Subho2403 <subhojeet.saha@goguardian.com>
2023-04-11 20:44:31 +0530

    style(design): addition of new Icons for Goals and Interventions [EV-38290]

---
+++ packages/icons/src/IconEfficacy.js
@@ -0,0 +1,384 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import withIconStyles from './HOC/withIconStyles'
+import SVG from './common/SVG'
+
+const IconEfficacy = (props) => (
+  <SVG
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    width="160"
+    height="143.505"
+    viewBox="0 0 160 143.505"
+    {...props}
+  >
+    <defs>
+      <filter
+        id="Rectangle_3717"
+        x="11.036"
+        y="37.897"
+        width="84.132"
+        height="54.631"
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dy="3" input="SourceAlpha" />
+        <feGaussianBlur stdDeviation="3" result="blur" />
+        <feFlood floodOpacity="0.059" />
+        <feComposite operator="in" in2="blur" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <filter
+        id="Rectangle_3726"
+        x="64.863"
+        y="37.897"
+        width="84.132"
+        height="54.631"
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dy="3" input="SourceAlpha" />
+        <feGaussianBlur stdDeviation="3" result="blur-2" />
+        <feFlood floodOpacity="0.059" />
+        <feComposite operator="in" in2="blur-2" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+    </defs>
+    <g
+      id="Group_11654"
+      data-name="Group 11654"
+      transform="translate(-1084 -297)"
+    >
+      <rect
+        id="Rectangle_3734"
+        data-name="Rectangle 3734"
+        width="160"
+        height="143.505"
+        rx="10"
+        transform="translate(1084 297)"
+        fill="#f5f5f5"
+      />
+      <g
+        id="Group_10939"
+        data-name="Group 10939"
+        transform="translate(1102 311.928)"
+      >
+        <rect
+          id="Rectangle_3716"
+          data-name="Rectangle 3716"
+          width="70.143"
+          height="44.923"
+          transform="translate(0.03 24.107)"
+          fill="#f2f2f2"
+        />
+        <g
+          transform="matrix(1, 0, 0, 1, -18, -14.93)"
+          filter="url(#Rectangle_3717)"
+        >
+          <rect
+            id="Rectangle_3717-2"
+            data-name="Rectangle 3717"
+            width="66.132"
+            height="36.631"
+            transform="translate(20.04 43.9)"
+            fill="#fff"
+          />
+        </g>
+        <rect
+          id="Rectangle_3718"
+          data-name="Rectangle 3718"
+          width="70.143"
+          height="2.98"
+          transform="translate(0 27.254)"
+          fill="#53b095"
+        />
+        <circle
+          id="Ellipse_532"
+          data-name="Ellipse 532"
+          cx="0.552"
+          cy="0.552"
+          r="0.552"
+          transform="translate(1.662 28.117)"
+          fill="#fff"
+        />
+        <circle
+          id="Ellipse_533"
+          data-name="Ellipse 533"
+          cx="0.552"
+          cy="0.552"
+          r="0.552"
+          transform="translate(3.759 28.117)"
+          fill="#fff"
+        />
+        <circle
+          id="Ellipse_534"
+          data-name="Ellipse 534"
+          cx="0.552"
+          cy="0.552"
+          r="0.552"
+          transform="translate(5.855 28.117)"
+          fill="#fff"
+        />
+        <rect
+          id="Rectangle_3719"
+          data-name="Rectangle 3719"
+          width="16.993"
+          height="29.029"
+          transform="translate(8.045 32.52)"
+          fill="#f2f2f2"
+        />
+        <rect
+          id="Rectangle_3720"
+          data-name="Rectangle 3720"
+          width="14.161"
+          height="7.991"
+          transform="translate(21.359 34.331)"
+          fill="#53b095"
+        />
+        <rect
+          id="Rectangle_3721"
+          data-name="Rectangle 3721"
+          width="14.161"
+          height="8.597"
+          transform="translate(21.359 42.204)"
+          fill="#f2f2f2"
+        />
+        <rect
+          id="Rectangle_3722"
+          data-name="Rectangle 3722"
+          width="14.161"
+          height="7.991"
+          transform="translate(21.359 50.657)"
+          fill="#f2f2f2"
+        />
+        <rect
+          id="Rectangle_3723"
+          data-name="Rectangle 3723"
+          width="16.993"
+          height="12.651"
+          transform="translate(34.275 33.98)"
+          fill="#f2f2f2"
+        />
+        <rect
+          id="Rectangle_3724"
+          data-name="Rectangle 3724"
+          width="16.993"
+          height="12.651"
+          transform="translate(34.275 46.56)"
+          fill="#f2f2f2"
+        />
+        <rect
+          id="Rectangle_3725"
+          data-name="Rectangle 3725"
+          width="70.143"
+          height="44.923"
+          transform="translate(53.857 24.107)"
+          fill="#f2f2f2"
+        />
+        <g
+          transform="matrix(1, 0, 0, 1, -18, -14.93)"
+          filter="url(#Rectangle_3726)"
+        >
+          <rect
+            id="Rectangle_3726-2"
+            data-name="Rectangle 3726"
+            width="66.132"
+            height="36.631"
+            transform="translate(73.86 43.9)"
+            fill="#fff"
+          />
+        </g>
+        <rect
+          id="Rectangle_3727"
+          data-name="Rectangle 3727"
+          width="70.143"
+          height="2.98"
+          transform="translate(53.827 27.254)"
+          fill="#53b095"
+        />
+        <circle
+          id="Ellipse_535"
+          data-name="Ellipse 535"
+          cx="0.552"
+          cy="0.552"
+          r="0.552"
+          transform="translate(70.096 28.117)"
+          fill="#fff"
+        />
+        <circle
+          id="Ellipse_536"
+          data-name="Ellipse 536"
+          cx="0.552"
+          cy="0.552"
+          r="0.552"
+          transform="translate(71.739 28.117)"
+          fill="#fff"
+        />
+        <circle
+          id="Ellipse_537"
+          data-name="Ellipse 537"
+          cx="0.552"
+          cy="0.552"
+          r="0.552"
+          transform="translate(73.381 28.117)"
+          fill="#fff"
+        />
+        <rect
+          id="Rectangle_3728"
+          data-name="Rectangle 3728"
+          width="16.993"
+          height="29.029"
+          transform="translate(98.977 32.52)"
+          fill="#f2f2f2"
+        />
+        <rect
+          id="Rectangle_3729"
+          data-name="Rectangle 3729"
+          width="34.275"
+          height="7.991"
+          transform="translate(69.537 34.33)"
+          fill="#53b095"
+        />
+        <rect
+          id="Rectangle_3730"
+          data-name="Rectangle 3730"
+          width="34.275"
+          height="8.597"
+          transform="translate(69.537 42.204)"
+          fill="#f2f2f2"
+        />
+        <rect
+          id="Rectangle_3731"
+          data-name="Rectangle 3731"
+          width="34.275"
+          height="7.991"
+          transform="translate(69.537 50.657)"
+          fill="#f2f2f2"
+        />
+        <rect
+          id="Rectangle_3732"
+          data-name="Rectangle 3732"
+          width="0.392"
+          height="21.421"
+          transform="translate(61.339)"
+          fill="#ccc"
+        />
+        <path
+          id="Path_3546"
+          data-name="Path 3546"
+          d="M277.756,121.795h-.392V108.66H199.339v13.135h-.392V108.268h78.809Z"
+          transform="translate(-176.69 -87.043)"
+          fill="#ccc"
+        />
+        <path
+          id="Path_3547"
+          data-name="Path 3547"
+          d="M590.679,380.267l-.3-1.476s-4.342-1.476-4.828,1.476Z"
+          transform="translate(-526.327 -332.821)"
+          fill="#e6e6e6"
+        />
+        <path
+          id="Path_3548"
+          data-name="Path 3548"
+          d="M354.926,675.016h-2.847l-1.354-10.98h4.2Z"
+          transform="translate(-297.168 -564.352)"
+          fill="#ffb8b8"
+        />
+        <path
+          id="Path_3549"
+          data-name="Path 3549"
+          d="M535.241,801.359h-9.179v-.116a3.573,3.573,0,0,1,3.573-3.573h5.607Z"
+          transform="translate(-476.294 -687.935)"
+          fill="#2f2e41"
+        />
+        <path
+          id="Path_3550"
+          data-name="Path 3550"
+          d="M422.073,675.016h2.847l1.354-10.98h-4.2Z"
+          transform="translate(-357.621 -564.352)"
+          fill="#ffb8b8"
+        />
+        <path
+          id="Path_3551"
+          data-name="Path 3551"
+          d="M615.4,797.67H621a3.573,3.573,0,0,1,3.573,3.572v.116H615.4Z"
+          transform="translate(-552.117 -687.935)"
+          fill="#2f2e41"
+        />
+        <path
+          id="Path_3552"
+          data-name="Path 3552"
+          d="M424.849,335.88l3.247-.3,2.066,9.742-.3,8.856-3.247,11.809-4.428-2.657,2.362-12.1Z"
+          transform="translate(-358.181 -287.435)"
+          fill="#2f2e41"
+        />
+        <path
+          id="Path_3553"
+          data-name="Path 3553"
+          d="M365.766,480.577l-.727,6.2.886,14.466V522.8l-5.609-.3-1.181-22.436-3.247-10.332-2.362,11.809-1.476,22.141-5.757-.461V501.459l-.148-15.864,2.066-6.2Z"
+          transform="translate(-294.978 -415.909)"
+          fill="#2f2e41"
+        />
+        <path
+          id="Path_3554"
+          data-name="Path 3554"
+          d="M551.3,382.7s2.066-2.952,5.9-.886l1.771,2.362,5.609,4.133-1.181,13.875,1.476,17.418-20.96-.3,3.543-17.418v-18.6Z"
+          transform="translate(-492.79 -339.424)"
+          fill="#2f2e41"
+        />
+        <path
+          id="Path_3555"
+          data-name="Path 3555"
+          d="M440.7,360.58l1.693,20.239-2.135-10.969Z"
+          transform="translate(-372.779 -307.289)"
+          opacity="0.25"
+        />
+        <path
+          id="Path_3556"
+          data-name="Path 3556"
+          d="M545.108,558.481a2.177,2.177,0,0,0-.587-3.287l.631-4.935-2.906-1.115-.793,6.982a2.189,2.189,0,0,0,3.655,2.355Z"
+          transform="translate(-488.687 -478.459)"
+          fill="#ffb8b8"
+        />
+        <rect
+          id="Rectangle_3733"
+          data-name="Rectangle 3733"
+          width="4.723"
+          height="2.067"
+          transform="translate(57.415 75.761) rotate(180)"
+          fill="#e6e6e6"
+        />
+        <path
+          id="Path_3557"
+          data-name="Path 3557"
+          d="M547.54,394.857l-3.1-2.509s-2.214-.148-2.509,3.1-.654,17.122-.654,17.122l-1.117,13.875,6.2.3,1.181-15.018Z"
+          transform="translate(-488.076 -348.462)"
+          fill="#2f2e41"
+        />
+        <circle
+          id="Ellipse_538"
+          data-name="Ellipse 538"
+          cx="5.928"
+          cy="5.928"
+          r="5.928"
+          transform="translate(53.706 43.009) rotate(-61.337)"
+          fill="#ffb8b8"
+        />
+        <path
+          id="Path_3558"
+          data-name="Path 3558"
+          d="M569.473,319.528a6.013,6.013,0,0,0,3.934-11.3,7.031,7.031,0,0,0-3.32-.6c-1.773.093-3.5.749-5.271.7l-.257.413-.969-.235-.208.77-.255-.009a1.47,1.47,0,0,0,1.244,2.057,5.745,5.745,0,0,0,1.117-.243,1.125,1.125,0,0,1,1.071.222c.335.367.189.939.119,1.431s.1,1.154.6,1.17c.254.008.495-.174.744-.126a.6.6,0,0,1,.395.7,2.778,2.778,0,0,1-.347.811,3.266,3.266,0,0,0,1.3,4.166"
+          transform="translate(-508.171 -273.654)"
+          fill="#2f2e41"
+        />
+        <path
+          id="Path_3559"
+          data-name="Path 3559"
+          d="M480.362,816.026H405.67a.2.2,0,0,1,0-.392h74.692a.2.2,0,0,1,0,.392Z"
+          transform="translate(-381.366 -702.377)"
+          fill="#ccc"
+        />
+      </g>
+    </g>
+  </SVG>
+)
+
+export default withIconStyles(IconEfficacy)
--- src/client/author/Reports/components/dataWarehouseReport/DataWarehouseReportCardsWrapper.js
+++ src/client/author/Reports/components/dataWarehouseReport/DataWarehouseReportCardsWrapper.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import { greyThemeDark3 } from '@edulastic/colors'
 import {
   IconWholeLearnerReport,
   IconMultipleAssessmentReportDW,
   IconAttendanceReport,
   IconDashboardReport,
+  IconSetGoals,
+  IconSurveyInsights,
+  IconEarlyWarning,
+  IconEfficacy,
 } from '@edulastic/icons'
 import ReportLinkCard from './common/components/ReportLinkCard'
 // import MoreReportsContainer from './common/components/MoreReportsContainer'
@@ -16,41 +20,115 @@ import {
   DW_DASHBOARD_URL,
 } from '../../common/constants/dataWarehouseReports'
 
+const reports = [
+  {
+    id: 1,
+    title: 'Academic Reports',
+    cards: [
+      {
+        id: 1,
+        IconThumbnail: IconDashboardReport,
+        title: 'Dashboard',
+        description:
+          "View key health checks for students' performance. Drill down to analyze and intervene.",
+        url: DW_DASHBOARD_URL,
+      },
+      {
+        id: 2,
+        IconThumbnail: IconMultipleAssessmentReportDW,
+        title: 'Performance Trends',
+        description:
+          "View whether the student's performance is improving over time &amp; take necessary interventions.",
+        url: DW_MAR_REPORT_URL,
+      },
+      {
+        id: 3,
+        IconThumbnail: IconWholeLearnerReport,
+        title: 'Whole Learner',
+        description:
+          "Get a complete understanding of a learner's academic &amp; associated indicators &amp; take necessary actions for the learner’s growth.",
+        url: DW_WLR_REPORT_URL,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: 'Non-Academic Reports',
+    cards: [
+      {
+        id: 1,
+        IconThumbnail: IconAttendanceReport,
+        title: 'Attendance Summary',
+        description:
+          'Monitor attendance and tardies, identify students at risk of chronic absenteeism, and intervene.',
+        url: DW_ATTENDANCE_REPORT_URL,
+      },
+      {
+        id: 2,
+        IconThumbnail: IconSurveyInsights,
+        title: 'Survey Insights',
+        description:
+          'Get insights into student’s responses to Skill Surveys and view competency trends.',
+        url: DW_MAR_REPORT_URL,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Goals and Interventions',
+    cards: [
+      {
+        id: 1,
+        IconThumbnail: IconSetGoals,
+        title: 'Set Goals & Interventions',
+        description: 'Set Goals and Interventions and track improvement.',
+        url: DW_DASHBOARD_URL,
+      },
+      {
+        id: 2,
+        IconThumbnail: IconEarlyWarning,
+        title: 'Early Warning',
+        description:
+          'View students at risk based on their academic and attendance performance and plan interventions.',
+        url: DW_MAR_REPORT_URL,
+      },
+      {
+        id: 3,
+        IconThumbnail: IconEfficacy,
+        title: 'Efficacy',
+        description:
+          'Compare student performance across tests pre and post-intervention.',
+        url: DW_WLR_REPORT_URL,
+      },
+    ],
+  },
+]
+
 const DataWarehoureReportCardsWrapper = ({ loc }) => {
   return (
-    <StyledCardsContainer>
-      <ReportLinkCard
-        IconThumbnail={IconDashboardReport}
-        title="Dashboard"
-        description="View key health checks for students' performance. Drill down to analyze and intervene."
-        url={DW_DASHBOARD_URL}
-        loc={loc}
-      />
-      <ReportLinkCard
-        IconThumbnail={IconMultipleAssessmentReportDW}
-        title="Performance Trends"
-        description="View whether the student's performance is improving over time &amp; take necessary interventions."
-        url={DW_MAR_REPORT_URL}
-        loc={loc}
-      />
-      <ReportLinkCard
-        IconThumbnail={IconWholeLearnerReport}
-        title="Whole Learner"
-        description="Get a complete understanding of a learner's academic &amp; associated indicators &amp; take necessary actions for the learner’s growth."
-        url={DW_WLR_REPORT_URL}
-        loc={loc}
-      />
-      <ReportLinkCard
-        IconThumbnail={IconAttendanceReport}
-        title="Attendance Summary"
-        description="Monitor attendance and tardies, identify students at risk of chronic absenteeism, and intervene."
-        url={DW_ATTENDANCE_REPORT_URL}
-        loc={loc}
-      />
-      {/* <MoreReportsContainer /> */}
-    </StyledCardsContainer>
+    <Container>
+      {reports.map((item) => (
+        <div>
+          <h2>{item.title}</h2>
+          <StyledCardsContainer>
+            {item.cards.map((card) => (
+              <ReportLinkCard {...card} loc={loc} />
+            ))}
+          </StyledCardsContainer>
+        </div>
+      ))}
+    </Container>
   )
 }
+
+const Container = styled.div`
+  h2 {
+    margin-bottom: 20px;
+    margin-top: 20px;
+    font-weight: bold;
+    color: ${greyThemeDark3};
+  }
+`
 
 const StyledCardsContainer = styled.div`
   display: grid;

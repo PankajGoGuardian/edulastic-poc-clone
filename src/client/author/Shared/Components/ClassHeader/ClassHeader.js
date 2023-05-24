9d9c28dcddffa5cb98a6a345053704cb40c51898
Subhojeet Saha <124559179+Subho2403@users.noreply.github.com>
2023-05-24 16:42:12 +0530

    fix(client): Showing standard based report even if testContentVisibility is hidden [EV-38244] (#19360)

--- src/client/author/Shared/Components/ClassHeader/ClassHeader.js
+++ src/client/author/Shared/Components/ClassHeader/ClassHeader.js
@@ -57,6 +57,7 @@ import {
   showScoreSelector,
   getIsShowUnAssignSelector,
   testActivtyLoadingSelector,
+  getAdditionalDataSelector,
 } from '../../../ClassBoard/ducks'
 import { toggleDeleteAssignmentModalAction } from '../../../sharedDucks/assignments'
 import {
@@ -475,6 +476,7 @@ class ClassHeader extends Component {
       schoologySyncAssignment,
       syncWithSchoologyClassroomInProgress,
       isProxiedByEAAccount,
+      testContentVisibility,
     } = this.props
     const {
       visible,
@@ -1036,12 +1038,20 @@ class ClassHeader extends Component {
                 </FeaturesSwitch>
 
                 <WithDisableMessage
-                  disabled={!isItemsVisible}
+                  disabled={
+                    !isItemsVisible &&
+                    testContentVisibility !==
+                      testContants.testContentVisibility.HIDDEN
+                  }
                   errMessage={t('common.testHidden')}
                 >
                   <HeaderTabs
                     to={`/author/standardsBasedReport/${assignmentId}/${classId}`}
-                    disabled={!isItemsVisible}
+                    disabled={
+                      !isItemsVisible &&
+                      testContentVisibility !==
+                        testContants.testContentVisibility.HIDDEN
+                    }
                     dataCy="StandardsBasedReport"
                     isActive={active === 'standard_report'}
                     icon={<IconNotes left={0} />}
@@ -1236,6 +1246,8 @@ const enhance = compose(
         state
       ),
       userRole: getUserRole(state),
+      testContentVisibility:
+        getAdditionalDataSelector(state)?.testContentVisibility || '',
     }),
     {
       loadTestActivity: receiveTestActivitydAction,

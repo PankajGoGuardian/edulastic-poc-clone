0e36d4500ba24902f21613ead299b3a9d8137842
Jitesh Kumar <jitesh.kumar@snapwiz.com>
2023-05-26 20:48:59 +0530

    fix(api): Preventing loading on default test setting and not restricting audio response type question for edulastic curator role [EV-38259] (#19176) (#19394)
    Co-authored-by: Nitesh Kumar Sharma <86227678+Nitesh377@users.noreply.github.com>
    Co-authored-by: Nitesh377 <nitesh.sharma@goguardian.com>

--- src/client/assessment/sagas/test.js
+++ src/client/assessment/sagas/test.js
@@ -412,7 +412,7 @@ function* loadTest({ payload }) {
     const isPublicUrl = window.location.href.includes(PUBLIC_URL_IDENTIFIER)
     const canLoadTestSettingWhilePreviewing = [
       preview,
-      userRole !== roleuser.STUDENT,
+      ![roleuser.STUDENT, roleuser.EDULASTIC_CURATOR].includes(userRole),
       !isPublicUrl,
       userAuthenticated,
     ].every((o) => !!o)

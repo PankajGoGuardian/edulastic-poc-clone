f9f5aa0401735c400cc0e04329f8b2434ae02cbc
shreyans-s <shreyans.s@goguardian.com>
2023-06-05 12:46:29 +0530

    feat(poc): Video Quiz [EV-38531]
    - Refactoring
    - Enable YouTube for educational if api key available

--- src/client/author/AssessmentPage/components/VideoPreview/CombinedPlayer.js
+++ src/client/author/AssessmentPage/components/VideoPreview/CombinedPlayer.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import { EduElse, EduIf, EduThen } from '@edulastic/common'
 import { StyledReactPlayer } from './styled'
 import { extractVideoId } from './utils'
 import YouTubePlayer from './YouTubePlayer'
+import appConfig from '../../../../../app-config'
 
 const CombinedPlayer = React.forwardRef(({ url, ...props }, ref) => {
-  const isYouTube = extractVideoId(url)
+  const showTYEducational = appConfig.edYouTubePlayerKey && extractVideoId(url)
 
-  return isYouTube ? (
-    <YouTubePlayer ref={ref} url={url} {...props} />
-  ) : (
-    <StyledReactPlayer ref={ref} url={url} {...props} />
+  const playerProps = {
+    ref,
+    url,
+    ...props,
+  }
+
+  return (
+    <EduIf condition={showTYEducational}>
+      <EduThen>
+        <YouTubePlayer {...playerProps} />
+      </EduThen>
+      <EduElse>
+        <StyledReactPlayer {...playerProps} />
+      </EduElse>
+    </EduIf>
   )
 })
 

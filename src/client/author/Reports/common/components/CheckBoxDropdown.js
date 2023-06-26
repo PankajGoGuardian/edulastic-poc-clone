180b375f20f6ef9ac4d27c3fa51cd961c85fa9e4
vishnu-nagar <107404195+vishnu-nagar@users.noreply.github.com>
2023-06-26 11:08:01 +0530

    feat(client): EW - add apply button for checkboxes [EV-39120] (#19566)

--- src/client/author/Reports/common/components/CheckBoxDropdown.js
+++ src/client/author/Reports/common/components/CheckBoxDropdown.js
@@ -1,4 +1,4 @@
-import { Checkbox, Tooltip } from 'antd'
+import { Checkbox } from 'antd'
 import React from 'react'
 import { StyledCheckBox } from '../styled'
 
@@ -11,21 +11,10 @@ const CheckBoxDropdown = ({
   return (
     <Checkbox.Group onChange={handleChange} defaultValue={defaultValues}>
       {data.map((d) => {
-        const shouldCheckboxBeDisabled =
-          defaultValues.length === 1 && defaultValues.includes(d.level)
-        const tooltipText = shouldCheckboxBeDisabled
-          ? 'Please select at least one risk band.'
-          : ''
         return (
-          <Tooltip title={tooltipText}>
-            <StyledCheckBox
-              key={d.level}
-              value={d.level}
-              disabled={shouldCheckboxBeDisabled}
-            >
-              <Label value={d} />
-            </StyledCheckBox>
-          </Tooltip>
+          <StyledCheckBox key={d.level} value={d.level}>
+            <Label value={d} />
+          </StyledCheckBox>
         )
       })}
     </Checkbox.Group>
--- src/client/author/Reports/subPages/dataWarehouseReports/EarlyWarningReport/components/Table/RiskFilter.js
+++ src/client/author/Reports/subPages/dataWarehouseReports/EarlyWarningReport/components/Table/RiskFilter.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Icon, Popover } from 'antd'
 import { IconFilter } from '@edulastic/icons'
+import { EduButton } from '@edulastic/common'
 import CheckBoxDropdown from '../../../../../common/components/CheckBoxDropdown'
 import { riskCheckBoxDropdownOptions, tableFilterTypes } from '../../utils'
 import { StyledEduButton } from '../../../common/components/styledComponents'
@@ -8,28 +9,52 @@ import CheckBoxLabel from './CheckBoxLabel'
 
 const RiskFilter = ({ tableFilters, setTableFilters }) => {
   const [showPopover, setShowPopover] = useState(false)
+  const [selected, setSelected] = useState([])
+  const [isApplyDisabled, setIsApplyDisabled] = useState(false)
 
   const handlePopoverChange = (newOpen) => {
     setShowPopover(newOpen)
   }
   const handleCheckBoxChange = (selections) => {
+    if (!selections.length) {
+      setIsApplyDisabled(true)
+    } else setIsApplyDisabled(false)
+    setSelected(selections)
+  }
+
+  const handleClick = () => {
     setTableFilters({
       ...tableFilters,
-      [tableFilterTypes.RISK]: selections,
+      [tableFilterTypes.RISK]: selected,
       [tableFilterTypes.PAGE]: 1,
     })
     setShowPopover(false)
   }
 
+  const applyButtonTitle = isApplyDisabled
+    ? 'Please select at least one risk band.'
+    : ''
+
   return (
     <Popover
       content={
-        <CheckBoxDropdown
-          Label={CheckBoxLabel}
-          data={riskCheckBoxDropdownOptions}
-          handleChange={handleCheckBoxChange}
-          defaultValues={tableFilters[tableFilterTypes.RISK]}
-        />
+        <>
+          <CheckBoxDropdown
+            Label={CheckBoxLabel}
+            data={riskCheckBoxDropdownOptions}
+            handleChange={handleCheckBoxChange}
+            defaultValues={tableFilters[tableFilterTypes.RISK]}
+          />
+          <EduButton
+            width="60px"
+            height="27px"
+            onClick={handleClick}
+            disabled={isApplyDisabled}
+            title={applyButtonTitle}
+          >
+            APPLY
+          </EduButton>
+        </>
       }
       trigger="click"
       placement="bottom"

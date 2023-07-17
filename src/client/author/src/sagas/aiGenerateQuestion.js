5d4d8cc3d139d4ef5cedbd0f6da738827b437aa1
aniket-snapwiz <aniket.hatti@goguardian.com>
2023-07-17 13:32:32 +0530

    fix(client): update notification messages

--- src/client/author/src/sagas/aiGenerateQuestion.js
+++ src/client/author/src/sagas/aiGenerateQuestion.js
@@ -5,8 +5,14 @@ import { setAIGeneratedQuestionStateAction } from '../actions/aiGenerateQuestion
 import { FETCH_AI_GENERATE_QUESTION } from '../constants/actions'
 
 function* fetchAIGeneratedQuestionSaga({ payload }) {
+  const failedMessage =
+    "Apologies for the inconvenience. We encountered an issue while generating questions based on the video's transcript. Please try again"
   try {
-    notification({ type: 'info', msg: 'Generating AI-Powered questions!' })
+    notification({
+      type: 'info',
+      msg:
+        "Generating personalized questions based on the video's content. This might take a moment, but we promise it'll be worth the wait!",
+    })
     yield put(
       setAIGeneratedQuestionStateAction({ apiStatus: 'INITIATED', result: [] })
     )
@@ -40,20 +46,19 @@ function* fetchAIGeneratedQuestionSaga({ payload }) {
     if (result?.length) {
       notification({
         type: 'success',
-        msg: 'Successfully generated AI-Powered questions!',
+        msg: `Great news! We have successfully generated ${result.length} questions based on the video. We encourage you to review the questions and make any necessary adjustments to ensure they meet your learning objectives and preferences. Feel free to customize the questions further, if desired.`,
       })
     } else {
       notification({
         type: 'error',
-        msg: 'Failed to generate AI-Powered questions.',
+        msg: failedMessage,
       })
     }
   } catch (err) {
     yield put(
       setAIGeneratedQuestionStateAction({ apiStatus: 'FAILED', result: [] })
     )
-    const errorMessage =
-      err.response.data?.message || 'Failed to generate AI question.'
+    const errorMessage = failedMessage
     notification({ msg: errorMessage })
   }
 }

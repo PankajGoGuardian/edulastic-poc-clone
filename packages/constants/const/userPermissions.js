f40b068b8525fa5b3a73710b365445d3412f51d5
Arpit Chandrakar <57706980+arpit-snapwiz@users.noreply.github.com>
2023-08-03 10:47:41 +0530

    fix(client): using correct keys to show permissions on FE [EV-37866] (#19725)

--- packages/constants/const/userPermissions.js
+++ packages/constants/const/userPermissions.js
@@ -5,6 +5,7 @@ const userPermissions = {
   DATA_OPS: 'data_ops',
   DATA_OPS_ONLY: 'data_ops_only',
   DATA_WAREHOUSE_REPORTS: 'data_warehouse_reports',
+  INSIGHTS_ONLY: 'insights_only',
 }
 
 module.exports = userPermissions

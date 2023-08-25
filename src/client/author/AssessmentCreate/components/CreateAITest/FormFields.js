c0d3959d2ac7f96db41c654f9f2b672c81b0953b
aniket-snapwiz <aniket.hatti@goguardian.com>
2023-08-25 11:04:41 +0530

    fix(client): pass standardsRequiredFields prop from StandardSet

--- src/client/author/AssessmentCreate/components/CreateAITest/FormFields.js
+++ src/client/author/AssessmentCreate/components/CreateAITest/FormFields.js
@@ -14,6 +14,7 @@ import { StyledFilterLabel, StyledRequired } from './styled'
 import { formFields } from './ducks/constants'
 
 import StandardSet from '../../../AssessmentPage/components/QuestionEditModal/common/StandardSet/StandardSet'
+import { standardsFields } from '../../../AssessmentPage/VideoQuiz/constants'
 
 const {
   TEST_NAME,
@@ -105,7 +106,10 @@ const FormFields = ({
             onUpdate={(data) => updateAlignment(data.alignment)}
             showIconBrowserBtn
             hideLabel
-            gradesSubjectMandatory
+            standardsRequiredFields={[
+              standardsFields.SUBJECT,
+              standardsFields.GRADES,
+            ]}
             isDocBased
           />
         </Col>

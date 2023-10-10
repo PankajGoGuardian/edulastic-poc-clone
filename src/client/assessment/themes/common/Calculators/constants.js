647a7408da40d608de58db9ba0720b58f3890583
Pankaj <pankaj.kumar@goguardian.com>
2023-10-10 17:02:15 +0530

    fix(poc): added height and width for matrix calculator [EV-40476]

--- src/client/assessment/themes/common/Calculators/constants.js
+++ src/client/assessment/themes/common/Calculators/constants.js
@@ -42,6 +42,7 @@ export const RND_PROPS = {
   [CALC_MODES.SCIENTIFIC_DESMOS]: { x: 0, y: 0, width: 600, height: 500 },
   [CALC_MODES.GRAPHING_DESMOS]: { x: 0, y: 0, width: 600, height: 400 },
   [CALC_MODES.GRAPHING_STATE_DESMOS]: { x: 0, y: 0, width: 600, height: 400 },
+  [CALC_MODES.MATRIX_DESMOS]: { x: 0, y: 0, width: 600, height: 500 },
   [CALC_MODES.BASIC_EDULASTIC]: { x: 0, y: 0, width: 350, height: 400 },
   [CALC_MODES.SCIENTIFIC_EDULASTIC]: { x: 0, y: 0, width: 500, height: 410 },
   [CALC_MODES.GRAPHING_GEOGEBRASCIENTIFIC]: {

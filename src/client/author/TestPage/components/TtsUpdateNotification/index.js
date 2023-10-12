5b479025a19f6bbea3865cab85e1083df8679200
shreyans-s <shreyans.s@goguardian.com>
2023-10-12 19:03:56 +0530

    feat(client): Text to speech notification changes [EV-40396]

--- src/client/author/TestPage/components/TtsUpdateNotification/index.js
+++ src/client/author/TestPage/components/TtsUpdateNotification/index.js
@@ -1,44 +1,17 @@
-import { useState, useEffect } from 'react'
-import { withRouter } from 'react-router-dom'
-import { uniqBy } from 'lodash'
-import qs from 'qs'
-import antdNotification from '@edulastic/common/src/components/Notification'
 import * as Fbs from '@edulastic/common/src/Firebase'
-import { roleuser } from '@edulastic/constants'
+import antdNotification from '@edulastic/common/src/components/Notification'
+import { uniqBy } from 'lodash'
+import { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 import { compose } from 'redux'
-import { getUser, getUserOrgId } from '../../../src/selectors/user'
-import { receiveAssignmentClassList } from '../../../src/actions/assignments'
 import { destroyNotificationMessage } from '../../../../common/components/Notification'
-// import { setAssignmentBulkActionStatus } from '../../ducks'
-import { getFilterFromSession } from '../../../../common/utils/helpers'
+import { getUser } from '../../../src/selectors/user'
 
 const ttsUpdateNotificationCollection = 'TTSUpdateNotification'
 
-const NotificationListener = ({
-  user,
-  location,
-  fetchAssignmentClassList,
-  // setBulkActionStatus,
-  history,
-  orgId,
-}) => {
+const NotificationListener = ({ user, location, history }) => {
   const [notificationIds, setNotificationIds] = useState([])
-  let districtId = ''
-  let testId = ''
-  const { termId = '', grades = [], assignedBy = '' } = getFilterFromSession({
-    key: 'assignments_filter',
-    userId: user._id,
-    districtId: orgId,
-  })
-  const { testType = '' } = qs.parse(location.search, {
-    ignoreQueryPrefix: true,
-  })
-  if (testType) {
-    const locationArray = location?.pathname?.split('/') || []
-    districtId = locationArray[locationArray?.length - 2] || ''
-    testId = locationArray[locationArray?.length - 1] || ''
-  }
 
   const ttsUpdateNotification = Fbs.useFirestoreRealtimeDocuments(
     (db) =>
@@ -61,18 +34,17 @@ const NotificationListener = ({
     uniqBy(docs, '__id').forEach((doc) => {
       const {
         message,
-        statusCode,
         successCount, // how many test has been process successfully
         totalCount, // how many total test should be processed
         status,
       } = doc
 
-      if (status === 'initiated' && !notificationIds.includes(doc.__id)) {
+      if (status === 'done' && !notificationIds.includes(doc.__id)) {
         let _message = message
 
         // updating message based upon notification document for bulk update assignment settings
         if (collection === ttsUpdateNotificationCollection) {
-          if (successCount === 0) {
+          if (successCount !== totalCount) {
             _message = `TTS failed for ${totalCount} assignments. Please try again.`
           } else {
             _message = `TTS updated for ${successCount} out of ${totalCount} assignments.`
@@ -80,7 +52,7 @@ const NotificationListener = ({
         }
 
         setNotificationIds([...notificationIds, doc.__id])
-        if (statusCode === 200) {
+        if (successCount === totalCount) {
           antdNotification({ type: 'success', msg: _message, key: doc.__id })
         } else {
           antdNotification({ msg: _message, key: doc.__id })
@@ -88,37 +60,21 @@ const NotificationListener = ({
 
         // if status is initiated and we are displaying, delete the notification document from firebase
         deleteNotificationDocument(doc.__id, collection)
-        if (districtId && testId && testType) {
-          fetchAssignmentClassList({
-            districtId,
-            testId,
-            testType,
-            termId,
-            pageNo: 1,
-            status: '',
-            grades,
-            assignedBy,
-          })
-        }
 
         // if user at assignments home page and bulk action has been processed successfully
-        const isAssignmentsHomePage =
-          !districtId &&
-          !testId &&
-          !testType &&
-          location?.pathname?.includes('author/assignments') &&
-          statusCode == 200
+        const isAssignmentsHomePage = location?.pathname?.includes(
+          'author/assignments'
+        )
 
         if (isAssignmentsHomePage) {
           setTimeout(() => history.push('author/assignments'), 3000)
         }
-        // setBulkActionStatus(false)
       }
     })
   }
 
   useEffect(() => {
-    if (user && roleuser.DA_SA_ROLE_ARRAY.includes(user.role)) {
+    if (user) {
       showUserNotifications(
         ttsUpdateNotification,
         ttsUpdateNotificationCollection
@@ -138,14 +94,7 @@ const NotificationListener = ({
 
 export default compose(
   withRouter,
-  connect(
-    (state) => ({
-      user: getUser(state),
-      orgId: getUserOrgId(state),
-    }),
-    {
-      fetchAssignmentClassList: receiveAssignmentClassList,
-      // setBulkActionStatus: setAssignmentBulkActionStatus,
-    }
-  )
+  connect((state) => ({
+    user: getUser(state),
+  }))
 )(NotificationListener)

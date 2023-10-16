af7eb9a3b6e1c1f4b654b1430209d16972605f1d
Subhojeet Saha <124559179+Subho2403@users.noreply.github.com>
2023-10-16 11:57:50 +0530

    fix(client): Added new route for test preview for magnifier iframe [EV-39661] [EV-40502] (#20074)

--- src/client/App.js
+++ src/client/App.js
@@ -81,6 +81,7 @@ import CreateClassModal from './author/ManageClass/components/ClassCreate'
 import CreateAssignmentModal from './author/AssignmentCreate'
 import BulkAssignNotification from './author/AssignTest/components/AdvancedOptons/BulkAssignNotification'
 import { SectionsStartPage } from './student/SectionsStart'
+import TestPreview from './author/Assignments/components/Container/TestPreview'
 
 const {
   ASSESSMENT,
@@ -901,6 +902,10 @@ class App extends Component {
                 <Route
                   path="/public/test/:id"
                   render={() => <TestDemoPlayer />}
+                />
+                <Route
+                  path="/public/test-preview/:testId"
+                  render={() => <TestPreview />}
                 />
                 <Route
                   path="/v1/testItem/:id"

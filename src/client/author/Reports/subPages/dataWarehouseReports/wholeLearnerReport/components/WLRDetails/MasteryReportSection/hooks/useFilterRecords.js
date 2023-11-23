00ae72dc5cf10a22e8ef901afdd27e3f7d6d72bb
Saraansh Baranwal <saraansh@users.noreply.github.com>
2023-11-23 14:35:26 +0530

    fix(client): whole learner report - fix filtering of table data using domain filters [EV-41053] (#20270)

--- src/client/author/Reports/subPages/dataWarehouseReports/wholeLearnerReport/components/WLRDetails/MasteryReportSection/hooks/useFilterRecords.js
+++ src/client/author/Reports/subPages/dataWarehouseReports/wholeLearnerReport/components/WLRDetails/MasteryReportSection/hooks/useFilterRecords.js
@@ -7,21 +7,31 @@ const useFilterRecords = (
   subject,
   curriculumId,
   checkedDomains
-) =>
-  // Note: record.domainId could be integer or string
-  useMemo(
-    () =>
-      records?.filter(
-        (record) =>
-          domain === 'All' ||
+) => {
+  /**
+   * NOTE: domainId, curriculumId is integer for standard records and string for domain records
+   */
+  const filteredRecords = useMemo(() => {
+    const _filteredRecords = records?.filter((record) => {
+      const flags = [
+        // flag to filter records by grade
+        grade === 'All' || record.grades.includes(grade),
+        // flag to filter records by subject
+        subject === 'All' || record.subject === subject,
+        // flag to filter records by standard set (curriculum)
+        curriculumId === 'All' ||
+          String(record.curriculumId) === String(curriculumId),
+        // flag to filter by union of selected and checked domains
+        domain === 'All' ||
           String(record.domainId) === String(domain) ||
-          (checkedDomains.includes(String(record.domainId)) &&
-            (grade === 'All' || record.grades.includes(grade)) &&
-            (subject === 'All' || record.subject === subject) &&
-            (curriculumId === 'All' ||
-              `${record.curriculumId}` === curriculumId))
-      ),
-    [records, domain, checkedDomains, grade, subject, curriculumId]
-  )
+          checkedDomains.includes(String(record.domainId)),
+      ]
+      return flags.every((f) => f)
+    })
+    return _filteredRecords
+  }, [records, domain, checkedDomains, grade, subject, curriculumId])
+
+  return filteredRecords
+}
 
 export default useFilterRecords

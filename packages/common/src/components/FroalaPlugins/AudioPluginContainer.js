a7a322a3e14e2e8b0b6abf27de410f529d107d34
Subhojeet Saha <124559179+Subho2403@users.noreply.github.com>
2023-11-27 11:56:30 +0530

    fix(client): Addition of Audio not supported text [EV-41084] (#20300)

--- packages/common/src/components/FroalaPlugins/AudioPluginContainer.js
+++ packages/common/src/components/FroalaPlugins/AudioPluginContainer.js
@@ -80,7 +80,7 @@ const AudioPopup = ({ EditorRef }) => {
     // EditorRef.current.selection.clear()
 
     EditorRef.current.html.insert(
-      `<audio contenteditable="false" controls="controls" src="${url}" controlsList="nodownload"></audio>`
+      `<audio contenteditable="false" controls="controls" src="${url}" controlsList="nodownload">Audio is not supported on this browser.</audio>`
     )
     // if html is inserted over using editor methods `saveStep` requires to be called to update the editor.
     EditorRef.current.undo.saveStep()

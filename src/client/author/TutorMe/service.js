d6c6450ed9b2e10a569d68c770ae936512766674
Avinash Thakur <97439243+80avin-sw@users.noreply.github.com>
2024-01-11 17:13:49 +0530

    fix(client): update tutorme-sdk version [EV-41394] (#20452) (#20480)
    * fix(client): update tutorme-sdk version [EV-41394]
    
    * fix(client): remove patches to complete tutorme SDK upgrade [EV-41394]

--- src/client/author/TutorMe/service.js
+++ src/client/author/TutorMe/service.js
@@ -1,7 +1,3 @@
-/**
- * NOTE: import order is relevant and should not be changed
- */
-import './patchTutormeReact18'
 import tutorMeService from '@goguardian/tutorme-sdk'
 import '@goguardian/tutorme-sdk/dist/style.css'
 
@@ -32,95 +28,68 @@ export async function initTutorMeService(user) {
 }
 
 /**
- * @typedef {Parameters<(typeof tutorme)['requestSession']>[0]} ISessionRequest
+ * @typedef {Parameters<(typeof tutorMeService)['requestSession']>[0]} ISessionRequest
+ * @typedef {Parameters<ISessionRequest['callback']>[0]} ISessionResult
  * @typedef {{
- *  grade: string;
- *  subject: string;
- *  subjectArea: string;
- *  tutoringLink?: string;
- *  notes?: string;
- *  standards: Required<ISessionRequest>['data']['standards'];
- *  cancelled: boolean;
- *  tutoringId?: string;
- *  studentTutorMeId?: string;
+ *  cancelled: false;
  *  step: number;
- * }} ISDKState
+ *  subjectArea: string;
+ *  subject: string;
+ *  notes: string;
+ *  grade: number;
+ *  tutoringId: string;
+ *  studentTutorMeId?: string;
+ *  tutoringLink: string;
+ *  standards: Required<ISessionResult>['data']['standards'];
+ * } | {
+ *  cancelled: true;
+ *  step: number;
+ * }} ISDKResponse
  */
 
 /**
  * @param {ISessionRequest} request
  * @param {Parameters<typeof init>[0]} user
- * @returns {Promise<ISDKState>} Success is indicated by the `.cancelled === false` property.
+ * @returns {Promise<ISDKResponse>} Success is indicated by the `.cancelled === false` property.
  */
 export async function createSessionRequest(request) {
-  return new Promise((resolve) => {
-    /** @type {ISDKState} */
-    const state = {
-      cancelled: true,
-      grade: request.data.grade,
-      subject: request.data.subject,
-      subjectArea: request.data.category,
+  return new Promise((resolve, reject) => {
+    try {
+      /** @type {ISDKResponse} */
+      tutorMeService.requestSession({
+        ...request,
+        callback: (response) => {
+          const { data = {}, cancelled, step } = response
+          if (cancelled) {
+            resolve({ cancelled, step })
+            return
+          }
+          const {
+            category: subjectArea,
+            subject,
+            description: notes,
+            grade,
+            tutoringId,
+            studentTutormeId: studentTutorMeId,
+            tutoringUrl: tutoringLink,
+            standards,
+          } = data
+          resolve({
+            subjectArea,
+            subject,
+            notes,
+            grade,
+            tutoringId,
+            studentTutorMeId: studentTutorMeId ?? String(studentTutorMeId),
+            tutoringLink,
+            standards,
+            cancelled,
+            step,
+          })
+        },
+      })
+    } catch (error) {
+      reject(error)
     }
-
-    tutorMeService.requestSession({
-      analyticsCallback: (event) => {
-        // FIXME use event.properties or result after the SDK is updated.
-        switch (event.event) {
-          case 'assign_tutoring_copy_link_click':
-          case 'assign_tutoring_request_flow_exit':
-            state.tutoringLink = [
-              ...document.querySelectorAll('.buttonLabel'),
-            ].find((d) => d.innerText.includes('https://'))?.innerText
-            break
-          case 'assign_tutoring_grade_level_select':
-            state.grade = event.properties.grade
-            break
-          case 'assign_tutoring_step3of4_submitted':
-            state.notes = document.querySelector(
-              'textarea[name="description"]'
-            )?.value
-            break
-          case 'assign_tutoring_step1of4_submitted':
-            state.grade = +document.querySelector('input[name=grade]')?.value
-            state.subject = document.querySelector(
-              'input[name=subject]'
-            )?.nextElementSibling?.lastChild?.innerText
-            state.subjectArea = document.querySelector(
-              'input[name=category]'
-            )?.nextElementSibling?.lastChild?.innerText
-            break
-          default:
-            break
-        }
-      },
-      ...request,
-      callback: (response) => {
-        const { data = {}, cancelled, step } = response
-        const {
-          // FIXME: wrong category fetched in response (got Early Math in place of Math)
-          category: subjectArea,
-          subject,
-          description: notes,
-          grade,
-          tutoringId,
-          studentTutormeId: studentTutorMeId,
-          tutoringUrl: tutoringLink,
-          standards,
-        } = data
-        resolve({
-          ...state,
-          subjectArea,
-          subject,
-          notes,
-          grade,
-          tutoringId,
-          studentTutorMeId: String(studentTutorMeId),
-          tutoringLink,
-          standards,
-          cancelled,
-          step,
-        })
-      },
-    })
   })
 }

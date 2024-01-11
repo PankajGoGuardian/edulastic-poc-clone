5ca7dc6dcc3850e8c774c3ba16f3ea2bc65a64b5
shreyans-s <129031826+shreyans-s@users.noreply.github.com>
2024-01-11 19:32:40 +0530

    fix(poc): Password was not asking for 2nd section test onwards [EV-41437] (#20482)

--- src/client/student/SectionsStart/ducks.js
+++ src/client/student/SectionsStart/ducks.js
@@ -16,6 +16,7 @@ const slice = createSlice({
   },
   reducers: {
     fetchSectionsData: (state) => {
+      state.isSectionsTestPasswordValidated = false
       state.isLoading = true
     },
     fetchSectionsDataSuccess: (state, { payload }) => {

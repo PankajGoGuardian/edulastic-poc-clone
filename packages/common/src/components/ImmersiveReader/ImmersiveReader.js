f87382fa69c3304bfd6764318f567bf3d3b2a942
shreyans-s <129031826+shreyans-s@users.noreply.github.com>
2024-01-18 17:11:03 +0530

    fix(client): Immersive reader updates for MathML/Latex content [EV-41654] (#20450)
    * fix(client): IR mathml POC
    
    * feat(poc): IR Reader mathML parsing
    
    * fix(poc): IR reading implemented fallback mechanism
    
    * fix(poc): IR reading implemented fallback mechanism
    
    * fix(poc): IR reading implemented fallback mechanism
    
    * fix(poc): IR reading implemented fallback mechanism
    
    * fix(poc): Breaking line after list and br tag as well

--- packages/common/src/components/ImmersiveReader/ImmersiveReader.js
+++ packages/common/src/components/ImmersiveReader/ImmersiveReader.js
@@ -10,33 +10,98 @@ import { compose } from 'redux'
 import { withNamespaces } from '@edulastic/localization'
 
 const { IR_CONTENT_SELECTOR, IR_MCQ_LABEL_SELECTOR } = question
+const NEW_LINE_SEPARATOR = '$$$___NEW_LINE___$$$'
+const INPUT_MATH_SEPARATOR = '$$$___INPUT_MATH___$$$'
 
 const ImmersiveReader = ({ t: i18translate, title, ImmersiveReaderButton }) => {
   const getImmersiveReaderArgs = async () => {
     try {
       const { token, subdomain } = await fetchIRtokenAndSubDomain()
 
-      const chunks = []
+      let chunks = []
       // eslint-disable-next-line func-names
       $(`.${IR_CONTENT_SELECTOR}`).each(function () {
         /** cloning jquery object to prevent modification in assessment player content, here we are manipulating html content. */
-        const cloned = $(this).clone()
+        const itemContent = $(this).clone()
 
-        // Remove span.katex-mathml (non visible math latex) elements from the cloned HTML
-        $(cloned).find('span.katex-mathml').remove()
+        // Cloning again to keep a reference for fallback in case of an exception.
+        const clonedItemContent = $(this).clone()
 
-        $(cloned)
+        $(itemContent)
           .find(`.${IR_MCQ_LABEL_SELECTOR}`)
           // eslint-disable-next-line func-names
           .each(function () {
             const labelText = $(this).text()
             $(this).text(`${labelText} `)
           })
-        const content = $(cloned).html()
-        chunks.push({
-          content,
-          mimeType: 'text/html',
-        })
+
+        const questionMaths = $(itemContent).find('.input__math')
+        if (questionMaths.length > 0) {
+          try {
+            // add new line for each div and p tags
+            $(itemContent).find('div, p, ul, li, br').after(NEW_LINE_SEPARATOR)
+
+            // remove math input with custom match string
+            $(itemContent)
+              .find('.input__math')
+              .replaceWith(INPUT_MATH_SEPARATOR)
+
+            // get all non math content
+            const nonMathContents = $(itemContent)
+              .text()
+              .split(INPUT_MATH_SEPARATOR)
+
+            // prepare chunks for IR
+            nonMathContents.forEach((text, index) => {
+              // joining all div and p with empty div for new line
+              const content = `<span>${text
+                .split(NEW_LINE_SEPARATOR)
+                .join('<div></div>')}</span>`
+
+              chunks.push({
+                content,
+                mimeType: 'text/html',
+              })
+
+              const mathContent = questionMaths?.[index]
+              if (mathContent) {
+                // reading mathML content
+                const mathMLContent = $(mathContent).find('.katex-mathml')
+                mathMLContent.find('semantics.edu').contents().unwrap()
+                mathMLContent.find('annotation').remove()
+
+                chunks.push({
+                  content: mathMLContent.html(),
+                  mimeType: 'application/mathml+xml',
+                })
+              }
+            })
+          } catch (err) {
+            captureSentryException(err, {
+              errorMessage: 'ImmersiveReader - unable to parse math content',
+            })
+
+            /**
+             * falling back to current implementation by
+             * Removing span.katex-mathml (non visible math latex) elements from the cloned HTML
+             *  */
+
+            $(clonedItemContent).find('span.katex-mathml').remove()
+
+            chunks = []
+            const content = $(clonedItemContent).html()
+            chunks.push({
+              content,
+              mimeType: 'text/html',
+            })
+          }
+        } else {
+          const content = $(itemContent).html()
+          chunks.push({
+            content,
+            mimeType: 'text/html',
+          })
+        }
       })
 
       const content = {

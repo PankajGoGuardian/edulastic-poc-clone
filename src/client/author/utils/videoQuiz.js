ccaed3225fd0cbd56341319316b11fb4d7cea213
Arpit Chandrakar <57706980+arpit-snapwiz@users.noreply.github.com>
2024-01-31 10:57:39 +0530

    fix(client): missing return statement getIsBuyAiSuiteAlertModalVisible utils [EV-41894] (#20609)

--- src/client/author/utils/videoQuiz.js
+++ src/client/author/utils/videoQuiz.js
@@ -3,8 +3,7 @@ import { testCategoryTypes } from '@edulastic/constants/const/test'
 export const getIsBuyAiSuiteAlertModalVisible = (
   testCategory = '',
   isRedirectToAddOn = false
-) => {
+) =>
   testCategory &&
-    testCategory === testCategoryTypes.VIDEO_BASED &&
-    isRedirectToAddOn
-}
+  testCategory === testCategoryTypes.VIDEO_BASED &&
+  isRedirectToAddOn

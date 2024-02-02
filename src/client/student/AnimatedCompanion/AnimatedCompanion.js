d561fc1b9c148cd079c2712dc5772509d8ac7baf
Arpit Chandrakar <arpit.chandrakar@goguardian.com>
2024-02-02 18:03:58 +0530

    fix: ui with popopver

--- src/client/student/AnimatedCompanion/AnimatedCompanion.js
+++ src/client/student/AnimatedCompanion/AnimatedCompanion.js
@@ -2,20 +2,23 @@ import React from 'react'
 import IconPearAssessLogoCompact from '@edulastic/icons/src/IconPearAssessLogoCompact'
 import styled from 'styled-components'
 import { Rnd } from 'react-rnd'
+import { Popover } from 'antd'
 
 const AnimatedCompanion = () => {
   return (
-    <RndWrapper
-      defaultPosition={{
-        x: 90,
-        y: 100,
-        width: 100,
-        height: 100,
-        bounds: 'window',
-      }}
-    >
-      <IconPearAssessLogoCompact width="60" height="60" />
-    </RndWrapper>
+    <Popover content={<h1>Hi!</h1>}>
+      <RndWrapper
+        defaultPosition={{
+          x: 90,
+          y: 100,
+          width: 100,
+          height: 100,
+          bounds: 'window',
+        }}
+      >
+        <IconPearAssessLogoCompact width="60" height="60" />
+      </RndWrapper>
+    </Popover>
   )
 }
 

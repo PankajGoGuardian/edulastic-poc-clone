56bb86f1d0626a797a1a47e2ce90054964b1caa6
Pankaj <pankaj.kumar@goguardian.com>
2024-02-02 18:13:28 +0530

    popover

--- src/client/student/AnimatedCompanion/AnimatedCompanion.js
+++ src/client/student/AnimatedCompanion/AnimatedCompanion.js
@@ -2,23 +2,47 @@ import React from 'react'
 import IconPearAssessLogoCompact from '@edulastic/icons/src/IconPearAssessLogoCompact'
 import styled from 'styled-components'
 import { Rnd } from 'react-rnd'
-import { Popover } from 'antd'
+import { Divider, Popover } from 'antd'
+
+const items = [
+  {
+    key: 'hints',
+    label: 'Hints',
+    onClick: (e) => console.log(`${e.key} is pressed`),
+  },
+  {
+    key: 'resources',
+    label: 'Resources',
+    onClick: (e) => console.log(`${e.key} is pressed`),
+  },
+]
+
+const content = (
+  <div>
+    {items.map((item, index) => (
+      <div key={item.key} onClick={item.onClick}>
+        {item.label}
+        {index != items.length - 1 && <Divider />}
+      </div>
+    ))}
+  </div>
+)
 
 const AnimatedCompanion = () => {
   return (
-    <Popover content={<h1>Hi!</h1>}>
-      <RndWrapper
-        defaultPosition={{
-          x: 90,
-          y: 100,
-          width: 100,
-          height: 100,
-          bounds: 'window',
-        }}
-      >
+    <RndWrapper
+      defaultPosition={{
+        x: 90,
+        y: 100,
+        width: 100,
+        height: 100,
+        bounds: 'window',
+      }}
+    >
+      <Popover content={content}>
         <IconPearAssessLogoCompact width="60" height="60" />
-      </RndWrapper>
-    </Popover>
+      </Popover>
+    </RndWrapper>
   )
 }
 

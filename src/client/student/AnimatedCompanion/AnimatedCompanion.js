f67ce1714bcf0be52fcfe5285a4d480f33d2c74d
Arpit Chandrakar <arpit.chandrakar@goguardian.com>
2024-02-02 17:57:59 +0530

    fix(ui): draggable

--- src/client/student/AnimatedCompanion/AnimatedCompanion.js
+++ src/client/student/AnimatedCompanion/AnimatedCompanion.js
@@ -1,46 +1,26 @@
 import React from 'react'
 import IconPearAssessLogoCompact from '@edulastic/icons/src/IconPearAssessLogoCompact'
 import styled from 'styled-components'
-import { Menu } from 'antd'
-import Dragger from 'antd/lib/upload/Dragger'
+import { Rnd } from 'react-rnd'
 
 const AnimatedCompanion = () => {
-  const items = [
-    {
-      key: 'hints',
-      label: 'Hints',
-      onClick: () => console.log('hint is clicked'),
-    },
-    {
-      key: 'resources',
-      label: 'Resoures',
-      onClick: () => console.log('resources is clicked'),
-    },
-  ]
   return (
-    <AnimatedCompanionWrapper>
-      <Menu>
-        <Menu.SubMenu
-          title={<IconPearAssessLogoCompact width="60px" height="40px" />}
-        >
-          {items.map((item) => (
-            <Menu.ItemGroup>
-              <Menu.Item key={item.key} onClick={item.onClick}>
-                {item.label}
-              </Menu.Item>
-            </Menu.ItemGroup>
-          ))}
-        </Menu.SubMenu>
-      </Menu>
-    </AnimatedCompanionWrapper>
+    <RndWrapper
+      defaultPosition={{
+        x: 90,
+        y: 100,
+        width: 100,
+        height: 100,
+        bounds: 'window',
+      }}
+    >
+      <IconPearAssessLogoCompact width="60" height="60" />
+    </RndWrapper>
   )
 }
 
 export default AnimatedCompanion
 
-const AnimatedCompanionWrapper = styled.div`
-  position: absolute;
-  top: 80%;
-  right: 5%;
-  z-index: 99999;
+const RndWrapper = styled(Rnd)`
+  z-index: 999;
 `

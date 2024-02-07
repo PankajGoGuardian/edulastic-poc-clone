6a49017758a99d1819e7b376145ec4334749ccf5
Ravi Ranjan Kumar <ravi.kumar@goguardian.com>
2024-02-07 20:39:30 +0530

    feat(client): added folder structure for completion report [EV-42052]

---
+++ src/client/author/Reports/subPages/multipleAssessmentReport/CompletionReport/components/table/TableHeader.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { TableHeaderContainer } from './styled'
+
+const TableHeader = () => {
+  return (
+    <TableHeaderContainer>
+      {/* Left Container */}
+      <div>{/* Table title */}</div>
+
+      {/* Right Container */}
+      <div>{/* Analyze filter */}</div>
+    </TableHeaderContainer>
+  )
+}
+
+export default TableHeader
---
+++ src/client/author/Reports/subPages/multipleAssessmentReport/CompletionReport/components/table/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TableHeader from './TableHeader'
+
+const Table = () => {
+  return (
+    <div>
+      <TableHeader />
+      {/* Table component */}
+    </div>
+  )
+}
+
+export default Table

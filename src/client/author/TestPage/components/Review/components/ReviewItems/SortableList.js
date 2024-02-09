8dbfe11fb70332db526e6f6d50fb3a18696bc386
arun-sasidharan <arun.sasidharan@goguardian.com>
2024-02-09 12:48:20 +0530

    fix(client): enable total points only for dynamic test [EV-41907]

--- src/client/author/TestPage/components/Review/components/ReviewItems/SortableList.js
+++ src/client/author/TestPage/components/Review/components/ReviewItems/SortableList.js
@@ -2,6 +2,8 @@ import { Collapse, Tooltip } from 'antd'
 import { IconSectionsCalculator } from '@edulastic/icons'
 import { isArray } from 'lodash'
 import React, { useState } from 'react'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
 import { SortableContainer } from 'react-sortable-hoc'
 import { darkGrey5 } from '@edulastic/colors'
 import { EduIf } from '@edulastic/common'
@@ -9,13 +11,15 @@ import { ITEM_GROUP_DELIVERY_TYPES } from '@edulastic/constants/const/test'
 import { SortableGroupItem, SortableSingleItem } from './SortableItem'
 import { InfoDiv, Text, Count, GroupCollapse } from './styled'
 import PassageConfirmationModal from '../../../PassageConfirmationModal/PassageConfirmationModal'
+import { isDynamicTestSelector } from '../../../../ducks'
 
 const { Panel } = Collapse
 
 const rightContent = (
   group,
   hasSections = false,
-  setShowAutoSelectScoreChangeModal
+  setShowAutoSelectScoreChangeModal,
+  isDynamicTest
 ) => {
   const {
     deliverItemsCount,
@@ -48,7 +52,10 @@ const rightContent = (
         </InfoDiv>
       )}
       <EduIf
-        condition={deliveryType === ITEM_GROUP_DELIVERY_TYPES.LIMITED_RANDOM}
+        condition={
+          isDynamicTest &&
+          deliveryType === ITEM_GROUP_DELIVERY_TYPES.LIMITED_RANDOM
+        }
       >
         <span
           onClick={(e) => {
@@ -66,138 +73,147 @@ const rightContent = (
   )
 }
 
-export default SortableContainer(
-  ({
-    items,
-    isEditable,
-    itemGroups,
-    isPublishers,
-    userRole,
-    isPowerPremiumAccount,
-    showGroupsPanel,
-    onSortGroup,
-    removeSingle,
-    removeMultiple,
-    hasSections,
-    setShowAutoSelectScoreChangeModal,
-    ...rest
-  }) => {
-    const [removalObj, setRemovalPassageItems] = useState()
-    const handleDelete = (item) => (removalId) => {
-      if (isArray(item)) {
-        setRemovalPassageItems({ items: item.map((x) => x._id), removalId })
-      } else {
-        removeSingle(removalId)
-      }
+const ReviewSection = ({
+  items,
+  isEditable,
+  itemGroups,
+  isPublishers,
+  userRole,
+  isPowerPremiumAccount,
+  showGroupsPanel,
+  onSortGroup,
+  removeSingle,
+  removeMultiple,
+  hasSections,
+  setShowAutoSelectScoreChangeModal,
+  isDynamicTest,
+  ...rest
+}) => {
+  const [removalObj, setRemovalPassageItems] = useState()
+  const handleDelete = (item) => (removalId) => {
+    if (isArray(item)) {
+      setRemovalPassageItems({ items: item.map((x) => x._id), removalId })
+    } else {
+      removeSingle(removalId)
     }
+  }
 
-    const handlePassageItemsConfirm = (value) => {
-      if (value) {
-        removeMultiple(removalObj.items)
-      } else {
-        removeSingle(removalObj.removalId)
-      }
-      setRemovalPassageItems(null)
+  const handlePassageItemsConfirm = (value) => {
+    if (value) {
+      removeMultiple(removalObj.items)
+    } else {
+      removeSingle(removalObj.removalId)
     }
+    setRemovalPassageItems(null)
+  }
 
-    const handleClosePassageConfirm = () => {
-      setRemovalPassageItems(null)
-    }
+  const handleClosePassageConfirm = () => {
+    setRemovalPassageItems(null)
+  }
 
-    const renderItem = (item, index, groupId) => {
-      if (isArray(item)) {
-        // when use index or item._id for a key, the SortableGroupItem was unmounted
-        // so there was a page blinking bug, when drag and drop
-        // for now, will use passageId for a key
-        return (
-          <SortableGroupItem
-            {...rest}
-            key={item[0].passageId}
-            disabled={!isEditable}
-            isEditable={isEditable}
-            items={item}
-            index={index}
-            collection={groupId}
-            groupId={groupId}
-            showGroupsPanel={showGroupsPanel}
-            removeItem={handleDelete(item)}
-            onSortEnd={onSortGroup(index)}
-            lockToContainerEdges
-            lockOffset={['10%', '10%']}
-          />
-        )
-      }
+  const renderItem = (item, index, groupId) => {
+    if (isArray(item)) {
+      // when use index or item._id for a key, the SortableGroupItem was unmounted
+      // so there was a page blinking bug, when drag and drop
+      // for now, will use passageId for a key
       return (
-        <SortableSingleItem
+        <SortableGroupItem
           {...rest}
-          key={item._id}
-          removeItem={handleDelete(item)}
+          key={item[0].passageId}
           disabled={!isEditable}
           isEditable={isEditable}
+          items={item}
           index={index}
           collection={groupId}
-          item={item}
-          isPublishers={isPublishers}
+          groupId={groupId}
+          showGroupsPanel={showGroupsPanel}
+          removeItem={handleDelete(item)}
+          onSortEnd={onSortGroup(index)}
+          lockToContainerEdges
+          lockOffset={['10%', '10%']}
         />
       )
     }
-
-    const groupIndexWithoutRestrictedContent = []
-    for (const [groupIndex, group] of Object.entries(itemGroups)) {
-      if (!group.premiumContentRestriction) {
-        groupIndexWithoutRestrictedContent.push(groupIndex)
-        break
-      }
-    }
     return (
-      <>
-        {!!removalObj && (
-          <PassageConfirmationModal
-            removing
-            visible={!!removalObj}
-            closeModal={handleClosePassageConfirm}
-            itemsCount={removalObj.items.length}
-            handleResponse={handlePassageItemsConfirm}
-          />
-        )}
-        {showGroupsPanel ? (
-          <GroupCollapse
-            defaultActiveKey={groupIndexWithoutRestrictedContent}
-            expandIconPosition="right"
-          >
-            {itemGroups.map((group, count) => (
-              <Panel
-                header={<span dataCy={group.groupName}>{group.groupName}</span>}
-                key={count}
-                extra={rightContent(
-                  group,
-                  hasSections,
-                  setShowAutoSelectScoreChangeModal
-                )}
-              >
-                {items.map((item, index) => {
-                  // EV-38941: here item can be an object or array of objects
-                  if (isArray(item)) {
-                    const currentGroupItems = (item || []).filter(
-                      (_item) => _item.groupId === group._id
-                    )
-                    if (currentGroupItems?.length) {
-                      return renderItem(currentGroupItems, index, group._id)
-                    }
-                    return null
-                  }
-                  if (item.groupId == group._id) {
-                    return renderItem(item, index, group._id)
-                  }
-                  return null
-                })}
-              </Panel>
-            ))}
-          </GroupCollapse>
-        ) : (
-          items.map((item, index) => renderItem(item, index))
-        )}
-      </>
+      <SortableSingleItem
+        {...rest}
+        key={item._id}
+        removeItem={handleDelete(item)}
+        disabled={!isEditable}
+        isEditable={isEditable}
+        index={index}
+        collection={groupId}
+        item={item}
+        isPublishers={isPublishers}
+      />
     )
   }
+
+  const groupIndexWithoutRestrictedContent = []
+  for (const [groupIndex, group] of Object.entries(itemGroups)) {
+    if (!group.premiumContentRestriction) {
+      groupIndexWithoutRestrictedContent.push(groupIndex)
+      break
+    }
+  }
+  return (
+    <>
+      {!!removalObj && (
+        <PassageConfirmationModal
+          removing
+          visible={!!removalObj}
+          closeModal={handleClosePassageConfirm}
+          itemsCount={removalObj.items.length}
+          handleResponse={handlePassageItemsConfirm}
+        />
+      )}
+      {showGroupsPanel ? (
+        <GroupCollapse
+          defaultActiveKey={groupIndexWithoutRestrictedContent}
+          expandIconPosition="right"
+        >
+          {itemGroups.map((group, count) => (
+            <Panel
+              header={<span dataCy={group.groupName}>{group.groupName}</span>}
+              key={count}
+              extra={rightContent(
+                group,
+                hasSections,
+                setShowAutoSelectScoreChangeModal,
+                isDynamicTest
+              )}
+            >
+              {items.map((item, index) => {
+                // EV-38941: here item can be an object or array of objects
+                if (isArray(item)) {
+                  const currentGroupItems = (item || []).filter(
+                    (_item) => _item.groupId === group._id
+                  )
+                  if (currentGroupItems?.length) {
+                    return renderItem(currentGroupItems, index, group._id)
+                  }
+                  return null
+                }
+                if (item.groupId == group._id) {
+                  return renderItem(item, index, group._id)
+                }
+                return null
+              })}
+            </Panel>
+          ))}
+        </GroupCollapse>
+      ) : (
+        items.map((item, index) => renderItem(item, index))
+      )}
+    </>
+  )
+}
+
+const enhance = compose(
+  SortableContainer,
+  connect((state) => ({
+    isDynamicTest: isDynamicTestSelector(state),
+  }))
 )
+
+export default enhance(ReviewSection)

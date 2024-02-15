26084bffbf141621ba40016f03f7061d6a5960c3
Alok68 <100820949+Alok68@users.noreply.github.com>
2024-02-15 10:24:20 +0530

    test(poc):Add data-cy attribute for DS report Use Case [EV-42153] (#20687)
    Co-authored-by: Alok Vats <alokvats@Aloks-MacBook-Pro.local>

--- src/client/author/Reports/components/dataWarehouseReport/importHistory/components/Modal/ConfirmationPopup.js
+++ src/client/author/Reports/components/dataWarehouseReport/importHistory/components/Modal/ConfirmationPopup.js
@@ -27,7 +27,9 @@ const ConfirmationPopup = ({ handleDeleteFile, closeModal, isVisible }) => {
         <EduButton onClick={() => closeModal()} isGhost>
           Cancel
         </EduButton>
-        <EduButton onClick={() => handleDeleteFile()}>Delete</EduButton>
+        <EduButton data-cy="deleteButton" onClick={() => handleDeleteFile()}>
+          Delete
+        </EduButton>
       </FlexContainer>
     </CustomModalStyled>
   )

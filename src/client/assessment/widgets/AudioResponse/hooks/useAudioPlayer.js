cdca42e03cf045b43029e2d90ae853b774cd0ba8
Aniket Hatti <75801936+aniket-snapwiz@users.noreply.github.com>
2024-02-29 11:25:08 +0530

    fix(client): check for loaded audio before updating duration [EV-42313] (#20824)
    Co-authored-by: aniket-snapwiz <aniket.hatti@goguardian.com>

--- src/client/assessment/widgets/AudioResponse/hooks/useAudioPlayer.js
+++ src/client/assessment/widgets/AudioResponse/hooks/useAudioPlayer.js
@@ -14,26 +14,28 @@ const useAudioPlayer = ({ audioUrl, onClickRerecord, setErrorData }) => {
       try {
         setIsAudioLoading(true)
         const loadedAudio = audioRef.current
-        /**
-         * Browser returns infinity duration.
-         * Majority browsers has this issue.
-         * Its a workaround to get correct duration of audio
-         */
-        while (loadedAudio.duration === Infinity) {
-          await new Promise((r) => setTimeout(r, 1000))
-          loadedAudio.currentTime = 10000000 * Math.random()
+        if (loadedAudio) {
+          /**
+           * Browser returns infinity duration.
+           * Majority browsers has this issue.
+           * Its a workaround to get correct duration of audio
+           */
+          while (loadedAudio.duration === Infinity) {
+            await new Promise((r) => setTimeout(r, 1000))
+            loadedAudio.currentTime = 10000000 * Math.random()
+          }
+          /**
+           * The audio element has issues of playing when audio loads,
+           * audio track seeks to end on load and plays partially.
+           * Below is the workaround so that audio starts from beginning
+           * and plays correctly. It is just like seeking the audio in between
+           * somewhere and then to start so that tracks and current time
+           * are set appropriately.
+           */
+          audioRef.current.currentTime = 1
+          audioRef.current.currentTime = 0
+          setIsAudioLoading(false)
         }
-        /**
-         * The audio element has issues of playing when audio loads,
-         * audio track seeks to end on load and plays partially.
-         * Below is the workaround so that audio starts from beginning
-         * and plays correctly. It is just like seeking the audio in between
-         * somewhere and then to start so that tracks and current time
-         * are set appropriately.
-         */
-        audioRef.current.currentTime = 1
-        audioRef.current.currentTime = 0
-        setIsAudioLoading(false)
       } catch (error) {
         const errorMessage = 'An error occured while loading the audio.'
         setErrorData({

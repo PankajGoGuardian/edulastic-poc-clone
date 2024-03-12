8465600aeefaab2ac8525b13c5e9041978536861
Arun Sasidharan <43816559+arun-sasidharan@users.noreply.github.com>
2024-03-12 16:26:21 +0530

    fix(client): do not clear test data when creating ai test [EV-42478] (#20910)
    Co-authored-by: arun-sasidharan <arun.sasidharan@goguardian.com>

--- src/client/author/AssessmentCreate/components/CreateAITest/hooks/useSaveForm.js
+++ src/client/author/AssessmentCreate/components/CreateAITest/hooks/useSaveForm.js
@@ -44,7 +44,7 @@ export const useSaveForm = ({
       setDefaultTest()
       clearCreatedItem()
     }
-  }, [addItems, isAiGeneratedTest])
+  }, [addItems])
 
   const [selectSectionVisible, setSelectSectionVisible] = useState(false)
   const [isVisible, setIsVisible] = useState(false)

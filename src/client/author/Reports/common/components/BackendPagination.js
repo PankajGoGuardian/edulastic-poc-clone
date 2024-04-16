fa9bb231157fb8a296318e1caa089ce850729da0
Saraansh Baranwal <saraansh@users.noreply.github.com>
2024-04-16 11:24:03 +0530

    feat(client): update `BackendPagination` to support multiple pages & unknown items count [EV-43183] (#21148)

--- src/client/author/Reports/common/components/BackendPagination.js
+++ src/client/author/Reports/common/components/BackendPagination.js
@@ -5,32 +5,50 @@ const BackendPagination = ({
   itemsCount,
   backendPagination,
   setBackendPagination,
-}) => (
-  <Pagination
-    style={{ margin: '10px' }}
-    current={backendPagination.page}
-    pageSize={backendPagination.pageSize}
-    onChange={(page) =>
-      setBackendPagination({
-        ...backendPagination,
-        page,
-      })
+  hasMultiplePages,
+}) => {
+  const currentItemsCount =
+    itemsCount ||
+    backendPagination.itemsCount ||
+    backendPagination.pageCount * backendPagination.pageSize
+
+  // show dots on page 2 when item count is unknown but report has multiple pages
+  const showDotsOnPageTwo = hasMultiplePages && !currentItemsCount
+
+  const defaultItemsCount = showDotsOnPageTwo
+    ? backendPagination.pageSize + 1 // items count to render for exactly two pagination boxes when totalCount is unknown
+    : 1 // items count to hide pagination when totalCount is 0
+
+  const itemRender = (current, type, originalElement) => {
+    if (type === 'page' && current === 2 && showDotsOnPageTwo) {
+      return <a>...</a>
     }
-    onShowSizeChange={(_, pageSize) =>
-      setBackendPagination({
-        ...backendPagination,
-        pageSize,
-      })
-    }
-    total={
-      itemsCount ||
-      backendPagination.itemsCount ||
-      backendPagination.pageCount * backendPagination.pageSize ||
-      1 // default count of items to hide pagination when count is 0
-    }
-    showSizeChanger={false}
-    hideOnSinglePage
-  />
-)
+    return originalElement
+  }
+
+  return (
+    <Pagination
+      style={{ margin: '15px 55px 15px 10px' }}
+      current={backendPagination.page}
+      pageSize={backendPagination.pageSize}
+      itemRender={itemRender}
+      onChange={(page) =>
+        setBackendPagination({
+          ...backendPagination,
+          page,
+        })
+      }
+      onShowSizeChange={(_, pageSize) =>
+        setBackendPagination({
+          ...backendPagination,
+          pageSize,
+        })
+      }
+      total={currentItemsCount || defaultItemsCount}
+      showSizeChanger={false}
+      hideOnSinglePage
+    />
+  )
+}
 
 export default BackendPagination

8728e7c4b4065a58e7188191f9e07f4767e7f43f
Aniket Hatti <75801936+aniket-snapwiz@users.noreply.github.com>
2024-04-19 13:13:55 +0530

    fix(client): set file title as docbased test title [EV-43266] (#21181)
    Co-authored-by: aniket-snapwiz <aniket.hatti@goguardian.com>

--- src/client/author/AssessmentCreate/ducks.js
+++ src/client/author/AssessmentCreate/ducks.js
@@ -151,7 +151,7 @@ const defaultPageStructure = [
 ]
 
 function* createAssessmentSaga({ payload }) {
-  let { fileURI = '' } = payload
+  let { fileURI = '', fileTitle = '' } = payload
   const { searchParam = '' } = payload
   let testItem
   let amountOfPDFPages = 0
@@ -159,6 +159,7 @@ function* createAssessmentSaga({ payload }) {
 
   try {
     if (payload.file) {
+      fileTitle = payload.file?.name || ''
       fileURI = yield call(
         uploadToS3,
         payload.file,
@@ -318,9 +319,13 @@ function* createAssessmentSaga({ payload }) {
         maxScore: testItem.maxScore,
         questions: {},
       }
+      // removing file extension
+      if (fileTitle) {
+        fileTitle = fileTitle.replace(/\.[^/.]+$/, '')
+      }
       const newAssessment = {
         ...initialTestState,
-        title: undefined,
+        title: fileTitle || undefined,
         createdBy: {
           id: user._id,
           name,
@@ -443,6 +448,7 @@ function* uploadToDriveSaga({ payload = {} }) {
     yield put(
       createAssessmentRequestAction({
         fileURI,
+        fileTitle: name,
         assessmentId,
         isAddPdf,
         merge,

b89365262f18233a05d882bfdaef7513aa7faa67
Jitesh Kumar <jitesh.kumar@snapwiz.com>
2024-04-24 13:39:13 +0530

    fix(client): review chnages and fixed invalid date [EV-43200]

--- src/client/author/Reports/subPages/multipleAssessmentReport/CompletionReport/components/chart/utils.js
+++ src/client/author/Reports/subPages/multipleAssessmentReport/CompletionReport/components/chart/utils.js
@@ -6,74 +6,77 @@ const calculateMinWidth = (value, total) => {
 }
 
 export const chartDataFormatter = (chartData = []) => {
-  // chart data is already sorted in required order
-  // reverse the order to show it in bar chart from right to left
-  const ascendingChartData = chartData.reverse()
-  return ascendingChartData.map((item) => {
-    const submittedWidth =
-      item.submitted === '0'
-        ? 0
-        : calculateMinWidth(item.submitted, item.assigned)
+  return chartData
+    .map((item) => {
+      const submittedWidth =
+        item.submitted === '0'
+          ? 0
+          : calculateMinWidth(item.submitted, item.assigned)
 
-    const inProgressWidth =
-      item.inProgress === '0'
-        ? 0
-        : calculateMinWidth(item.inProgress, item.assigned)
+      const inProgressWidth =
+        item.inProgress === '0'
+          ? 0
+          : calculateMinWidth(item.inProgress, item.assigned)
 
-    const notStartedWidth =
-      item.notStarted === '0'
-        ? 0
-        : calculateMinWidth(item.notStarted, item.assigned)
+      const notStartedWidth =
+        item.notStarted === '0'
+          ? 0
+          : calculateMinWidth(item.notStarted, item.assigned)
 
-    const absentWidth =
-      item.absent === '0' ? 0 : calculateMinWidth(item.absent, item.assigned)
+      const absentWidth =
+        item.absent === '0' ? 0 : calculateMinWidth(item.absent, item.assigned)
 
-    const gradedWidth =
-      item.graded === '0' ? 0 : calculateMinWidth(item.graded, item.assigned)
-    const notOpenWidth =
-      item.notOpen === '0' ? 0 : calculateMinWidth(item.notOpen, item.assigned)
+      const gradedWidth =
+        item.graded === '0' ? 0 : calculateMinWidth(item.graded, item.assigned)
+      const notOpenWidth =
+        item.notOpen === '0'
+          ? 0
+          : calculateMinWidth(item.notOpen, item.assigned)
 
-    const totalWidth =
-      gradedWidth +
-      absentWidth +
-      notStartedWidth +
-      inProgressWidth +
-      submittedWidth +
-      notOpenWidth
-
-    return {
-      testName: item.testName,
-      gradedPercentage: ((item.graded / item.assigned) * 100).toFixed(2),
-      submittedPercentage: ((item.submitted / item.assigned) * 100).toFixed(2),
-      inProgressPercentage: ((item.inProgress / item.assigned) * 100).toFixed(
-        2
-      ),
-      notStartedPercentage: ((item.notStarted / item.assigned) * 100).toFixed(
-        2
-      ),
-      notOpenPercentage: ((item.notOpen / item.assigned) * 100).toFixed(2),
-      absentPercentage: ((item.absent / item.assigned) * 100).toFixed(2),
-      gradedContribution: (gradedWidth / totalWidth) * 100,
-      inProgressContribution: (inProgressWidth / totalWidth) * 100,
-      notStartedContribution: (notStartedWidth / totalWidth) * 100,
-      submittedContribution: (submittedWidth / totalWidth) * 100,
-      absentContribution: (absentWidth / totalWidth) * 100,
-      notOpenContribution: (notOpenWidth / totalWidth) * 100,
-      graded: item.graded,
-      submitted: item.submitted,
-      inProgress: item.inProgress,
-      notStarted: item.notStarted,
-      notOpen: item.notOpen,
-      absent: item.absent,
-      totalCount: item.assigned,
-      testDate: +item.assessmentDate
-        ? formatDate(item.assessmentDate)
-        : 'Not Open',
-      testId: item.testId,
-      testType: item.testType,
-      totalTests: item.totalRows,
-    }
-  })
+      const totalWidth =
+        gradedWidth +
+        absentWidth +
+        notStartedWidth +
+        inProgressWidth +
+        submittedWidth +
+        notOpenWidth
+      const formattedDate = formatDate(item.assessmentDate)
+      const testDate =
+        formattedDate === 'Invalid date' ? 'Not Open' : formattedDate
+      return {
+        testName: item.testName,
+        gradedPercentage: ((item.graded / item.assigned) * 100).toFixed(2),
+        submittedPercentage: ((item.submitted / item.assigned) * 100).toFixed(
+          2
+        ),
+        inProgressPercentage: ((item.inProgress / item.assigned) * 100).toFixed(
+          2
+        ),
+        notStartedPercentage: ((item.notStarted / item.assigned) * 100).toFixed(
+          2
+        ),
+        notOpenPercentage: ((item.notOpen / item.assigned) * 100).toFixed(2),
+        absentPercentage: ((item.absent / item.assigned) * 100).toFixed(2),
+        gradedContribution: (gradedWidth / totalWidth) * 100,
+        inProgressContribution: (inProgressWidth / totalWidth) * 100,
+        notStartedContribution: (notStartedWidth / totalWidth) * 100,
+        submittedContribution: (submittedWidth / totalWidth) * 100,
+        absentContribution: (absentWidth / totalWidth) * 100,
+        notOpenContribution: (notOpenWidth / totalWidth) * 100,
+        graded: item.graded,
+        submitted: item.submitted,
+        inProgress: item.inProgress,
+        notStarted: item.notStarted,
+        notOpen: item.notOpen,
+        absent: item.absent,
+        totalCount: item.assigned,
+        testDate,
+        testId: item.testId,
+        testType: item.testType,
+        totalTests: item.totalRows,
+      }
+    })
+    .reverse()
 }
 
 export const barsLabelFormatter = (value, payload, height) => {
